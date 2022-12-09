81b807b862be93440a65eaec80deca600efca671
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-09 06:47:38 +0000

    Added functions tuned to output specific machine code.

---
+++ top/mach.zig
@@ -0,0 +1,88 @@
+//! Prefer Zig over assembly wherever possible. The aim of these functions is to
+//! achieve a specific output. See each section for more precise reasoning.
+
+// For operations with no comptime operands and register-sized integers prefer
+// the following four functions. These reference the assembly directly below,
+// so no truncation is needed to circumvent Zig's requirements for shift_amt.
+pub inline fn shlx64(value: u64, shift_amt: u64) u64 {
+    return shlx(u64, value, shift_amt);
+}
+pub inline fn shlx32(value: u32, shift_amt: u32) u32 {
+    return shlx(u32, value, shift_amt);
+}
+pub inline fn shrx64(value: u64, shift_amt: u64) u64 {
+    return shrx(u64, value, shift_amt);
+}
+pub inline fn shrx32(value: u32, shift_amt: u32) u32 {
+    return shrx(u32, value, shift_amt);
+}
+inline fn shlx(comptime T: type, value: T, shift_amt: T) T {
+    return asm ("shlx" ++ switch (T) {
+            u32 => "l",
+            u64 => "q",
+            else => @compileError("invalid operand type for this instruction"),
+        } ++ " %[shift_amt], %[value], %[ret]"
+        : [ret] "=r" (-> T),
+        : [value] "r" (value),
+          [shift_amt] "r" (shift_amt),
+    );
+}
+inline fn shrx(comptime T: type, value: T, shift_amt: T) T {
+    return asm ("shrx" ++ switch (T) {
+            u32 => "l",
+            u64 => "q",
+            else => @compileError("invalid operand type for this instruction"),
+        } ++ " %[shift_amt], %[value], %[ret]"
+        : [ret] "=r" (-> T),
+        : [value] "r" (value),
+          [shift_amt] "r" (shift_amt),
+    );
+}
+// Conditional moves--for integers prefer dedicated functions. Prevents lazy-
+// evaluating the conditional values by ordering their evaluation before the
+// function is called.
+pub inline fn cmov8(b: bool, t_value: u8, f_value: u8) u8 {
+    return if (b) t_value else f_value;
+}
+pub inline fn cmov16(b: bool, t_value: u16, f_value: u16) u16 {
+    return if (b) t_value else f_value;
+}
+pub inline fn cmov32(b: bool, t_value: u32, f_value: u32) u32 {
+    return if (b) t_value else f_value;
+}
+pub inline fn cmov64(b: bool, t_value: u64, f_value: u64) u64 {
+    return if (b) t_value else f_value;
+}
+pub inline fn cmov8z(b: bool, t_value: u8) u8 {
+    return if (b) t_value else 0;
+}
+pub inline fn cmov16z(b: bool, t_value: u16) u16 {
+    return if (b) t_value else 0;
+}
+pub inline fn cmov32z(b: bool, t_value: u32) u32 {
+    return if (b) t_value else 0;
+}
+pub inline fn cmov64z(b: bool, t_value: u64) u64 {
+    return if (b) t_value else 0;
+}
+pub inline fn cmovu(comptime Unsigned: type, b: bool, t_value: Unsigned, f_value: Unsigned) u64 {
+    return if (b) t_value else f_value;
+}
+pub inline fn cmovi(comptime Signed: type, b: bool, t_value: Signed, f_value: Signed) u64 {
+    return if (b) t_value else f_value;
+}
+pub inline fn cmovuz(comptime Unsigned: type, b: bool, t_value: Unsigned) u64 {
+    return if (b) t_value else 0;
+}
+pub inline fn cmoviz(comptime Signed: type, b: bool, t_value: Signed) u64 {
+    return if (b) t_value else 0;
+}
+pub inline fn cmovV(comptime b: bool, any: anytype) @TypeOf(if (b) any) {
+    return if (b) any;
+}
+pub inline fn cmovx(b: bool, t_value: anytype, f_value: @TypeOf(t_value)) @TypeOf(t_value) {
+    return if (b) t_value else f_value;
+}
+pub inline fn cmovxZ(b: bool, t_value: anytype) ?@TypeOf(t_value) {
+    return if (b) t_value else null;
+}
