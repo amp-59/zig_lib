23f9a1542f07a78afa1d48d379b4b13a961b9117
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-10 02:50:53 +0000

    Added test for (double) approximation algorithm.

---
+++ top/algo-test.zig
@@ -0,0 +1,35 @@
+const lit = @import("./lit.zig");
+const proc = @import("./proc.zig");
+const algo = @import("./algo.zig");
+const file = @import("./file.zig");
+const builtin = @import("./builtin.zig");
+
+pub usingnamespace proc.start;
+
+fn write(buf: []u8, ss: []const []const u8) u64 {
+    var len: u64 = 0;
+    for (ss) |s| {
+        for (s) |c, i| buf[len + i] = c;
+        len += s.len;
+    }
+    return len;
+}
+fn print(buf: []u8, ss: []const []const u8) void {
+    file.noexcept.write(2, buf[0..write(buf, ss)]);
+}
+pub fn main() void {
+    var buf: [4096]u8 = undefined;
+    var s_count: u64 = 1;
+
+    while (s_count != lit.max_bit_u32) : (s_count += 1) {
+        const s_lu_counts: u32 = algo.packDoubleApproxB(s_count);
+        const t_count: u64 = algo.unpackDoubleApproxB(s_lu_counts);
+        if (t_count - s_count == 0) {
+            print(&buf, &[_][]const u8{
+                builtin.fmt.ud64(s_count).readAll(),           " ",
+                builtin.fmt.ub64(s_count).readAll(),           " ",
+                builtin.fmt.ud64(t_count - s_count).readAll(), "\n",
+            });
+        }
+    }
+}
