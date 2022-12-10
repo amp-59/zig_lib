b5388a97bab5de2e1a78408b720a8ae6b8d7b560
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-10 01:27:52 +0000

    Added more specialised arithmetic wrappers.

--- top/mach.zig
+++ top/mach.zig
@@ -1,3 +1,7 @@
+//! Miscellaneous low level functions: Primarily related to packing, bit-masks
+//! and bit-shifts. For operator wrappers which unambiguously mimic Zig's
+//! default behaviour see builtin.zig.
+//!
 //! Prefer Zig over assembly wherever possible. The aim of these functions is to
 //! achieve a specific output. See each section for more precise reasoning.
 
@@ -86,4 +90,106 @@ pub inline fn cmovx(b: bool, t_value: anytype, f_value: @TypeOf(t_value)) @TypeO
 }
 pub inline fn cmovxZ(b: bool, t_value: anytype) ?@TypeOf(t_value) {
     return if (b) t_value else null;
+}
+
+// Basic arithmetic operations--safety off by default:
+inline fn add(comptime T: type, arg1: T, arg2: T) T {
+    return arg1 +% arg2;
+}
+inline fn sub(comptime T: type, arg1: T, arg2: T) T {
+    return arg1 -% arg2;
+}
+inline fn mul(comptime T: type, arg1: T, arg2: T) T {
+    return arg1 *% arg2;
+}
+inline fn div(comptime T: type, arg1: T, arg2: T) T {
+    return arg1 / arg2;
+}
+pub inline fn sub64(arg1: u64, arg2: u64) u64 {
+    return sub(u64, arg1, arg2);
+}
+pub inline fn mul64(arg1: u64, arg2: u64) u64 {
+    return mul(u64, arg1, arg2);
+}
+pub inline fn add64(arg1: u64, arg2: u64) u64 {
+    return add(u64, arg1, arg2);
+}
+pub inline fn div64(arg1: u64, arg2: u64) u64 {
+    return div(u64, arg1, arg2);
+}
+pub inline fn sub32(arg1: u32, arg2: u32) u32 {
+    return sub(u32, arg1, arg2);
+}
+pub inline fn mul32(arg1: u32, arg2: u32) u32 {
+    return mul(u32, arg1, arg2);
+}
+pub inline fn add32(arg1: u32, arg2: u32) u32 {
+    return add(u32, arg1, arg2);
+}
+pub inline fn div32(arg1: u32, arg2: u32) u32 {
+    return div(u32, arg1, arg2);
+}
+pub inline fn sub16(arg1: u16, arg2: u16) u16 {
+    return sub(u16, arg1, arg2);
+}
+pub inline fn mul16(arg1: u16, arg2: u16) u16 {
+    return mul(u16, arg1, arg2);
+}
+pub inline fn add16(arg1: u16, arg2: u16) u16 {
+    return add(u16, arg1, arg2);
+}
+pub inline fn div16(arg1: u16, arg2: u16) u16 {
+    return div(u16, arg1, arg2);
+}
+pub inline fn sub8(arg1: u8, arg2: u8) u8 {
+    return sub(u8, arg1, arg2);
+}
+pub inline fn mul8(arg1: u8, arg2: u8) u8 {
+    return mul(u8, arg1, arg2);
+}
+pub inline fn add8(arg1: u8, arg2: u8) u8 {
+    return add(u8, arg1, arg2);
+}
+pub inline fn div8(arg1: u8, arg2: u8) u8 {
+    return div(u8, arg1, arg2);
+}
+// Basic bit-wise operations--same behaviour as builtin.
+inline fn @"and"(comptime T: type, arg1: T, arg2: T) T {
+    return arg1 & arg2;
+}
+inline fn @"or"(comptime T: type, arg1: T, arg2: T) T {
+    return arg1 | arg2;
+}
+inline fn xor(comptime T: type, arg1: T, arg2: T) T {
+    return arg1 ^ arg2;
+}
+pub inline fn and64(arg1: u64, arg2: u64) u64 {
+    return @"and"(u64, arg1, arg2);
+}
+pub inline fn andn64(arg1: u64, arg2: u64) u64 {
+    return @"and"(u64, arg1, ~arg2);
+}
+pub inline fn or64(arg1: u64, arg2: u64) u64 {
+    return @"or"(u64, arg1, arg2);
+}
+pub inline fn orn64(arg1: u64, arg2: u64) u64 { // Not a real instruction
+    return @"or"(u64, arg1, ~arg2);
+}
+pub inline fn xor64(arg1: u64, arg2: u64) u64 {
+    return xor(u64, arg1, arg2);
+}
+pub inline fn and32(arg1: u32, arg2: u32) u32 {
+    return @"and"(u32, arg1, arg2);
+}
+pub inline fn andn32(arg1: u32, arg2: u32) u32 {
+    return @"and"(u32, arg1, ~arg2);
+}
+pub inline fn or32(arg1: u32, arg2: u32) u32 {
+    return @"or"(u32, arg1, arg2);
+}
+pub inline fn orn32(arg1: u32, arg2: u32) u32 {
+    return @"or"(u32, arg1, ~arg2);
+}
+pub inline fn xor32(arg1: u32, arg2: u32) u32 {
+    return xor(u32, arg1, arg2);
 }
