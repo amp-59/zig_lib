156fd076a8064f6630dc77a97c12322aa70a609a
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-10 02:40:21 +0000

    Added basic time system functions. Made untested algorithms private.

--- top/algo.zig
+++ top/algo.zig
@@ -3,46 +3,46 @@ const mach = @import("./mach.zig");
 const builtin = @import("./builtin.zig");
 
 /// Derives and packs approximation counts.
-pub fn packSingleApproxB(n_bytes: u64) u16 {
+fn packSingleApproxB(n_bytes: u64) u16 {
     const n_bytes_clz: u8 = builtin.lzcnt(u64, n_bytes);
     const l_bytes_cls: u8 = builtin.lzcnt(u64, ~mach.shlx64(n_bytes, n_bytes_clz));
     return builtin.shl(u16, n_bytes_clz, 8) | l_bytes_cls;
 }
-pub fn unpackSingleApproxB(s_counts_l: u64) u64 {
+fn unpackSingleApproxB(s_counts_l: u64) u64 {
     const n_bytes_clz: u64 = mach.shrx64(s_counts_l, 8);
     return mach.shrx64(~mach.shrx64(lit.max_bit_u64, s_counts_l), n_bytes_clz);
 }
 /// Derives, packs, and shifts the approximation counts, as required by the
 /// container technique.
-pub fn packSingleApproxA(n_bytes: u64) u64 {
+fn packSingleApproxA(n_bytes: u64) u64 {
     const s_counts_l: u16 = packSingleApproxB(n_bytes);
     return mach.shlx64(s_counts_l, 48);
 }
 /// Shifts and unpacks the approximation counts, and computes the approximation.
-pub fn unpackSingleApproxA(s_counts_h: u64) u64 {
+fn unpackSingleApproxA(s_counts_h: u64) u64 {
     const s_counts_l: u16 = builtin.shrY(u64, u16, s_counts_h, 48);
     return unpackSingleApproxB(s_counts_l);
 }
-pub fn approx(n_bytes: u64) u64 {
+fn approx(n_bytes: u64) u64 {
     const n_bytes_clz: u8 = builtin.lzcnt(u64, n_bytes);
     const l_bytes_cls: u8 = builtin.lzcnt(u64, ~mach.shlx64(n_bytes, n_bytes_clz));
     return reverseApproximateAbove(n_bytes_clz, l_bytes_cls);
 }
 /// Derives and packs approximation counts.
-pub fn packAlignedApproxB(n_bytes: u64) u16 {
+fn packAlignedApproxB(n_bytes: u64) u16 {
     const n_bytes_pop: u8 = builtin.popcnt(u64, n_bytes);
     const n_bytes_clz: u8 = builtin.tzcnt(u64, n_bytes);
     return builtin.shl(u16, n_bytes_pop, 8) | builtin.sub(u16, 64, (n_bytes_pop + n_bytes_clz));
 }
 /// Unpacks approximation counts and computes the approximation.
-pub fn unpackAlignedApproxB(s_counts_l: u16) u64 {
+fn unpackAlignedApproxB(s_counts_l: u16) u64 {
     const n_bytes_pop: u8 = builtin.maskZ(u16, u8, s_counts_l);
     const n_bytes_ctz: u8 = mach.shr16T(u8, s_counts_l, 8);
     return mach.shlx64(mach.shlx64(1, n_bytes_pop) -% 1, n_bytes_ctz);
 }
 /// Derives, packs, and shifts the approximation counts, as required by the
 /// container technique.
-pub fn packAlignedApproxA(n_bytes: u64) u64 {
+fn packAlignedApproxA(n_bytes: u64) u64 {
     const s_counts_l: u16 = packAlignedApproxB(n_bytes);
     return mach.shlx64(s_counts_l, 48);
 }
@@ -56,61 +56,50 @@ pub fn alignedApprox(n_bytes: u64) u64 {
     const n_bytes_clz: u8 = builtin.lzcnt(u64, n_bytes);
     return mach.shlx64(mach.shlx64(1, n_bytes_pop) -% 1, 64 -% (n_bytes_pop + n_bytes_clz));
 }
-inline fn reverseApproximateAbove(n_bytes_clz: u8, l_bytes_cls: u8) u64 {
+fn reverseApproximateAbove(n_bytes_clz: u8, l_bytes_cls: u8) u64 {
     return mach.shrx64(~builtin.shr(u64, lit.max_bit_u64, l_bytes_cls), n_bytes_clz);
 }
-inline fn reverseApproximateBelow(m_bytes_clz: u8, o_bytes_cls: u8) u64 {
+fn reverseApproximateBelow(m_bytes_clz: u8, o_bytes_cls: u8) u64 {
     return mach.shrx64(~builtin.shr(u64, lit.max_bit_u64, o_bytes_cls -% 1), m_bytes_clz) +% 1;
 }
 // The following functions require 32 bits total.
-inline fn packDouble(l_bytes_clz: u8, l_bytes_cls: u8, m_bytes_clz: u8, m_bytes_cls: u8) u32 {
+fn packDouble(l_bytes_clz: u8, l_bytes_cls: u8, m_bytes_clz: u8, m_bytes_cls: u8) u32 {
     const s_lb_counts: u16 = builtin.pack16(l_bytes_clz, l_bytes_cls);
     const s_ub_counts: u16 = builtin.pack16(m_bytes_clz, m_bytes_cls -% 1);
     return builtin.pack32(s_lb_counts, s_ub_counts);
 }
-inline fn unpackDouble(l_bytes_clz: u8, l_bytes_ctz: u64, m_bytes_clz: u8, o_bytes_ctz: u64) u64 {
+fn unpackDouble(l_bytes_clz: u8, l_bytes_ctz: u64, m_bytes_clz: u8, o_bytes_ctz: u64) u64 {
     const o_bytes: u64 = mach.shrx64(l_bytes_ctz, l_bytes_clz);
     const p_bytes: u64 = mach.shrx64(o_bytes_ctz, m_bytes_clz);
     return mach.sub64(o_bytes, p_bytes);
 }
-pub inline fn partialPackSingleApprox(n_bytes: u64) u16 {
+pub fn partialPackSingleApprox(n_bytes: u64) u16 {
     const n_bytes_clz: u8 = builtin.lzcnt(u64, n_bytes);
     const l_bytes_cls: u8 = builtin.lzcnt(u64, ~mach.shlx64(n_bytes, n_bytes_clz));
     const s_lb_counts: u16 = builtin.pack16(n_bytes_clz, l_bytes_cls);
     return s_lb_counts;
 }
-pub inline fn partialUnpackSingleApprox(s_lb_counts: u16) u64 {
+pub fn partialUnpackSingleApprox(s_lb_counts: u16) u64 {
     const n_bytes_clz: u8 = mach.mask8H(s_lb_counts);
     const l_bytes_ctz: u64 = ~mach.shrx64(lit.max_bit_u64, s_lb_counts);
     const o_bytes: u64 = mach.shrx64(l_bytes_ctz, n_bytes_clz);
     return o_bytes;
 }
-pub inline fn partialPackDoubleApprox(n_bytes: u64, o_bytes: u64) u16 {
+pub fn partialPackDoubleApprox(n_bytes: u64, o_bytes: u64) u16 {
     const m_bytes: u64 = mach.sub64(o_bytes, n_bytes);
     const m_bytes_clz: u8 = builtin.lzcnt(u64, m_bytes);
     const m_bytes_cls: u8 = builtin.lzcnt(u64, ~mach.shlx64(m_bytes, m_bytes_clz));
     const s_ub_counts: u16 = builtin.pack16(m_bytes_clz, m_bytes_cls -% 1);
     return s_ub_counts;
 }
-pub inline fn partialUnpackDoubleApprox(o_bytes: u64, s_ub_counts: u16) u64 {
+pub fn partialUnpackDoubleApprox(o_bytes: u64, s_ub_counts: u16) u64 {
     const o_bytes_cls: u8 = mach.mask8H(s_ub_counts);
     const o_bytes_ctz: u64 = ~mach.shrx64(lit.max_bit_u64, s_ub_counts);
     const p_bytes: u64 = mach.shrx64(o_bytes_ctz, o_bytes_cls);
     const s_bytes: u64 = mach.sub64(o_bytes, p_bytes +% 1);
     return s_bytes;
 }
-/// Derives and packs approximation counts.
-pub fn packDoubleApproxBOld(n_bytes: u64) u32 {
-    const n_bytes_clz: u8 = builtin.lzcnt(u64, n_bytes);
-    const n_bytes_cls: u8 = builtin.lzcnt(u64, ~mach.shlx64(n_bytes, n_bytes_clz));
-    const n_bytes_ctz: u64 = ~mach.shrx64(lit.max_bit_u64, n_bytes_cls);
-    const o_bytes: u64 = mach.shrx64(n_bytes_ctz, n_bytes_clz);
-    const m_bytes: u64 = mach.sub64(o_bytes, n_bytes);
-    const m_bytes_clz: u8 = builtin.lzcnt(u64, m_bytes);
-    const m_bytes_cls: u8 = builtin.lzcnt(u64, ~mach.shlx64(m_bytes, m_bytes_clz));
-    return packDouble(n_bytes_clz, n_bytes_cls, m_bytes_clz, m_bytes_cls);
-}
-pub inline fn packDoubleApproxB(k_bytes: u64) u32 {
+pub fn packDoubleApproxB(k_bytes: u64) u32 {
     const l_bytes_clz: u8 = builtin.lzcnt(u64, k_bytes);
     const l_bytes_cls: u8 = builtin.lzcnt(u64, ~mach.shlx64(k_bytes, l_bytes_clz));
     const l_bytes_ctz: u64 = ~mach.shr64(lit.max_bit_u64, l_bytes_cls);
---
+++ top/time.zig
@@ -0,0 +1,202 @@
+const sys = @import("./sys.zig");
+const mach = @import("./mach.zig");
+const meta = @import("./meta.zig");
+const builtin = @import("./builtin.zig");
+const abstract = @import("./abstract.zig");
+
+pub const TimeSpec = extern struct { sec: u64 = 0, nsec: u64 = 0 };
+pub const MonotonicClock = enum { raw, coarse };
+pub const RealClock = enum { alarm, coarse };
+pub const BootClock = enum { alarm };
+pub const CPUClock = enum { thread, process };
+
+pub const days_in_month: [12]u8 = .{ 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31, 29 };
+pub const days_per_year: u64 = 365;
+pub const days_per_4y: u64 = (days_per_year * 4) + 1;
+pub const days_per_400y: u64 = (days_per_year * 400) + 97;
+pub const days_per_100y: u64 = (days_per_year * 100) + 24;
+pub const leap_epoch: u64 = 946_684_800 + (86_400 * (31 + 29));
+
+pub const Clock = meta.EnumBitField(enum(u64) {
+    realtime = CLOCK.REALTIME,
+    monotonic = CLOCK.MONOTONIC,
+    process_cputime_id = CLOCK.PROCESS_CPUTIME_ID,
+    thread_cputime_id = CLOCK.THREAD_CPUTIME_ID,
+    monotonic_raw = CLOCK.MONOTONIC_RAW,
+    realtime_coarse = CLOCK.REALTIME_COARSE,
+    monotonic_coarse = CLOCK.MONOTONIC_COARSE,
+    boottime = CLOCK.BOOTTIME,
+    realtime_alarm = CLOCK.REALTIME_ALARM,
+    boottime_alarm = CLOCK.BOOTTIME_ALARM,
+    tai = CLOCK.TAI,
+    const CLOCK = sys.CLOCK;
+});
+pub const Month = enum {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+};
+pub fn realClock(comptime clock_spec: ?RealClock) !TimeSpec {
+    var ts: TimeSpec = undefined;
+    const flags: Clock = .{ .tag = if (clock_spec) |spec|
+        switch (spec) {
+            .alarm => .realtime_alarm,
+            .coarse => .realtime_coarse,
+        }
+    else
+        .REALTIME };
+    try sys.clock_gettime(flags.val, @ptrToInt(&ts));
+    return ts;
+}
+pub fn montonicClock(comptime clock_spec: ?MonotonicClock) !TimeSpec {
+    var ts: TimeSpec = undefined;
+    const flags: Clock = .{ .tag = if (clock_spec) |spec|
+        switch (spec) {
+            .raw => .monotonic_raw,
+            .coarse => .monotonic_coarse,
+        }
+    else
+        .MONOTONIC };
+    try sys.clock_gettime(flags.val, @ptrToInt(&ts));
+    return ts;
+}
+pub fn cpuClock(comptime clock_spec: CPUClock) !TimeSpec {
+    var ts: TimeSpec = undefined;
+    const flags: Clock = .{ .tag = switch (clock_spec) {
+        .process => .process_cputime_id,
+        .thread => .thread_cputime_id,
+    } };
+    try sys.clock_gettime(flags.val, @ptrToInt(&ts));
+    return ts;
+}
+pub fn bootClock(comptime clock_spec: ?BootClock) !TimeSpec {
+    var ts: TimeSpec = undefined;
+    const flags: Clock = .{ .tag = if (clock_spec) |spec|
+        switch (spec) {
+            .alarm => .boottime_alarm,
+        }
+    else
+        .BOOTTIME };
+    try sys.clock_gettime(flags.val, @ptrToInt(&ts));
+    return ts;
+}
+pub fn nanoSleepB(req: TimeSpec) !void {
+    try sys.nanosleep(@ptrToInt(&req), 0);
+}
+pub fn nanoSleepA(req: TimeSpec) !TimeSpec {
+    var rem: TimeSpec = undefined;
+    try sys.nanosleep(@ptrToInt(&req), @ptrToInt(&rem));
+    return rem;
+}
+const SleepSpec = struct {
+    return_type: type = void,
+    errors: ?[]const sys.ErrorCode = sys.nanosleep_errors,
+    logging: bool = builtin.is_verbose,
+
+    pub usingnamespace sys.FunctionInterfaceSpec(SleepSpec);
+};
+pub fn sleep(comptime spec: SleepSpec, ts: TimeSpec) spec.Unwrapped(.nanosleep) {
+    if (spec.call(.nanosleep, .{ @ptrToInt(&ts), 0 })) {
+        return;
+    } else |nanosleep_error| {
+        return nanosleep_error;
+    }
+}
+
+pub const DateTime = extern struct {
+    sec: u8,
+    min: u8,
+    hour: u8,
+    mday: u8,
+    wday: u8,
+    mon: u8,
+    yday: u16,
+    year: u64,
+    pub fn getSecond(dt: DateTime) u8 {
+        return dt.sec;
+    }
+    pub fn getMinute(dt: DateTime) u8 {
+        return dt.min;
+    }
+    pub fn getHour(dt: DateTime) u8 {
+        return dt.hour;
+    }
+    pub fn getYear(dt: DateTime) u64 {
+        return dt.year + 2000;
+    }
+    pub fn getMonth(dt: DateTime) u8 {
+        return dt.mon + 1;
+    }
+    pub fn getWeekDay(dt: DateTime) u8 {
+        return dt.wday + 1;
+    }
+    pub fn getMonthDay(dt: DateTime) u8 {
+        return dt.mday + 1;
+    }
+    pub fn getYearDay(dt: DateTime) u16 {
+        return dt.yday + 1;
+    }
+    pub inline fn init(epoch_seconds: u64) DateTime {
+        if (epoch_seconds < leap_epoch) {
+            @panic("TODO: Dates before epoch");
+        } else {
+            const secs: u64 = epoch_seconds - leap_epoch;
+            const days: u64 = secs / 86_400;
+            var rem_secs: u64 = secs % 86_400;
+            var rem_days: u64 = days % days_per_400y;
+            const qc_cycles: u64 = days / days_per_400y;
+            const c_cycles: u64 = blk: {
+                const cycles: u64 = rem_days / days_per_100y;
+                break :blk cycles - builtin.int(u64, cycles == 4);
+            };
+            rem_days -= c_cycles * days_per_100y;
+            const q_cycles: u64 = blk: {
+                const cycles: u64 = rem_days / days_per_4y;
+                break :blk cycles - builtin.int(u64, cycles == 25);
+            };
+            rem_days -= q_cycles * days_per_4y;
+            const rem_years: u64 = blk: {
+                const rem: u64 = rem_days / days_per_year;
+                break :blk rem - builtin.int(u64, rem == 4);
+            };
+            rem_days -= rem_years * days_per_year;
+            const years: u64 = rem_years +
+                (4 * q_cycles) + (100 * c_cycles) + (400 * qc_cycles);
+            const leap_day: u64 = blk: {
+                const b0: bool = rem_years == 0;
+                const b1: bool = builtin.int2v(bool, q_cycles != 0, c_cycles == 0);
+                break :blk builtin.int2v(u64, b0, b1);
+            };
+            const year_day: u64 = blk: {
+                const leap_days: u64 = rem_days + 31 + 28 + leap_day;
+                const leap_year: u64 = days_per_year + leap_day;
+                break :blk leap_days - mach.cmov64z(leap_year < leap_days, leap_year);
+            };
+            var months: u8 = 0;
+            while (rem_days > days_in_month[months]) : (months += 1) {
+                rem_days -= days_in_month[months];
+            }
+            months -%= mach.cmov8z(months > 9, 12);
+            const year: u64 = years + builtin.int(u64, months > 9);
+            return .{
+                .yday = @intCast(u16, year_day),
+                .mday = @intCast(u8, rem_days),
+                .wday = @intCast(u8, (days + 3) % 7),
+                .mon = @intCast(u8, months +% 2),
+                .hour = @intCast(u8, rem_secs / 3600),
+                .min = @intCast(u8, (rem_secs / 60) % 60),
+                .sec = @intCast(u8, rem_secs % 60),
+                .year = year,
+            };
+        }
+    }
+};
