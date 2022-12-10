31eb64fa511dbf0feb68c053b1f271fa040954d9
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-10 03:03:27 +0000

    Added specifications for high level system file functions.

--- top/file.zig
+++ top/file.zig
@@ -1,4 +1,5 @@
 const sys = @import("./sys.zig");
+const mem = @import("./mem.zig");
 const meta = @import("./meta.zig");
 const time = @import("./time.zig");
 const builtin = @import("./builtin.zig");
@@ -256,6 +257,275 @@ pub const ModeSpec = struct {
             }
         }
     }
+};
+pub const MakeDirSpec = struct {
+    options: Options = .{},
+    mode: ModeSpec = ModeSpec.dir_mode,
+    errors: ?[]const sys.ErrorCode = sys.mkdir_errors,
+    return_type: type = void,
+    logging: bool = builtin.is_verbose,
+    const Specification = @This();
+    const Options = struct {
+        exclusive: bool = true,
+    };
+    pub usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+pub const RemoveDirSpec = struct {
+    errors: ?[]const sys.ErrorCode = sys.rmdir_errors,
+    return_type: type = void,
+    logging: bool = builtin.is_verbose,
+    const Specification = @This();
+    const special_fn = sys.special.rmdir;
+    pub usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+pub const CreateSpec = struct {
+    options: Options = .{},
+    mode: ModeSpec = ModeSpec.file_mode,
+    errors: ?[]const sys.ErrorCode = sys.open_errors,
+    return_type: type = u64,
+    logging: bool = builtin.is_verbose,
+    const Specification = @This();
+    const Options = struct {
+        exclusive: bool = true,
+        temporary: bool = false,
+        close_on_exec: bool = true,
+        write: ?OpenSpec.Write = .truncate,
+        read: bool = false,
+    };
+    fn flags(comptime spec: CreateSpec) Open {
+        var flags_bitfield: Open = .{ .tag = .create };
+        if (spec.options.exclusive) {
+            flags_bitfield.set(.exclusive);
+        }
+        if (spec.options.close_on_exec) {
+            flags_bitfield.set(.close_on_exec);
+        }
+        if (spec.options.temporary) {
+            flags_bitfield.set(.temporary);
+        }
+        if (spec.options.write) |w| {
+            if (spec.options.read) {
+                flags_bitfield.set(.read_write);
+            } else {
+                flags_bitfield.set(.write_only);
+            }
+            switch (w) {
+                .append => {
+                    flags_bitfield.set(.append);
+                },
+                .truncate => {
+                    flags_bitfield.set(.truncate);
+                },
+            }
+        } else if (spec.options.read) {
+            flags_bitfield.set(.read_only);
+        }
+        return flags_bitfield;
+    }
+    pub usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+pub const UnlinkSpec = struct {
+    errors: ?[]const sys.ErrorCode = sys.unlink_errors,
+    return_type: type = void,
+    logging: bool = builtin.is_verbose,
+    const Specification = @This();
+    pub usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+pub const PathSpec = struct {
+    options: Options = .{},
+    errors: ?[]const sys.ErrorCode = sys.open_errors,
+    return_type: type = u64,
+    logging: bool = builtin.is_verbose,
+    const Specification = @This();
+    const Options = struct {
+        directory: bool = true,
+        no_follow: bool = true,
+        close_on_exec: bool = true,
+    };
+    pub fn flags(comptime spec: PathSpec) Open {
+        var flags_bitfield: Open = .{ .tag = .path };
+        if (spec.options.no_follow) {
+            flags_bitfield.set(.no_follow);
+        }
+        if (spec.options.close_on_exec) {
+            flags_bitfield.set(.close_on_exec);
+        }
+        if (spec.options.directory) {
+            flags_bitfield.set(.directory);
+        }
+        return flags_bitfield;
+    }
+    pub usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+pub const OpenSpec = struct {
+    options: Options,
+    return_type: type = u64,
+    errors: ?[]const sys.ErrorCode = sys.open_errors,
+    logging: bool = builtin.is_verbose,
+    const Specification = @This();
+    const Options = struct {
+        read: bool = true,
+        write: ?Write = null,
+        directory: bool = false,
+        temporary: bool = false,
+        no_atime: bool = false,
+        no_ctty: bool = true,
+        no_follow: bool = true,
+        no_block: bool = false,
+        no_cache: bool = false,
+        close_on_exec: bool = true,
+    };
+    const Write = enum { append, truncate };
+    pub fn flags(comptime spec: OpenSpec) Open {
+        var flags_bitfield: Open = .{ .val = 0 };
+        if (spec.options.no_cache) {
+            flags_bitfield.set(.no_cache);
+        }
+        if (spec.options.no_atime) {
+            flags_bitfield.set(.no_atime);
+        }
+        if (spec.options.no_follow) {
+            flags_bitfield.set(.no_follow);
+        }
+        if (spec.options.no_block) {
+            flags_bitfield.set(.no_block);
+        }
+        if (spec.options.no_ctty) {
+            flags_bitfield.set(.no_ctty);
+        }
+        if (spec.options.close_on_exec) {
+            flags_bitfield.set(.close_on_exec);
+        }
+        if (spec.options.temporary) {
+            flags_bitfield.set(.temporary);
+        }
+        if (spec.options.directory) {
+            flags_bitfield.set(.directory);
+        }
+        if (spec.options.write) |w| {
+            if (spec.options.read) {
+                flags_bitfield.set(.read_write);
+            } else {
+                flags_bitfield.set(.write_only);
+            }
+            switch (w) {
+                .append => {
+                    flags_bitfield.set(.append);
+                },
+                .truncate => {
+                    flags_bitfield.set(.truncate);
+                },
+            }
+        } else if (spec.options.read) {
+            flags_bitfield.set(.read_only);
+        }
+        return flags_bitfield;
+    }
+    usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+pub const CloseSpec = struct {
+    logging: bool = builtin.is_verbose,
+    errors: ?[]const sys.ErrorCode = sys.close_errors,
+    return_type: type = void,
+    const Specification = @This();
+    usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+pub const StatSpec = struct {
+    options: Options = .{},
+    errors: ?[]const sys.ErrorCode = sys.stat_errors,
+    return_type: type = void,
+
+    const STAT = sys.AT;
+    const Specification = @This();
+    const Options = struct {
+        no_follow: bool = false,
+    };
+    fn flags(comptime spec: StatSpec) Open {
+        var flags_bitfield: Open = .{ .val = 0 };
+        if (spec.options.no_follow) {
+            flags_bitfield.set(.no_follow);
+        }
+        return flags_bitfield;
+    }
+    usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+pub const ReadLinkSpec = struct {
+    options: Options = .{},
+    errors: ?[]const sys.ErrorCode = sys.readlink_errors,
+    return_type: type = u64,
+    logging: bool = builtin.is_verbose,
+
+    const STAT = sys.AT;
+    const Specification = @This();
+    const Options = struct {
+        no_follow: bool = false,
+    };
+    fn flags(comptime spec: StatSpec) Open {
+        var flags_bitfield: Open = .{ .val = 0 };
+        if (spec.options.no_follow) {
+            flags_bitfield.set(.no_follow);
+        }
+        return flags_bitfield;
+    }
+    usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
+
+// TODO: Define default options suited to mapping files
+pub const MapSpec = struct {
+    options: Options,
+    errors: ?[]const sys.ErrorCode = sys.mmap_errors,
+    return_type: type = void,
+    logging: bool = builtin.is_verbose,
+    const Specification = @This();
+    const Options = struct {
+        anonymous: bool,
+        visibility: Visibility,
+        read: bool,
+        write: bool,
+        exec: bool,
+        populate: bool,
+        grows_down: bool,
+        sync: bool,
+    };
+    const Visibility = enum { shared, shared_validate, private };
+    pub fn flags(comptime spec: MapSpec) mem.Map {
+        var flags_bitfield: mem.Map = .{ .tag = .fixed_no_replace };
+        switch (spec.options.visibility) {
+            .private => flags_bitfield.set(.private),
+            .shared => flags_bitfield.set(.shared),
+            .shared_validate => flags_bitfield.set(.shared_validate),
+        }
+        if (spec.options.anonymous) {
+            flags_bitfield.set(.anonymous);
+        }
+        if (spec.options.grows_down) {
+            flags_bitfield.set(.grows_down);
+            flags_bitfield.set(.stack);
+        }
+        if (spec.options.populate) {
+            builtin.static.assert(spec.options.visibility == .private);
+            flags_bitfield.set(.populate);
+        }
+        if (spec.options.sync) {
+            builtin.static.assert(spec.options.visibility == .shared_validate);
+            flags_bitfield.set(.sync);
+        }
+        return flags_bitfield;
+    }
+    pub fn prot(comptime spec: MapSpec) mem.Prot {
+        var prot_bitfield: mem.Prot = .{ .val = 0 };
+        if (spec.options.read) {
+            prot_bitfield.set(.read);
+        }
+        if (spec.options.write) {
+            prot_bitfield.set(.write);
+        }
+        if (spec.options.exec) {
+            prot_bitfield.set(.exec);
+        }
+        return prot_bitfield;
+    }
+    usingnamespace sys.FunctionInterfaceSpec(Specification);
 };
 
 pub fn read(fd: u64, read_buf: []u8, count: u64) !u64 {
