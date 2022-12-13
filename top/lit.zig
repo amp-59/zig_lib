939a7e2c934abab0e60cca613e60fd56d3d4b5c3
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-13 18:21:23 +0000

    Added various commonly useful escape sequences.

--- top/lit.zig
+++ top/lit.zig
@@ -59,8 +59,94 @@ pub const ud8 = [256][:0]const u8{
     "240", "241", "242", "243", "244", "245", "246", "247", "248", "249",
     "250", "251", "252", "253", "254", "255",
 };
+pub const esc_hex_sequences = [256][:0]const u8{
+    "\\x00", "\\x01", "\\x02", "\\x03", "\\x04", "\\x05", "\\x06", "\\x07", "\\x08",
+    "\\x09", "\\x0a", "\\x0b", "\\x0c", "\\x0d", "\\x0e", "\\x0f", "\\x10", "\\x11",
+    "\\x12", "\\x13", "\\x14", "\\x15", "\\x16", "\\x17", "\\x18", "\\x19", "\\x1a",
+    "\\x1b", "\\x1c", "\\x1d", "\\x1e", "\\x1f", "\\x20", "\\x21", "\\x22", "\\x23",
+    "\\x24", "\\x25", "\\x26", "\\x27", "\\x28", "\\x29", "\\x2a", "\\x2b", "\\x2c",
+    "\\x2d", "\\x2e", "\\x2f", "\\x30", "\\x31", "\\x32", "\\x33", "\\x34", "\\x35",
+    "\\x36", "\\x37", "\\x38", "\\x39", "\\x3a", "\\x3b", "\\x3c", "\\x3d", "\\x3e",
+    "\\x3f", "\\x40", "\\x41", "\\x42", "\\x43", "\\x44", "\\x45", "\\x46", "\\x47",
+    "\\x48", "\\x49", "\\x4a", "\\x4b", "\\x4c", "\\x4d", "\\x4e", "\\x4f", "\\x50",
+    "\\x51", "\\x52", "\\x53", "\\x54", "\\x55", "\\x56", "\\x57", "\\x58", "\\x59",
+    "\\x5a", "\\x5b", "\\x5c", "\\x5d", "\\x5e", "\\x5f", "\\x60", "\\x61", "\\x62",
+    "\\x63", "\\x64", "\\x65", "\\x66", "\\x67", "\\x68", "\\x69", "\\x6a", "\\x6b",
+    "\\x6c", "\\x6d", "\\x6e", "\\x6f", "\\x70", "\\x71", "\\x72", "\\x73", "\\x74",
+    "\\x75", "\\x76", "\\x77", "\\x78", "\\x79", "\\x7a", "\\x7b", "\\x7c", "\\x7d",
+    "\\x7e", "\\x7f", "\\x80", "\\x81", "\\x82", "\\x83", "\\x84", "\\x85", "\\x86",
+    "\\x87", "\\x88", "\\x89", "\\x8a", "\\x8b", "\\x8c", "\\x8d", "\\x8e", "\\x8f",
+    "\\x90", "\\x91", "\\x92", "\\x93", "\\x94", "\\x95", "\\x96", "\\x97", "\\x98",
+    "\\x99", "\\x9a", "\\x9b", "\\x9c", "\\x9d", "\\x9e", "\\x9f", "\\xa0", "\\xa1",
+    "\\xa2", "\\xa3", "\\xa4", "\\xa5", "\\xa6", "\\xa7", "\\xa8", "\\xa9", "\\xaa",
+    "\\xab", "\\xac", "\\xad", "\\xae", "\\xaf", "\\xb0", "\\xb1", "\\xb2", "\\xb3",
+    "\\xb4", "\\xb5", "\\xb6", "\\xb7", "\\xb8", "\\xb9", "\\xba", "\\xbb", "\\xbc",
+    "\\xbd", "\\xbe", "\\xbf", "\\xc0", "\\xc1", "\\xc2", "\\xc3", "\\xc4", "\\xc5",
+    "\\xc6", "\\xc7", "\\xc8", "\\xc9", "\\xca", "\\xcb", "\\xcc", "\\xcd", "\\xce",
+    "\\xcf", "\\xd0", "\\xd1", "\\xd2", "\\xd3", "\\xd4", "\\xd5", "\\xd6", "\\xd7",
+    "\\xd8", "\\xd9", "\\xda", "\\xdb", "\\xdc", "\\xdd", "\\xde", "\\xdf", "\\xe0",
+    "\\xe1", "\\xe2", "\\xe3", "\\xe4", "\\xe5", "\\xe6", "\\xe7", "\\xe8", "\\xe9",
+    "\\xea", "\\xeb", "\\xec", "\\xed", "\\xee", "\\xef", "\\xf0", "\\xf1", "\\xf2",
+    "\\xf3", "\\xf4", "\\xf5", "\\xf6", "\\xf7", "\\xf8", "\\xf9", "\\xfa", "\\xfb",
+    "\\xfc", "\\xfd", "\\xfe", "\\xff",
+};
 pub const int_prefixes = .{
     "", "", "0b", "", "", "", "", "", "0o", "", "", "", "0d", "", "", "", "0x",
     "", "", "",   "", "", "", "", "", "",   "", "", "", "",   "", "", "", "",
     "", "", "0z", "", "", "", "", "", "",   "", "", "", "",   "", "", "", "",
+};
+pub const fx = struct {
+    pub const none: []const u8 = "\x1b\x5b\x30\x6d";
+    pub const color = struct {
+        pub const fg = struct {
+            pub const black: []const u8 = "\x1b\x5b\x33\x30\x6d";
+            pub const red: []const u8 = "\x1b\x5b\x33\x31\x6d";
+            pub const green: []const u8 = "\x1b\x5b\x33\x32\x6d";
+            pub const yellow: []const u8 = "\x1b\x5b\x33\x33\x6d";
+            pub const blue: []const u8 = "\x1b\x5b\x33\x34\x6d";
+            pub const magenta: []const u8 = "\x1b\x5b\x33\x35\x6d";
+            pub const cyan: []const u8 = "\x1b\x5b\x33\x36\x6d";
+            pub const white: []const u8 = "\x1b\x5b\x33\x37\x6d";
+            pub const hi_red: []const u8 = "\x1b\x5b\x39\x31\x6d";
+            pub const hi_green: []const u8 = "\x1b\x5b\x39\x32\x6d";
+            pub const hi_yellow: []const u8 = "\x1b\x5b\x39\x33\x6d";
+            pub const hi_blue: []const u8 = "\x1b\x5b\x39\x34\x6d";
+            pub const hi_magenta: []const u8 = "\x1b\x5b\x39\x35\x6d";
+            pub const hi_cyan: []const u8 = "\x1b\x5b\x39\x36\x6d";
+            pub const max_red: []const u8 = "\x1b\x5b\x33\x38\x3b\x35\x3b\x31\x39\x36\x6d";
+            pub const max_blue: []const u8 = "\x1b\x5b\x33\x38\x3b\x35\x3b\x32\x37\x6d";
+            pub const dark_green: []const u8 = "\x1b\x5b\x33\x38\x3b\x35\x3b\x32\x32\x6d";
+            pub const max_green_alt: []const u8 = "\x1b\x5b\x33\x38\x3b\x35\x3b\x37\x36\x6d";
+            pub const orange: []const u8 = "\x1b\x5b\x33\x38\x3b\x35\x3b\x32\x30\x32\x6d";
+            pub const purple: []const u8 = "\x1b\x5b\x33\x38\x3b\x35\x3b\x39\x39\x6d";
+            pub const aqua: []const u8 = "\x1b\x5b\x33\x38\x3b\x35\x3b\x31\x35\x33\x6d";
+            pub const max_white: []const u8 = "\x1b\x5b\x33\x38\x3b\x35\x3b\x32\x33\x31\x6d";
+        };
+        pub const bg = packed union {
+            pub const black: []const u8 = "\x1b\x5b\x34\x30\x6d";
+            pub const red: []const u8 = "\x1b\x5b\x34\x31\x6d";
+            pub const green: []const u8 = "\x1b\x5b\x34\x32\x6d";
+            pub const yellow: []const u8 = "\x1b\x5b\x34\x33\x6d";
+            pub const blue: []const u8 = "\x1b\x5b\x34\x34\x6d";
+            pub const magenta: []const u8 = "\x1b\x5b\x34\x35\x6d";
+            pub const cyan: []const u8 = "\x1b\x5b\x34\x36\x6d";
+            pub const white: []const u8 = "\x1b\x5b\x34\x37\x6d";
+            pub const hi_red: []const u8 = "\x1b\x5b\x31\x30\x31\x6d";
+            pub const hi_green: []const u8 = "\x1b\x5b\x31\x30\x32\x6d";
+            pub const hi_yellow: []const u8 = "\x1b\x5b\x31\x30\x33\x6d";
+            pub const hi_blue: []const u8 = "\x1b\x5b\x31\x30\x34\x6d";
+            pub const hi_magenta: []const u8 = "\x1b\x5b\x31\x30\x35\x6d";
+            pub const hi_cyan: []const u8 = "\x1b\x5b\x31\x30\x36\x6d";
+            pub const hi_white: []const u8 = "\x1b\x5b\x31\x30\x37\x6d";
+        };
+    };
+    pub const style = struct {
+        pub const bold: []const u8 = "\x1b\x5b\x31\x6d";
+        pub const faint: []const u8 = "\x1b\x5b\x32\x6d";
+        pub const italic: []const u8 = "\x1b\x5b\x33\x6d";
+        pub const underline: []const u8 = "\x1b\x5b\x34\x6d";
+        pub const inverted: []const u8 = "\x1b\x5b\x37\x6d";
+        pub const invisible: []const u8 = "\x1b\x5b\x38\x6d";
+        pub const strikeout: []const u8 = "\x1b\x5b\x39\x6d";
+    };
 };
