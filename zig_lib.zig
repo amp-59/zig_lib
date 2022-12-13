ba8121f90314a9f351cec82c614cd9d9cf58313f
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-13 18:20:16 +0000

    Updated project directory.

--- zig_lib.zig
+++ zig_lib.zig
@@ -1,7 +1,7 @@
-const builtin = @import("./top/builtin.zig");
-const sys = @import("./top/sys.zig");
-const proc = @import("./top/proc.zig");
-const meta = @import("./top/meta.zig");
-const mach = @import("./top/mach.zig");
-const file = @import("./top/file.zig");
-const algo = @import("./top/algo.zig");
+pub const builtin = @import("./top/builtin.zig");
+pub const sys = @import("./top/sys.zig");
+pub const proc = @import("./top/proc.zig");
+pub const meta = @import("./top/meta.zig");
+pub const mach = @import("./top/mach.zig");
+pub const file = @import("./top/file.zig");
+pub const algo = @import("./top/algo.zig");
