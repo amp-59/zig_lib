b05aed266c75f189fdc603b483fc7ed44a3a7c1d
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-15 13:16:43 +0000

    Added mapping functions relevant to threading.

---
+++ top/thread.zig
@@ -0,0 +1,100 @@
+const mem = @import("./mem.zig");
+const sys = @import("./sys.zig");
+const proc = @import("./proc.zig");
+const mach = @import("./mach.zig");
+const meta = @import("./meta.zig");
+const builtin = @import("./builtin.zig");
+
+pub fn map(comptime spec: MapSpec, arena_index: u8) spec.Replaced(.mmap, u64) {
+    const arena: mem.Arena = .{ .index = arena_index };
+    const up_addr: u64 = arena.end();
+    const s_bytes: u64 = 8192;
+    const st_addr: u64 = up_addr - s_bytes;
+    const mmap_prot: mem.Prot = spec.prot();
+    const mmap_flags: mem.Map = spec.flags();
+    if (spec.call(.mmap, .{ st_addr, s_bytes, mmap_prot.val, mmap_flags.val, ~@as(u64, 0), 0 })) {
+        if (spec.logging) {
+            mem.debug.mapNotice(st_addr, s_bytes);
+        }
+    } else |map_error| {
+        if (spec.logging or builtin.is_correct) {
+            mem.debug.mapError(map_error, st_addr, s_bytes);
+        }
+        return map_error;
+    }
+    return st_addr;
+}
+pub fn unmap(comptime spec: mem.UnmapSpec, arena_index: u8) spec.Unwrapped(.munmap) {
+    const arena: mem.Arena = .{ .index = arena_index };
+    const up_addr = arena.end();
+    const st_addr = mach.alignA64(up_addr - 8192, 4096);
+    const len: u64 = up_addr - st_addr;
+    if (spec.call(.munmap, .{ st_addr, up_addr - st_addr })) {
+        if (spec.logging) {
+            mem.debug.unmapNotice(st_addr, len);
+        }
+    } else |unmap_error| {
+        if (spec.logging or builtin.is_correct) {
+            mem.debug.unmapError(unmap_error, st_addr, len);
+        }
+        return unmap_error;
+    }
+    try meta.wrap(mem.unmap(spec, st_addr, len));
+}
+
+pub const MapSpec = struct {
+    options: Options,
+    errors: ?[]const sys.ErrorCode = sys.mmap_errors,
+    return_type: type = void,
+    logging: bool = builtin.is_verbose,
+    const Specification = @This();
+    const Visibility = enum { shared, shared_validate, private };
+    const Options = struct {
+        visibility: Visibility = .private,
+        anonymous: bool = true,
+        populate: bool = true,
+        read: bool = true,
+        write: bool = true,
+        exec: bool = true,
+        grows_down: bool = true,
+        sync: bool = false,
+    };
+    pub fn flags(comptime spec: Specification) mem.Map {
+        var flags_bitfield: mem.Map = .{ .tag = .fixed_no_replace };
+        switch (spec.options.visibility) {
+            .private => flags_bitfield.set(.private),
+            .shared => flags_bitfield.set(.shared),
+            .shared_validate => flags_bitfield.set(.shared_validate),
+        }
+        if (spec.options.anonymous) {
+            flags_bitfield.set(.anonymous);
+        }
+        if (spec.options.grows_down) {
+            flags_bitfield.set(.grows_down);
+            flags_bitfield.set(.stack);
+        }
+        if (spec.options.populate) {
+            builtin.static.assert(spec.options.visibility == .private);
+            flags_bitfield.set(.populate);
+        }
+        if (spec.options.sync) {
+            builtin.static.assert(spec.options.visibility == .shared_validate);
+            flags_bitfield.set(.sync);
+        }
+        return flags_bitfield;
+    }
+    pub fn prot(comptime spec: Specification) mem.Prot {
+        var prot_bitfield: mem.Prot = .{ .val = 0 };
+        if (spec.options.read) {
+            prot_bitfield.set(.read);
+        }
+        if (spec.options.write) {
+            prot_bitfield.set(.write);
+        }
+        if (spec.options.exec) {
+            prot_bitfield.set(.exec);
+        }
+        return prot_bitfield;
+    }
+    pub usingnamespace sys.FunctionInterfaceSpec(Specification);
+};
