1c630440230fef15572acf1fe6867efdc7099fcd
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-23 01:55:09 +0000

    Added configuration presets.

---
+++ top/preset.zig
@@ -0,0 +1,60 @@
+const mem = @import("./mem.zig");
+const file = @import("./file.zig");
+const builtin = @import("./builtin.zig");
+
+pub const allocator = opaque {
+    pub const options = opaque {
+        pub const small: mem.AllocatorOptions = .{
+            .count_branches = false,
+            .count_allocations = false,
+            .count_useful_bytes = false,
+            .check_parametric_binding = false,
+        };
+    };
+    pub const logging = opaque {
+        pub const verbose: mem.AllocatorLogging = .{
+            .arena = builtin.Logging.verbose,
+            .head = true,
+            .sentinel = true,
+            .metadata = true,
+            .branches = true,
+            .map = builtin.Logging.verbose,
+            .unmap = builtin.Logging.verbose,
+            .remap = builtin.Logging.verbose,
+            .advise = builtin.Logging.verbose,
+            .allocate = true,
+            .reallocate = true,
+            .reinterpret = true,
+            .deallocate = true,
+        };
+        pub const silent: mem.AllocatorLogging = .{
+            .arena = builtin.Logging.silent,
+            .head = false,
+            .sentinel = false,
+            .metadata = false,
+            .branches = false,
+            .map = builtin.Logging.silent,
+            .unmap = builtin.Logging.silent,
+            .remap = builtin.Logging.silent,
+            .advise = builtin.Logging.silent,
+            .allocate = false,
+            .reallocate = false,
+            .reinterpret = false,
+            .deallocate = false,
+        };
+    };
+};
+pub const map = opaque {
+    pub const options = opaque {
+        pub const object: file.MapSpec.Options = .{
+            .visibility = .private,
+            .anonymous = false,
+            .read = true,
+            .write = false,
+            .exec = true,
+            .populate = true,
+            .grows_down = false,
+            .sync = false,
+        };
+    };
+};
