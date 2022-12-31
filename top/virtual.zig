412578ec0070bad7cd8d12c7bc7c27c71cac42a3
amp-59 <114923809+amp-59@users.noreply.github.com>
2022-12-31 01:15:12 +0000

    Added more virtual data structures.

--- top/virtual.zig
+++ top/virtual.zig
@@ -2,21 +2,17 @@
 //!
 //! The address space is currently divided 127 times to allow allocators and
 //! threads to manage mapped segments without possibility of collision.
-
 const mach = @import("./mach.zig");
 const meta = @import("./meta.zig");
 const builtin = @import("./builtin.zig");
-
 pub const Arena = struct {
     low: usize,
     high: usize,
     options: Options = .{},
-
     const Options = struct {
         thread_safe: bool = false,
     };
 };
-
 /// Maybe make generic on Endian.
 /// Right now it is difficult to get Zig vectors to produce consistent results,
 /// so this is not an option.
@@ -36,7 +32,6 @@ pub fn DiscreteBitSet(comptime bits: u16) type {
         const word_size: u8 = @bitSizeOf(usize);
         const real_bit_size: u16 = meta.alignAW(bits);
         const data_info: builtin.Type = @typeInfo(Data);
-
         pub fn positionToShiftAmount(pos: Index) u8 {
             if (data_info == .Array) {
                 return builtin.sub(u8, word_size - 1, builtin.rem(u8, pos, word_size));
@@ -72,10 +67,9 @@ pub fn DiscreteBitSet(comptime bits: u16) type {
 }
 pub fn ThreadSafeSet(comptime divisions: u16) type {
     return struct {
-        bits: [divisions]u8 = .{0} ** divisions,
-
+        bits: Data = .{0} ** divisions,
         const SafeSet: type = @This();
-
+        const Data: type = [divisions]u8;
         pub fn set(safe_set: *SafeSet, index: usize) void {
             safe_set.bits[index] = 255;
         }
@@ -109,7 +103,147 @@ pub fn ThreadSafeSet(comptime divisions: u16) type {
         }
     };
 }
-
+fn GenericMultiSet(
+    comptime spec: ExactAddressSpaceSpec,
+    comptime directory: ExactAddressSpaceSpec.Directory(spec),
+    comptime Set: type,
+) type {
+    return struct {
+        bits: Set = .{},
+        const MultiSet: type = @This();
+        const Index: type = ExactAddressSpaceSpec.Index(spec);
+        pub fn set(multi_set: *MultiSet, comptime index: Index) void {
+            return multi_set.bits[directory[index].field_index].set(directory[index].arena_index);
+        }
+        pub fn unset(multi_set: *MultiSet, comptime index: Index) void {
+            return multi_set.bits[directory[index].field_index].unset(directory[index].arena_index);
+        }
+        pub fn atomicSet(multi_set: *MultiSet, comptime index: Index) bool {
+            return multi_set.bits[directory[index].field_index].atomicSet(directory[index].arena_index);
+        }
+        pub fn atomicUnset(multi_set: *MultiSet, comptime index: Index) bool {
+            return multi_set.bits[directory[index].field_index].atomicUnset(directory[index].arena_index);
+        }
+        fn check(multi_set: *MultiSet, comptime index: Index) bool {
+            return multi_set.bits[directory[index].field_index].check(directory[index].arena_index);
+        }
+    };
+}
+pub const ExactAddressSpaceSpec = struct {
+    list: List,
+    options: Options = .{},
+    const Specification: type = @This();
+    const List = []const Arena;
+    const Options = struct {
+        prefer_word_size: bool = true,
+    };
+    fn Index(comptime spec: ExactAddressSpaceSpec) type {
+        return meta.LeastRealBitSize(spec.list.len);
+    }
+    fn Metadata(comptime spec: ExactAddressSpaceSpec) type {
+        return struct { field_index: Index(spec), arena_index: Index(spec) };
+    }
+    fn Directory(comptime spec: ExactAddressSpaceSpec) type {
+        return [spec.list.len]Metadata(spec);
+    }
+    fn Implementation(comptime spec: ExactAddressSpaceSpec) type {
+        var directory: Directory(spec) = undefined;
+        var fields: []const builtin.StructField = meta.empty;
+        var thread_safe_state: bool = spec.list[0].options.thread_safe;
+        var arena_index: Index(spec) = 0;
+        for (spec.list) |arena, index| {
+            if (thread_safe_state and !arena.options.thread_safe) {
+                const T: type = ThreadSafeSet(arena_index + 1);
+                fields = fields ++ meta.parcel(meta.structField(T, builtin.fmt.ci(fields.len), .{}));
+                directory[index] = .{ .arena_index = 0, .field_index = fields.len };
+                arena_index = 1;
+            } else if (!thread_safe_state and arena.options.thread_safe) {
+                const T: type = DiscreteBitSet(arena_index + 1);
+                fields = fields ++ meta.parcel(meta.structField(T, builtin.fmt.ci(fields.len), .{}));
+                directory[index] = .{ .arena_index = 0, .field_index = fields.len };
+                arena_index = 1;
+            } else {
+                directory[index] = .{ .arena_index = arena_index, .field_index = fields.len };
+                arena_index += 1;
+            }
+            thread_safe_state = arena.options.thread_safe;
+        }
+        if (thread_safe_state) {
+            const T: type = ThreadSafeSet(arena_index + 1);
+            fields = fields ++ meta.parcel(meta.structField(T, builtin.fmt.ci(fields.len), .{}));
+        } else {
+            const T: type = DiscreteBitSet(arena_index + 1);
+            fields = fields ++ meta.parcel(meta.structField(T, builtin.fmt.ci(fields.len), .{}));
+        }
+        const T: type = @Type(meta.tupleInfo(fields));
+        return GenericMultiSet(spec, directory, T);
+    }
+};
+/// Exact:
+/// Pro: * Arbitrary ranges.
+///      * Maps directly to an arena.
+///
+/// Con: * Arena index must be known at compile time.
+///      * Inversion is expensive.
+///      * Constructing the bit set fields can be expensive at compile time.
+///
+pub fn ExactAddressSpace(comptime spec: ExactAddressSpaceSpec) type {
+    return struct {
+        impl: Implementation = .{},
+        const AddressSpace = @This();
+        pub const Implementation: type = spec.Implementation();
+        pub const Index: type = Implementation.Index;
+        pub const addr_spec: ExactAddressSpaceSpec = spec;
+        pub fn unset(address_space: *AddressSpace, comptime index: Index) bool {
+            const ret: bool = address_space.impl.check(index);
+            if (ret) address_space.impl.unset(index);
+            return ret;
+        }
+        pub fn set(address_space: *AddressSpace, comptime index: Index) bool {
+            const ret: bool = address_space.impl.check(index);
+            if (!ret) address_space.impl.set(index);
+            return !ret;
+        }
+        pub fn acquire(address_space: *AddressSpace, comptime index: Index) !void {
+            if (!address_space.set(index)) {
+                return error.UnderSupply;
+            }
+        }
+        pub fn release(address_space: *AddressSpace, comptime index: Index) !void {
+            if (!address_space.unset(index)) {
+                return error.OverSupply;
+            }
+        }
+        fn atomicUnset(address_space: *AddressSpace, comptime index: Index) bool {
+            if (!spec.list[index].options.thread_safe) {
+                @compileError("arena is not thread safe");
+            }
+            return address_space.impl.atomicUnset(index);
+        }
+        fn atomicSet(address_space: *AddressSpace, comptime index: Index) bool {
+            if (!spec.list[index].options.thread_safe) {
+                @compileError("arena is not thread safe");
+            }
+            return address_space.impl.atomicSet(index);
+        }
+        pub fn atomicAcquire(address_space: *AddressSpace, comptime index: Index) !void {
+            if (!address_space.atomicSet(index)) {
+                return error.UnderSupply;
+            }
+        }
+        pub fn atomicRelease(address_space: *AddressSpace, comptime index: Index) !void {
+            if (!address_space.atomicUnset(index)) {
+                return error.OverSupply;
+            }
+        }
+        pub fn low(index: Index) usize {
+            return spec.list[index].low;
+        }
+        pub fn high(index: Index) usize {
+            return spec.list[index].high;
+        }
+    };
+}
 const FormulaicAddressSpaceSpec = struct {
     low: usize = 0x40000000,
     high: usize = ~@as(usize, 0),
@@ -149,9 +283,9 @@ pub fn FormulaicAddressSpace(comptime spec: FormulaicAddressSpaceSpec) type {
     return struct {
         impl: Implementation = .{},
         const AddressSpace = @This();
-        const Index: type = meta.AlignSizeAW(builtin.ShiftAmount(u64));
-        const Implementation: type = spec.Implementation();
-        const addr_spec: FormulaicAddressSpaceSpec = spec;
+        pub const Index: type = meta.AlignSizeAW(builtin.ShiftAmount(u64));
+        pub const Implementation: type = spec.Implementation();
+        pub const addr_spec: FormulaicAddressSpaceSpec = spec;
         const max_bit: usize = 1 << 47;
         const len: usize = blk: {
             const mask: usize = spec.alignment - 1;
@@ -162,13 +296,12 @@ pub fn FormulaicAddressSpace(comptime spec: FormulaicAddressSpaceSpec) type {
             builtin.static.assertBelowOrEqual(usize, spec.low, high(0));
             builtin.static.assertAboveOrEqual(usize, spec.high, low(spec.divisions));
         }
-
-        fn unset(address_space: *AddressSpace, index: Index) bool {
+        pub fn unset(address_space: *AddressSpace, index: Index) bool {
             const ret: bool = address_space.impl.check(index);
-            if (ret) address_space.bits.unset(index);
+            if (ret) address_space.impl.unset(index);
             return ret;
         }
-        fn set(address_space: *AddressSpace, index: Index) bool {
+        pub fn set(address_space: *AddressSpace, index: Index) bool {
             const ret: bool = address_space.impl.check(index);
             if (!ret) address_space.impl.set(index);
             return ret;
@@ -187,17 +320,13 @@ pub fn FormulaicAddressSpace(comptime spec: FormulaicAddressSpaceSpec) type {
             if (!spec.options.thread_safe) {
                 @compileError("address space is not thread safe");
             }
-            const ret: bool = address_space.impl.check(index);
-            if (ret) address_space.bits.unset(index);
-            return ret;
+            return address_space.impl.atomicUnset(index);
         }
         fn atomicSet(address_space: *AddressSpace, index: Index) bool {
             if (!spec.options.thread_safe) {
                 @compileError("address space is not thread safe");
             }
-            const ret: bool = address_space.impl.check(index);
-            if (!ret) address_space.impl.atomicSet(index);
-            return ret;
+            return address_space.impl.atomicSet(index);
         }
         pub fn atomicAcquire(address_space: *AddressSpace, index: Index) !void {
             if (!address_space.atomicSet(index)) {
