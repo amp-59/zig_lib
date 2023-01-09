ba59b8e7ad21a37a960fcf50df7fc2bcc0e87249
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-09 11:59:08 +0000

    Minor restructuring.

--- test/itos.zig
+++ test/itos.zig
@@ -2,8 +2,10 @@ const srg = @import("../zig_lib.zig");
 const mem = srg.mem;
 const sys = srg.sys;
 const proc = srg.proc;
+const mach = srg.mach;
 const meta = srg.meta;
 const file = srg.file;
+const preset = srg.preset;
 const builtin = srg.builtin;
 
 const opts = @import("./opts.zig");
@@ -16,8 +18,15 @@ const Radix = enum(u5) {
     dec = 10,
     hex = 16,
 };
+fn noSuchOption(opt_arg: []const u8) void {
+    var print_array: mem.StaticString(4096) = undefined;
+    print_array.impl.ub_word = 0;
+    print_array.writeAny(mem.ptr_wr_spec, [3][]const u8{ "unrecognised output mode: '", opt_arg, "'\n-o, --output=     x,d,o,b\n" });
+    file.noexcept.write(2, print_array.readAll());
+}
 const Options = struct {
     output: Radix = .hex,
+    input: ?[:0]const u8 = null,
 };
 fn validateOutputMode(options: *Options, opt_arg: [:0]const u8) void {
     if (mem.testEqualMany(u8, "hex", opt_arg) or
@@ -37,31 +46,27 @@ fn validateOutputMode(options: *Options, opt_arg: [:0]const u8) void {
     {
         options.output = .oct;
     } else {
-        file.noexcept.write(2, "unrecognised output mode: '");
-        file.noexcept.write(2, opt_arg);
-        file.noexcept.write(2, "'\n");
-        file.noexcept.write(2,
-            \\-o, --output=     x,d,o,b
-            \\
-        );
+        noSuchOption(opt_arg);
         sys.exit(2);
     }
 }
+fn loopInner(options: Options, arg: []const u8) !void {
+    file.noexcept.write(1, switch (options.output) {
+        .hex => builtin.fmt.ux64(try builtin.parse.any(u64, arg)).readAll(),
+        .dec => builtin.fmt.ud64(try builtin.parse.any(u64, arg)).readAll(),
+        .oct => builtin.fmt.uo64(try builtin.parse.any(u64, arg)).readAll(),
+        .bin => builtin.fmt.ub64(try builtin.parse.any(u64, arg)).readAll(),
+    });
+    file.noexcept.write(1, "\n");
+}
 pub fn main(args_in: [][*:0]u8) !void {
     var args: [][*:0]u8 = args_in;
-
     const options: Options = opts.getOpts(Options, &args, &[_]opts.GenericOptions(Options){
         .{ .decl = .output, .short = "-o", .long = "--output", .assign = .{ .convert = validateOutputMode } },
     });
     var i: u64 = 1;
     while (i != args.len) {
-        file.noexcept.write(1, switch (options.output) {
-            .hex => builtin.fmt.ux64(try builtin.parse.any(u64, meta.manyToSlice(args[i]))).readAll(),
-            .dec => builtin.fmt.ud64(try builtin.parse.any(u64, meta.manyToSlice(args[i]))).readAll(),
-            .oct => builtin.fmt.uo64(try builtin.parse.any(u64, meta.manyToSlice(args[i]))).readAll(),
-            .bin => builtin.fmt.ub64(try builtin.parse.any(u64, meta.manyToSlice(args[i]))).readAll(),
-        });
-        file.noexcept.write(1, "\n");
+        try loopInner(options, meta.manyToSlice(args[i]));
         i += 1;
     }
 }
