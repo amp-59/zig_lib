8abb9e87740df82877803485ec9f3297eae13711
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-09 01:56:26 +0000

    Integer maxima literals now more literal. Added a table of characters and escape sequences useful for rendering strings containing escape sequences.

--- top/lit.zig
+++ top/lit.zig
@@ -1,22 +1,22 @@
 //! Constants. More useful as a reference than as an import.
 // If the programmer can remember to use these, the LHS name may be more helpful
 // to the reader than the RHS expression.
-pub const max_val_u8: u8 = ~@as(u8, 0);
-pub const max_val_u16: u16 = ~@as(u16, 0);
-pub const max_val_u32: u32 = ~@as(u32, 0);
-pub const max_val_u64: u64 = ~@as(u64, 0);
-pub const max_bit_u8: u8 = 1 + (max_val_u8 >> 1);
-pub const max_bit_u16: u16 = 1 + (max_val_u16 >> 1);
-pub const max_bit_u32: u32 = 1 + (max_val_u32 >> 1);
-pub const max_bit_u64: u64 = 1 + (max_val_u64 >> 1);
-pub const max_val_i8: i8 = max_val_u8 >> 1;
-pub const max_val_i16: i16 = max_val_u16 >> 1;
-pub const max_val_i32: i32 = max_val_u32 >> 1;
-pub const max_val_i64: i64 = max_val_u64 >> 1;
-pub const max_bit_i8: i8 = 1 + (max_val_i8 >> 1);
-pub const max_bit_i16: i16 = 1 + (max_val_i16 >> 1);
-pub const max_bit_i32: i32 = 1 + (max_val_i32 >> 1);
-pub const max_bit_i64: i64 = 1 + (max_val_i64 >> 1);
+pub const max_val_u8: u8 = 0xff;
+pub const max_val_u16: u16 = 0xffff;
+pub const max_val_u32: u32 = 0xffffffff;
+pub const max_val_u64: u64 = 0xffffffffffffffff;
+pub const max_bit_u8: u8 = 0x80;
+pub const max_bit_u16: u16 = 0x8000;
+pub const max_bit_u32: u32 = 0x80000000;
+pub const max_bit_u64: u64 = 0x8000000000000000;
+pub const max_val_i8: i8 = 0x7f;
+pub const max_val_i16: i16 = 0x7fff;
+pub const max_val_i32: i32 = 0x7fffffff;
+pub const max_val_i64: i64 = 0x7fffffffffffffff;
+pub const max_bit_i8: i8 = 0x40;
+pub const max_bit_i16: i16 = 0x4000;
+pub const max_bit_i32: i32 = 0x40000000;
+pub const max_bit_i64: i64 = 0x4000000000000000;
 // So that basic formatters do not need to compute a safe buffer length.
 pub const u8d_max_len: u64 = 3;
 pub const u8x_max_len: u64 = 5;
@@ -28,7 +28,7 @@ pub const i64d_max_len: u64 = 20;
 pub const i64x_max_len: u64 = 19;
 /// Unconfirmed theory that using these is faster to compile when naming tuple
 /// fields.
-pub const ud8 = [256][:0]const u8{
+pub const ud8 = [256][]const u8{
     "0",   "1",   "2",   "3",   "4",   "5",   "6",   "7",   "8",   "9",
     "10",  "11",  "12",  "13",  "14",  "15",  "16",  "17",  "18",  "19",
     "20",  "21",  "22",  "23",  "24",  "25",  "26",  "27",  "28",  "29",
@@ -56,7 +56,7 @@ pub const ud8 = [256][:0]const u8{
     "240", "241", "242", "243", "244", "245", "246", "247", "248", "249",
     "250", "251", "252", "253", "254", "255",
 };
-pub const esc_hex_sequences = [256][:0]const u8{
+pub const esc_hex_sequences = [256][]const u8{
     "\\x00", "\\x01", "\\x02", "\\x03", "\\x04", "\\x05", "\\x06", "\\x07", "\\x08",
     "\\x09", "\\x0a", "\\x0b", "\\x0c", "\\x0d", "\\x0e", "\\x0f", "\\x10", "\\x11",
     "\\x12", "\\x13", "\\x14", "\\x15", "\\x16", "\\x17", "\\x18", "\\x19", "\\x1a",
@@ -87,15 +87,63 @@ pub const esc_hex_sequences = [256][:0]const u8{
     "\\xf3", "\\xf4", "\\xf5", "\\xf6", "\\xf7", "\\xf8", "\\xf9", "\\xfa", "\\xfb",
     "\\xfc", "\\xfd", "\\xfe", "\\xff",
 };
-pub const int_prefixes = .{
+pub const lit_hex_sequences: [256][]const u8 = .{
+    "\\x00", "\\x01", "\\x02", "\\x03", "\\x04", "\\x05", "\\x06", "\\x07", "\\x08",
+    "\\x09", "\\x0a", "\\x0b", "\\x0c", "\\x0d", "\\x0e", "\\x0f", "\\x10", "\\x11",
+    "\\x12", "\\x13", "\\x14", "\\x15", "\\x16", "\\x17", "\\x18", "\\x19", "\\x1a",
+    "\\x1b", "\\x1c", "\\x1d", "\\x1e", "\\x1f", " ",     "!",     "\"",    "#",
+    "$",     "%",     "&",     "'",     "(",     ")",     "*",     "+",     ",",
+    "-",     ".",     "/",     "0",     "1",     "2",     "3",     "4",     "5",
+    "6",     "7",     "8",     "9",     ":",     ";",     "<",     "=",     ">",
+    "?",     "@",     "A",     "B",     "C",     "D",     "E",     "F",     "G",
+    "H",     "I",     "J",     "K",     "L",     "M",     "N",     "O",     "P",
+    "Q",     "R",     "S",     "T",     "U",     "V",     "W",     "X",     "Y",
+    "Z",     "[",     "\\",    "]",     "^",     "_",     "`",     "a",     "b",
+    "c",     "d",     "e",     "f",     "g",     "h",     "i",     "j",     "k",
+    "l",     "m",     "n",     "o",     "p",     "q",     "r",     "s",     "t",
+    "u",     "v",     "w",     "x",     "y",     "z",     "{",     "|",     "}",
+    "~",     "\\x7f", "\\x80", "\\x81", "\\x82", "\\x83", "\\x84", "\\x85", "\\x86",
+    "\\x87", "\\x88", "\\x89", "\\x8a", "\\x8b", "\\x8c", "\\x8d", "\\x8e", "\\x8f",
+    "\\x90", "\\x91", "\\x92", "\\x93", "\\x94", "\\x95", "\\x96", "\\x97", "\\x98",
+    "\\x99", "\\x9a", "\\x9b", "\\x9c", "\\x9d", "\\x9e", "\\x9f", "\\xa0", "\\xa1",
+    "\\xa2", "\\xa3", "\\xa4", "\\xa5", "\\xa6", "\\xa7", "\\xa8", "\\xa9", "\\xaa",
+    "\\xab", "\\xac", "\\xad", "\\xae", "\\xaf", "\\xb0", "\\xb1", "\\xb2", "\\xb3",
+    "\\xb4", "\\xb5", "\\xb6", "\\xb7", "\\xb8", "\\xb9", "\\xba", "\\xbb", "\\xbc",
+    "\\xbd", "\\xbe", "\\xbf", "\\xc0", "\\xc1", "\\xc2", "\\xc3", "\\xc4", "\\xc5",
+    "\\xc6", "\\xc7", "\\xc8", "\\xc9", "\\xca", "\\xcb", "\\xcc", "\\xcd", "\\xce",
+    "\\xcf", "\\xd0", "\\xd1", "\\xd2", "\\xd3", "\\xd4", "\\xd5", "\\xd6", "\\xd7",
+    "\\xd8", "\\xd9", "\\xda", "\\xdb", "\\xdc", "\\xdd", "\\xde", "\\xdf", "\\xe0",
+    "\\xe1", "\\xe2", "\\xe3", "\\xe4", "\\xe5", "\\xe6", "\\xe7", "\\xe8", "\\xe9",
+    "\\xea", "\\xeb", "\\xec", "\\xed", "\\xee", "\\xef", "\\xf0", "\\xf1", "\\xf2",
+    "\\xf3", "\\xf4", "\\xf5", "\\xf6", "\\xf7", "\\xf8", "\\xf9", "\\xfa", "\\xfb",
+    "\\xfc", "\\xfd", "\\xfe", "\\xff",
+};
+const Range = extern struct { lower: u8, upper: u8 };
+pub const character_ranges = opaque {
+    pub const print: Range = .{ .lower = 0x20, .upper = 0x7e };
+    pub const lower: Range = .{ .lower = 0x61, .upper = 0x7a };
+    pub const upper: Range = .{ .lower = 0x41, .upper = 0x5a };
+    pub const digit: Range = .{ .lower = 0x30, .upper = 0x39 };
+};
+pub const character_classes = opaque {
+    pub const alpha: [2]Range = .{ character_ranges.upper, character_ranges.lower };
+    pub const alnum: [3]Range = .{ character_ranges.upper, character_ranges.lower, character_ranges.digit };
+    pub const punct: [4]Range = .{
+        .{ .lower = 33, .upper = 47 },
+        .{ .lower = 58, .upper = 64 },
+        .{ .lower = 91, .upper = 96 },
+        .{ .lower = 123, .upper = 126 },
+    };
+};
+pub const int_prefixes: [51][]const u8 = [_][]const u8{
     "", "", "0b", "", "", "", "", "", "0o", "", "", "", "0d", "", "", "", "0x",
     "", "", "",   "", "", "", "", "", "",   "", "", "", "",   "", "", "", "",
     "", "", "0z", "", "", "", "", "", "",   "", "", "", "",   "", "", "", "",
 };
-pub const fx = struct {
+pub const fx = opaque {
     pub const none: []const u8 = "\x1b\x5b\x30\x6d";
-    pub const color = struct {
-        pub const fg = struct {
+    pub const color = opaque {
+        pub const fg = opaque {
             pub const black: []const u8 = "\x1b\x5b\x33\x30\x6d";
             pub const red: []const u8 = "\x1b\x5b\x33\x31\x6d";
             pub const green: []const u8 = "\x1b\x5b\x33\x32\x6d";
@@ -122,7 +170,7 @@ pub const fx = struct {
                 return mcode(.{ 38, 5, 255 - @min(23, index) });
             }
         };
-        pub const bg = packed union {
+        pub const bg = opaque {
             pub const black: []const u8 = "\x1b\x5b\x34\x30\x6d";
             pub const red: []const u8 = "\x1b\x5b\x34\x31\x6d";
             pub const green: []const u8 = "\x1b\x5b\x34\x32\x6d";
@@ -140,7 +188,7 @@ pub const fx = struct {
             pub const hi_white: []const u8 = "\x1b\x5b\x31\x30\x37\x6d";
         };
     };
-    pub const style = struct {
+    pub const style = opaque {
         pub const bold: []const u8 = "\x1b\x5b\x31\x6d";
         pub const faint: []const u8 = "\x1b\x5b\x32\x6d";
         pub const italic: []const u8 = "\x1b\x5b\x33\x6d";
