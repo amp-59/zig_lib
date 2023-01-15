64c89b55e508b853764595976352352b39e758b4
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-15 01:40:32 +0000

    Added generic conversion function for option arguments.

--- test/opts.zig
+++ test/opts.zig
@@ -21,30 +21,38 @@ pub fn GenericOptions(comptime Options: type) type {
             boolean: bool,
             argument,
             convert: fn (*Options, [:0]const u8) void,
+            generic: fn (*Options, comptime [:0]const u8, [:0]const u8) void,
         },
         descr: ?[]const u8 = null,
         clobber: bool = true,
 
         const Option = @This();
-        fn getOptInternal(comptime flag: Option, options: *Options, args: *[][*:0]u8, idx: u64, off: u64) void {
+        fn getOptInternal(comptime flag: Option, options: *Options, args: *[][*:0]u8, index: u64, offset: u64) void {
             switch (flag.assign) {
                 .boolean => |value| {
-                    proc.shift(args, idx);
+                    proc.shift(args, index);
                     @field(options, @tagName(flag.decl)) = value;
                 },
                 .argument => {
-                    if (off == 0) {
-                        proc.shift(args, idx);
+                    if (offset == 0) {
+                        proc.shift(args, index);
                     }
-                    @field(options, @tagName(flag.decl)) = meta.manyToSlice(args.*[idx])[off..];
-                    proc.shift(args, idx);
+                    @field(options, @tagName(flag.decl)) = meta.manyToSlice(args.*[index])[offset..];
+                    proc.shift(args, index);
                 },
                 .convert => |convert| {
-                    if (off == 0) {
-                        proc.shift(args, idx);
+                    if (offset == 0) {
+                        proc.shift(args, index);
                     }
-                    convert(options, meta.manyToSlice(args.*[idx])[off..]);
-                    proc.shift(args, idx);
+                    convert(options, meta.manyToSlice(args.*[index])[offset..]);
+                    proc.shift(args, index);
+                },
+                .generic => |generic| {
+                    if (offset == 0) {
+                        proc.shift(args, index);
+                    }
+                    generic(options, @tagName(flag.decl), meta.manyToSlice(args.*[index])[offset..]);
+                    proc.shift(args, index);
                 },
             }
         }
@@ -55,44 +63,44 @@ pub fn getOpts(comptime Options: type, args: *[][*:0]u8, all_options: []const Ge
     if (args.len == 0) {
         return options;
     }
-    var idx: u64 = 1;
-    lo: while (idx != args.len) {
+    var index: u64 = 1;
+    lo: while (index != args.len) {
         inline for (all_options) |option| {
-            if (idx == args.len) {
+            if (index == args.len) {
                 break :lo;
             }
-            const arg1: [:0]const u8 = meta.manyToSlice(args.*[idx]);
+            const arg1: [:0]const u8 = meta.manyToSlice(args.*[index]);
             if (option.long) |long_switch| {
                 if (mem.testEqualMany(u8, long_switch, arg1)) {
-                    option.getOptInternal(&options, args, idx, 0);
+                    option.getOptInternal(&options, args, index, 0);
                     continue :lo;
                 }
                 const assign_long_switch: []const u8 = long_switch ++ "=";
                 if (mem.testEqualManyFront(u8, assign_long_switch, arg1)) {
-                    option.getOptInternal(&options, args, idx, assign_long_switch.len);
+                    option.getOptInternal(&options, args, index, assign_long_switch.len);
                     continue :lo;
                 }
             }
             if (option.short) |short_switch| {
                 if (mem.testEqualMany(u8, short_switch, arg1)) {
-                    option.getOptInternal(&options, args, idx, 0);
+                    option.getOptInternal(&options, args, index, 0);
                     continue :lo;
                 }
                 if (mem.testEqualManyFront(u8, short_switch, arg1)) {
-                    option.getOptInternal(&options, args, idx, short_switch.len);
+                    option.getOptInternal(&options, args, index, short_switch.len);
                     continue :lo;
                 }
             }
         }
-        const arg1: [:0]const u8 = meta.manyToSlice(args.*[idx]);
+        const arg1: [:0]const u8 = meta.manyToSlice(args.*[index]);
         if (mem.testEqualMany(u8, "--", arg1)) {
-            proc.shift(args, idx);
+            proc.shift(args, index);
             break :lo;
         }
         if (mem.testEqualManyFront(u8, "--", arg1)) {
             debug.badLongSwitchHelp(Options, all_options, arg1);
         }
-        idx += 1;
+        index += 1;
     }
     return options;
 }
@@ -100,7 +108,6 @@ pub fn getOpts(comptime Options: type, args: *[][*:0]u8, all_options: []const Ge
 const debug = opaque {
     const about_opt_0_s: []const u8 = "opt:            '";
     const about_opt_1_s: []const u8 = "opt-error:      '";
-
     fn badLongSwitchHelp(comptime Options: type, comptime all_options: []const GenericOptions(Options), arg: [:0]const u8) void {
         var array: mem.StaticString(4096) = .{};
         const bad_arg: []const u8 = mem.readBeforeFirstEqualManyOrElse(u8, "=", arg);
