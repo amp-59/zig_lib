2e49b4e5fdde4b00a26b22f3918dffc7a4e1348e
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-18 05:29:59 +0000

    Fixed trivial error.

--- test/cat.zig
+++ test/cat.zig
@@ -11,12 +11,16 @@ pub fn main(args_in: [][*:0]u8) !void {
     if (args_in.len == 1) {
         return;
     }
-    for (args_in) |arg_in| {
+    for (args_in[1..]) |arg_in| {
         const arg: [:0]const u8 = meta.manyToSlice(arg_in);
-        const fd: u64 = try file.open(.{ .options = .{ .read = true } }, arg);
+        const fd: u64 = file.open(.{ .options = .{ .read = true } }, arg) catch {
+            continue;
+        };
         defer file.close(.{ .errors = null }, fd);
         const lb_addr: u64 = 0x40000000;
-        const up_addr: u64 = try file.map(.{ .options = .{ .visibility = .private } }, lb_addr, fd);
+        const up_addr: u64 = file.map(.{ .options = .{ .visibility = .private } }, lb_addr, fd) catch {
+            continue;
+        };
         const s_bytes: u64 = mach.alignA(up_addr - lb_addr, 4096);
         defer mem.unmap(.{ .errors = null }, lb_addr, s_bytes);
         file.noexcept.write(1, mem.pointerMany(u8, lb_addr, s_bytes));
