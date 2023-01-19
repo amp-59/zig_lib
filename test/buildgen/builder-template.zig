7b52b8454a60d361f7bab5810393ab71b3cbcb40
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-19 20:21:43 +0000

    * Replaced placeholder modules with actual modules. * Added builder utility functions to context.

--- test/buildgen/builder-template.zig
+++ test/buildgen/builder-template.zig
@@ -1,11 +1,13 @@
 //! start
-const sys = struct {};
-const mem = struct {};
-const file = struct {};
-const meta = struct {};
-const proc = struct {};
-const preset = struct {};
-const builtin = struct {};
+const srg = @import("zig_lib");
+const mem = srg.mem;
+const sys = srg.sys;
+const fmt = srg.fmt;
+const proc = srg.proc;
+const file = srg.file;
+const meta = srg.meta;
+const preset = srg.preset;
+const builtin = srg.builtin;
 // start-document builder-struct.zig
 const fmt_spec: mem.ReinterpretSpec = blk: {
     var tmp: mem.ReinterpretSpec = preset.reinterpret.fmt;
@@ -17,7 +19,6 @@ pub const BuildCmdSpec = struct {
     max_args: u64 = 1024,
     Allocator: ?type = null,
 };
-
 pub const AddressSpace = preset.address_space.exact_8;
 pub const Allocator = mem.GenericArenaAllocator(.{
     .arena_index = 0,
@@ -28,10 +29,8 @@ pub const String = Allocator.StructuredVectorLowAligned(u8, 8);
 pub const Pointers = Allocator.StructuredVector([*:0]u8);
 pub const StaticString = mem.StructuredAutomaticVector(u8, null, max_len, 8, .{});
 pub const StaticPointers = mem.StructuredAutomaticVector([*:0]u8, null, max_args, 8, .{});
-
 const max_len: u64 = 65536;
 const max_args: u64 = 512;
-
 pub const BuildCmd = struct {
     const Builder: type = @This();
     const zig: [:0]const u8 = "zig";
@@ -81,7 +80,7 @@ fn makeArgs(array: anytype, args: anytype) u64 {
     var idx: u64 = 0;
     for (array.readAll()) |c, i| {
         if (c == 0) {
-            args.writeOne(array.referManyWithSentinelAt(idx, 0).ptr);
+            args.writeOne(array.referManyWithSentinelAt(0, idx).ptr);
             idx = i + 1;
         }
     }
@@ -139,7 +138,6 @@ pub const Macro = struct {
     value: ?[]const u8,
     quote: bool = false,
     const Format = @This();
-
     fn looksLikePath(format: Format) bool {
         var no_sep: u64 = 0;
         if (format.value) |value| {
@@ -189,9 +187,56 @@ pub const Context = struct {
     global_cache_dir: [:0]const u8,
     args: [][*:0]u8,
     vars: [][*:0]u8,
-
+    allocator: *Allocator,
+    array: *ArrayU,
+    pub const ArrayU = Allocator.UnstructuredHolder(8, 8);
     pub fn path(ctx: *Context, name: [:0]const u8) Path {
-        return .{ .ctx = ctx, .relative = name };
+        return .{ .ctx = ctx, .relative = ctx.dupeWithSentinel(u8, 0, name) };
+    }
+    pub fn dupe(ctx: *Context, comptime T: type, values: []const T) []const T {
+        ctx.array.writeMany(T, values);
+        return ctx.array.referManyBack(T, .{ .count = values.len });
+    }
+    pub fn dupeWithSentinel(ctx: *Context, comptime T: type, comptime sentinel: T, values: [:sentinel]const T) [:sentinel]const T {
+        ctx.array.writeMany(T, values);
+        ctx.array.referOneUndefined(T).* = sentinel;
+        defer ctx.array.define(T, .{ .count = 1 });
+        return ctx.array.referManyWithSentinelBack(T, 0, .{ .count = values.len });
+    }
+    pub fn addExecutable(
+        ctx: *Context,
+        comptime name: [:0]const u8,
+        comptime pathname: [:0]const u8,
+        comptime args: Args(name),
+    ) BuildCmd {
+        var ret: BuildCmd = .{
+            .root = pathname,
+            .cmd = .exe,
+            .name = name,
+        };
+        ret.zig_exe = ctx.zig_exe;
+        comptime var macros: []const Macro = args.macros orelse meta.empty;
+        macros = comptime args.setMacro(macros, "is_correct");
+        macros = comptime args.setMacro(macros, "is_verbose");
+        if (args.build_mode) |build_mode| {
+            ret.O = build_mode;
+        }
+        if (args.emit_bin_path) |bin_path| {
+            ret.emit_bin = .{ .yes = ctx.path(bin_path) };
+        }
+        if (args.emit_asm_path) |asm_path| {
+            ret.emit_asm = .{ .yes = ctx.path(asm_path) };
+        }
+        ret.omit_frame_pointer = false;
+        ret.single_threaded = true;
+        ret.static = true;
+        ret.enable_cache = true;
+        ret.compiler_rt = false;
+        ret.strip = true;
+        ret.main_pkg_path = ctx.build_root;
+        ret.macros = macros;
+        ret.packages = args.packages;
+        return ret;
     }
 };
 pub const Path = struct {
@@ -211,4 +256,44 @@ pub const Path = struct {
         return len;
     }
 };
+fn Args(comptime name: [:0]const u8) type {
+    return struct {
+        make_step_name: [:0]const u8 = name,
+        make_step_desc: [:0]const u8 = "Build " ++ name,
+        run_step_name: [:0]const u8 = "run-" ++ name,
+        run_step_desc: [:0]const u8 = "...",
+        emit_bin_path: ?[:0]const u8 = "zig-out/bin/" ++ name,
+        emit_asm_path: ?[:0]const u8 = "zig-out/bin/" ++ name ++ ".s",
+        emit_analysis_path: ?[:0]const u8 = "zig-out/bin/" ++ name ++ ".analysis",
+        build_mode: ?@TypeOf(builtin.zig.mode) = null,
+        build_working_directory: bool = false,
+        is_test: ?bool = null,
+        is_support: ?bool = null,
+        is_correct: ?bool = null,
+        is_perf: ?bool = null,
+        is_verbose: ?bool = null,
+        is_tolerant: ?bool = null,
+        define_build_root: bool = true,
+        define_build_working_directory: bool = true,
+        is_large_test: bool = false,
+        strip: bool = true,
+        packages: ?Packages = null,
+        macros: ?Macros = null,
+        fn setMacro(
+            comptime args: @This(),
+            comptime macros: []const Macro,
+            comptime field_name: [:0]const u8,
+        ) []const Macro {
+            comptime {
+                if (@field(args, field_name)) |field| {
+                    return meta.concat(Macro, macros, .{
+                        .name = field_name,
+                        .value = if (field) "1" else "0",
+                    });
+                }
+                return macros;
+            }
+        }
+    };
+}
 // finish-document builder-types.zig
