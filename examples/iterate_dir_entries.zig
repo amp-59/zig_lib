becf175bcc283717ac1650cb0c639655837cb350
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-20 10:01:34 +0000

    Created examples directory, added existing examples.

---
+++ examples/iterate_dir_entries.zig
@@ -0,0 +1,47 @@
+const srg = @import("zig_lib");
+const mem = srg.mem;
+const file = srg.file;
+const proc = srg.proc;
+const meta = srg.meta;
+const preset = srg.preset;
+
+pub usingnamespace proc.start;
+
+pub const AddressSpace = preset.address_space.exact_8;
+
+pub const is_correct: bool = false;
+pub const is_verbose: bool = false;
+
+const Allocator = mem.GenericArenaAllocator(.{
+    .arena_index = 0,
+    .options = .{ .require_filo_free = false },
+    .logging = preset.allocator.logging.silent,
+});
+const DirStream = file.GenericDirStream(.{ .Allocator = Allocator });
+
+pub fn main(args: [][*:0]u8) !void {
+    // Acquire dynamic memory boilerplate
+    var address_space: AddressSpace = .{};
+    var allocator: Allocator = try Allocator.init(&address_space);
+    defer allocator.deinit(&address_space);
+
+    for (args[1..]) |arg| {
+        // Open directory a path
+        var dir: DirStream = try DirStream.init(&allocator, meta.manyToSlice(arg));
+        defer dir.deinit(&allocator);
+
+        // Convert stream to linked list
+        var list: DirStream.ListView = dir.list();
+
+        var index: u64 = 1;
+        while (index != list.count) : (index += 1) {
+            if (list.at(index)) |entry| {
+                // Write directory entry name to stderr
+                file.noexcept.write(2, entry.name());
+                file.noexcept.write(2, "\n");
+            } else |_| {
+                break;
+            }
+        }
+    }
+}
