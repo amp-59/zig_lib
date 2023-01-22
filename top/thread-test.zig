910c66cb5dbcac3102908413dd1d8280f1c243f9
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-22 14:42:13 +0000

    Added labels to address spaces.

--- top/thread-test.zig
+++ top/thread-test.zig
@@ -9,66 +9,56 @@ const file = @import("./file.zig");
 const time = @import("./time.zig");
 const thread = @import("./thread.zig");
 const preset = @import("./preset.zig");
+const virtual = @import("./virtual.zig");
 const builtin = @import("./builtin.zig");
 
 pub usingnamespace proc.start;
 
 pub const is_correct: bool = true;
-pub const AddressSpace = preset.address_space.exact_8;
 
-const ThreadSpace = mem.GenericDiscreteSubAddressSpace(.{
+const AddressSpace = mem.GenericDiscreteAddressSpace(.{
+    .label = "super",
     .list = meta.slice(mem.Arena, .{
-        .{ .lb_addr = 0x40000000, .up_addr = 0x41000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x41000000, .up_addr = 0x42000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x42000000, .up_addr = 0x43000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x43000000, .up_addr = 0x44000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x44000000, .up_addr = 0x45000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x45000000, .up_addr = 0x46000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x46000000, .up_addr = 0x47000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x47000000, .up_addr = 0x48000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x48000000, .up_addr = 0x49000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x49000000, .up_addr = 0x4a000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x4a000000, .up_addr = 0x4b000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x4b000000, .up_addr = 0x4c000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x4c000000, .up_addr = 0x4d000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x4d000000, .up_addr = 0x4e000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x4e000000, .up_addr = 0x4f000000, .options = .{ .thread_safe = true } },
-        .{ .lb_addr = 0x4f000000, .up_addr = 0x50000000, .options = .{ .thread_safe = true } },
+        .{ .lb_addr = 0x00040000000, .up_addr = 0x10000000000 },
+        .{ .lb_addr = 0x10000000000, .up_addr = 0x20000000000 },
+        .{ .lb_addr = 0x20000000000, .up_addr = 0x30000000000 },
+        .{ .lb_addr = 0x30000000000, .up_addr = 0x40000000000 },
+        .{ .lb_addr = 0x40000000000, .up_addr = 0x50000000000 },
+        .{ .lb_addr = 0x50000000000, .up_addr = 0x60000000000 },
+        .{ .lb_addr = 0x60000000000, .up_addr = 0x70000000000 },
+        .{ .lb_addr = 0x70000000000, .up_addr = 0x80000000000 },
+        .{ .lb_addr = 0x80000000000, .up_addr = 0x90000000000 },
     }),
-}, AddressSpace);
+    .subspace = meta.slice(meta.Generic, .{virtual.generic(.{
+        .label = "thread",
+        //.lb_addr = 0x10000000000,
+        //.up_addr = 0x40400000000,
+        //.divisions = 16,
+        //.options = .{ .thread_safe = true },
+        .list = list,
+    })}),
+});
+const list = meta.slice(mem.Arena, .{
+    .{ .lb_addr = 0x10000000000, .up_addr = 0x10100000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x10100000000, .up_addr = 0x10200000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x10200000000, .up_addr = 0x10300000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x10300000000, .up_addr = 0x10400000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x20000000000, .up_addr = 0x20100000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x20100000000, .up_addr = 0x20200000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x20200000000, .up_addr = 0x20300000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x20300000000, .up_addr = 0x20400000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x30000000000, .up_addr = 0x30100000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x30100000000, .up_addr = 0x30200000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x30200000000, .up_addr = 0x30300000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x30300000000, .up_addr = 0x30400000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x40000000000, .up_addr = 0x40100000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x40100000000, .up_addr = 0x40200000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x40200000000, .up_addr = 0x40300000000, .options = .{ .thread_safe = true } },
+    .{ .lb_addr = 0x40300000000, .up_addr = 0x40400000000, .options = .{ .thread_safe = true } },
+});
+const ThreadSpace = AddressSpace.SubSpace(0);
 
 const PrintArray = mem.StaticString(4096);
-
-fn renderExactList(
-    comptime SuperAddressSpace: type,
-    comptime arena_index: SuperAddressSpace.Index,
-    comptime thread_count: u8,
-    comptime stack_size: u64,
-) void {
-    const render_spec: fmt.RenderSpec = .{
-        .radix = 16,
-        .omit_trailing_comma = true,
-        .infer_type_names = true,
-        .infer_type_names_recursively = true,
-    };
-    const lb_addr: u64 = SuperAddressSpace.low(arena_index);
-    var list: [16]mem.Arena = undefined;
-    var thread_index: u8 = 0;
-    var offset: u64 = 0;
-    var array: PrintArray = undefined;
-    array.impl.ub_word = 0;
-    while (thread_index != thread_count) : (thread_index += 1) {
-        list[thread_index] = .{
-            .lb_addr = lb_addr + offset,
-            .up_addr = lb_addr + offset + stack_size,
-            .options = .{ .thread_safe = true },
-        };
-        offset += list[thread_index].capacity();
-    }
-    array.writeAny(preset.reinterpret.fmt, meta.tuple(.{fmt.render(render_spec, list)}));
-    file.noexcept.write(2, array.readAll());
-}
-
 const ThreadAllocatorSpec = struct {
     AddressSpace: type,
 };
@@ -79,15 +69,21 @@ fn ThreadAllocator(comptime spec: ThreadAllocatorSpec) type {
 }
 
 pub fn main() !void {
-    comptime var address_space: AddressSpace = .{};
-    var thread_space: ThreadSpace = address_space.reserve(ThreadSpace);
+    var address_space: AddressSpace = .{};
+    var thread_space: ThreadSpace = .{};
 
     comptime var thread_index: u8 = 0;
-    inline while (thread_index != ThreadSpace.addr_spec.list.len) : (thread_index += 1) {
+    inline while (thread_index != comptime ThreadSpace.addr_spec.count()) : (thread_index += 1) {
         try mem.static.acquire(.{}, ThreadSpace, &thread_space, thread_index);
     }
     thread_index = 0;
-    inline while (thread_index != ThreadSpace.addr_spec.list.len) : (thread_index += 1) {
+    inline while (thread_index != comptime ThreadSpace.addr_spec.count()) : (thread_index += 1) {
         try mem.static.release(.{}, ThreadSpace, &thread_space, thread_index);
     }
+
+    var array: PrintArray = .{};
+
+    array.writeAny(preset.reinterpret.fmt, .{ fmt.render(.{ .radix = 2, .omit_default_fields = false }, address_space), '\n' });
+    array.writeAny(preset.reinterpret.fmt, .{ fmt.render(.{ .radix = 2, .omit_default_fields = false }, thread_space), '\n' });
+    builtin.debug.write(array.readAll());
 }
