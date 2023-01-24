f3db11608d04c9c22cdbb6c0ff2c3ea61484d23d
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-24 02:36:24 +0000

    Removed consolidated files.

--- test/buildgen/imports-template.zig
+++
@@ -1,7 +0,0 @@
-const sys = @import("./sys.zig");
-const mem = @import("./mem.zig");
-const file = @import("./file.zig");
-const meta = @import("./meta.zig");
-const proc = @import("./proc.zig");
-const preset = @import("./preset.zig");
-const builtin = @import("./builtin.zig");
