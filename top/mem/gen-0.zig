f1e23bc8d7156294a0f49d051279e3a189537a1e
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-27 16:36:51 +0000

    Updated imports.

--- top/mem/gen-0.zig
+++ top/mem/gen-0.zig
@@ -1,19 +1,28 @@
-const mem = @import("./mem.zig");
-const fmt = @import("./fmt.zig");
-const meta = @import("./meta.zig");
-const file = @import("./file.zig");
-const proc = @import("./proc.zig");
-const preset = @import("./preset.zig");
-const builtin = @import("./builtin.zig");
+const mem = @import("./../mem.zig");
+const fmt = @import("./../fmt.zig");
+const meta = @import("./../meta.zig");
+const file = @import("./../file.zig");
+const proc = @import("./../proc.zig");
+const preset = @import("./../preset.zig");
+const builtin = @import("./../builtin.zig");
+
+pub const is_verbose: bool = false;
+pub const is_silent: bool = true;
 
 pub usingnamespace proc.start;
+
+pub const AddressSpace = preset.address_space.formulaic_128;
+pub const Allocator = mem.GenericArenaAllocator(.{
+    .arena_index = 0,
+    .errors = preset.allocator.errors.noexcept,
+});
+pub const Array = Allocator.StructuredHolder(u8);
+// pub const Array = mem.StaticString(65536);
 
 fn slices(comptime T: type) *[]const T {
     var ptrs: []const T = meta.empty;
     return &ptrs;
 }
-const all_types = slices(type);
-
 const create_spec: file.CreateSpec = .{ .options = .{ .exclusive = false } };
 const close_spec: file.CloseSpec = .{ .errors = null };
 const fmt_spec = .{
@@ -22,11 +31,62 @@ const fmt_spec = .{
     .omit_trailing_comma = true,
 };
 
-const Context = struct {
-    modes: AbstractSpec.Modes = .{},
-    fields: AbstractSpec.Fields = .{},
-    techniques: AbstractSpec.Techniques = .{},
+pub const Context = struct {
+    modes: Modes = .{},
+    fields: Fields = .{},
+    techs: Techniques = .{},
 };
+pub const Modes = struct {
+    read_write: bool = false,
+    resize: bool = false,
+    stream: bool = false,
+};
+pub const Fields = struct {
+    automatic_storage: bool = false,
+    allocated_byte_address: bool = false,
+    undefined_byte_address: bool = false,
+    unallocated_byte_address: bool = false,
+    unstreamed_byte_address: bool = false,
+};
+pub const Techniques = struct {
+    automatic: bool = false,
+    dynamic: bool = false,
+    static: bool = false,
+    parametric: bool = false,
+
+    structured_layout: bool = false,
+    unstructured_layout: bool = false,
+
+    lazy_alignment: bool = false,
+    unit_alignment: bool = false,
+    disjunct_alignment: bool = false,
+
+    single_packed_approximate_capacity: bool = false,
+    double_packed_approximate_capacity: bool = false,
+
+    pub const mutex = .{
+        .kind = .{
+            .automatic,
+            .dynamic,
+            .static,
+            .parametric,
+        },
+        .capacity = .{
+            .single_packed_approximate,
+            .double_packed_approximate,
+        },
+        .alignment = .{
+            .unit,
+            .lazy,
+            .disjunct,
+        },
+        .layout = .{
+            .structured,
+            .unstructured,
+        },
+    };
+};
+
 pub const AbstractSpec = union(enum) {
     automatic_storage: ReadWrite(union {
         _: Automatic,
@@ -61,67 +121,35 @@ pub const AbstractSpec = union(enum) {
         unstreamed_byte_address: Stream(Parametric),
     })),
 
-    pub const Modes = struct {
-        read_write: bool = false,
-        resize: bool = false,
-        stream: bool = false,
-    };
-    pub const Fields = struct {
-        automatic_storage: bool = false,
-        allocated_byte_address: bool = false,
-        undefined_byte_address: bool = false,
-        unallocated_byte_address: bool = false,
-        unstreamed_byte_address: bool = false,
-    };
-    pub const Techniques = struct {
-        automatic: bool = false,
-        dynamic: bool = false,
-        static: bool = false,
-        parametric: bool = false,
-
-        structured_layout: bool = false,
-        unstructured_layout: bool = false,
-
-        lazy_alignment: bool = false,
-        unit_alignment: bool = false,
-        disjunct_alignment: bool = false,
-
-        single_packed_approximate_capacity: bool = false,
-        double_packed_approximate_capacity: bool = false,
-
-        pub const mutex = .{
-            .kind = .{
-                .automatic,
-                .dynamic,
-                .static,
-                .parametric,
-            },
-            .capacity = .{
-                .single_packed_approximate,
-                .double_packed_approximate,
-            },
-            .alignment = .{
-                .unit,
-                .lazy,
-                .disjunct,
-            },
-            .layout = .{
-                .structured,
-                .unstructured,
-            },
-        };
-    };
-
+    fn ReadWrite(comptime T: type) type {
+        return (union(enum) { read_write: T });
+    }
+    fn Stream(comptime T: type) type {
+        return (union(enum) { stream: T });
+    }
+    fn Resize(comptime T: type) type {
+        return (union(enum) { resize: T });
+    }
+    const Automatic = union { automatic: union {
+        structured_layout: AutomaticStuctured,
+    } };
+    const Static = union { static: union {
+        structured_layout: NoSuperAlignment(StructuredStatic),
+        unstructured_layout: NoSuperAlignment(UnstructuredStatic),
+    } };
+    const Dynamic = union { dynamic: union {
+        structured_layout: NoSuperAlignment(Structured),
+        unstructured_layout: NoSuperAlignment(Unstructured),
+    } };
+    const Parametric = union { parametric: union {
+        structured_layout: NoPackedAlignment(StructuredParametric),
+        unstructured_layout: NoPackedAlignment(UnstructuredParametric),
+    } };
     fn NoSuperAlignment(comptime S: type) type {
         return (union(enum) {
             unit_alignment: S,
             lazy_alignment: S,
             disjunct_alignment: S,
-        });
-    }
-    fn AutoAlignment(comptime S: type) type {
-        return (union(enum) {
-            auto_alignment: S,
         });
     }
     fn NoPackedAlignment(comptime S: type) type {
@@ -144,32 +172,6 @@ pub const AbstractSpec = union(enum) {
             disjunct_alignment: S,
         });
     }
-    fn ReadWrite(comptime T: type) type {
-        return (union(enum) { read_write: T });
-    }
-    fn Stream(comptime T: type) type {
-        return (union(enum) { stream: T });
-    }
-    fn Resize(comptime T: type) type {
-        return (union(enum) { resize: T });
-    }
-
-    const Automatic = union { automatic: union {
-        structured_layout: AutomaticStuctured,
-    } };
-    const Static = union { static: union {
-        structured_layout: NoSuperAlignment(StructuredStatic),
-        unstructured_layout: NoSuperAlignment(UnstructuredStatic),
-    } };
-    const Dynamic = union { dynamic: union {
-        structured_layout: NoSuperAlignment(Structured),
-        unstructured_layout: NoSuperAlignment(Unstructured),
-    } };
-    const Parametric = union { parametric: union {
-        structured_layout: NoPackedAlignment(StructuredParametric),
-        unstructured_layout: NoPackedAlignment(UnstructuredParametric),
-    } };
-
     const AutomaticStuctured = struct {
         child: type,
         sentinel: in_out(*const anyopaque) = null,
@@ -239,244 +241,81 @@ pub const AbstractSpec = union(enum) {
     const _Allocator = AllocatorWithArenaIndex;
 };
 
-pub const AddressSpace = preset.address_space.exact_8;
-pub const Allocator = mem.GenericArenaAllocator(.{
-    .arena_index = 0,
-    .errors = preset.allocator.errors.noexcept,
-});
-pub const Array = Allocator.StructuredHolder(u8);
-fn BinaryFilter(comptime T: type) type {
+pub fn BinaryFilter(comptime T: type) type {
     return (struct { []const T, []const T });
 }
-
-fn typeIndex(comptime types: []const type, comptime T: type) ?comptime_int {
+pub fn typeIndex(comptime types: []const type, comptime T: type) ?comptime_int {
     for (types) |U, index| {
         if (U == T) return index;
     }
     return null;
 }
-
 noinline fn summariseAbstractSpec(array: *Array, comptime T: type, ctx: Context) void {
+    const types: *[]const type = comptime slices(type);
+    array.writeMany("const gen = @import(\"./gen-0.zig\");\n");
+    array.writeMany("pub const summary = [_]struct { gen.Context, type }{\n");
+    summariseAbstractSpecInternal(array, types, T, ctx);
+    array.writeMany("};\n");
+    array.writeMany("pub const AbstractParams: [" ++ comptime builtin.fmt.ci(types.len) ++ "]type = .{\n");
+    inline for (types.*) |U| {
+        array.writeFormat(comptime fmt.any(U));
+        array.writeMany(",\n");
+    }
+    array.writeMany("};\n");
+}
+fn summaryItem(array: *Array, ctx: Context, type_index: u64) void {
+    array.writeMany(".{ .{ .modes = ");
+    array.writeFormat(fmt.render(fmt_spec, ctx.modes));
+    array.writeMany(", .fields = ");
+    array.writeFormat(fmt.render(fmt_spec, ctx.fields));
+    array.writeMany(", .techs = ");
+    array.writeFormat(fmt.render(fmt_spec, ctx.techs));
+    array.writeMany(" }, AbstractParams[");
+    array.writeFormat(fmt.ud64(type_index));
+    array.writeMany("], },\n");
+}
+inline fn summariseAbstractSpecInternal(array: *Array, comptime types: *[]const type, comptime T: type, ctx: Context) void {
     const type_info: builtin.Type = @typeInfo(T);
     if (type_info == .Union) {
         inline for (type_info.Union.fields) |field| {
             var next: Context = ctx;
-            if (@hasField(AbstractSpec.Modes, field.name)) {
+            if (@hasField(Modes, field.name)) {
                 @field(next.modes, field.name) = true;
             }
-            if (@hasField(AbstractSpec.Fields, field.name)) {
+            if (@hasField(Fields, field.name)) {
                 @field(next.fields, field.name) = true;
             }
-            if (@hasField(AbstractSpec.Techniques, field.name)) {
-                @field(next.techniques, field.name) = true;
+            if (@hasField(Techniques, field.name)) {
+                @field(next.techs, field.name) = true;
             }
-            if (builtin.testEqual(Context, next, ctx)) {
+            if (builtin.testEqual(Context, next, ctx) and
+                !(field.name.len == 1 and field.name[0] == '_'))
+            {
                 unreachable;
             }
-            summariseAbstractSpec(array, field.type, next);
+            summariseAbstractSpecInternal(array, types, field.type, next);
         }
     } else if (type_info == .Struct) {
-        array.writeMany(".{ .mode = ");
-        array.writeFormat(fmt.render(fmt_spec, ctx.modes));
-        array.writeMany(", .fields = ");
-        array.writeFormat(fmt.render(fmt_spec, ctx.fields));
-        array.writeMany(", .techniques = ");
-        array.writeFormat(fmt.render(fmt_spec, ctx.techniques));
-        array.writeMany(", .params = ");
-        array.writeFormat(comptime fmt.any(T));
-        array.writeMany(" },\n");
-    }
-}
-
-fn gatherContainerTypesInternal(comptime types: []const type, comptime T: type) []const type {
-    var ret: []const type = types;
-    if (!comptime meta.isContainer(T)) {
-        return ret;
-    }
-    if (typeIndex(ret, T) == null and @typeInfo(T) == .Struct) {
-        ret = meta.concat(type, ret, T);
-    }
-    const type_info = meta.resolve(@typeInfo(T));
-    inline for (type_info.fields) |field| {
-        const Field = @TypeOf(field);
-        if (@hasField(Field, "type")) {
-            if (typeIndex(types, field.type) == null) {
-                ret = gatherContainerTypesInternal(ret, field.type);
+        const type_index: u64 = comptime blk: {
+            if (typeIndex(types.*, T)) |index| {
+                break :blk index;
+            } else {
+                meta.concatEqu(type, types, T);
+                break :blk types.len - 1;
             }
-        }
+        };
+        summaryItem(array, ctx, type_index);
     }
-    return ret;
-}
-fn gatherContainerTypes(comptime T: type) []const type {
-    return gatherContainerTypesInternal(meta.empty, T);
-}
-
-fn getMutuallyExclusiveOptions(comptime any: anytype) []const []const u8 {
-    switch (@typeInfo(@TypeOf(any))) {
-        .Struct => |struct_info| {
-            var names: []const []const u8 = meta.empty;
-            inline for (struct_info.fields) |field| {
-                for (getMutuallyExclusiveOptions(@field(any, field.name))) |name| {
-                    if (struct_info.is_tuple) {
-                        names = meta.concat([]const u8, names, name);
-                    } else {
-                        names = meta.concat([]const u8, names, name ++ "_" ++ field.name);
-                    }
-                }
-            }
-            return names;
-        },
-        .EnumLiteral => {
-            return meta.parcel([]const u8, @tagName(any));
-        },
-        else => @compileError(@typeName(@TypeOf(any))),
-    }
-}
-fn typeNames(comptime types: []const type) []const u8 {
-    var ret: []const u8 = ".{ ";
-    for (types) |T| {
-        ret = ret ++ @typeName(T) ++ ", ";
-    }
-    return ret ++ "}";
-}
-fn fieldNamesSuperSet(comptime types: []const type) []const []const u8 {
-    var all_field_names: []const []const u8 = meta.empty;
-    var all_field_types: []const type = meta.empty;
-    for (types) |T| {
-        lo: for (meta.resolve(@typeInfo(T)).fields) |field| {
-            for (all_field_names) |field_name, type_index| {
-                if (mem.testEqualMany(u8, field.name, field_name)) {
-                    builtin.static.assertEqual(type, all_field_types[type_index], field.type);
-                    continue :lo;
-                }
-            }
-            all_field_names = meta.concat([]const u8, all_field_names, field.name);
-            all_field_types = meta.concat(type, all_field_types, field.type);
-        }
-    }
-    return all_field_names;
-}
-fn declNamesSuperSet(comptime types: []const type) []const []const u8 {
-    var all_decl_names: []const []const u8 = meta.empty;
-    var all_decl_types: []const type = meta.empty;
-    for (types) |T| {
-        lo: for (meta.resolve(@typeInfo(T)).decls) |decl| {
-            for (all_decl_names) |decl_name, type_index| {
-                if (mem.testEqualMany(u8, decl.name, decl_name)) {
-                    builtin.static.assertEqual(type, all_decl_types[type_index], decl.type);
-                    continue :lo;
-                }
-            }
-            all_decl_names = meta.concat([]const u8, all_decl_names, decl.name);
-            all_decl_types = meta.concat(type, all_decl_types, decl.type);
-        }
-    }
-    return all_decl_names;
-}
-
-fn haveField(comptime types: []const type, comptime field_name: []const u8) BinaryFilter(type) {
-    comptime var t: []const type = meta.empty;
-    comptime var f: []const type = meta.empty;
-    inline for (types) |T| {
-        if (@hasField(T, field_name)) {
-            t = meta.concat(type, t, T);
-        } else {
-            f = meta.concat(type, f, T);
-        }
-    }
-    return .{ f, t };
-}
-fn haveDecl(comptime types: []const type, comptime field_name: []const u8) BinaryFilter(type) {
-    comptime var t: []const type = meta.empty;
-    comptime var f: []const type = meta.empty;
-    inline for (types) |T| {
-        if (@hasDecl(T, field_name)) {
-            t = meta.concat(type, t, T);
-        } else {
-            f = meta.concat(type, f, T);
-        }
-    }
-    return .{ f, t };
-}
-fn writeHasFieldDeductionInternal(array: *Array, comptime types: []const type, comptime field_names: []const []const u8) void {
-    if (field_names.len == 0) {
-        return array.writeMany("return " ++ comptime typeNames(types) ++ ";\n");
-    }
-    const filtered: BinaryFilter(type) = comptime haveField(types, field_names[0]);
-    if (filtered[1].len != 0) {
-        array.writeMany("if (@hasField(T, \"" ++ field_names[0] ++ "\")) {\n");
-        writeDeclaration(array, field_names[0], meta.Field(filtered[1][0], field_names[0]));
-        if (filtered[1].len == 1) {
-            array.writeMany("return " ++ @typeName(filtered[1][0]) ++ ";\n");
-        } else {
-            writeHasFieldDeductionInternal(array, filtered[1], field_names[1..]);
-        }
-    }
-    if (filtered[0].len != 0) {
-        if (filtered[1].len != 0) {
-            array.writeMany("} else {\n");
-        }
-        if (filtered[0].len == 1) {
-            array.writeMany("return " ++ @typeName(filtered[0][0]) ++ ";\n");
-        } else {
-            writeHasFieldDeductionInternal(array, filtered[0], field_names[1..]);
-        }
-    }
-    if (filtered[1].len != 0) {
-        array.writeMany("}\n");
-    }
-}
-fn writeHasDeclDeductionInternal(array: *Array, comptime types: []const type, comptime decl_names: []const []const u8) void {
-    if (decl_names.len == 0) {
-        return array.writeMany("return " ++ comptime typeNames(types) ++ ";\n");
-    }
-    const filtered: BinaryFilter(type) = comptime haveDecl(types, decl_names[0]);
-    if (filtered[1].len != 0) {
-        array.writeMany("if (@hasDecl(T, \"" ++ decl_names[0] ++ "\")) {\n");
-        if (filtered[1].len == 1) {
-            array.writeMany("return " ++ @typeName(filtered[1][0]) ++ ";\n");
-        } else {
-            writeHasDeclDeductionInternal(filtered[1], decl_names[1..]);
-        }
-    }
-    if (filtered[0].len != 0) {
-        if (filtered[1].len != 0) {
-            array.writeMany("} else {\n");
-        }
-        if (filtered[0].len == 1) {
-            array.writeMany("return " ++ @typeName(filtered[0][0]) ++ ";\n");
-        } else {
-            writeHasDeclDeductionInternal(filtered[0], decl_names[1..]);
-        }
-    }
-    if (filtered[1].len != 0) {
-        array.writeMany("}\n");
-    }
-}
-fn writeHasFieldDeduction(array: *Array, comptime types: []const type) void {
-    array.writeMany("comptime {");
-    @setEvalBranchQuota(~@as(u32, 0));
-    writeHasFieldDeductionInternal(array, types, comptime fieldNamesSuperSet(types));
-    array.writeMany("}");
-}
-fn writeHasDeclDeduction(array: *Array, comptime types: []const type) void {
-    array.writeMany("comptime {");
-    @setEvalBranchQuota(~@as(u32, 0));
-    writeHasDeclDeductionInternal(array, types, comptime declNamesSuperSet(types));
-    array.writeMany("}");
-}
-fn writeDeclaration(array: *Array, comptime decl_name: []const u8, comptime decl_type: type) void {
-    array.writeMany("const " ++ decl_name ++ ": " ++ @typeName(decl_type) ++ " = undefined;\n");
 }
 pub fn main() !void {
+    @setEvalBranchQuota(~@as(u32, 0));
     var address_space: AddressSpace = .{};
     var allocator: Allocator = try Allocator.init(&address_space);
     var array: Array = Array.init(&allocator);
     array.increment(&allocator, 1024 * 1024 * 4);
-    array.writeMany("const summary = [_]Context{");
+    // var array: Array = undefined;
+    // array.undefineAll();
     summariseAbstractSpec(&array, AbstractSpec, .{});
-    array.writeMany("};");
-
     const fd: u64 = try file.create(create_spec, builtin.build_root.? ++ "/top/mem/mem-template.zig");
     defer file.close(close_spec, fd);
     try file.write(fd, array.readAll(allocator));
