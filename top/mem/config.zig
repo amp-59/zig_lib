3493bd1e0a0662debc7a8a27ac0b632ef8e957b9
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-30 19:52:44 +0000

    Added more configuration constants.

--- top/mem/config.zig
+++ top/mem/config.zig
@@ -1,4 +1,6 @@
 //! Control various aspects of `memgen`
 
 pub const prefer_operator_wrapper: bool = true;
-pub const packed_capacity_low: bool = true;
+pub const packed_capacity_low: bool = false;
+pub const minimise_indirection: bool = true;
+pub const minimise_declaration: bool = true;
