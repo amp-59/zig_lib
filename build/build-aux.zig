84f8cc5674ee594c6f0f8599686f89505cdc78cf
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-30 00:51:47 +0000

    Updated build program.

--- build/build-aux.zig
+++ build/build-aux.zig
@@ -6,20 +6,33 @@ const util = @import("./util.zig");
 // PROGRAM FILES ///////////////////////////////////////////////////////////////
 
 fn memoryImplementation(builder: *build.Builder) void {
+    const stage_0_products: [2][]const u8 = .{
+        "top/mem/abstract_params.zig",
+        "top/mem/impl_details.zig",
+    };
+    const stage_1_products: [1][]const u8 = .{
+        "top/mem/type_specs.zig",
+    };
+    const stage_3_products: [1][]const u8 = .{
+        "top/mem/reference.zig",
+    };
+
     const mem_gen = builder.step("mem_gen", "generate containers according to specification");
     const mem_gen_0 = util.addProjectExecutable(builder, "mem_gen_0", "top/mem/gen-0-aux.zig", .{ .build_mode = .ReleaseSmall });
     const mem_gen_1 = util.addProjectExecutable(builder, "mem_gen_1", "top/mem/gen-1-aux.zig", .{ .build_mode = .ReleaseSmall });
     const mem_gen_2 = util.addProjectExecutable(builder, "mem_gen_2", "top/mem/gen-2-aux.zig", .{ .build_mode = .ReleaseSmall });
-    mem_gen.dependOn(&mem_gen_0.run().step);
-    mem_gen_1.step.dependOn(&builder.addFmt(&[2][]const u8{
-        "top/mem/abstract_params.zig",
-        "top/mem/impl_details.zig",
-    }).step);
-    mem_gen.dependOn(&mem_gen_1.run().step);
-    mem_gen.dependOn(&builder.addFmt(&[1][]const u8{
-        "top/mem/type_spec.zig",
-    }).step);
-    mem_gen.dependOn(&mem_gen_2.run().step);
+    const mem_gen_3 = util.addProjectExecutable(builder, "mem_gen_3", "top/mem/gen-3-aux.zig", .{ .build_mode = .ReleaseSmall });
+    const mem_gen_4 = util.addProjectExecutable(builder, "mem_gen_4", "top/mem/gen-4-aux.zig", .{ .build_mode = .ReleaseSmall });
+
+    mem_gen_1.step.dependOn(&mem_gen_0.run().step);
+    mem_gen_1.step.dependOn(&builder.addFmt(&stage_0_products).step);
+    mem_gen_2.step.dependOn(&mem_gen_1.run().step);
+    mem_gen_2.step.dependOn(&builder.addFmt(&stage_1_products).step);
+    mem_gen_3.step.dependOn(&mem_gen_2.run().step);
+    mem_gen_4.step.dependOn(&mem_gen_3.run().step);
+    mem_gen_4.step.dependOn(&builder.addFmt(&stage_3_products).step);
+
+    mem_gen.dependOn(&mem_gen_4.run().step);
 }
 
 pub fn main(builder: *build.Builder) !void {
