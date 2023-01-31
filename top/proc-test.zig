0eb0cf333cd789124754109bb452b4aa56b3d43f
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-01-31 11:00:14 +0000

    Added test program for process related functions.

---
+++ top/proc-test.zig
@@ -0,0 +1,21 @@
+const fmt = @import("./fmt.zig");
+const mem = @import("./mem.zig");
+const proc = @import("./proc.zig");
+const file = @import("./file.zig");
+const preset = @import("./preset.zig");
+const testing = @import("./testing.zig");
+
+pub usingnamespace proc.start;
+pub const is_silent: bool = true;
+pub const is_verbose: bool = false;
+
+pub const AddressSpace = preset.address_space.exact_8;
+
+pub fn main(_: anytype, _: anytype, aux: *const anyopaque) !void {
+    file.unlink(.{}, "./dump") catch {};
+    const fd: u64 = try file.create(.{}, "./dump");
+    defer file.close(.{ .errors = null }, fd);
+    var array: mem.StaticString(8192) = .{};
+    array.writeMany(@intToPtr(*const [8192]u8, proc.auxiliaryValue(aux, .vdso_addr).?));
+    try file.write(fd, array.readAll());
+}
