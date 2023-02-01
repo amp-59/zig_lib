8696841f3cf4975476dd53ca4dfa64e401bf7275
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-01 04:17:21 +0000

    Updated library build program to include `memgen`.

--- build.zig
+++ build.zig
@@ -1,4 +1,4 @@
-pub const build = if (true) @import("build/build-aux.zig").main else main;
+pub const build = if (false) @import("build/build-aux.zig").main else main;
 pub const srg = @import("./zig_lib.zig");
 const mem = srg.mem;
 const meta = srg.meta;
@@ -55,41 +55,47 @@ const std_parser_args = .{
 const parsedir_std_macros: builder.Macros = meta.slice(builder.Macro, .{.{ .name = "test_subject", .value = "\"std\"" }});
 const parsedir_lib_macros: builder.Macros = meta.slice(builder.Macro, .{.{ .name = "test_subject", .value = "\"lib\"" }});
 
+// zig fmt: off
+
+fn memgen(ctx: *builder.Context) !void {
+    _ = ctx.addExecutable("mem_gen_0",      "top/mem/gen-0-aux.zig", .{ .build_mode = .ReleaseSmall });
+    _ = ctx.addExecutable("mem_gen_1",      "top/mem/gen-1-aux.zig", .{ .build_mode = .ReleaseSmall });
+    _ = ctx.addExecutable("mem_gen_2",      "top/mem/gen-2-aux.zig", .{ .build_mode = .ReleaseSmall });
+    _ = ctx.addExecutable("mem_gen_3",      "top/mem/gen-3-aux.zig", .{ .build_mode = .ReleaseSmall });
+    _ = ctx.addExecutable("mem_gen_4",      "top/mem/gen-4-aux.zig", .{ .build_mode = .ReleaseSmall });
+    _ = ctx.addExecutable("mem_gen_5",      "top/mem/gen-5-aux.zig", .{ .build_mode = .ReleaseSmall });
+}
 pub fn main(ctx: *builder.Context) !void {
-    // Top test programs:
-    _ = ctx.addExecutable("builtin_test", "top/builtin-test.zig", minor_test_args);
-    _ = ctx.addExecutable("meta_test", "top/meta-test.zig", minor_test_args);
-    _ = ctx.addExecutable("mem_test", "top/mem-test.zig", minor_test_args);
-    _ = ctx.addExecutable("algo_test", "top/algo-test.zig", algo_test_args);
-    _ = ctx.addExecutable("file_test", "top/file-test.zig", minor_test_args);
-    _ = ctx.addExecutable("list_test", "top/list-test.zig", minor_test_args);
-    _ = ctx.addExecutable("fmt_test", "top/fmt-test.zig", fmt_test_args);
-    _ = ctx.addExecutable("render_test", "top/render-test.zig", minor_test_args);
-    _ = ctx.addExecutable("thread_test", "top/thread-test.zig", minor_test_args);
-    _ = ctx.addExecutable("virtual_test", "top/virtual-test.zig", minor_test_args);
-
+    // Top test programs
+    _ = ctx.addExecutable("builtin_test",   "top/builtin-test.zig", minor_test_args);
+    _ = ctx.addExecutable("meta_test",      "top/meta-test.zig",    minor_test_args);
+    _ = ctx.addExecutable("mem_test",       "top/mem-test.zig",     minor_test_args);
+    _ = ctx.addExecutable("algo_test",      "top/algo-test.zig",    algo_test_args);
+    _ = ctx.addExecutable("file_test",      "top/file-test.zig",    minor_test_args);
+    _ = ctx.addExecutable("list_test",      "top/list-test.zig",    minor_test_args);
+    _ = ctx.addExecutable("fmt_test",       "top/fmt-test.zig",     fmt_test_args);
+    _ = ctx.addExecutable("render_test",    "top/render-test.zig",  minor_test_args);
+    _ = ctx.addExecutable("thread_test",    "top/thread-test.zig",  minor_test_args);
+    _ = ctx.addExecutable("virtual_test",   "top/virtual-test.zig", minor_test_args);
     // More complete test programs:
-    _ = ctx.addExecutable("mca", "test/mca.zig", fast_test_args);
-    _ = ctx.addExecutable("treez", "test/treez.zig", small_test_args);
-    _ = ctx.addExecutable("itos", "test/itos.zig", small_test_args);
-    _ = ctx.addExecutable("cat", "test/cat.zig", fast_test_args);
-    _ = ctx.addExecutable("hello", "test/hello.zig", small_test_args);
-    _ = ctx.addExecutable("readelf", "test/readelf.zig", minor_test_args);
-    _ = ctx.addExecutable("parsedir", "test/parsedir.zig", fast_test_args);
-
+    _ = ctx.addExecutable("mca",            "test/mca.zig",         fast_test_args);
+    _ = ctx.addExecutable("treez",          "test/treez.zig",       small_test_args);
+    _ = ctx.addExecutable("itos",           "test/itos.zig",        small_test_args);
+    _ = ctx.addExecutable("cat",            "test/cat.zig",         fast_test_args);
+    _ = ctx.addExecutable("hello",          "test/hello.zig",       small_test_args);
+    _ = ctx.addExecutable("readelf",        "test/readelf.zig",     minor_test_args);
+    _ = ctx.addExecutable("parsedir",       "test/parsedir.zig",    fast_test_args);
     // Other test programs:
-    _ = ctx.addExecutable("impl_test", "top/impl-test.zig", .{});
-    _ = ctx.addExecutable("container_test", "top/container-test.zig", .{});
-    _ = ctx.addExecutable("parse_test", "top/parse-test.zig", .{});
-
-    _ = ctx.addExecutable("lib_parser", "./test/parsedir.zig", lib_parser_args);
-    _ = ctx.addExecutable("std_parser", "./test/parsedir.zig", std_parser_args);
-
+    _ = ctx.addExecutable("impl_test",      "top/impl-test.zig",        .{});
+    _ = ctx.addExecutable("container_test", "top/container-test.zig",   .{});
+    _ = ctx.addExecutable("parse_test",     "top/parse-test.zig",       .{});
+    _ = ctx.addExecutable("lib_parser",     "test/parsedir.zig",        lib_parser_args);
+    _ = ctx.addExecutable("std_parser",     "test/parsedir.zig",        std_parser_args);
     // Examples
-    _ = ctx.addExecutable("readdir", "examples/iterate_dir_entries.zig", small_test_args);
-    _ = ctx.addExecutable("dynamic", "./examples/dynamic_alloc.zig", small_test_args);
-    _ = ctx.addExecutable("address_space", "./examples/custom_address_space.zig", small_test_args);
+    _ = ctx.addExecutable("readdir",        "examples/iterate_dir_entries.zig",     small_test_args);
+    _ = ctx.addExecutable("dynamic",        "examples/dynamic_alloc.zig",           small_test_args);
+    _ = ctx.addExecutable("address_space",  "examples/custom_address_space.zig",    small_test_args);
 
     // Generators:
-    // _ = ctx.addExecutable("builder_gen", "top/builder-gen.zig", small_test_args);
+    try memgen(ctx);
 }
