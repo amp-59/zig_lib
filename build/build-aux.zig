f43077069dacd82276e6a9e5bc38f3a39817adf6
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-01 04:30:47 +0000

    Added a new stage to `memgen`.

--- build/build-aux.zig
+++ build/build-aux.zig
@@ -16,19 +16,19 @@ fn memoryImplementation(builder: *build.Builder) void {
     const mem_gen_2 = util.addProjectExecutable(builder, "mem_gen_2", "top/mem/gen-2-aux.zig", .{ .build_mode = .ReleaseSmall });
     const mem_gen_3 = util.addProjectExecutable(builder, "mem_gen_3", "top/mem/gen-3-aux.zig", .{ .build_mode = .ReleaseSmall });
     const mem_gen_4 = util.addProjectExecutable(builder, "mem_gen_4", "top/mem/gen-4-aux.zig", .{ .build_mode = .ReleaseSmall });
+    const mem_gen_5 = util.addProjectExecutable(builder, "mem_gen_5", "top/mem/gen-5-aux.zig", .{ .build_mode = .ReleaseSmall });
 
     mem_gen_1.step.dependOn(&mem_gen_0.run().step);
     mem_gen_2.step.dependOn(&mem_gen_1.run().step);
     mem_gen_3.step.dependOn(&mem_gen_2.run().step);
     mem_gen_4.step.dependOn(&mem_gen_3.run().step);
     mem_gen_4.step.dependOn(&builder.addFmt(&stage_3_products).step);
-
+    mem_gen_5.step.dependOn(&mem_gen_2.run().step);
     mem_gen.dependOn(&mem_gen_4.run().step);
 }
 
 pub fn main(builder: *build.Builder) !void {
     util.Context.init(builder);
-
     // Minor test programs:
     _ = util.addProjectExecutable(builder, "builtin_test", "top/builtin-test.zig", .{ .build_root = true, .is_correct = true, .is_verbose = true });
     _ = util.addProjectExecutable(builder, "meta_test", "top/meta-test.zig", .{ .is_correct = true, .is_verbose = true });
@@ -42,7 +42,6 @@ pub fn main(builder: *build.Builder) !void {
     _ = util.addProjectExecutable(builder, "proc_test", "top/proc-test.zig", .{ .is_correct = true, .is_verbose = true, .is_test = false });
     _ = util.addProjectExecutable(builder, "thread_test", "top/thread-test.zig", .{ .is_test = true, .build_root = true });
     _ = util.addProjectExecutable(builder, "virtual_test", "top/virtual-test.zig", .{ .is_correct = true, .is_verbose = true });
-
     // More complete test programs:
     _ = util.addProjectExecutable(builder, "mca", "test/mca.zig", .{ .build_mode = .ReleaseFast, .is_correct = false, .is_verbose = false });
     _ = util.addProjectExecutable(builder, "treez", "test/treez.zig", .{ .build_mode = .ReleaseSmall, .is_correct = false, .is_verbose = false });
@@ -52,18 +51,15 @@ pub fn main(builder: *build.Builder) !void {
     _ = util.addProjectExecutable(builder, "readelf", "test/readelf.zig", .{ .build_root = true });
     _ = util.addProjectExecutable(builder, "parsedir", "test/parsedir.zig", .{ .build_mode = .ReleaseFast, .build_root = true });
     _ = util.addProjectExecutable(builder, "pathsplit", "test/pathsplit.zig", .{ .build_root = true });
-
     _ = util.addProjectExecutable(builder, "print_all", "test/print_all_decls.zig", .{ .is_large_test = true, .build_root = true });
     // Other test programs:
     _ = util.addProjectExecutable(builder, "impl_test", "top/impl-test.zig", .{ .is_large_test = true, .build_root = true });
     _ = util.addProjectExecutable(builder, "container_test", "top/container-test.zig", .{ .is_large_test = true, .build_root = true });
     _ = util.addProjectExecutable(builder, "builder_test", "top/builder-test.zig", .{ .is_large_test = true, .build_root = true });
-
     // Examples
     _ = util.addProjectExecutable(builder, "readdir", "examples/iterate_dir_entries.zig", .{ .build_mode = .ReleaseSmall });
     _ = util.addProjectExecutable(builder, "restack", "examples/remap_stack.zig", .{});
     _ = util.addProjectExecutable(builder, "dynamic", "examples/dynamic_alloc.zig", .{});
-
     // Generators:
     _ = util.addProjectExecutable(builder, "builder_gen", "top/builder-gen.zig", .{ .build_mode = .ReleaseSmall });
     memoryImplementation(builder);
