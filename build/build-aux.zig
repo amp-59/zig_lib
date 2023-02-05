cbcc04a74bb9a0efd4741b356390444e7a70a400
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-05 23:56:11 +0000

    * Removed builder test program, which only functions with library builder. * Added new files for renamed and split generator stages.

--- build/build-aux.zig
+++ build/build-aux.zig
@@ -25,6 +25,21 @@ fn memoryImplementation(builder: *build.Builder) void {
     mem_gen_4.step.dependOn(&builder.addFmt(&stage_3_products).step);
     mem_gen_5.step.dependOn(&mem_gen_2.run().step);
     mem_gen.dependOn(&mem_gen_4.run().step);
+
+    const spec_to_abstract = util.addProjectExecutable(builder, "spec_to_abstract", "top/mem/spec_to_abstract.zig", .{ .build_mode = .ReleaseSmall });
+    _ = spec_to_abstract;
+    const spec_to_detail = util.addProjectExecutable(builder, "spec_to_detail", "top/mem/spec_to_detail.zig", .{ .build_mode = .ReleaseSmall });
+    _ = spec_to_detail;
+    const spec_to_options = util.addProjectExecutable(builder, "spec_to_options", "top/mem/spec_to_options.zig", .{ .build_mode = .ReleaseSmall });
+    _ = spec_to_options;
+    const abstract_to_type_spec = util.addProjectExecutable(builder, "abstract_to_type_spec", "top/mem/abstract_to_type_spec.zig", .{ .build_mode = .ReleaseSmall });
+    _ = abstract_to_type_spec;
+    const detail_to_options = util.addProjectExecutable(builder, "detail_to_specifiers", "top/mem/detail_to_specifiers.zig", .{ .build_mode = .ReleaseSmall });
+    _ = detail_to_options;
+    const detail_to_groups = util.addProjectExecutable(builder, "detail_to_groups", "top/mem/detail_to_groups.zig", .{ .build_mode = .ReleaseSmall });
+    _ = detail_to_groups;
+    const detail_to_variants = util.addProjectExecutable(builder, "detail_to_variants", "top/mem/detail_to_variants.zig", .{ .build_mode = .ReleaseSmall });
+    _ = detail_to_variants;
 }
 
 pub fn main(builder: *build.Builder) !void {
@@ -55,7 +70,6 @@ pub fn main(builder: *build.Builder) !void {
     // Other test programs:
     _ = util.addProjectExecutable(builder, "impl_test", "top/impl-test.zig", .{ .is_large_test = true, .build_root = true });
     _ = util.addProjectExecutable(builder, "container_test", "top/container-test.zig", .{ .is_large_test = true, .build_root = true });
-    _ = util.addProjectExecutable(builder, "builder_test", "top/builder-test.zig", .{ .is_large_test = true, .build_root = true });
     // Examples
     _ = util.addProjectExecutable(builder, "readdir", "examples/iterate_dir_entries.zig", .{ .build_mode = .ReleaseSmall });
     _ = util.addProjectExecutable(builder, "restack", "examples/remap_stack.zig", .{});
