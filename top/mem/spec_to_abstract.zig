ca10cd07c608d2653e55e65a814426b54bd0b8a6
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-06 20:00:48 +0000

    * Updated to use common string. * Corrected path to be relative to output directory.

--- top/mem/spec_to_abstract.zig
+++ top/mem/spec_to_abstract.zig
@@ -6,7 +6,6 @@ const builtin = @import("./../builtin.zig");
 
 const gen = @import("./gen.zig");
 
-pub const Array = mem.StaticString(65536);
 pub const is_verbose: bool = false;
 pub const is_silent: bool = true;
 
@@ -18,13 +17,13 @@ pub fn typeIndex(comptime types: []const type, comptime T: type) bool {
     for (types) |U| if (U == T) return false;
     return true;
 }
-fn writeAbstractParametersStruct(array: *Array, comptime T: type) void {
+fn writeAbstractParametersStruct(array: *gen.String, comptime T: type) void {
     array.writeMany("    ");
     array.writeFormat(comptime fmt.TypeFormat(.{ .omit_trailing_comma = true }){ .value = T });
     array.writeMany(",\n");
 }
 inline fn writeAbstractParametersInternal(
-    array: *Array,
+    array: *gen.String,
     comptime types: *[]const type,
     comptime T: type,
 ) void {
@@ -40,20 +39,20 @@ inline fn writeAbstractParametersInternal(
         }
     }
 }
-fn writeAbstractParameters(array: *Array, comptime types: *[]const type) void {
+fn writeAbstractParameters(array: *gen.String, comptime types: *[]const type) void {
     array.writeMany("pub const abstract_params = [_]type{\n");
     writeAbstractParametersInternal(array, types, gen.AbstractSpec);
     array.writeMany("};\n");
 }
-pub fn specToAbstract() void {
+pub fn specToAbstract(array: *gen.String) void {
     const types: *[]const type = comptime slices(type);
-    var array: Array = .{};
-    gen.writeImports(&array, @src(), &.{.{ .name = "gen", .path = "./gen.zig" }});
-    writeAbstractParameters(&array, types);
-    gen.writeFile(&array, "memgen_abstract_0.zig");
+    gen.writeImports(array, @src(), &.{.{ .name = "gen", .path = "./../../gen.zig" }});
+    writeAbstractParameters(array, types);
+    gen.writeFile(array, "memgen_abstract.zig");
 }
 pub export fn _start() noreturn {
     @setAlignStack(16);
-    specToAbstract();
+    var array: gen.String = gen.String.init(builtin.debug.impendingBytes(1024 * 1024));
+    specToAbstract(&array);
     gen.exit(0);
 }
