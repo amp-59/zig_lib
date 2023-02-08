6d857cc3662095e9a8c53af8b84e45c91ac040f2
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-08 13:10:27 +0000

    Tried to make build runner script more portable.

--- support/switch_build_runner.sh
+++ support/switch_build_runner.sh
@@ -1,49 +1,52 @@
 #!/bin/bash
-switch_build_runner () {
-    local -r script_realpath="$(/usr/bin/realpath "$0")";
-    local -r support_dir="$(/usr/bin/dirname "$script_realpath")";
-    local -r zig_exe="$(/usr/bin/which zig)";
-    local -r zig_real_exe="$(/usr/bin/realpath "$zig_exe")";
-    local -r zig_lib_dir="$(/usr/bin/dirname "$zig_real_exe")/lib";
-    local -r zig_build_runner="$zig_lib_dir/build_runner.zig";
-    local -r zl_build_runner="$(/usr/bin/realpath "$support_dir/../build_runner.zig")";
-    local -r zl_zig_build="$(/usr/bin/realpath "$support_dir/../build.zig")";
-    local -r zig_build_runner_bkp="$zig_build_runner.bkp";
-    if /usr/bin/test -L "$zig_build_runner"; then
-        local -r build_runner_link_target="$(/usr/bin/realpath "$zig_build_runner")";
-        if /usr/bin/test "$build_runner_link_target" -ef "$zl_build_runner"; then
-            if /usr/bin/test -f "$zig_build_runner_bkp"; then
-                if ! /usr/bin/rm "$zig_build_runner"; then
-                    return 2;
-                fi;
-                if ! /usr/bin/mv -i "$zig_build_runner_bkp" "$zig_build_runner"; then
-                    return 2;
-                fi;
-                if /usr/bin/test -f "$zl_zig_build"; then
-                    /usr/bin/sed -i 's/pub const build = if (false)/pub const build = if (true)/' "$zl_zig_build";
-                fi;
-                echo "build runner: std"
-            else
-                echo error: "would move back original zig build runner, but original file is missing"
+hash zig;
+readonly error="error:";
+readonly blank="      ";
+readonly script_realpath="$(realpath "$0")";
+readonly support_dir="$(dirname "$script_realpath")";
+readonly zig_exe="$(hash -t zig)";
+readonly zig_real_exe="$(realpath "$zig_exe")";
+readonly zig_lib_dir="$(dirname "$zig_real_exe")/lib";
+readonly zig_build_runner="$zig_lib_dir/build_runner.zig";
+readonly zl_build_runner="$(realpath "$support_dir/../build_runner.zig")";
+readonly zl_zig_build="$(realpath "$support_dir/../build.zig")";
+readonly zig_build_runner_bkp="$zig_build_runner.bkp";
+if test -L "$zig_build_runner"; then
+    readonly build_runner_link_target="$(realpath "$zig_build_runner")";
+    if test "$build_runner_link_target" -ef "$zl_build_runner"; then
+        if test -f "$zig_build_runner_bkp"; then
+            if ! rm "$zig_build_runner"; then
                 return 2;
             fi;
+            if ! mv -i "$zig_build_runner_bkp" "$zig_build_runner"; then
+                return 2;
+            fi;
+            if test -f "$zl_zig_build"; then
+                sed -i 's/pub const build = if (false)/pub const build = if (true)/' "$zl_zig_build";
+            fi;
+            echo "build runner: std"
         else
-            echo error: "expected link to zig_lib build runner, but found other file: '$zig_build_runner' -> '$build_runner_link_target' != '$zl_build_runner'";
+            echo $error "would move back original zig build runner, but original file is missing"
             return 2;
         fi;
-    elif /usr/bin/test -f "$zig_build_runner"; then
-        if ! /usr/bin/mv -i "$zig_build_runner" "$zig_build_runner_bkp"; then
-            return 2;
-        fi;
-        if ! /usr/bin/ln -s "$zl_build_runner" "$zig_build_runner"; then
-            return 2;
-        fi;
-        if /usr/bin/test -f "$zl_zig_build"; then
-            /usr/bin/sed -i 's/pub const build = if (true)/pub const build = if (false)/' "$zl_zig_build";
-        fi;
-        echo "build runner: zl"
     else
-        echo error: "'$zig_build_runner': no such file or directory; did nothing"
+        echo $error "expected link to zig_lib build runner, but found other file:";
+        echo $blank "'$zig_build_runner' -> ";
+        echo $blank "'$build_runner_link_target' != ";
+        echo $blank "'$zl_build_runner'";
+        return 2;
     fi;
-}
-switch_build_runner;
+elif /usr/bin/test -f "$zig_build_runner"; then
+    if ! /usr/bin/mv -i "$zig_build_runner" "$zig_build_runner_bkp"; then
+        return 2;
+    fi;
+    if ! /usr/bin/ln -s "$zl_build_runner" "$zig_build_runner"; then
+        return 2;
+    fi;
+    if /usr/bin/test -f "$zl_zig_build"; then
+        /usr/bin/sed -i 's/pub const build = if (true)/pub const build = if (false)/' "$zl_zig_build";
+    fi;
+    echo "build runner: zl"
+else
+    echo $error "'$zig_build_runner': no such file or directory; did nothing"
+fi;
