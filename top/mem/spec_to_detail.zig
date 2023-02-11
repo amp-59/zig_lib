31a5f58c3d64b02ef8b8691d8a8ce1f247843dde
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-11 13:14:35 +0000

    * Corrected bad find-and-replace. * Updated imports.

--- top/mem/spec_to_detail.zig
+++ top/mem/spec_to_detail.zig
@@ -1,10 +1,10 @@
 //! This stage summarises the abstract specification.
-const mem = @import("./../mem.zig");
-const fmt = @import("./../fmt.zig");
-const proc = @import("./../proc.zig");
-const meta = @import("./../meta.zig");
-const preset = @import("./../preset.zig");
-const builtin = @import("./../builtin.zig");
+const mem = @import("../mem.zig");
+const fmt = @import("../fmt.zig");
+const proc = @import("../proc.zig");
+const meta = @import("../meta.zig");
+const preset = @import("../preset.zig");
+const builtin = @import("../builtin.zig");
 const gen = @import("./gen.zig");
 const out = struct {
     usingnamespace @import("./detail.zig");
@@ -39,31 +39,26 @@ fn writeUnspecifiedDetailInternal(array: *gen.String, comptime T: type, impl_det
             writeUnspecifiedDetailInternal(array, field.type, impl_detail);
         }
     } else if (type_info == .Struct) {
-        impl_detail.index = typeId(T);
-        writeimpl_detailStruct(array, impl_detail.*);
+        impl_detail.index = meta.typeId(T);
+        writeDetailStruct(array, impl_detail.*);
     }
 }
-fn writeimpl_detailStruct(array: *gen.String, impl_detail: detail.Detail) void {
+fn writeDetailStruct(array: *gen.String, impl_detail: out.Detail) void {
     array.writeMany("    ");
     array.writeFormat(impl_detail);
     array.writeMany(",\n");
 }
 fn writeUnspecifiedDetails(array: *gen.String) void {
-    var impl_detail: detail.Detail = .{};
+    var impl_detail: out.Detail = .{};
     gen.writeImports(array, @src(), &.{.{ .name = "out", .path = "../../detail.zig" }});
     array.writeMany("pub const impl_details: []const out.Detail = &[_]out.Detail{");
-    writeUnspecifiedDetailInternal(array, abstract_spec.AbstractSpec, &impl_detail);
+    writeUnspecifiedDetailInternal(array, out.AbstractSpec, &impl_detail);
     array.writeMany("};\n");
 }
-pub fn specToDetail(array: *gen.String) void {
-    writeUnspecifiedDetails(array);
-    gen.writeAuxiliarySourceFile(array, "impl_details.zig");
-}
-pub export fn _start() noreturn {
-    @setAlignStack(16);
+fn specToDetail() void {
     var array: gen.String = undefined;
     array.undefineAll();
-    array.undefineAll();
-    specToDetail(&array);
-    gen.exit(0);
+    writeUnspecifiedDetails(&array);
+    gen.writeAuxiliarySourceFile(&array, "impl_details.zig");
 }
+pub const main = specToDetail;
