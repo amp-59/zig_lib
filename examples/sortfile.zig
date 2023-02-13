1ff015424b9a6a08acf5c6f29c734221bde48053
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-13 13:10:30 +0000

    Updated behaviour to copy to a buffer before sorting.

--- examples/sortfile.zig
+++ examples/sortfile.zig
@@ -9,7 +9,8 @@ const builtin = srg.builtin;
 
 pub usingnamespace proc.start;
 
-const start: u64 = 0x40000000;
+const fbuf_start: u64 = 0x40000000;
+const mbuf_start: u64 = 0x10000000000;
 
 const map_spec: file.MapSpec = .{
     .options = .{ .visibility = .shared, .read = true, .write = true },
@@ -23,7 +24,33 @@ const unmap_spec: mem.UnmapSpec = .{
 const open_spec: file.OpenSpec = .{
     .options = .{ .read = true, .write = .append },
 };
-
+fn sortBuf(buf: []u64) void {
+    if (!algo.isSortedAsc(u64, buf)) {
+        algo.shellSortAscTransform(u64, algo.approx, buf);
+        algo.shellSortAscTransform(u64, algo.approxDouble, buf);
+        algo.shellSortAsc(u64, buf);
+        mem.copy(fbuf_start, mbuf_start, @sizeOf(u64) * buf.len, 8);
+    }
+}
+fn showFile(buf: []u64) void {
+    var array: mem.StaticString(1024 * 1024) = undefined;
+    var line: u64 = 0;
+    array.undefineAll();
+    for (buf) |value| {
+        if (array.avail() < 128) {
+            builtin.debug.write(array.readAll());
+            array.undefineAll();
+        }
+        const len: u64 = array.len();
+        array.writeFormat(fmt.ux64(value));
+        array.writeMany(", \t");
+        line +%= array.len() -% len;
+        if (line >= 104) {
+            array.writeOne('\n');
+            line = 0;
+        }
+    }
+}
 pub fn main(args: [][*:0]u8) !void {
     if (args.len != 3) {
         builtin.debug.logFault(
@@ -37,24 +64,16 @@ pub fn main(args: [][*:0]u8) !void {
     const fd: u64 = try file.open(open_spec, target);
     defer file.close(close_spec, fd);
 
-    const finish: u64 = try file.map(map_spec, start, fd);
-    defer mem.unmap(unmap_spec, start, finish - start);
-
-    const buf: []u64 = mem.pointerMany(u64, start, (finish - start) / 8);
+    const fbuf_finish: u64 = try file.map(map_spec, fbuf_start, fd);
+    const bytes: u64 = fbuf_finish - fbuf_start;
+    defer mem.unmap(unmap_spec, fbuf_start, bytes);
+    try mem.map(.{ .options = .{} }, mbuf_start, bytes);
+    mem.copy(mbuf_start, fbuf_start, bytes, 8);
 
     if (mem.testEqualMany(u8, "sort", command)) {
-        return algo.layeredShellSortAsc(u64, buf);
+        return sortBuf(mem.pointerMany(u64, mbuf_start, bytes / 8));
     }
     if (mem.testEqualMany(u8, "show", command)) {
-        var array: mem.StaticString(4096) = undefined;
-        array.undefineAll();
-        for (buf) |value| {
-            if (array.avail() < 128) {
-                builtin.debug.write(array.readAll());
-                array.undefineAll();
-            }
-            array.writeFormat(fmt.ux64(value));
-            array.writeOne('\n');
-        }
+        return showFile(mem.pointerMany(u64, fbuf_start, bytes / 8));
     }
 }
