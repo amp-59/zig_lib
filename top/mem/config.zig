47f122f532c252f1901a556b3f6605b5fb503e49
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-14 22:04:56 +0000

    Added hints to the purpose and effect of some configuration constants.

--- top/mem/config.zig
+++ top/mem/config.zig
@@ -1,10 +1,21 @@
 //! Control various aspects of `memgen`
 
 pub const word_size_type: type = u64;
+
 pub const prefer_operator_wrapper: bool = true;
+/// If `true` approximate length counts are stored in the least significant
+/// bits. This makes finding the length more efficient. The efficiency of
+/// finding addresses on the same word is theoretically unchanged.
 pub const packed_capacity_low: bool = false;
 pub const minimise_indirection: bool = true;
+/// Omit declarations within containers which are not strictly necessary.
+/// Increases node count while decreasing line count.
 pub const minimise_declaration: bool = true;
 pub const show_eliminated_options: bool = false;
+/// Improved binary size when multiple types of container are used. Slightly
+/// worse binary size if only one is used.
 pub const implement_write_inline: bool = false;
 pub const implement_count_as_one: bool = false;
+/// If enabled functions returning slices accept an additional parameter to
+/// define the length of the returned pointer.
+pub const user_defined_length: bool = true;
