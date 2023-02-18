bc49bdef74b7514adbb94785997c16e03b8fe447
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-18 08:55:01 +0000

    Updated.

--- examples/allocators.zig
+++ examples/allocators.zig
@@ -7,9 +7,18 @@ const preset = srg.preset;
 const virtual = srg.virtual;
 const testing = srg.testing;
 
+pub usingnamespace proc.start;
 pub const AddressSpace = virtual.GenericRegularAddressSpace(multi_arena);
 
-pub usingnamespace proc.start;
+// This is 1GiB. I allow this much for the binary mapping. Not sure if sound,
+// but the binary mapping should start at ~64K, so it would need to be big to
+// overlap. If it did the program would abort due to MAP_FIXED_NOREPLACE.
+const start: u64 = 0x40000000;
+// Also 1GiB. Would rather not do something weird like size = start.
+// This is the size of each arena.
+const size: u64 = 1024 * 1024;
+const count: u64 = 1024;
+const finish: u64 = start + (size * count);
 
 const Allocator = mem.GenericRtArenaAllocator(.{
     .AddressSpace = AddressSpace,
@@ -19,25 +28,19 @@ const Allocator = mem.GenericRtArenaAllocator(.{
         .require_unmap = false,
     },
 });
-const Allocators = mem.StaticArray(Allocator, count);
-
-// This is 1GiB. I allow this much for the binary mapping. Not sure if sound,
-// but the binary mapping should start at ~64K, so it would need to be big to
-// overlap. If it did the program would abort due to MAP_FIXED_NOREPLACE.
-const start: u64 = 0x40000000;
-// Also 1GiB. Would rather not do something weird like size = start.
-// This is the size of each arena.
-const size: u64 = 1024 * 1024;
-const count: u64 = 16;
-const finish: u64 = start + (size * count);
-
 const multi_arena: virtual.RegularMultiArena = .{
     .label = "1024x1GiB",
     .lb_addr = start,
     .up_addr = finish,
     .divisions = count,
-    .options = .{ .require_map = true, .require_unmap = true },
+    .logging = preset.address_space.logging.verbose,
+    .options = .{
+        .require_map = true,
+        .require_unmap = true,
+    },
 };
+
+const Allocators = mem.StaticArray(Allocator, count);
 
 fn init(address_space: *AddressSpace, allocators: *Allocators) !void {
     var arena_index: AddressSpace.Index = 0;
@@ -48,7 +51,7 @@ fn init(address_space: *AddressSpace, allocators: *Allocators) !void {
 fn deinit(address_space: *AddressSpace, allocators: *Allocators) void {
     var arena_index: AddressSpace.Index = 0;
     while (arena_index != count) : (arena_index +%= 1) {
-        allocators.referOneAt(arena_index).deinit(address_space);
+        allocators.referOneAt(arena_index).deinit(address_space, arena_index);
     }
 }
 pub fn main() !void {
