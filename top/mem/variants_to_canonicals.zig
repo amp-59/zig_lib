8da83e481351e1cf757929b59e255a8c7a92075e
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-20 12:41:14 +0000

    * Removed unused functions. * Removed custom `_start`.

--- top/mem/variants_to_canonicals.zig
+++ top/mem/variants_to_canonicals.zig
@@ -1,7 +1,9 @@
 const sys = @import("../sys.zig");
 const mem = @import("../mem.zig");
 const meta = @import("../meta.zig");
+const proc = @import("../proc.zig");
 const builtin = @import("../builtin.zig");
+
 const gen = @import("./gen.zig");
 const out = struct {
     usingnamespace @import("./detail.zig");
@@ -11,31 +13,25 @@ const out = struct {
     usingnamespace @import("./zig-out/src/impl_variants.zig");
     usingnamespace @import("./zig-out/src/canonical.zig");
 };
-const Container = struct { out.Layout, out.Kind, out.Mode };
-const Uniques = mem.StaticArray(Container, 256);
+
+pub usingnamespace proc.start;
+pub const is_verbose: bool = false;
+pub const is_silent: bool = true;
+
 const Array = mem.StaticArray(u8, 1024 * 1024);
 
-fn writeOneUnique(uniques: *Uniques, value: Container) void {
-    for (uniques.readAll()) |unique| {
-        if (builtin.testEqual(Container, value, unique)) return;
-    }
-    uniques.writeOne(value);
-}
-fn variantsToCanonical(array: *Array) void {
-    gen.writeImports(array, @src(), &.{.{ .name = "out", .path = "./canonical.zig" }});
+fn variantsToCanonical() void {
+    var array: Array = undefined;
+    array.undefineAll();
+    gen.writeGenerator(&array, @src());
+    gen.writeImport(&array, "out", "./canonical.zig");
     array.writeMany("pub const canonicals: []const out.Canonical = &[_]out.Canonical{\n");
     for (out.impl_variants) |variant| {
         array.writeMany("    ");
-        gen.writeStructOfEnum(array, out.Canonical, out.Canonical.convert(variant));
+        gen.writeStructOfEnum(&array, out.Canonical, out.Canonical.convert(variant));
         array.writeMany(",\n");
     }
     array.writeMany("};\n");
-    gen.writeAuxiliarySourceFile(array, "canonicals.zig");
+    gen.writeAuxiliarySourceFile(&array, "canonicals.zig");
 }
-pub export fn _start() noreturn {
-    @setAlignStack(16);
-    var array: Array = undefined;
-    array.undefineAll();
-    variantsToCanonical(&array);
-    sys.call(.exit, .{}, noreturn, .{0});
-}
+pub const main = variantsToCanonical;
