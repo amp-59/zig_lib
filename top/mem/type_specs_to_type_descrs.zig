fc5e5a9f6f2b98cd8387fe3cacf6b99a3c245f3f
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-20 12:32:41 +0000

    * Removed custom `_start`. * Updated usage of boilerplate functions. * Simplified formatting of output. * Defined minimal global configuration constants.

--- top/mem/type_specs_to_type_descrs.zig
+++ top/mem/type_specs_to_type_descrs.zig
@@ -1,18 +1,30 @@
 const sys = @import("../sys.zig");
 const mem = @import("../mem.zig");
 const fmt = @import("../fmt.zig");
+const proc = @import("../proc.zig");
 const meta = @import("../meta.zig");
 const builtin = @import("../builtin.zig");
 const testing = @import("../testing.zig");
+
 const gen = @import("./gen.zig");
 const out = @import("./zig-out/src/type_specs.zig");
 
+pub usingnamespace proc.start;
+pub const is_verbose: bool = false;
+pub const is_silent: bool = true;
+
 const Array = mem.StaticArray(u8, 1024 * 1024);
 
-fn mapContainersToParameters(array: *Array) void {
-    const fmt_spec = .{ .infer_type_names = true, .ignore_formatter_decls = true };
-    gen.writeImports(array, @src(), &.{.{ .name = "gen", .path = "../../gen.zig" }});
-    array.writeMany("pub const type_descrs = ");
+const fmt_spec = .{
+    .infer_type_names = true,
+    .ignore_formatter_decls = true,
+};
+fn mapContainersToParameters() void {
+    var array: Array = undefined;
+    array.undefineAll();
+    gen.writeGenerator(&array, @src());
+    gen.writeImport(&array, "gen", "../../gen.zig");
+    array.writeMany("pub const type_descrs=");
     array.writeMany(
         \\&[_]struct {
         \\    params: gen.TypeDescr,
@@ -21,27 +33,21 @@ fn mapContainersToParameters(array: *Array) void {
         \\}{ 
     );
     inline for (out.type_specs) |type_spec| {
-        array.writeMany(".{ .params = ");
+        array.writeMany(".{.params=");
         array.writeFormat(fmt.render(fmt_spec, comptime gen.TypeDescr.init(type_spec.params)));
         array.writeMany(",\n");
-        array.writeMany(".specs = &.{\n");
+        array.writeMany(".specs=&.{\n");
         inline for (type_spec.specs) |spec| {
             array.writeFormat(fmt.render(fmt_spec, comptime gen.TypeDescr.init(spec)));
             array.writeMany(",\n");
         }
         array.writeMany("},\n");
-        array.writeMany(".vars = ");
+        array.writeMany(".vars=");
         array.writeFormat(fmt.render(fmt_spec, comptime gen.TypeDescr.init(type_spec.vars)));
         array.writeMany("},\n");
     }
     array.overwriteManyBack("};");
     array.writeMany("\n");
-    gen.writeAuxiliarySourceFile(array, "type_descrs.zig");
+    gen.writeAuxiliarySourceFile(&array, "type_descrs.zig");
 }
-pub export fn _start() noreturn {
-    @setAlignStack(16);
-    var array: Array = undefined;
-    array.undefineAll();
-    mapContainersToParameters(&array);
-    sys.call(.exit, .{}, noreturn, .{0});
-}
+pub const main = mapContainersToParameters;
