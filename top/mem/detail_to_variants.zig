31d5e5a541cd42e2165b2c5c52a85b0c801e6971
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-20 12:22:49 +0000

    Added a type `Index` to variants auxiliary file.

--- top/mem/detail_to_variants.zig
+++ top/mem/detail_to_variants.zig
@@ -1,6 +1,7 @@
 const sys = @import("../sys.zig");
 const mem = @import("../mem.zig");
 const meta = @import("../meta.zig");
+const proc = @import("../proc.zig");
 const builtin = @import("../builtin.zig");
 
 const gen = @import("./gen.zig");
@@ -13,6 +14,10 @@ const out = struct {
     usingnamespace @import("./zig-out/src/specifiers.zig");
 };
 
+pub usingnamespace proc.start;
+pub const is_verbose: bool = false;
+pub const is_silent: bool = true;
+
 const Array = mem.StaticArray(u8, 1024 * 1024);
 
 fn writeVariantStructInternal(array: *Array, impl_detail: out.Detail, specs: out.Specifiers) void {
@@ -20,7 +25,7 @@ fn writeVariantStructInternal(array: *Array, impl_detail: out.Detail, specs: out
     array.writeFormat(impl_detail.more(out.DetailMore, specs));
     array.writeMany(",\n");
 }
-fn writeVariantStruct(array: *Array, comptime impl_detail: out.Detail) void {
+fn writeVariantStruct(array: *Array, comptime impl_detail: out.Detail) u16 {
     const type_spec: gen.TypeSpecMap = out.type_specs[impl_detail.index];
     var vars: u16 = 0;
     while (vars <= @truncate(meta.Child(type_spec.vars), ~@as(u64, 0))) : (vars +%= 1) {
@@ -35,20 +40,31 @@ fn writeVariantStruct(array: *Array, comptime impl_detail: out.Detail) void {
         }
         writeVariantStructInternal(array, impl_detail, specs);
     }
+    return vars;
 }
-fn detailToVariants(array: *Array) void {
-    gen.writeImports(array, @src(), &.{.{ .name = "out", .path = "../../detail_more.zig" }});
-    array.writeMany("pub const impl_variants: []const out.DetailMore = &[_]out.DetailMore{\n");
-    inline for (out.impl_details) |impl_detail| {
-        writeVariantStruct(array, impl_detail);
-    }
-    array.writeMany("};\n");
-    gen.writeAuxiliarySourceFile(array, "impl_variants.zig");
-}
-pub export fn _start() noreturn {
-    @setAlignStack(16);
+fn detailToVariants() void {
     var array: Array = undefined;
     array.undefineAll();
-    detailToVariants(&array);
-    sys.call(.exit, .{}, noreturn, .{0});
+    gen.writeGenerator(&array, @src());
+    gen.writeImport(&array, "out", "../../detail_more.zig");
+    array.writeMany("pub const impl_variants: []const out.DetailMore = &[_]out.DetailMore{\n");
+    var vars: u64 = 0;
+    inline for (out.impl_details) |impl_detail| {
+        vars +%= writeVariantStruct(&array, impl_detail);
+    }
+    array.writeMany("};\n");
+    array.writeMany("pub const Index = ");
+    switch (vars) {
+        0...255 => {
+            array.writeMany("u8;");
+        },
+        256...65535 => {
+            array.writeMany("u16;");
+        },
+        else => {
+            array.writeMany("u32;");
+        },
+    }
+    gen.writeAuxiliarySourceFile(&array, "impl_variants.zig");
 }
+pub const main = detailToVariants;
