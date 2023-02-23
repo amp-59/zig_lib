f8fdb44531946717a9e103fb3e4e0c3651fb9c48
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-23 01:05:42 +0000

    Split attributes to new file.

---
+++ top/mem/attr.zig
@@ -0,0 +1,181 @@
+pub fn GenericStructOfBool(comptime Struct: type) type {
+    return (struct {
+        pub fn countTrue(bit_field: Struct) u64 {
+            var ret: u64 = 0;
+            inline for (@typeInfo(Struct).Struct.fields) |field| {
+                ret +%= @boolToInt(@field(bit_field, field.name));
+            }
+            return ret;
+        }
+        pub fn formatWrite(format: Struct, array: anytype) void {
+            if (countTrue(format) == 0) {
+                array.writeMany(".{}");
+            } else {
+                array.writeMany(".{");
+                inline for (@typeInfo(Struct).Struct.fields) |field| {
+                    if (@field(format, field.name)) {
+                        array.writeMany("." ++ field.name ++ "=true,");
+                    }
+                }
+                array.undefine(1);
+                array.writeOne('}');
+            }
+        }
+        pub fn formatLength(format: Struct) u64 {
+            var len: u64 = 3;
+            if (countTrue(format) != 0) {
+                len -%= 1;
+                inline for (@typeInfo(Struct).Struct.fields) |field| {
+                    if (@field(format, field.name)) {
+                        len +%= 1 +% field.name.len +% 6;
+                    }
+                }
+            }
+            return len;
+        }
+    });
+}
+
+pub const Kinds = packed struct {
+    automatic: bool = false,
+    dynamic: bool = false,
+    static: bool = false,
+    parametric: bool = false,
+    pub usingnamespace GenericStructOfBool(Kinds);
+};
+pub const Layouts = packed struct {
+    structured: bool = false,
+    unstructured: bool = false,
+    pub usingnamespace GenericStructOfBool(Layouts);
+};
+pub const Modes = packed struct {
+    read_write: bool = false,
+    resize: bool = false,
+    stream: bool = false,
+    pub usingnamespace GenericStructOfBool(Modes);
+};
+pub const Fields = packed struct {
+    automatic_storage: bool = false,
+    allocated_byte_address: bool = false,
+    undefined_byte_address: bool = false,
+    unallocated_byte_address: bool = false,
+    unstreamed_byte_address: bool = false,
+    pub usingnamespace GenericStructOfBool(Fields);
+};
+pub const Managers = packed struct {
+    allocate: bool = false,
+    reallocate: bool = false,
+    resize: bool = false,
+    move: bool = false,
+    convert: bool = false,
+    pub usingnamespace GenericStructOfBool(Managers);
+};
+pub const Techniques = packed struct {
+    auto_alignment: bool = false,
+    lazy_alignment: bool = false,
+    unit_alignment: bool = false,
+    disjunct_alignment: bool = false,
+    single_packed_approximate_capacity: bool = false,
+    double_packed_approximate_capacity: bool = false,
+    arena_relative: bool = false,
+    address_space_relative: bool = false,
+    pub const Options = struct {
+        capacity: ?enum {
+            single_packed_approximate,
+            double_packed_approximate,
+        },
+        alignment: enum {
+            auto,
+            unit,
+            lazy,
+            disjunct,
+        },
+        relative: enum {
+            arena,
+            address_space,
+        },
+    };
+    pub usingnamespace GenericStructOfBool(Techniques);
+};
+pub const Option = struct {
+    kind: Option.Kind,
+    info: Info,
+    pub const Kind = enum {
+        standalone,
+        mutually_exclusive_optional,
+        mutually_exclusive_mandatory,
+    };
+    pub const Usage = enum {
+        eliminate_boolean_false,
+        eliminate_boolean_true,
+        test_boolean,
+        compare_enumeration,
+        compare_optional_enumeration,
+    };
+    pub const Info = struct {
+        field_name: []const u8,
+        field_field_names: []const []const u8,
+    };
+    pub fn len(comptime option: Option) u64 {
+        return option.info.field_field_names.len;
+    }
+    pub fn count(comptime option: Option, comptime Detail: type, toplevel_impl_group: []const Detail) u64 {
+        var ret: u64 = 0;
+        var techs: Techniques = .{};
+        inline for (@typeInfo(Techniques).Struct.fields) |field| {
+            for (toplevel_impl_group) |impl_variant| {
+                if (@field(impl_variant.techs, field.name)) {
+                    @field(techs, field.name) = true;
+                }
+            }
+        }
+        inline for (option.info.field_field_names) |field_name| {
+            ret +%= @boolToInt(@field(techs, field_name));
+        }
+        return ret;
+    }
+    pub fn names(comptime option: Option, comptime Detail: type, toplevel_impl_group: []const Detail, buf: [][]const u8) []const []const u8 {
+        var techs: Techniques = .{};
+        inline for (@typeInfo(Techniques).Struct.fields) |field| {
+            for (toplevel_impl_group) |impl_variant| {
+                if (@field(impl_variant.techs, field.name)) {
+                    @field(techs, field.name) = true;
+                }
+            }
+        }
+        var idx: u64 = 0;
+        inline for (option.info.field_field_names) |field_name| {
+            if (@field(techs, field_name)) {
+                buf[idx] = field_name;
+                idx +%= 1;
+            }
+        }
+        return buf[0..idx];
+    }
+    pub fn usage(comptime option: Option, comptime Detail: type, toplevel_impl_group: []const Detail) Usage {
+        const value: u64 = option.count(Detail, toplevel_impl_group);
+        switch (option.kind) {
+            .standalone => switch (value) {
+                0 => return .eliminate_boolean_false,
+                1 => return .test_boolean,
+                else => unreachable,
+            },
+            .mutually_exclusive_optional => switch (value) {
+                0 => return .eliminate_boolean_false,
+                1 => return .test_boolean,
+                else => return .compare_optional_enumeration,
+            },
+            .mutually_exclusive_mandatory => switch (value) {
+                0 => return .eliminate_boolean_false,
+                1 => return .eliminate_boolean_true,
+                else => return .compare_enumeration,
+            },
+        }
+    }
+    pub fn fieldName(comptime option: Option, comptime index: u64) []const u8 {
+        return option.info.field_field_names[index];
+    }
+    pub fn tagName(comptime option: Option, comptime index: u64) []const u8 {
+        return option.fieldName(index)[0 .. option.fieldName(index).len - (option.info.field_name.len + 1)];
+    }
+};
