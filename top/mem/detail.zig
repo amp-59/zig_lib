34062f438cbcb8b95ddd7d23172ab93315b73679
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-23 19:00:31 +0000

    Filled-in missing definitions to match `DetailMore`.

--- top/mem/detail.zig
+++ top/mem/detail.zig
@@ -10,23 +10,52 @@ pub const Detail = packed struct {
     fields: attr.Fields = .{},
     techs: attr.Techniques = .{},
 
-    pub fn formatWrite(detail: Detail, array: anytype) void {
-        array.writeMany(".{ .index = ");
-        gen.writeIndex(array, detail.index);
-        array.writeMany(", .kinds = ");
+    const index_init: [:0]const u8 = ".index=";
+    const kinds_init: [:0]const u8 = ",.kinds=";
+    const layouts_init: [:0]const u8 = ",.layouts=";
+    const modes_init: [:0]const u8 = ",.modes=";
+    const managers_init: [:0]const u8 = ",.managers=";
+    const fields_init: [:0]const u8 = ",.fields=";
+    const techs_init: [:0]const u8 = ",.techs=";
+
+    pub fn formatWrite(detail: *const Detail, array: anytype) void {
+        array.writeMany(".{");
+        array.writeMany(index_init);
+        array.writeFormat(gen.fmt.ud8(detail.index));
+        array.writeMany(kinds_init);
         array.writeFormat(detail.kinds);
-        array.writeMany(", .layouts = ");
+        array.writeMany(layouts_init);
         array.writeFormat(detail.layouts);
-        array.writeMany(", .modes = ");
+        array.writeMany(modes_init);
         array.writeFormat(detail.modes);
-        array.writeMany(", .managers = ");
+        array.writeMany(managers_init);
         array.writeFormat(detail.managers);
-        array.writeMany(", .fields = ");
+        array.writeMany(fields_init);
         array.writeFormat(detail.fields);
-        array.writeMany(", .techs = ");
+        array.writeMany(techs_init);
         array.writeFormat(detail.techs);
-        array.writeMany(" }");
+        array.writeMany("}");
     }
+    pub fn formatLength(detail: *const Detail) u64 {
+        var len: u64 = 0;
+        len +%= 2 +% index_init.len;
+        len +%= gen.fmt.ud8(detail.index).formatLength();
+        len +%= kinds_init.len;
+        len +%= detail.kinds.formatLength();
+        len +%= layouts_init.len;
+        len +%= detail.layouts.formatLength();
+        len +%= modes_init.len;
+        len +%= detail.modes.formatLength();
+        len +%= managers_init.len;
+        len +%= detail.managers.formatLength();
+        len +%= fields_init.len;
+        len +%= detail.fields.formatLength();
+        len +%= techs_init.len;
+        len +%= detail.techs.formatLength();
+        len +%= 1;
+        return len;
+    }
+
     pub fn less(detail: *const Detail, comptime DetailLess: type) DetailLess {
         return .{
             .index = detail.index,
