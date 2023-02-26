59495fb8ba23e6b0f36f4e30f2862d45d2626d30
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-02-26 19:58:52 +0000

    Updated library build program to include memory implementation generator.

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
 const _ = struct {
-    pub const build = if (true) @import("build/build-aux.zig").main else main;
+    pub const build = if (false) @import("build/build-aux.zig").main else main;
 };
 pub usingnamespace @"_";
 
@@ -68,43 +68,90 @@ const parsedir_lib_macros: []const build.Macro = meta.slice(build.Macro, .{.{
 // TODO Create super type for build options permitting the creation of
 // dependencies, etc. Like step.
 // zig fmt: off
-pub fn main(builder: *build.Builder) !void {
+pub fn main(allocator: *build.Allocator, builder: *build.Builder) !void {
     // Top test programs
-    const builtin_test: *build.Target   = builder.addExecutable("builtin_test",   "top/builtin-test.zig", minor_test_args);
-    const meta_test: *build.Target      = builder.addExecutable("meta_test",      "top/meta-test.zig",    minor_test_args);
-    const mem_test: *build.Target       = builder.addExecutable("mem_test",       "top/mem-test.zig",     minor_test_args);
-    const algo_test: *build.Target      = builder.addExecutable("algo_test",      "top/algo-test.zig",    algo_test_args);
-    const file_test: *build.Target      = builder.addExecutable("file_test",      "top/file-test.zig",    minor_test_args);
-    const list_test: *build.Target      = builder.addExecutable("list_test",      "top/list-test.zig",    minor_test_args);
-    const fmt_test: *build.Target       = builder.addExecutable("fmt_test",       "top/fmt-test.zig",     fmt_test_args);
-    const render_test: *build.Target    = builder.addExecutable("render_test",    "top/render-test.zig",  minor_test_args);
-    const thread_test: *build.Target    = builder.addExecutable("thread_test",    "top/thread-test.zig",  minor_test_args);
-    const virtual_test: *build.Target   = builder.addExecutable("virtual_test",   "top/virtual-test.zig", minor_test_args);
-    const build_test: *build.Target     = builder.addExecutable("build_test",     "top/build-test.zig",   small_test_args);
-    for (.{ builtin_test, meta_test, mem_test, algo_test, file_test, list_test, fmt_test, render_test, thread_test, virtual_test, build_test }) |_| {}
-
+    const builtin_test: *build.Target   = builder.addTarget(.{}, allocator, "builtin_test",   "top/builtin-test.zig");
+    const meta_test: *build.Target      = builder.addTarget(.{}, allocator, "meta_test",      "top/meta-test.zig");
+    const mem_test: *build.Target       = builder.addTarget(.{}, allocator, "mem_test",       "top/mem-test.zig");
+    const algo_test: *build.Target      = builder.addTarget(.{}, allocator, "algo_test",      "top/algo-test.zig");
+    const file_test: *build.Target      = builder.addTarget(.{}, allocator, "file_test",      "top/file-test.zig");
+    const list_test: *build.Target      = builder.addTarget(.{}, allocator, "list_test",      "top/list-test.zig");
+    const fmt_test: *build.Target       = builder.addTarget(.{}, allocator, "fmt_test",       "top/fmt-test.zig");
+    const render_test: *build.Target    = builder.addTarget(.{}, allocator, "render_test",    "top/render-test.zig");
+    const thread_test: *build.Target    = builder.addTarget(.{}, allocator, "thread_test",    "top/thread-test.zig");
+    const virtual_test: *build.Target   = builder.addTarget(.{}, allocator, "virtual_test",   "top/virtual-test.zig");
+    const build_test: *build.Target     = builder.addTarget(.{}, allocator, "build_test",     "top/build-test.zig");
     // More complete test programs:
-    const mca: *build.Target            = builder.addExecutable("mca",            "test/mca.zig",         fast_test_args);
-    const treez: *build.Target          = builder.addExecutable("treez",          "test/treez.zig",       small_test_args);
-    const itos: *build.Target           = builder.addExecutable("itos",           "test/itos.zig",        small_test_args);
-    const cat: *build.Target            = builder.addExecutable("cat",            "test/cat.zig",         fast_test_args);
-    const hello: *build.Target          = builder.addExecutable("hello",          "test/hello.zig",       small_test_args);
-    const readelf: *build.Target        = builder.addExecutable("readelf",        "test/readelf.zig",     minor_test_args);
-    const parsedir: *build.Target       = builder.addExecutable("parsedir",       "test/parsedir.zig",    fast_test_args);
-    for (.{ mca, treez, itos, cat, hello, readelf, parsedir }) |_| {}
-
+    const mca: *build.Target            = builder.addTarget(.{}, allocator, "mca",            "test/mca.zig");
+    const treez: *build.Target          = builder.addTarget(.{}, allocator, "treez",          "test/treez.zig");
+    const itos: *build.Target           = builder.addTarget(.{}, allocator, "itos",           "test/itos.zig");
+    const cat: *build.Target            = builder.addTarget(.{}, allocator, "cat",            "test/cat.zig");
+    const hello: *build.Target          = builder.addTarget(.{}, allocator, "hello",          "test/hello.zig");
+    const readelf: *build.Target        = builder.addTarget(.{}, allocator, "readelf",        "test/readelf.zig");
+    const parsedir: *build.Target       = builder.addTarget(.{}, allocator, "parsedir",       "test/parsedir.zig");
     // Other test programs:
-    const impl_test: *build.Target          = builder.addExecutable("impl_test",      "top/impl-test.zig",        .{});
-    const container_test: *build.Target     = builder.addExecutable("container_test", "top/container-test.zig",   .{});
-    const parse_test: *build.Target         = builder.addExecutable("parse_test",     "top/parse-test.zig",       .{});
-    const lib_parser_test: *build.Target    = builder.addExecutable("lib_parser",     "test/parsedir.zig",        lib_parser_args);
-    const std_parser_test: *build.Target    = builder.addExecutable("std_parser",     "test/parsedir.zig",        std_parser_args);
-    for (.{ impl_test, container_test, parse_test, lib_parser_test, std_parser_test }) |_| {}
-
+    const impl_test: *build.Target          = builder.addTarget(.{}, allocator, "impl_test",      "top/impl-test.zig");
+    const container_test: *build.Target     = builder.addTarget(.{}, allocator, "container_test", "top/container-test.zig");
+    const parse_test: *build.Target         = builder.addTarget(.{}, allocator, "parse_test",     "top/parse-test.zig");
+    const lib_parser_test: *build.Target    = builder.addTarget(.{}, allocator, "lib_parser",     "test/parsedir.zig");
+    const std_parser_test: *build.Target    = builder.addTarget(.{}, allocator, "std_parser",     "test/parsedir.zig");
     // Examples
-    const readdir: *build.Target            = builder.addExecutable("readdir",        "examples/iterate_dir_entries.zig",     small_test_args);
-    const dynamic: *build.Target            = builder.addExecutable("dynamic",        "examples/dynamic_alloc.zig",           small_test_args);
-    const address_space: *build.Target      = builder.addExecutable("address_space",  "examples/custom_address_space.zig",    small_test_args);
-    for (.{ readdir, dynamic, address_space }) |_| {}
+    const readdir: *build.Target            = builder.addTarget(.{}, allocator, "readdir",        "examples/iterate_dir_entries.zig");
+    const dynamic: *build.Target            = builder.addTarget(.{}, allocator, "dynamic",        "examples/dynamic_alloc.zig");
+    const address_space: *build.Target      = builder.addTarget(.{}, allocator, "address_space",  "examples/custom_address_space.zig");
 
+    // Memory implementation generator
+    const memgen: *build.Group                      = builder.addGroup(allocator, "memgen");
+    {
+    const expr_test: *build.Target                  = memgen.addTarget(.{}, allocator, "expr_test",                 "top/mem/expr-test.zig");
+    const abstract_params: *build.Target            = memgen.addTarget(.{}, allocator, "abstract_params.zig",       "top/mem/abstract_params-aux.zig");
+    const impl_detail: *build.Target                = memgen.addTarget(.{}, allocator, "impl_detail.zig",           "top/mem/spec_to_detail.zig");
+    const options: *build.Target                    = memgen.addTarget(.{}, allocator, "mg_options",                "top/mem/options-aux.zig");
+    const type_specs: *build.Target                 = memgen.addTarget(.{}, allocator, "mg_type_specs",             "top/mem/type_specs-aux.zig");
+    const type_descr: *build.Target                 = memgen.addTarget(.{}, allocator, "mg_type_descrs",            "top/mem/type_descrs-aux.zig");
+    const impl_variants: *build.Target              = memgen.addTarget(.{}, allocator, "mg_impl_variants",          "top/mem/impl_variants-aux.zig");
+    const canonical: *build.Target                  = memgen.addTarget(.{}, allocator, "mg_canonical",              "top/mem/canonical-aux.zig");
+    const canonicals: *build.Target                 = memgen.addTarget(.{}, allocator, "mg_canonicals",             "top/mem/canonicals-aux.zig");
+    const containers: *build.Target                 = memgen.addTarget(.{}, allocator, "mg_containers",             "top/mem/containers-aux.zig");
+    const kinds: *build.Target                      = memgen.addTarget(.{}, allocator, "mg_kinds",                  "top/mem/kinds-aux.zig");
+    const generate_functions: *build.Target         = memgen.addTarget(.{}, allocator, "generate_functions",        "top/mem/variants_to_functions.zig");
+    const specifications: *build.Target             = memgen.addTarget(.{}, allocator, "mg_specifications",         "top/mem/specifications-aux.zig");
+    const generate_specifications: *build.Target    = memgen.addTarget(.{}, allocator, "generate_specifications",   "top/mem/generate_specifications.zig");
+    const generate_references: *build.Target        = memgen.addTarget(.{}, allocator, "generate_references",       "top/mem/generate_references.zig");
+    const generate_parameters: *build.Target        = memgen.addTarget(.{}, allocator, "generate_parameters",       "top/mem/generate_parameters.zig");
+    const generate_containers: *build.Target        = memgen.addTarget(.{}, allocator, "generate_containers",       "top/mem/generate_containers.zig");
+
+    type_specs.dependOnRun(allocator, abstract_params);
+    type_descr.dependOnRun(allocator, type_specs);
+    impl_variants.dependOnRun(allocator, type_specs);
+    impl_variants.dependOnRun(allocator, impl_detail);
+    canonicals.dependOnRun(allocator, impl_variants);
+    canonicals.dependOnRun(allocator, canonical);
+    containers.dependOnRun(allocator, canonicals);
+    kinds.dependOnRun(allocator, canonicals);
+    generate_functions.dependOnRun(allocator, impl_variants);
+    generate_functions.dependOnRun(allocator, expr_test);
+    specifications.dependOnRun(allocator, containers);
+    generate_specifications.dependOnRun(allocator, options);
+    generate_specifications.dependOnRun(allocator, type_descr);
+    generate_specifications.dependOnRun(allocator, specifications);
+    generate_references.dependOnRun(allocator, containers);
+    generate_references.dependOnRun(allocator, generate_specifications);
+    generate_parameters.dependOnRun(allocator, options);
+    generate_parameters.dependOnRun(allocator, type_descr);
+    generate_parameters.dependOnRun(allocator, containers);
+    generate_containers.dependOnRun(allocator, generate_parameters);
+
+    }
+
+    // zig fmt: on
+
+    for ([_]*build.Target{
+        builtin_test, meta_test,    mem_test,   algo_test,
+        file_test,    list_test,    fmt_test,   render_test,
+        thread_test,  virtual_test, build_test,
+    }) |_| {}
+    for (.{ mca, treez, itos, cat, hello, readelf, parsedir }) |_| {}
+    for (.{ impl_test, container_test, parse_test, lib_parser_test, std_parser_test }) |_| {}
+    for (.{ readdir, dynamic, address_space }) |_| {}
 }
