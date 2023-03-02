97c6fce66af0e45901b83cfb74131417a39578fc
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-02 12:20:23 +0000

    * Updated imports. * Updated usage of `Detail*`.

--- top/mem/canonical.zig
+++ top/mem/canonical.zig
@@ -1,10 +1,9 @@
 const gen = @import("./gen.zig");
 const attr = @import("./attr.zig");
-const out = struct {
-    usingnamespace @import("./detail.zig");
-    usingnamespace @import("./detail_more.zig");
-    usingnamespace @import("./zig-out/src/specifiers.zig");
-};
+const detail = @import("./detail.zig");
+
+const Specifiers = @import("./zig-out/src/specifiers.zig").Specifiers;
+
 pub const CanonicalSpec = struct {
     type_name: []const u8 = "Canonical",
     fields: []const CanonicalFieldSpec,
@@ -21,47 +20,47 @@ pub const kinds: CanonicalFieldSpec = .{
     .src_type = attr.Kinds,
     .dst_name = "kind",
     .dst_type_name = "Kind",
-    .detail = out.Detail,
+    .detail = detail.Base,
 };
 pub const layouts: CanonicalFieldSpec = .{
     .src_name = "layouts",
     .src_type = attr.Layouts,
     .dst_name = "layout",
     .dst_type_name = "Layout",
-    .detail = out.Detail,
+    .detail = detail.Base,
 };
 pub const modes: CanonicalFieldSpec = .{
     .src_name = "modes",
     .src_type = attr.Modes,
     .dst_name = "mode",
     .dst_type_name = "Mode",
-    .detail = out.Detail,
+    .detail = detail.Base,
 };
 pub const managers: CanonicalFieldSpec = .{
     .src_name = "managers",
     .src_type = attr.Managers,
     .dst_name = "manager",
     .dst_type_name = "Manager",
-    .detail = out.Detail,
+    .detail = detail.Base,
 };
 pub const fields: CanonicalFieldSpec = .{
     .src_type = attr.Fields,
     .src_name = "fields",
     .dst_name = "field",
     .dst_type_name = "Field",
-    .detail = out.Detail,
+    .detail = detail.Base,
 };
 pub const techs: CanonicalFieldSpec = .{
     .src_name = "techs",
     .src_type = attr.Techniques,
     .dst_name = "tech",
     .dst_type_name = "Technique",
-    .detail = out.Detail,
+    .detail = detail.Base,
 };
 pub const specs: CanonicalFieldSpec = .{
     .src_name = "specs",
-    .src_type = out.Specifiers,
+    .src_type = Specifiers,
     .dst_name = "spec",
     .dst_type_name = "Specifier",
-    .detail = out.DetailMore,
+    .detail = detail.More,
 };
