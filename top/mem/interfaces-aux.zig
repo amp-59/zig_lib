a33a23e5f0add268816fb5c34fac9eaa22a23d30
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-02 12:36:06 +0000

    * Removed output import. * Added `Index` declaration. * Minimised output formatting. Added line break.

--- top/mem/interfaces-aux.zig
+++ top/mem/interfaces-aux.zig
@@ -19,27 +19,32 @@ const out = struct {
 const Array = mem.StaticArray(u8, 1024 * 1024);
 
 fn variantsToInterface(array: *Array, comptime field_name: []const u8, kind_group: []const out.Index) void {
-    array.writeMany("&.{ ");
+    array.writeMany("&.{");
+    var len: u64 = 0;
     for (kind_group) |impl_index| {
         if (@field(out.impl_variants[impl_index].managers, field_name)) {
+            len +%= 1;
+            if (len % 8 == 0) array.writeMany("\n   ");
             array.writeFormat(fmt.ud64(impl_index));
-            array.writeMany(", ");
+            array.writeOne(',');
         }
     }
-    array.overwriteOneBack('}');
-    array.writeOne(',');
+    array.writeMany("},\n");
 }
 fn variantsToInterfaces() void {
     var array: Array = undefined;
     array.undefineAll();
-    gen.writeImport(&array, "out", "./impl_variants.zig");
-    array.writeMany("pub const interfaces: []const []const []const out.Index = &[_][]const []const out.Index{\n");
+    array.writeMany("const Index=" ++ @typeName(out.Index) ++ ";\n");
     inline for (@typeInfo(attr.Managers).Struct.fields) |field| {
-        array.writeMany("&.{\n");
+        array.writeMany("pub const " ++ field.name ++ ":[]const[]const Index=&[_][]const Index{\n");
         for (out.kinds) |kind_group| {
             variantsToInterface(&array, field.name, kind_group);
         }
-        array.writeMany("},\n");
+        array.writeMany("};\n");
+    }
+    array.writeMany("pub const interfaces:[]const[]const[]const Index=&[_][]const[]const Index{\n");
+    inline for (@typeInfo(attr.Managers).Struct.fields) |field| {
+        array.writeMany(field.name ++ ",");
     }
     array.writeMany("};\n");
     gen.writeAuxiliarySourceFile(&array, "interfaces.zig");
