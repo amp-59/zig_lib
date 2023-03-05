d5db26c2d41c133f20d6a303062cff84d736a143
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-05 08:44:09 +0000

    Made main operation a function, as this is required to use `return` instead of `exit`.

--- support/switch_build_runner.sh
+++ support/switch_build_runner.sh
@@ -12,42 +12,47 @@ readonly zig_build_runner="$zig_lib_dir/build_runner.zig";
 readonly zl_build_runner="$(realpath "$support_dir/../build_runner.zig")";
 readonly zl_zig_build="$(realpath "$support_dir/../build.zig")";
 readonly zig_build_runner_bkp="$zig_build_runner.bkp";
-if test -L "$zig_build_runner"; then
-    readonly build_runner_link_target="$(realpath "$zig_build_runner")";
-    if test "$build_runner_link_target" -ef "$zl_build_runner"; then
-        if test -f "$zig_build_runner_bkp"; then
-            if ! rm "$zig_build_runner"; then
+
+fn () 
+{
+    if test -L "$zig_build_runner"; then
+        readonly build_runner_link_target="$(realpath "$zig_build_runner")";
+        if test "$build_runner_link_target" -ef "$zl_build_runner"; then
+            if test -f "$zig_build_runner_bkp"; then
+                if ! rm "$zig_build_runner"; then
+                    return 2;
+                fi;
+                if ! mv -i "$zig_build_runner_bkp" "$zig_build_runner"; then
+                    return 2;
+                fi;
+                if test -f "$zl_zig_build"; then
+                    sed -i 's/pub const build = if (false)/pub const build = if (true)/' "$zl_zig_build";
+                fi;
+                echo "std"
+            else
+                echo $error "would move back original zig build runner, but original file is missing"
                 return 2;
             fi;
-            if ! mv -i "$zig_build_runner_bkp" "$zig_build_runner"; then
-                return 2;
-            fi;
-            if test -f "$zl_zig_build"; then
-                sed -i 's/pub const build = if (false)/pub const build = if (true)/' "$zl_zig_build";
-            fi;
-            echo "std"
         else
-            echo $error "would move back original zig build runner, but original file is missing"
+            echo $error "expected link to zig_lib build runner, but found other file:";
+            echo $blank "'$zig_build_runner' -> ";
+            echo $blank "'$build_runner_link_target' != ";
+            echo $blank "'$zl_build_runner'";
             return 2;
         fi;
+    elif test -f "$zig_build_runner"; then
+        if ! mv -i "$zig_build_runner" "$zig_build_runner_bkp"; then
+            return 2;
+        fi;
+        if ! ln -s "$zl_build_runner" "$zig_build_runner"; then
+            return 2;
+        fi;
+        if test -f "$zl_zig_build"; then
+            sed -i 's/pub const build = if (true)/pub const build = if (false)/' "$zl_zig_build";
+        fi;
+        echo "zl"
     else
-        echo $error "expected link to zig_lib build runner, but found other file:";
-        echo $blank "'$zig_build_runner' -> ";
-        echo $blank "'$build_runner_link_target' != ";
-        echo $blank "'$zl_build_runner'";
-        return 2;
+        echo $error "'$zig_build_runner': no such file or directory; did nothing"
     fi;
-elif test -f "$zig_build_runner"; then
-    if ! mv -i "$zig_build_runner" "$zig_build_runner_bkp"; then
-        return 2;
-    fi;
-    if ! ln -s "$zl_build_runner" "$zig_build_runner"; then
-        return 2;
-    fi;
-    if test -f "$zl_zig_build"; then
-        sed -i 's/pub const build = if (true)/pub const build = if (false)/' "$zl_zig_build";
-    fi;
-    echo "zl"
-else
-    echo $error "'$zig_build_runner': no such file or directory; did nothing"
-fi;
+}
+fn;
