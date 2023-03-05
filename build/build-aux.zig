bcca0b5b327afe239cea3775efa9c196dd706edc
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-05 21:52:12 +0000

    Removed everything from standard build program except `generate_build`.

--- build/build-aux.zig
+++ build/build-aux.zig
@@ -1,47 +1,8 @@
 const std = @import("std");
 const build = std.build;
-
 const util = @import("./util.zig");
-const small = .{ .build_mode = .ReleaseSmall };
 
 pub fn main(builder: *build.Builder) !void {
     util.Context.init(builder);
-    // Minor test programs:
-    _ = util.addProjectExecutable(builder, "builtin_test", "top/builtin-test.zig", .{ .build_root = true, .runtime_assertions = true, .is_verbose = true });
-    _ = util.addProjectExecutable(builder, "meta_test", "top/meta-test.zig", .{ .runtime_assertions = true, .is_verbose = true });
-    _ = util.addProjectExecutable(builder, "mem_test", "top/mem-test.zig", .{ .runtime_assertions = true, .is_verbose = true, .strip = true });
-    _ = util.addProjectExecutable(builder, "algo_test", "top/algo-test.zig", .{ .build_mode = .ReleaseFast, .runtime_assertions = true, .is_verbose = true });
-    _ = util.addProjectExecutable(builder, "file_test", "top/file-test.zig", .{ .runtime_assertions = true, .is_verbose = true });
-    _ = util.addProjectExecutable(builder, "list_test", "top/list-test.zig", .{ .runtime_assertions = true, .is_verbose = true });
-    _ = util.addProjectExecutable(builder, "fmt_test", "top/fmt-test.zig", .{ .build_mode = .Debug, .runtime_assertions = true, .is_verbose = true });
-    _ = util.addProjectExecutable(builder, "render_test", "top/render-test.zig", .{ .runtime_assertions = true, .is_verbose = true });
-    _ = util.addProjectExecutable(builder, "parse_test", "top/parse-test.zig", .{ .runtime_assertions = true, .is_verbose = true, .is_test = false });
-    _ = util.addProjectExecutable(builder, "proc_test", "top/proc-test.zig", .{ .runtime_assertions = true, .is_verbose = true, .is_test = false });
-    _ = util.addProjectExecutable(builder, "thread_test", "top/thread-test.zig", .{ .is_test = true, .build_root = true });
-    _ = util.addProjectExecutable(builder, "virtual_test", "top/virtual-test.zig", .{ .runtime_assertions = true, .is_verbose = true });
-    // More complete test programs:
-    _ = util.addProjectExecutable(builder, "mca", "test/mca.zig", .{ .build_mode = .ReleaseFast, .runtime_assertions = false, .is_verbose = false });
-    _ = util.addProjectExecutable(builder, "treez", "test/treez.zig", .{ .build_mode = .ReleaseSmall, .runtime_assertions = false, .is_verbose = false });
-    _ = util.addProjectExecutable(builder, "itos", "test/itos.zig", .{ .build_mode = .ReleaseSmall, .runtime_assertions = false, .is_verbose = false });
-    _ = util.addProjectExecutable(builder, "cat", "test/cat.zig", .{ .build_mode = .ReleaseFast, .runtime_assertions = false, .is_verbose = false });
-    _ = util.addProjectExecutable(builder, "hello", "test/hello.zig", .{ .build_mode = .ReleaseSmall, .runtime_assertions = false, .is_verbose = false });
-    _ = util.addProjectExecutable(builder, "readelf", "test/readelf.zig", .{ .build_root = true });
-    _ = util.addProjectExecutable(builder, "parsedir", "test/parsedir.zig", .{ .build_mode = .ReleaseFast, .build_root = true });
-    _ = util.addProjectExecutable(builder, "pathsplit", "test/pathsplit.zig", .{ .build_root = true });
-    _ = util.addProjectExecutable(builder, "size_per_config", "test/size_per_config.zig", .{ .build_root = true });
-    _ = util.addProjectExecutable(builder, "print_all", "test/print_all_decls.zig", .{ .is_large_test = true, .build_root = true });
-    // Other test programs:
-    _ = util.addProjectExecutable(builder, "impl_test", "top/impl-test.zig", .{ .is_large_test = true, .build_root = true });
-    _ = util.addProjectExecutable(builder, "build_test", "top/build-test.zig", .{ .build_root = true, .build_mode = .Debug });
-    _ = util.addProjectExecutable(builder, "container_test", "top/container-test.zig", .{ .is_large_test = true, .build_root = true });
-    // Examples
-    _ = util.addProjectExecutable(builder, "readdir", "examples/iterate_dir_entries.zig", small);
-    _ = util.addProjectExecutable(builder, "dynamic", "examples/dynamic_alloc.zig", .{});
-    _ = util.addProjectExecutable(builder, "sortfile", "examples/sortfile.zig", .{ .build_mode = .ReleaseFast });
-    _ = util.addProjectExecutable(builder, "allocators", "examples/allocators.zig", .{ .build_mode = .ReleaseSmall });
-    // Generators:
     _ = util.addProjectExecutable(builder, "generate_build", "top/build/generate_build.zig", .{ .build_mode = .ReleaseSmall });
-}
-inline fn dependOn(dependant: *build.CompileStep, dependency: *build.CompileStep) void {
-    dependant.step.dependOn(&dependency.run().step);
 }
