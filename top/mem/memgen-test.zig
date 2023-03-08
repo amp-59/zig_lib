6de68b9c3ac5f1116a010233c5d1d0976b08c082
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-08 19:05:17 +0000

    Added boilerplate.

--- top/mem/memgen-test.zig
+++ top/mem/memgen-test.zig
@@ -1,2 +1,9 @@
+const gen = @import("./gen.zig");
+const proc = gen.proc;
+
 const containers = @import("./containers.zig");
+const references = @import("./references.zig");
+
+pub usingnamespace proc.start;
+
 pub fn main() void {}
