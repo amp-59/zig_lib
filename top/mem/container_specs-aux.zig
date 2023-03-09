0d197611267ec1bff445b131177ee09932734f6f
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-09 04:24:09 +0000

    * Refactored `Array` parameter. * Added `writeImplementation` function. * Updated usage of `TypeDescrformat`.

--- top/mem/container_specs-aux.zig
+++ top/mem/container_specs-aux.zig
@@ -12,10 +12,12 @@ const attr = @import("./attr.zig");
 const detail = @import("./detail.zig");
 const config = @import("./config.zig");
 const out = struct {
+    usingnamespace @import("./zig-out/src/type_specs.zig");
     usingnamespace @import("./zig-out/src/options.zig");
     usingnamespace @import("./zig-out/src/type_descrs.zig");
     usingnamespace @import("./zig-out/src/impl_variants.zig");
     usingnamespace @import("./zig-out/src/containers.zig");
+    usingnamespace @import("./zig-out/src/specifications.zig");
 };
 pub usingnamespace proc.start;
 
@@ -44,132 +46,166 @@ const Array = struct {
     params: String,
     indices: StringArray,
 };
-fn writeOptionsInternal(array: *Array, field_name: []const u8, usage: attr.Option.Usage, field_names: []const []const u8) void {
+fn writeOptionsInternal(options: *String, field_name: []const u8, usage: attr.Option.Usage, field_names: []const []const u8) void {
     switch (usage) {
         .test_boolean => {
-            array.options.writeMany(field_names[0]);
-            array.options.writeMany(":bool,\n");
+            options.writeMany(field_names[0]);
+            options.writeMany(":bool,\n");
         },
         .compare_optional_enumeration => {
-            array.options.writeMany(field_name);
-            array.options.writeMany(":?enum{\n");
+            options.writeMany(field_name);
+            options.writeMany(":?enum{\n");
             for (field_names) |name| {
-                array.options.writeMany(name[0 .. name.len - (field_name.len + 1)]);
-                array.options.writeMany(",\n");
+                options.writeMany(name[0 .. name.len - (field_name.len + 1)]);
+                options.writeMany(",\n");
             }
-            array.options.undefine(1);
-            array.options.overwriteManyBack("}");
-            array.options.writeMany(",\n");
+            options.undefine(1);
+            options.overwriteManyBack("}");
+            options.writeMany(",\n");
         },
         .compare_enumeration => {
-            array.options.writeMany(field_name);
-            array.options.writeMany(":enum{\n");
+            options.writeMany(field_name);
+            options.writeMany(":enum{\n");
             for (field_names) |name| {
-                array.options.writeMany(name[0 .. name.len - (field_name.len + 1)]);
-                array.options.writeMany(",\n");
+                options.writeMany(name[0 .. name.len - (field_name.len + 1)]);
+                options.writeMany(",\n");
             }
-            array.options.undefine(1);
-            array.options.overwriteManyBack("}");
-            array.options.writeMany(",\n");
+            options.undefine(1);
+            options.overwriteManyBack("}");
+            options.writeMany(",\n");
         },
         .eliminate_boolean_false => {
             if (config.show_eliminated_options) {
-                array.options.writeMany("comptime ");
-                array.options.writeMany(field_name);
-                array.options.writeMany(":bool=true,\n");
+                options.writeMany("comptime ");
+                options.writeMany(field_name);
+                options.writeMany(":bool=true,\n");
             }
         },
         .eliminate_boolean_true => {
             if (config.show_eliminated_options) {
-                array.options.writeMany("comptime ");
-                array.options.writeMany(field_name);
-                array.options.writeMany(":bool=false,\n");
+                options.writeMany("comptime ");
+                options.writeMany(field_name);
+                options.writeMany(":bool=false,\n");
             }
         },
     }
 }
-fn writeOptions(array: *Array, toplevel_impl_group: []const detail.More) ?u64 {
-    const no_decl_len: u64 = array.options.len();
-    array.options.writeMany("const Options");
-    array.options.writeFormat(fmt.ud64(array.indices.len()));
-    array.options.writeMany("=struct{");
-    const no_fields_len: u64 = array.options.len();
+fn writeOptions(options: *String, indices: *StringArray, toplevel_impl_group: []const detail.More) ?u64 {
+    const no_decl_len: u64 = options.len();
+    options.writeMany("const Options");
+    options.writeFormat(fmt.ud64(indices.len()));
+    options.writeMany("=struct{");
+    const no_fields_len: u64 = options.len();
     inline for (out.options) |option| {
         var buf: [option.len()][]const u8 = undefined;
         writeOptionsInternal(
-            array,
+            options,
             option.info.field_name,
             option.usage(detail.More, toplevel_impl_group),
             option.names(detail.More, toplevel_impl_group, &buf),
         );
     }
-    const fields_len: u64 = array.options.len() - no_fields_len;
+    const fields_len: u64 = options.len() - no_fields_len;
     if (fields_len == 0) {
-        array.options.undefine(array.options.len() - no_decl_len);
+        options.undefine(options.len() - no_decl_len);
         return null;
     }
-    if (writeOneUniqueOptionsStruct(array, array.options.readManyAt(no_fields_len)[0..fields_len])) |dupe_index| {
-        array.options.undefine(array.options.len() - no_decl_len);
+    if (writeOneUniqueOptionsStruct(indices, options.readManyAt(no_fields_len)[0..fields_len])) |dupe_index| {
+        options.undefine(options.len() - no_decl_len);
         return dupe_index;
     }
-    array.options.writeMany("};\n");
-    return array.indices.len() - 1;
+    options.writeMany("};\n");
+    return indices.len() - 1;
 }
-fn writeOneUniqueOptionsStruct(array: *Array, new: []const u8) ?u64 {
-    for (array.indices.readAll(), 0..) |unique, index| {
+fn writeOneUniqueOptionsStruct(indices: *StringArray, new: []const u8) ?u64 {
+    for (indices.readAll(), 0..) |unique, index| {
         if (mem.testEqualMany(u8, unique, new)) {
             return index;
         }
     } else {
-        array.indices.writeOne(new);
+        indices.writeOne(new);
     }
     return null;
 }
-fn writeContainerGroup(allocator: *Allocator, array: *Array, ctn_group: []const out.Index) void {
-    array.params.writeMany("pub const ");
-    out.impl_variants[ctn_group[0]].writeContainerName(&array.params);
-    array.params.writeMany("Spec=struct{\n");
-    for (out.type_descrs[out.impl_variants[ctn_group[0]].index].params.type_decl.Composition[1]) |field| {
-        gen.writeField(&array.params, field[0], field[1]);
+// fn Implementation(spec: Specification) type {
+//     const child: type = spec.child;
+//     const count: u64 = spec.count;
+//     const low_alignment: u64 = lowAlignment(spec);
+//     const options: Options0 = spec.options;
+//     if (spec.sentinel) |sentinel| {
+//         reference.Specification5.Implementation(.{
+//             .child = child,
+//             .sentinel = sentinel,
+//             .count = count,
+//             .low_alignment = low_alignment,
+//         }, options);
+//     } else {
+//         reference.Specification4.Implementation(.{
+//             .child = child,
+//             .count = count,
+//             .low_alignment = low_alignment,
+//         }, options);
+//     }
+// }
+fn writeImplementation(params: *String, ctn_index: u64, spec_index: *u64) void {
+    params.writeMany("fn Implementation(spec:Specification)type{\n");
+    for (out.specifications[ctn_index]) |spec_group| {
+        if (spec_group.len != 0) {
+            params.writeMany("reference.Specification");
+            params.writeFormat(fmt.ud64(spec_index.*));
+            params.writeMany(".Implementation(spec, spec.options);\n");
+        }
+        spec_index.* +%= 1;
     }
+    params.writeMany("}\n");
+}
+fn writeContainerGroup(allocator: *Allocator, options: *String, params: *String, indices: *StringArray, ctn_index: u64, spec_index: *u64) void {
+    const ctn_group: []const out.Index = out.containers[ctn_index];
+    params.writeMany("pub const ");
+    out.impl_variants[ctn_group[0]].writeContainerName(params);
+    params.writeMany("Spec=struct{\n");
+
+    for (out.type_descrs[out.impl_variants[ctn_group[0]].index].params.type_decl.Composition.fields) |field| {
+        gen.writeField(params, field.name, field.type);
+    }
+
     const buf: []detail.More = allocator.allocateIrreversible(detail.More, ctn_group.len);
     var impl_index: u16 = 0;
     while (impl_index != ctn_group.len) : (impl_index +%= 1) {
         buf[impl_index] = out.impl_variants[ctn_group[impl_index]];
     }
-    if (writeOptions(array, buf)) |decl_idx| {
-        array.params.writeMany("options:Options");
-        array.params.writeFormat(fmt.ud64(decl_idx));
-        array.params.writeMany(",");
+    if (writeOptions(options, indices, buf)) |decl_idx| {
+        params.writeMany("options:Options");
+        params.writeFormat(fmt.ud64(decl_idx));
+        params.writeMany(",");
     }
-    array.params.writeMany("const Specification=@This();\n");
-    array.params.writeMany("};\n");
+    params.writeMany("const Specification=@This();\n");
+    writeImplementation(params, ctn_index, spec_index);
+    params.writeMany("};\n");
 }
 fn generateParameters() !void {
     var address_space: AddressSpace = .{};
     var allocator: Allocator = try Allocator.init(&address_space);
-    var array: Array = .{
-        .options = String.init(&allocator, 1024 * 1024),
-        .params = String.init(&allocator, 1024 * 1024),
-        .indices = StringArray.init(&allocator, 512),
-    };
-    array.options.undefineAll();
-    array.params.undefineAll();
-    gen.writeImport(&array.options, "mach", "../mach.zig");
-    gen.writeImport(&array.options, "meta", "../meta.zig");
-    gen.writeImport(&array.options, "builtin", "../builtin.zig");
-    gen.writeImport(&array.options, "reference", "references.zig");
-    gen.copySourceFile(&array.params, "container-template.zig");
-    array.options.writeMany(gen.subTemplate(array.params.readAllWithSentinel(0), "container-template.zig").?);
-    array.params.undefineAll();
+    var options: String = String.init(&allocator, 1024 * 1024);
+    var params: String = String.init(&allocator, 1024 * 1024);
+    var indices: StringArray = StringArray.init(&allocator, 512);
+    options.undefineAll();
+    params.undefineAll();
+    gen.writeImport(&options, "mach", "../mach.zig");
+    gen.writeImport(&options, "meta", "../meta.zig");
+    gen.writeImport(&options, "builtin", "../builtin.zig");
+    gen.writeImport(&options, "reference", "references.zig");
+    gen.copySourceFile(&params, "container-template.zig");
+    options.writeMany(gen.subTemplate(params.readAllWithSentinel(0), "container-template.zig").?);
+    params.undefineAll();
+    var spec_index: u64 = 0;
     var ctn_index: u64 = 0;
     while (ctn_index != out.containers.len) : (ctn_index +%= 1) {
         const save: Allocator.Save = allocator.save();
         defer allocator.restore(save);
-        const ctn_group: []const out.Index = out.containers[ctn_index];
-        writeContainerGroup(&allocator, &array, ctn_group);
+        writeContainerGroup(&allocator, &options, &params, &indices, ctn_index, &spec_index);
     }
-    gen.writeSourceFile(&array.options, "containers.zig");
-    gen.appendSourceFile(&array.params, "containers.zig");
+    gen.writeSourceFile(&options, "containers.zig");
+    gen.appendSourceFile(&params, "containers.zig");
 }
 pub const main = generateParameters;
