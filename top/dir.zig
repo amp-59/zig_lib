8971f00aede52b626bbec4fd9b48677e085a2ae9
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-09 20:00:52 +0000

    Moved `List` outside of `GenericDirStream`.

--- top/dir.zig
+++ top/dir.zig
@@ -22,7 +22,7 @@ pub const DirStreamOptions = struct {
     init_read_all: bool = true,
     shrink_after_read: bool = true,
     make_list: bool = true,
-    close_when_done: bool = true,
+    close_on_deinit: bool = true,
 };
 pub const DirStreamLogging = struct {
     open: builtin.Logging.AcquireErrorFault = .{},
@@ -91,129 +91,25 @@ pub fn GenericDirStream(comptime spec: DirStreamSpec) type {
                 return dirent.kind == .socket;
             }
         };
-        const List = opaque {
-            const name_offset: u8 = @offsetOf(file.DirectoryEntry, "array");
-            const reclen_offset: u8 = @offsetOf(file.DirectoryEntry, "reclen");
-            const offset_offset: u8 = @offsetOf(file.DirectoryEntry, "offset");
-            // Often the dot and dot-dot directory entries will not be the first
-            // and second entries, and the addresses of the head and sentinel
-            // nodes of our mangled list must be known--otherwise we would have
-            // to store more metadata in the DirStream object.
-            fn shiftBothBlocks(s_lb_addr: u64, major_src_addr: u64, minor_src_addr: u64) void {
-                disordered += 1;
-                const major_save: [24]u8 = @intToPtr(*const [24]u8, major_src_addr).*;
-                const major_dst_addr: u64 = s_lb_addr;
-                const minor_save: [24]u8 = @intToPtr(*const [24]u8, minor_src_addr).*;
-                const minor_dst_addr: u64 = s_lb_addr + 24;
-                const upper_src_addr: u64 = @max(major_src_addr, minor_src_addr);
-                const lower_src_addr: u64 = @min(major_src_addr, minor_src_addr);
-                var t_lb_addr: u64 = upper_src_addr;
-                while (t_lb_addr != lower_src_addr + 24) {
-                    t_lb_addr -= 8;
-                    @intToPtr(*u64, t_lb_addr + 24).* = @intToPtr(*u64, t_lb_addr).*;
-                }
-                t_lb_addr = lower_src_addr;
-                while (t_lb_addr != major_dst_addr) {
-                    t_lb_addr -= 8;
-                    @intToPtr(*u64, t_lb_addr + 48).* = @intToPtr(*u64, t_lb_addr).*;
-                }
-                @intToPtr(*[24]u8, major_dst_addr).* = major_save;
-                @intToPtr(*[24]u8, minor_dst_addr).* = minor_save;
-            }
-            // 11780 = [2]u8{ 4, 46 }, i.e. "d."
-            // 46    = [2]u8{ 46, 0 }, i.e. ".\x00"
-            // 1070  = [2]u8{ 46, 4 }
-            // 11776 = [2]u8{ 0, 46 }
-            // This method is about 25% faster than testing if starts with ".\0"
-            // and "..\0", and cmoving the relevant index. I suppose this
-            // respects endian, but have never tested it.
-            fn classifyNameExperimental(addrs: *[3]u64, s_lb_addr: u64) void {
-                const w0: u16 = @intToPtr(*const u16, s_lb_addr + 18).*;
-                const w1: u16 = @intToPtr(*const u16, s_lb_addr + 20).*;
-                const j0: u8 = @boolToInt(w0 == if (builtin.is_little) 11780 else 1070);
-                const j1: u8 = j0 << @boolToInt(w1 == if (builtin.is_little) 46 else 11776);
-                const j2: u8 = j1 & (@as(u8, 1) << @boolToInt(w1 != 0));
-                addrs[j2] = s_lb_addr;
-            }
-            fn mangle(s_lb_addr: u64) void {
-                const len: u16 = @intToPtr(*u16, s_lb_addr + 16).*;
-                const a_0: u64 = (s_lb_addr + len) -% 8;
-                const a_1: u16 = 64 -% @clz(@intToPtr(*const u64, a_0).*);
-                const a_2: u16 = (len + (1 + (a_1 / 8))) - (name_offset + 8);
-                const a_3: *u8 = @intToPtr(*u8, s_lb_addr + name_offset + (a_2 - 1));
-                const name_len: u16 = a_2 -% @boolToInt(a_3.* == 0);
-                @intToPtr(*u8, s_lb_addr + name_offset + name_len).* = 0;
-                @intToPtr(*u16, s_lb_addr + reclen_offset).* = name_len;
-                @intToPtr(*u64, s_lb_addr + offset_offset).* = 0;
-            }
-            fn rectifyEntryOrder(s_lb_addr: u64) u64 {
-                var addrs: [3]u64 = .{0} ** 3;
-                var t_lb_addr: u64 = s_lb_addr;
-                classifyNameExperimental(&addrs, t_lb_addr);
-                t_lb_addr = nextAddress(t_lb_addr);
-                classifyNameExperimental(&addrs, t_lb_addr);
-                while (addrs[1] *% addrs[2] == 0) : (t_lb_addr = nextAddress(t_lb_addr)) {
-                    classifyNameExperimental(&addrs, t_lb_addr);
-                }
-                if (!(builtin.int2a(bool, addrs[1] == s_lb_addr, addrs[2] == s_lb_addr + 24))) {
-                    shiftBothBlocks(s_lb_addr, addrs[1], addrs[2]);
-                }
-                return s_lb_addr;
-            }
-            /// Converts linux directory stream to a linked list without moving
-            /// or copying. '..' directory sacrificed to make room for the list
-            /// sentinel node. Do not touch.
-            pub fn interleaveXorListNodes(dir: DirStream) u64 {
-                const s_lb_addr: u64 = rectifyEntryOrder(dir.blk.aligned_byte_address());
-                const s_up_addr: u64 = dir.blk.undefined_byte_address();
-                const t_node_addr: u64 = nextAddress(s_lb_addr);
-                var s_node_addr: u64 = s_lb_addr;
-                var p_node_addr: u64 = 0;
-                var i_node_addr: u64 = nextAddress(t_node_addr);
-                mangle(s_node_addr);
-                ListView.Node.Link.mutate(s_node_addr, 0, t_node_addr);
-                ListView.Node.Link.mutate(t_node_addr, s_node_addr, 0);
-                mangle(t_node_addr);
-                var count: u64 = 1;
-                while (i_node_addr < s_up_addr) : (count += 1) {
-                    ListView.Node.Link.mutate(s_node_addr, p_node_addr, i_node_addr);
-                    ListView.Node.Link.mutate(i_node_addr, s_node_addr, t_node_addr);
-                    ListView.Node.Link.mutate(t_node_addr, i_node_addr, 0);
-                    p_node_addr = s_node_addr;
-                    s_node_addr = i_node_addr;
-                    i_node_addr = nextAddress(i_node_addr);
-                    mangle(s_node_addr);
-                }
-                return count;
-            }
-            pub fn nextAddress(s_lb_addr: u64) u64 {
-                return s_lb_addr + @intToPtr(*u16, s_lb_addr + reclen_offset).*;
-            }
-        };
+        fn links(blk: Block) ListView.Links {
+            return .{
+                .major = blk.aligned_byte_address(),
+                .minor = blk.aligned_byte_address() + 48,
+            };
+        }
         pub const dir_spec: DirStreamSpec = spec;
         const dir_open_spec: file.OpenSpec = .{
-            .options = .{
-                .write = null,
-                .directory = true,
-                .read = true,
-            },
+            .options = .{ .write = null, .directory = true, .read = true },
+            .errors = dir_spec.errors.open,
             .logging = dir_spec.logging.open,
         };
         const dir_close_spec: file.CloseSpec = .{
-            .errors = .{},
+            .errors = dir_spec.errors.close,
             .logging = dir_spec.logging.close,
         };
-        pub var disordered: u64 = 0;
 
         pub fn list(dir: *DirStream) ListView {
-            return .{
-                .links = .{
-                    .major = dir.blk.aligned_byte_address(),
-                    .minor = dir.blk.aligned_byte_address() + 48,
-                },
-                .count = dir.count,
-                .index = 0,
-            };
+            return .{ .links = links(dir.blk), .count = dir.count, .index = 0 };
         }
         fn clear(s_lb_addr: u64, s_bytes: u64) void {
             mem.set(s_lb_addr, @as(u8, 0), s_bytes);
@@ -255,7 +151,11 @@ pub fn GenericDirStream(comptime spec: DirStreamSpec) type {
                 try ret.readAll(allocator);
             }
             if (dir_spec.options.make_list) {
-                ret.count = List.interleaveXorListNodes(ret);
+                ret.count = List.interleaveXorListNodes(
+                    ListView.Node,
+                    ret.blk.aligned_byte_address(),
+                    ret.blk.undefined_byte_address(),
+                );
             }
             return ret;
         }
@@ -268,7 +168,11 @@ pub fn GenericDirStream(comptime spec: DirStreamSpec) type {
                 try ret.readAll(allocator);
             }
             if (dir_spec.options.make_list) {
-                ret.count = List.interleaveXorListNodes(ret);
+                ret.count = List.interleaveXorListNodes(
+                    ListView.Node,
+                    ret.blk.aligned_byte_address(),
+                    ret.blk.undefined_byte_address(),
+                );
             }
             return ret;
         }
@@ -280,5 +184,106 @@ pub fn GenericDirStream(comptime spec: DirStreamSpec) type {
                 dir.fd = 0;
             }
         }
+        comptime {
+            if (dir_spec.options.make_list) {
+                builtin.static.assert(dir_spec.options.init_read_all);
+            }
+        }
     });
 }
+const List = opaque {
+    const name_offset: u8 = @offsetOf(file.DirectoryEntry, "array");
+    const reclen_offset: u8 = @offsetOf(file.DirectoryEntry, "reclen");
+    const offset_offset: u8 = @offsetOf(file.DirectoryEntry, "offset");
+    // Often the dot and dot-dot directory entries will not be the first
+    // and second entries, and the addresses of the head and sentinel
+    // nodes of our mangled list must be known--otherwise we would have
+    // to store more metadata in the DirStream object.
+    fn shiftBothBlocks(s_lb_addr: u64, major_src_addr: u64, minor_src_addr: u64) void {
+        const major_save: [24]u8 = @intToPtr(*const [24]u8, major_src_addr).*;
+        const major_dst_addr: u64 = s_lb_addr;
+        const minor_save: [24]u8 = @intToPtr(*const [24]u8, minor_src_addr).*;
+        const minor_dst_addr: u64 = s_lb_addr + 24;
+        const upper_src_addr: u64 = @max(major_src_addr, minor_src_addr);
+        const lower_src_addr: u64 = @min(major_src_addr, minor_src_addr);
+        var t_lb_addr: u64 = upper_src_addr;
+        while (t_lb_addr != lower_src_addr + 24) {
+            t_lb_addr -= 8;
+            @intToPtr(*u64, t_lb_addr + 24).* = @intToPtr(*u64, t_lb_addr).*;
+        }
+        t_lb_addr = lower_src_addr;
+        while (t_lb_addr != major_dst_addr) {
+            t_lb_addr -= 8;
+            @intToPtr(*u64, t_lb_addr + 48).* = @intToPtr(*u64, t_lb_addr).*;
+        }
+        @intToPtr(*[24]u8, major_dst_addr).* = major_save;
+        @intToPtr(*[24]u8, minor_dst_addr).* = minor_save;
+    }
+    // 11780 = [2]u8{ 4, 46 }, i.e. "d."
+    // 46    = [2]u8{ 46, 0 }, i.e. ".\x00"
+    // 1070  = [2]u8{ 46, 4 }
+    // 11776 = [2]u8{ 0, 46 }
+    // This method is about 25% faster than testing if starts with ".\0"
+    // and "..\0", and cmoving the relevant index. I suppose this
+    // respects endian, but have never tested it.
+    fn classifyNameExperimental(addrs: *[3]u64, s_lb_addr: u64) void {
+        const w0: u16 = @intToPtr(*const u16, s_lb_addr + 18).*;
+        const w1: u16 = @intToPtr(*const u16, s_lb_addr + 20).*;
+        const j0: u8 = @boolToInt(w0 == if (builtin.is_little) 11780 else 1070);
+        const j1: u8 = j0 << @boolToInt(w1 == if (builtin.is_little) 46 else 11776);
+        const j2: u8 = j1 & (@as(u8, 1) << @boolToInt(w1 != 0));
+        addrs[j2] = s_lb_addr;
+    }
+    fn mangle(s_lb_addr: u64) void {
+        const len: u16 = @intToPtr(*u16, s_lb_addr + 16).*;
+        const a_0: u64 = (s_lb_addr + len) -% 8;
+        const a_1: u16 = 64 -% @clz(@intToPtr(*const u64, a_0).*);
+        const a_2: u16 = (len + (1 + (a_1 / 8))) - (name_offset + 8);
+        const a_3: *u8 = @intToPtr(*u8, s_lb_addr + name_offset + (a_2 - 1));
+        const name_len: u16 = a_2 -% @boolToInt(a_3.* == 0);
+        @intToPtr(*u8, s_lb_addr + name_offset + name_len).* = 0;
+        @intToPtr(*u16, s_lb_addr + reclen_offset).* = name_len;
+        @intToPtr(*u64, s_lb_addr + offset_offset).* = 0;
+    }
+    fn rectifyEntryOrder(s_lb_addr: u64) void {
+        var addrs: [3]u64 = .{0} ** 3;
+        var t_lb_addr: u64 = s_lb_addr;
+        classifyNameExperimental(&addrs, t_lb_addr);
+        t_lb_addr = nextAddress(t_lb_addr);
+        classifyNameExperimental(&addrs, t_lb_addr);
+        while (addrs[1] *% addrs[2] == 0) : (t_lb_addr = nextAddress(t_lb_addr)) {
+            classifyNameExperimental(&addrs, t_lb_addr);
+        }
+        if (!(builtin.int2a(bool, addrs[1] == s_lb_addr, addrs[2] == s_lb_addr + 24))) {
+            shiftBothBlocks(s_lb_addr, addrs[1], addrs[2]);
+        }
+    }
+    /// Converts linux directory stream to a linked list without moving
+    /// or copying. '..' directory sacrificed to make room for the list
+    /// sentinel node. Do not touch.
+    pub fn interleaveXorListNodes(comptime Node: type, s_lb_addr: u64, s_up_addr: u64) u64 {
+        rectifyEntryOrder(s_lb_addr);
+        const t_node_addr: u64 = nextAddress(s_lb_addr);
+        var s_node_addr: u64 = s_lb_addr;
+        var p_node_addr: u64 = 0;
+        var i_node_addr: u64 = nextAddress(t_node_addr);
+        mangle(s_node_addr);
+        Node.Link.mutate(s_node_addr, 0, t_node_addr);
+        Node.Link.mutate(t_node_addr, s_node_addr, 0);
+        mangle(t_node_addr);
+        var count: u64 = 1;
+        while (i_node_addr < s_up_addr) : (count += 1) {
+            Node.Link.mutate(s_node_addr, p_node_addr, i_node_addr);
+            Node.Link.mutate(i_node_addr, s_node_addr, t_node_addr);
+            Node.Link.mutate(t_node_addr, i_node_addr, 0);
+            p_node_addr = s_node_addr;
+            s_node_addr = i_node_addr;
+            i_node_addr = nextAddress(i_node_addr);
+            mangle(s_node_addr);
+        }
+        return count;
+    }
+    pub fn nextAddress(s_lb_addr: u64) u64 {
+        return s_lb_addr + @intToPtr(*u16, s_lb_addr + reclen_offset).*;
+    }
+};
