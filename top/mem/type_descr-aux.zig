97b70ef4034abc4b78040dcde9833f0d922e4d2b
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-10 12:06:36 +0000

    Updated output usage of `TypeDescrFormat` types.

--- top/mem/type_descr-aux.zig
+++ top/mem/type_descr-aux.zig
@@ -14,34 +14,32 @@ pub usingnamespace proc.start;
 pub const logging_override: builtin.Logging.Override = preset.logging.override.silent;
 
 const Array = mem.StaticArray(u8, 1024 * 1024);
-
-const TypeDescrFormat = fmt.GenericTypeDescrFormat(.{ .options = .{ .default_field_values = true } });
 const fmt_spec: fmt.RenderSpec = .{ .infer_type_names = true, .ignore_formatter_decls = true };
 
 fn mapContainersToParameters() void {
     var array: Array = undefined;
     array.undefineAll();
     gen.writeImport(&array, "gen", "../../gen.zig");
-    array.writeMany("pub const TypeDescrFormat=gen.fmt.GenericTypeDescrFormat(.{.options=.{.default_field_values=true}});\n");
     array.writeMany(
-        \\pub const type_descrs=&[_]struct {
-        \\    params: TypeDescrFormat,
-        \\    specs: []const TypeDescrFormat,
-        \\    vars: TypeDescrFormat,
-        \\}{
+        \\pub const DescrSet = struct {
+        \\    params: gen.TypeDescrFormat,
+        \\    specs: []const gen.TypeDescrFormat,
+        \\    vars: gen.TypeDescrFormat,
+        \\};
     );
+    array.writeMany("pub const type_descrs=&[_]DescrSet{");
     inline for (out.type_specs) |type_spec| {
         array.writeMany(".{.params=");
-        array.writeFormat(fmt.render(fmt_spec, comptime TypeDescrFormat.init(type_spec.params)));
+        array.writeFormat(fmt.render(fmt_spec, comptime gen.ProtoTypeDescrFormat.init(type_spec.params)));
         array.writeMany(",\n");
         array.writeMany(".specs=&.{\n");
         inline for (type_spec.specs) |spec| {
-            array.writeFormat(fmt.render(fmt_spec, comptime TypeDescrFormat.init(spec)));
+            array.writeFormat(fmt.render(fmt_spec, comptime gen.ProtoTypeDescrFormat.init(spec)));
             array.writeMany(",\n");
         }
         array.writeMany("},\n");
         array.writeMany(".vars=");
-        array.writeFormat(fmt.render(fmt_spec, comptime TypeDescrFormat.init(type_spec.vars)));
+        array.writeFormat(fmt.render(fmt_spec, comptime gen.ProtoTypeDescrFormat.init(type_spec.vars)));
         array.writeMany("},\n");
     }
     array.overwriteManyBack("};");
