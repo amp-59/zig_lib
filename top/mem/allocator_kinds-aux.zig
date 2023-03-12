83e2288153f63878c2bcf5f90e5fc2bc3f2da07e
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-12 19:21:28 +0000

    Added stage for generating kind switch functions for allocator functions.

---
+++ top/mem/allocator_kinds-aux.zig
@@ -0,0 +1,32 @@
+const gen = @import("./gen.zig");
+const mem = gen.mem;
+const proc = gen.proc;
+const algo = gen.algo;
+const preset = gen.preset;
+const builtin = gen.builtin;
+const attr = @import("./attr.zig");
+const alloc_fn = @import("./alloc_fn.zig");
+
+pub usingnamespace proc.start;
+pub const logging_override: builtin.Logging.Override = preset.logging.override.silent;
+
+const Array = mem.StaticString(1024 * 1024);
+pub fn main() void {
+    var array: Array = undefined;
+    array.undefineAll();
+    gen.writeImport(&array, "alloc_fn", "../../alloc_fn.zig");
+    const writeKind = attr.Fn.static.writeKindSwitch;
+
+    const Pair = attr.Fn.static.Pair(alloc_fn.Fn);
+    const allocate: Pair = attr.Fn.static.prefixSubTagNew(alloc_fn.Fn, .allocate);
+    const deallocate: Pair = attr.Fn.static.prefixSubTag(alloc_fn.Fn, allocate[0], .deallocate);
+    const resize: Pair = attr.Fn.static.prefixSubTag(alloc_fn.Fn, deallocate[0], .resize);
+    const reallocate: Pair = attr.Fn.static.prefixSubTag(alloc_fn.Fn, resize[0], .reallocate);
+
+    writeKind(alloc_fn.Fn, &array, .allocate, allocate[1]);
+    writeKind(alloc_fn.Fn, &array, .deallocate, deallocate[1]);
+    writeKind(alloc_fn.Fn, &array, .reallocate, reallocate[1]);
+    writeKind(alloc_fn.Fn, &array, .resize, resize[1]);
+
+    gen.writeAuxiliarySourceFile(&array, "allocator_kinds.zig");
+}
