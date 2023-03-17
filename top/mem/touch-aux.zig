eb006df51c04c07298d8e6a238ebccd2371b703a
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-17 17:16:06 +0000

    Updated usage of `writeSourceFile`.

--- top/mem/touch-aux.zig
+++ top/mem/touch-aux.zig
@@ -1,12 +1,18 @@
 const gen = @import("./gen.zig");
 const mem = gen.mem;
 const proc = gen.proc;
+const preset = gen.preset;
+const builtin = gen.builtin;
+
+pub usingnamespace proc.start;
+
+pub const logging_override: builtin.Logging.Override = preset.logging.override.silent;
+
 const Array = mem.StaticString(0);
+
 pub fn main() void {
-    var array: Array = undefined;
-    array.undefineAll();
-    gen.writeSourceFile(&array, "containers.zig");
-    gen.writeSourceFile(&array, "references.zig");
-    gen.writeAuxiliarySourceFile(&array, "container_kinds.zig");
-    gen.writeAuxiliarySourceFile(&array, "reference_kinds.zig");
+    gen.writeSourceFile("containers.zig", u8, &.{});
+    gen.writeSourceFile("references.zig", u8, &.{});
+    gen.writeAuxiliarySourceFile("container_kinds.zig", u8, &.{});
+    gen.writeAuxiliarySourceFile("reference_kinds.zig", u8, &.{});
 }
