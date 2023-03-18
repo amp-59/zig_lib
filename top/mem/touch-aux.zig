f4365ca7eda2a4e4d6befe1f1cdf0fe71b35c6d5
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-18 11:48:16 +0000

    Changed calls to `write*` functions to create-and-close.

--- top/mem/touch-aux.zig
+++ top/mem/touch-aux.zig
@@ -1,6 +1,7 @@
 const gen = @import("./gen.zig");
 const mem = gen.mem;
 const proc = gen.proc;
+const file = gen.file;
 const preset = gen.preset;
 const builtin = gen.builtin;
 
@@ -10,9 +11,28 @@ pub const logging_override: builtin.Logging.Override = preset.logging.override.s
 
 const Array = mem.StaticString(0);
 
+const build_root: [:0]const u8 = @cImport({}).build_root;
+
+const mkdir_spec: file.MakeDirSpec = .{
+    .errors = .{},
+};
+const create_spec: file.CreateSpec = .{
+    .errors = .{},
+    .options = .{ .write = .truncate, .exclusive = false },
+};
+const close_spec: file.CloseSpec = .{
+    .errors = .{},
+};
+
 pub fn main() void {
-    gen.writeSourceFile("containers.zig", u8, &.{});
-    gen.writeSourceFile("references.zig", u8, &.{});
-    gen.writeAuxiliarySourceFile("container_kinds.zig", u8, &.{});
-    gen.writeAuxiliarySourceFile("reference_kinds.zig", u8, &.{});
+    const zig_out_dir: [:0]const u8 = build_root ++ "/top/mem/zig-out";
+    const zig_out_src_dir: [:0]const u8 = zig_out_dir ++ "/src";
+
+    file.makeDir(mkdir_spec, zig_out_dir);
+    file.makeDir(mkdir_spec, zig_out_src_dir);
+
+    file.close(close_spec, file.create(create_spec, gen.primaryFile("containers.zig")));
+    file.close(close_spec, file.create(create_spec, gen.primaryFile("references.zig")));
+    file.close(close_spec, file.create(create_spec, gen.auxiliaryFile("container_kinds.zig")));
+    file.close(close_spec, file.create(create_spec, gen.auxiliaryFile("reference_kinds.zig")));
 }
