4e6217d1077446d500d5e5c0345f02fc6acccf0c
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-19 00:57:54 +0000

    Added test program for `serial` namespace.

---
+++ top/serial-test.zig
@@ -0,0 +1,8 @@
+const proc = @import("./proc.zig");
+const serial = @import("./serial.zig");
+const builtin = @import("./builtin.zig");
+const testing = @import("./testing.zig");
+
+pub usingnamespace proc.start;
+
+pub fn main() void {}
