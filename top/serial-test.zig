97854612d0311755632d8954a1d38488dff1fa1f
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-20 18:42:52 +0000

    Added test function to show the discrepancy between what the serialiser estimates and what the allocator allocates.

--- top/serial-test.zig
+++ top/serial-test.zig
@@ -1,5 +1,6 @@
 const mem = @import("./mem.zig");
 const proc = @import("./proc.zig");
+const meta = @import("./meta.zig");
 const serial = @import("./serial.zig");
 const preset = @import("./preset.zig");
 const builtin = @import("./builtin.zig");
@@ -7,10 +8,15 @@ const testing = @import("./testing.zig");
 
 pub usingnamespace proc.start;
 
+pub const logging_override: builtin.Logging.Override = preset.logging.override.verbose;
+pub const runtime_assertions: bool = true;
+
 const AddressSpace = preset.address_space.regular_128;
 const Allocator = mem.GenericArenaAllocator(.{
     .AddressSpace = AddressSpace,
     .arena_index = 0,
+    .logging = preset.allocator.logging.silent,
+    .errors = preset.allocator.errors.noexcept,
 });
 
 const S = struct {
@@ -31,30 +37,25 @@ pub fn main() !void {
     var address_space: AddressSpace = .{};
     var allocator: Allocator = try Allocator.init(&address_space);
 
-    var ss: S = .{
-        .t = 25,
-        .u = .{
-            .ss = &.{
-                "one,two,three",
-                "four,five,six",
-            },
-        },
-        .s = .{
-            .s = &.{
-                .s0 = "seven,eight,nine",
-                .s1 = "ten,eleven,twelve",
-            },
-        },
-    };
-
-    serial.length(S, ss);
-    serial.length(@TypeOf(ss.s.s), ss.s.s);
-
-    const s2 = serial.write(allocator, S, ss);
-
-    serial.length(S, s2);
-    serial.length(@TypeOf(s2.s.s), s2.s.s);
-
-    const x = allocator.ub_addr - allocator.lb_addr;
-    _ = x;
+    for (0..0x100) |offset| {
+        const s = allocator.save();
+        defer allocator.restore(s);
+        allocator.ub_addr +%= offset;
+        allocator.alignAbove(serial.maxAlignment(S, 0));
+        const start = allocator.ub_addr;
+        var ss: S = .{
+            .t = 25,
+            .s = .{ .s = &.{ .s0 = "seven,eight,nine", .s1 = "ten,eleven,twelve,thirteen" } },
+            .u = .{ .ss = &.{ "one,two,three", "four,five,six" } },
+        };
+        const len_0: u64 = serial.length(S, ss);
+        const s2 = try meta.wrap(serial.write(&allocator, S, ss));
+        const len_1: u64 = serial.length(S, s2);
+        builtin.assertEqual(u64, len_0, len_1);
+        const x = allocator.ub_addr - start;
+        testing.print(.{
+            "len_0:\t", len_0, ", len_1:\t", len_1, ", ",
+            "x:\t",     x,     '\n',
+        });
+    }
 }
