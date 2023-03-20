0bf6d7aa09b046ca8a1575e27e3e40d99adf38d6
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-20 18:41:17 +0000

    Added branch to get out of main.

--- top/mem/memgen-test.zig
+++ top/mem/memgen-test.zig
@@ -1,39 +1,52 @@
 const gen = @import("./gen.zig");
+const mem = gen.mem;
+const fmt = gen.fmt;
 const proc = gen.proc;
+const file = gen.file;
 const meta = gen.meta;
+const preset = gen.preset;
+const serial = gen.serial;
+const builtin = gen.builtin;
 const testing = gen.testing;
 
 const attr = @import("./attr.zig");
 
-const containers = @import("./containers.zig");
-const references = @import("./references.zig");
-
-const details: []const attr.More = meta.bytesToSlice(attr.More, @embedFile("./zig-out/src/detail_raw"));
-
 pub usingnamespace proc.start;
 
-const NoSerial = struct {
-    kind: attr.Kinds.Tag,
-    layout: attr.Layouts.Tag,
-    modes: attr.Modes,
-    fields: attr.Fields,
-    techs: attr.Techniques,
-    specs: attr.Specifiers,
-};
+pub const render_spec = .{ .radix = 16 };
+pub const logging_override: builtin.Logging.Override = preset.logging.override.verbose;
 
-pub fn main() void {
+pub const runtime_assertions: bool = false;
+
+const Allocator = mem.GenericArenaAllocator(.{
+    .AddressSpace = AddressSpace,
+    .arena_index = 0,
+    .logging = preset.allocator.logging.silent,
+    .errors = preset.allocator.errors.noexcept,
+    .options = preset.allocator.options.small,
+});
+const FAllocator = mem.GenericArenaAllocator(.{
+    .AddressSpace = AddressSpace,
+    .arena_index = 1,
+    .logging = preset.allocator.logging.silent,
+    .errors = preset.allocator.errors.noexcept,
+    .options = preset.allocator.options.small,
+});
+const AddressSpace = mem.GenericRegularAddressSpace(.{
+    .lb_offset = 0x40000000,
+    .divisions = 128,
+    .logging = preset.address_space.logging.silent,
+    .errors = preset.address_space.errors.noexcept,
+    .options = .{},
+});
+
+const Array = mem.StaticString(2 * 1024 * 1024);
+pub fn main() !void {
     if (return) {}
-    var no_serial: [details.len]NoSerial = undefined;
+    var address_space: AddressSpace = .{};
+    var fallocator: FAllocator = FAllocator.init(&address_space);
 
-    for (details, 0..) |detail, index| {
-        no_serial[index] = .{
-            .kind = detail.kind,
-            .layout = detail.layout,
-            .modes = detail.modes,
-            .fields = detail.fields,
-            .techs = detail.techs,
-            .specs = detail.specs,
-        };
-    }
-    testing.uniqueSet(NoSerial, &no_serial);
+    @setEvalBranchQuota(3000);
+    const strings = try serial.deserialize3(attr.Specifier, &fallocator, gen.auxiliaryFile("spec_sets"));
+    try serial.serialize3(attr.Specifier, &fallocator, gen.auxiliaryFile("spec_sets"), strings);
 }
