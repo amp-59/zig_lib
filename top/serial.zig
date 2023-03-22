d816927c728d31959739b4ea7fc49c9c7f8d060d
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-22 20:53:20 +0000

    * Added formatting convention for the rest of the file, to give the   argument of `meta.wrap` its own line. As this is not realistic for the   entire project it is likely to be temporary. * Removed unnecessary `meta.wrap` from `allocator.allocate`. * Added `@constCast` in two new places.

--- top/serial.zig
+++ top/serial.zig
@@ -99,95 +99,65 @@ fn readPointerMany(comptime pointer_info: builtin.Type.Pointer, addr: u64, offse
     any.* = next;
     return len;
 }
-pub fn read(addr: u64, offset: u64, any: anytype) u64 {
-    switch (@typeInfo(@TypeOf(any.*))) {
-        .Struct => |struct_info| {
-            return readStruct(struct_info, addr, offset, any);
-        },
-        .Union => |union_info| {
-            return readUnion(union_info, addr, offset, any);
-        },
-        .Pointer => |pointer_info| {
-            if (pointer_info.size == .One) {
-                return readPointerOne(pointer_info, addr, offset, @constCast(any));
-            }
-            if (pointer_info.size == .Slice) {
-                return readPointerSlice(pointer_info, addr, offset, @constCast(any));
-            }
-            if (pointer_info.size == .Many) {
-                return readPointerMany(pointer_info, addr, offset, @constCast(any));
-            }
-        },
-        else => return offset,
-    }
-}
-fn writeStruct(
-    comptime struct_info: builtin.Type.Struct,
-    allocator: anytype,
-    addr: u64,
-    any: anytype,
-) @TypeOf(any) {
+fn writeStruct(comptime struct_info: builtin.Type.Struct, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
     const T: type = @TypeOf(any);
     var ret: T = any;
     inline for (struct_info.fields) |field| {
-        @field(ret, field.name) = try meta.wrap(write(allocator, addr, @field(any, field.name)));
+        @field(ret, field.name) = try meta.wrap(
+            write(allocator, addr, @field(any, field.name)),
+        );
     }
     return ret;
 }
-fn writeUnion(
-    comptime union_info: builtin.Type.Union,
-    allocator: anytype,
-    addr: u64,
-    any: anytype,
-) @TypeOf(any) {
+fn writeUnion(comptime union_info: builtin.Type.Union, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
     const T: type = @TypeOf(any);
     if (union_info.tag_type) |tag_type| {
         inline for (union_info.fields) |field| {
             if (any == @field(tag_type, field.name)) {
-                return @unionInit(T, field.name, try meta.wrap(write(allocator, addr, @field(any, field.name))));
+                return @unionInit(T, field.name, try meta.wrap(
+                    write(allocator, addr, @field(any, field.name)),
+                ));
             }
         }
     }
     return any;
 }
-fn writePointerOne(
-    comptime pointer_info: builtin.Type.Pointer,
-    allocator: anytype,
-    addr: u64,
-    any: anytype,
-) @TypeOf(any) {
-    const ret: @TypeOf(any) = try meta.wrap(allocator.createIrreversible(pointer_info.child));
-    ret.* = try meta.wrap(write(allocator, addr, any.*));
+fn writePointerOne(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
+    const ret: @TypeOf(any) = try meta.wrap(
+        allocator.createIrreversible(pointer_info.child),
+    );
+    ret.* = try meta.wrap(
+        write(allocator, addr, any.*),
+    );
     return toOffset(ret, addr);
 }
-fn writePointerSlice(
-    comptime pointer_info: builtin.Type.Pointer,
-    allocator: anytype,
-    addr: u64,
-    any: anytype,
-) @TypeOf(any) {
-    if (pointer_info.sentinel) |sentinel| {
-        const ret: @TypeOf(any) = try meta.wrap(allocator.allocateWithSentinelIrreversible(pointer_info.child, any.len, sentinel));
+fn writePointerSlice(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
+    if (pointer_info.sentinel) |sentinel_ptr| {
+        const sentinel: pointer_info.child =
+            comptime mem.pointerOpaque(pointer_info.child, sentinel_ptr).*;
+        const ret: @TypeOf(any) = try meta.wrap(
+            allocator.allocateWithSentinelIrreversible(pointer_info.child, any.len, sentinel),
+        );
         for (ret, 0..) |*ptr, i| {
-            ptr.* = try meta.wrap(write(allocator, addr, any[i]));
+            ptr.* = try meta.wrap(
+                write(allocator, addr, any[i]),
+            );
         }
         return toOffset(ret, addr);
     } else {
-        const ret: @TypeOf(any) = try meta.wrap(allocator.allocateIrreversible(pointer_info.child, any.len));
+        const ret: @TypeOf(any) = try meta.wrap(
+            allocator.allocateIrreversible(pointer_info.child, any.len),
+        );
         for (ret, 0..) |*ptr, i| {
-            ptr.* = try meta.wrap(write(allocator, addr, any[i]));
+            ptr.* = try meta.wrap(
+                write(allocator, addr, any[i]),
+            );
         }
         return toOffset(ret, addr);
     }
 }
-fn writePointerMany(
-    comptime pointer_info: builtin.Type.Pointer,
-    allocator: anytype,
-    addr: u64,
-    any: anytype,
-    comptime sentinel: pointer_info.child,
-) @TypeOf(any) {
-    return writePointerSlice(pointer_info, allocator, addr, meta.manyToSlice(any), sentinel).ptr;
+fn writePointerMany(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
+    return writePointerSlice(pointer_info, allocator, addr, meta.manyToSlice(any)).ptr;
 }
 pub fn write(allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*).allocate_payload(@TypeOf(any)) {
     switch (@typeInfo(@TypeOf(any))) {
@@ -211,6 +181,28 @@ pub fn write(allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*).a
         else => return any,
     }
 }
+pub fn read(addr: u64, offset: u64, any: anytype) u64 {
+    switch (@typeInfo(@TypeOf(any.*))) {
+        .Struct => |struct_info| {
+            return readStruct(struct_info, addr, offset, any);
+        },
+        .Union => |union_info| {
+            return readUnion(union_info, addr, offset, any);
+        },
+        .Pointer => |pointer_info| {
+            if (pointer_info.size == .One) {
+                return readPointerOne(pointer_info, addr, offset, @constCast(any));
+            }
+            if (pointer_info.size == .Slice) {
+                return readPointerSlice(pointer_info, addr, offset, @constCast(any));
+            }
+            if (pointer_info.size == .Many) {
+                return readPointerMany(pointer_info, addr, offset, @constCast(any));
+            }
+        },
+        else => return offset,
+    }
+}
 fn genericSerializeSlicesLoop(comptime T: type, comptime lvl: u64, s_ab_addr: u64, any: anytype) u64 {
     var t_ab_addr: u64 = s_ab_addr;
     if (lvl == 0) {
@@ -224,7 +216,7 @@ fn genericSerializeSlicesLoop(comptime T: type, comptime lvl: u64, s_ab_addr: u6
 fn genericDeserializeSlicesLoop(comptime S: type, comptime lvl: u64, addr: u64, s_aligned_bytes: u64, any: anytype) u64 {
     var t_aligned_bytes: u64 = s_aligned_bytes;
     if (lvl == 0) {
-        any.* = mem.pointerMany(S, addr +% s_aligned_bytes, any.len);
+        @constCast(any).* = mem.pointerMany(S, addr +% s_aligned_bytes, any.len);
         return s_aligned_bytes +% (any.len *% @sizeOf(S));
     } else for (any.*) |*value| {
         t_aligned_bytes = genericDeserializeSlicesLoop(meta.Child(S), lvl - 1, addr, t_aligned_bytes, value);
@@ -234,16 +226,20 @@ fn genericDeserializeSlicesLoop(comptime S: type, comptime lvl: u64, addr: u64, 
 fn genericSerializeValuesLoop(comptime T: type, allocator: anytype, s_ab_addr: u64, s_up_addr: u64, any: anytype) @TypeOf(allocator.*).allocate_payload(u64) {
     var t_ab_addr: u64 = s_ab_addr;
     if (@TypeOf(any) == T) {
-        mem.pointerOne(T, t_ab_addr).* = try meta.wrap(write(allocator, s_up_addr, any));
+        mem.pointerOne(T, t_ab_addr).* = try meta.wrap(
+            write(allocator, s_up_addr, any),
+        );
         return t_ab_addr +% @sizeOf(T);
     } else for (any) |value| {
-        t_ab_addr = try meta.wrap(genericSerializeValuesLoop(T, allocator, t_ab_addr, s_up_addr, value));
+        t_ab_addr = try meta.wrap(
+            genericSerializeValuesLoop(T, allocator, t_ab_addr, s_up_addr, value),
+        );
     }
     return t_ab_addr;
 }
 fn genericDeserializeValuesLoop(comptime T: type, s_up_addr: u64, s_aligned_bytes: u64, any: anytype) u64 {
     var t_aligned_bytes: u64 = s_aligned_bytes;
-    if (@TypeOf(any) == *T) {
+    if (@TypeOf(@constCast(any)) == *T) {
         return read(s_up_addr, t_aligned_bytes, any);
     } else for (any.*) |*value| {
         t_aligned_bytes = genericDeserializeValuesLoop(T, s_up_addr, t_aligned_bytes, value);
@@ -254,13 +250,19 @@ pub fn genericSerializeInternal(allocator: anytype, s_ab_addr: u64, any: anytype
     const S: type = @TypeOf(any);
     const T: type = meta.SliceChild(S);
     const s_up_addr: u64 = s_ab_addr +% length(T, any);
-    try meta.wrap(allocator.mapBelow(s_up_addr));
-    try meta.wrap(allocator.allocate(s_up_addr));
+    try meta.wrap(
+        allocator.mapBelow(s_up_addr),
+    );
+    allocator.allocate(s_up_addr);
     var t_ab_addr: u64 = s_ab_addr;
     inline for (0..comptime meta.sliceLevel(S)) |lvl| {
-        t_ab_addr = try meta.wrap(genericSerializeSlicesLoop(T, lvl, t_ab_addr, any));
+        t_ab_addr = try meta.wrap(
+            genericSerializeSlicesLoop(T, lvl, t_ab_addr, any),
+        );
     } else {
-        t_ab_addr = try meta.wrap(genericSerializeValuesLoop(T, allocator, t_ab_addr, s_up_addr, any));
+        t_ab_addr = try meta.wrap(
+            genericSerializeValuesLoop(T, allocator, t_ab_addr, s_up_addr, any),
+        );
     }
     return mem.pointerMany(u8, s_ab_addr, allocator.unallocated_byte_address() - s_ab_addr);
 }
@@ -279,7 +281,9 @@ pub fn serialize(allocator: anytype, pathname: [:0]const u8, sets: anytype) !voi
     const save = allocator.save();
     defer allocator.restore(save);
     const s_ab_addr: u64 = allocator.alignAbove(16);
-    const bytes: []const u8 = try meta.wrap(genericSerializeInternal(allocator, s_ab_addr, sets));
+    const bytes: []const u8 = try meta.wrap(
+        genericSerializeInternal(allocator, s_ab_addr, sets),
+    );
     const fd: u64 = try file.create(create_spec, pathname);
     try file.write(write_spec, fd, bytes);
     try file.close(close_spec, fd);
@@ -288,15 +292,21 @@ pub fn deserialize(comptime S: type, allocator: anytype, pathname: [:0]const u8)
     const fd: u64 = try file.open(open_spec, pathname);
     const t_ab_addr: u64 = allocator.alignAbove(16);
     const st: file.Stat = try file.fstat(stat_spec, fd);
-    const buf: []u8 = try meta.wrap(allocator.allocateIrreversible(u8, st.size));
+    const buf: []u8 = try meta.wrap(
+        allocator.allocateIrreversible(u8, st.size),
+    );
     try file.read(read_spec, fd, buf, st.size);
     try file.close(close_spec, fd);
-    return try meta.wrap(genericDeserializeInternal(S, t_ab_addr));
+    return try meta.wrap(
+        genericDeserializeInternal(S, t_ab_addr),
+    );
 }
 fn allocateFile(allocator: anytype, pathname: [:0]const u8) !void {
     const fd: u64 = try file.open(open_spec, pathname);
     const st: file.Stat = try file.fstat(stat_spec, fd);
-    const buf: []u8 = try meta.wrap(allocator.allocateIrreversible(u8, st.size));
+    const buf: []u8 = try meta.wrap(
+        allocator.allocateIrreversible(u8, st.size),
+    );
     try file.read(read_spec, fd, buf, st.size);
     try file.close(close_spec, fd);
 }
