23721799fa282e52e59807879041afa2126acacd
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-26 21:08:35 +0000

    Added test functions for both kinds of thread-safe address space.

--- top/thread-test.zig
+++ top/thread-test.zig
@@ -14,31 +14,20 @@ const builtin = @import("./builtin.zig");
 
 pub usingnamespace proc.start;
 
-pub const runtime_assertions: bool = true;
+pub const logging_override: builtin.Logging.Override = preset.logging.override.verbose;
 
-const AddressSpace = mem.GenericDiscreteAddressSpace(.{
-    .label = "super",
-    .list = meta.slice(mem.Arena, .{
-        .{ .lb_addr = 0x00040000000, .up_addr = 0x10000000000 },
-        .{ .lb_addr = 0x10000000000, .up_addr = 0x20000000000 },
-        .{ .lb_addr = 0x20000000000, .up_addr = 0x30000000000 },
-        .{ .lb_addr = 0x30000000000, .up_addr = 0x40000000000 },
-        .{ .lb_addr = 0x40000000000, .up_addr = 0x50000000000 },
-        .{ .lb_addr = 0x50000000000, .up_addr = 0x60000000000 },
-        .{ .lb_addr = 0x60000000000, .up_addr = 0x70000000000 },
-        .{ .lb_addr = 0x70000000000, .up_addr = 0x80000000000 },
-        .{ .lb_addr = 0x80000000000, .up_addr = 0x90000000000 },
-    }),
-    .subspace = meta.slice(meta.Generic, .{virtual.generic(.{
-        .label = "thread",
-        //.lb_addr = 0x10000000000,
-        //.up_addr = 0x40400000000,
-        //.divisions = 16,
-        //.options = .{ .thread_safe = true },
-        .list = list,
-    })}),
+const discrete_list: []const mem.Arena = meta.slice(mem.Arena, .{
+    .{ .lb_addr = 0x00040000000, .up_addr = 0x10000000000 },
+    .{ .lb_addr = 0x10000000000, .up_addr = 0x20000000000 },
+    .{ .lb_addr = 0x20000000000, .up_addr = 0x30000000000 },
+    .{ .lb_addr = 0x30000000000, .up_addr = 0x40000000000 },
+    .{ .lb_addr = 0x40000000000, .up_addr = 0x50000000000 },
+    .{ .lb_addr = 0x50000000000, .up_addr = 0x60000000000 },
+    .{ .lb_addr = 0x60000000000, .up_addr = 0x70000000000 },
+    .{ .lb_addr = 0x70000000000, .up_addr = 0x80000000000 },
+    .{ .lb_addr = 0x80000000000, .up_addr = 0x90000000000 },
 });
-const list = meta.slice(mem.Arena, .{
+const list: []const mem.Arena = meta.slice(mem.Arena, .{
     .{ .lb_addr = 0x10000000000, .up_addr = 0x10100000000, .options = .{ .thread_safe = true } },
     .{ .lb_addr = 0x10100000000, .up_addr = 0x10200000000, .options = .{ .thread_safe = true } },
     .{ .lb_addr = 0x10200000000, .up_addr = 0x10300000000, .options = .{ .thread_safe = true } },
@@ -56,34 +45,97 @@ const list = meta.slice(mem.Arena, .{
     .{ .lb_addr = 0x40200000000, .up_addr = 0x40300000000, .options = .{ .thread_safe = true } },
     .{ .lb_addr = 0x40300000000, .up_addr = 0x40400000000, .options = .{ .thread_safe = true } },
 });
-const ThreadSpace = AddressSpace.SubSpace(0);
 
 const PrintArray = mem.StaticString(4096);
-const ThreadAllocatorSpec = struct {
-    AddressSpace: type,
-};
-fn ThreadAllocator(comptime spec: ThreadAllocatorSpec) type {
-    return struct {
-        pub const allocator_spec: ThreadAllocatorSpec = spec;
-    };
-}
 
-pub fn main() !void {
+fn testThreadSafeRegular() !void {
+    const AddressSpace = mem.GenericDiscreteAddressSpace(.{
+        .label = "super",
+        .list = meta.slice(mem.Arena, .{
+            .{ .lb_addr = 0x00040000000, .up_addr = 0x10000000000 },
+            .{ .lb_addr = 0x10000000000, .up_addr = 0x20000000000 },
+            .{ .lb_addr = 0x20000000000, .up_addr = 0x30000000000 },
+            .{ .lb_addr = 0x30000000000, .up_addr = 0x40000000000 },
+            .{ .lb_addr = 0x40000000000, .up_addr = 0x50000000000 },
+            .{ .lb_addr = 0x50000000000, .up_addr = 0x60000000000 },
+            .{ .lb_addr = 0x60000000000, .up_addr = 0x70000000000 },
+            .{ .lb_addr = 0x70000000000, .up_addr = 0x80000000000 },
+            .{ .lb_addr = 0x80000000000, .up_addr = 0x90000000000 },
+        }),
+        .subspace = meta.slice(meta.Generic, .{virtual.generic(.{
+            .label = "thread",
+            .tag_type = enum(u8) {
+                unset = 0,
+                working = 0b1010101,
+                set = 255,
+            },
+            .lb_addr = 0x10000000000,
+            .up_addr = 0x40400000000,
+            .divisions = 16,
+            .options = .{ .thread_safe = true },
+            //.list = list,
+        })}),
+    });
+    const ThreadSpace = AddressSpace.SubSpace(0);
+
     var address_space: AddressSpace = .{};
+    _ = address_space;
+    var thread_space: ThreadSpace = .{};
+
+    var thread_index: u8 = 0;
+    while (thread_index != ThreadSpace.addr_spec.count()) : (thread_index += 1) {
+        if (thread_space.atomicTransform(thread_index, .unset, .set)) {}
+    }
+    thread_index = 0;
+    while (thread_index != ThreadSpace.addr_spec.count()) : (thread_index += 1) {
+        try builtin.expect(thread_space.atomicTransform(thread_index, .set, .working));
+    }
+    thread_index = 0;
+    while (thread_index != ThreadSpace.addr_spec.count()) : (thread_index += 1) {
+        try builtin.expect(thread_space.atomicTransform(thread_index, .working, .unset));
+    }
+
+    var array: PrintArray = .{};
+    _ = array;
+}
+fn testThreadSafeDiscrete() !void {
+    const AddressSpace = mem.GenericDiscreteAddressSpace(.{
+        .label = "super",
+        .list = discrete_list,
+        .subspace = meta.slice(meta.Generic, .{virtual.generic(.{
+            .label = "thread",
+            .tag_type = enum(u8) {
+                unset = 0,
+                working = 1,
+                set = 255,
+            },
+            .list = list,
+        })}),
+    });
+    const ThreadSpace = AddressSpace.SubSpace(0);
+
+    var address_space: AddressSpace = .{};
+    _ = address_space;
     var thread_space: ThreadSpace = .{};
 
     comptime var thread_index: u8 = 0;
     inline while (thread_index != comptime ThreadSpace.addr_spec.count()) : (thread_index += 1) {
-        try mem.acquireStatic(ThreadSpace, &thread_space, thread_index);
+        if (thread_space.atomicTransform(thread_index, .unset, .set)) {}
     }
     thread_index = 0;
     inline while (thread_index != comptime ThreadSpace.addr_spec.count()) : (thread_index += 1) {
-        mem.releaseStatic(ThreadSpace, &thread_space, thread_index);
+        try builtin.expect(thread_space.atomicTransform(thread_index, .set, .working));
+    }
+    thread_index = 0;
+    inline while (thread_index != comptime ThreadSpace.addr_spec.count()) : (thread_index += 1) {
+        try builtin.expect(thread_space.atomicTransform(thread_index, .working, .unset));
     }
 
     var array: PrintArray = .{};
+    _ = array;
+}
 
-    array.writeAny(preset.reinterpret.fmt, .{ fmt.render(.{ .radix = 2, .omit_default_fields = false }, address_space), '\n' });
-    array.writeAny(preset.reinterpret.fmt, .{ fmt.render(.{ .radix = 2, .omit_default_fields = false }, thread_space), '\n' });
-    builtin.debug.write(array.readAll());
+pub fn main() !void {
+    try meta.wrap(testThreadSafeDiscrete());
+    try meta.wrap(testThreadSafeDiscrete());
 }
