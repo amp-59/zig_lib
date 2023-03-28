ea6c2bfbe225c3c00ee56822ce88f3be8a930fbe
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-28 06:40:31 +0000

    Added call to `writeEnv` in standard library build program `main`.

--- build/build-aux.zig
+++ build/build-aux.zig
@@ -4,5 +4,6 @@ const util = @import("./util.zig");
 
 pub fn main(builder: *build.Builder) !void {
     util.Context.init(builder);
+    try util.writeEnv(builder);
     _ = util.addProjectExecutable(builder, "generate_build", "top/build/generate_build.zig", .{ .build_mode = .ReleaseSmall });
 }
