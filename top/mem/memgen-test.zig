d47ca1c6aa46da35356c3418b31588834d3bab38
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-29 04:53:21 +0000

    Minimised file for later use.

--- top/mem/memgen-test.zig
+++ top/mem/memgen-test.zig
@@ -9,6 +9,7 @@ const serial = gen.serial;
 const builtin = gen.builtin;
 const testing = gen.testing;
 
+const config = @import("./config.zig");
 const attr = @import("./attr.zig");
 
 pub usingnamespace proc.start;
@@ -18,35 +19,13 @@ pub const logging_override: builtin.Logging.Override = preset.logging.override.v
 
 pub const runtime_assertions: bool = false;
 
-const Allocator = mem.GenericArenaAllocator(.{
-    .AddressSpace = AddressSpace,
-    .arena_index = 0,
-    .logging = preset.allocator.logging.silent,
-    .errors = preset.allocator.errors.noexcept,
-    .options = preset.allocator.options.small,
-});
-const FAllocator = mem.GenericArenaAllocator(.{
-    .AddressSpace = AddressSpace,
-    .arena_index = 1,
-    .logging = preset.allocator.logging.silent,
-    .errors = preset.allocator.errors.noexcept,
-    .options = preset.allocator.options.small,
-});
-const AddressSpace = mem.GenericRegularAddressSpace(.{
-    .lb_offset = 0x40000000,
-    .divisions = 128,
-    .logging = preset.address_space.logging.silent,
-    .errors = preset.address_space.errors.noexcept,
-    .options = .{},
-});
-
 const Array = mem.StaticString(2 * 1024 * 1024);
-pub fn main() !void {
-    if (return) {}
-    var address_space: AddressSpace = .{};
-    var fallocator: FAllocator = FAllocator.init(&address_space);
 
-    @setEvalBranchQuota(3000);
-    const strings = try serial.deserialize3(attr.Specifier, &fallocator, gen.auxiliaryFile("spec_sets"));
-    try serial.serialize3(attr.Specifier, &fallocator, gen.auxiliaryFile("spec_sets"), strings);
+const DeserializeAllocator = config.DeserializeAllocator;
+const SerializeAllocator = config.SerializeAllocator;
+
+pub fn main() !void {
+    var address_space: config.AddressSpace = .{};
+    var allocator: DeserializeAllocator = DeserializeAllocator.init(&address_space);
+    _ = allocator;
 }
