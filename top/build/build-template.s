08a7b8287b9ab9778cb1e5764a37aa45a28538cb
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-03-29 04:23:49 +0000

    Updated builder assembly.

--- top/build/build-template.s
+++ top/build/build-template.s
@@ -1,5 +1,8 @@
     .section .text.
     .intel_syntax noprefix
+
+    .globl  asmMaxWidths
+    .type   asmMaxWidths,@function
 asmMaxWidths:
     push    r15
     push    r14
@@ -57,6 +60,8 @@ asmMaxWidths:
     pop     r15
     ret
 
+    .globl  asmWriteAllCommands
+    .type   asmWriteAllCommands,@function
 asmWriteAllCommands:
     push    rbp
     push    r15
@@ -64,287 +69,100 @@ asmWriteAllCommands:
     push    r13
     push    r12
     push    rbx
-    mov     rax, qword ptr [rdi + 88]
-    cmp     rax, qword ptr [rdi + 80]
-    je      .asmWriteAllCommands58
-    mov     r9, qword ptr [rdi + 72]
-    mov     rcx, qword ptr [r9 + 8]
-    test    rcx, rcx
-    je      .asmWriteAllCommands58
-    lea     rdi, [rsi + 96]
-    lea     rbp, [rsi + rdx + 4]
-    lea     rax, [rsi + 4]
-    lea     r10, [rsi + 100]
-    xor     r8d, r8d
-    mov     qword ptr [rsp - 32], rdi
-    lea     rdi, [rsi + rdx + 100]
-    mov     qword ptr [rsp - 40], rax
-    mov     qword ptr [rsp - 16], r10
-    mov     qword ptr [rsp - 48], rbp
-    mov     qword ptr [rsp - 24], rdi
-    jmp     .asmWriteAllCommands4
-    .p2align    4, 0x90
-.asmWriteAllCommands3:
-    mov     r9, qword ptr [rsp - 8]
-    mov     rcx, qword ptr [r9 + 8]
-    test    rcx, rcx
-    je      .asmWriteAllCommands59
-.asmWriteAllCommands4:
-    mov     rax, qword ptr [r9]
-    mov     qword ptr [rsp - 8], rcx
-    lea     rcx, [rsi + r8]
-    mov     r10, qword ptr [rax + 8]
-    test    r10, r10
-    je      .asmWriteAllCommands18
-    mov     rbx, qword ptr [rax]
-    cmp     r10, 16
-    jb      .asmWriteAllCommands7
-    mov     rax, rcx
-    sub     rax, rbx
-    cmp     rax, 128
-    jae     .asmWriteAllCommands8
-.asmWriteAllCommands7:
-    xor     edi, edi
-    jmp     .asmWriteAllCommands16
-.asmWriteAllCommands8:
-    cmp     r10, 128
-    jae     .asmWriteAllCommands10
-    xor     edi, edi
-    jmp     .asmWriteAllCommands14
-.asmWriteAllCommands10:
-    mov     rax, qword ptr [rsp - 32]
-    mov     rdi, r10
-    xor     ebp, ebp
-    and     rdi, -128
-    add     rax, r8
-    .p2align    4, 0x90
-.asmWriteAllCommands11:
-    vmovups ymm0, ymmword ptr [rbx + rbp]
-    vmovups ymm1, ymmword ptr [rbx + rbp + 32]
-    vmovups ymm2, ymmword ptr [rbx + rbp + 64]
-    vmovups ymm3, ymmword ptr [rbx + rbp + 96]
-    vmovups ymmword ptr [rax + rbp - 96], ymm0
-    vmovups ymmword ptr [rax + rbp - 64], ymm1
-    vmovups ymmword ptr [rax + rbp - 32], ymm2
-    vmovups ymmword ptr [rax + rbp], ymm3
-    sub     rbp, -128
-    cmp     rdi, rbp
-    jne     .asmWriteAllCommands11
-    mov     rbp, qword ptr [rsp - 48]
-    cmp     r10, rdi
-    je      .asmWriteAllCommands18
-    test    r10b, 112
-    je      .asmWriteAllCommands16
-.asmWriteAllCommands14:
-    mov     rax, rdi
-    mov     rdi, r10
-    and     rdi, -16
-    .p2align    4, 0x90
-.asmWriteAllCommands15:
-    vmovups xmm0, xmmword ptr [rbx + rax]
-    vmovups xmmword ptr [rcx + rax], xmm0
-    add     rax, 16
-    cmp     rdi, rax
-    jne     .asmWriteAllCommands15
-    jmp     .asmWriteAllCommands17
-    .p2align    4, 0x90
-.asmWriteAllCommands16:
-    movzx   eax, byte ptr [rbx + rdi]
-    mov     byte ptr [rcx + rdi], al
-    inc     rdi
-.asmWriteAllCommands17:
-    cmp     r10, rdi
-    jne     .asmWriteAllCommands16
-.asmWriteAllCommands18:
-    mov     word ptr [rcx + r10], 2618
-    lea     r8, [r8 + r10 + 2]
-    mov     rax, qword ptr [r9]
-    mov     r13, qword ptr [rax + 32]
-    mov     r9, qword ptr [rax + 40]
-    cmp     r9, r13
-    je      .asmWriteAllCommands3
-    mov     r14, qword ptr [rax + 24]
-    jmp     .asmWriteAllCommands22
-    .p2align    4, 0x90
-.asmWriteAllCommands20:
-    xor     eax, eax
-.asmWriteAllCommands21:
-    lea     rcx, [rax + r15]
-    lea     r8, [rax + r15 + 1]
-    mov     r14, r10
-    mov     byte ptr [rsi + rcx], 10
-    cmp     r9, r13
-    je      .asmWriteAllCommands3
-.asmWriteAllCommands22:
-    mov     r10, qword ptr [r14 + 8]
-    test    r10, r10
-    je      .asmWriteAllCommands3
-    mov     dword ptr [rsi + r8], 538976288
-    lea     r15, [r8 + 4]
-    mov     rcx, qword ptr [r14]
-    mov     r11, qword ptr [rcx + 8]
-    test    r11, r11
-    je      .asmWriteAllCommands26
-    mov     rcx, qword ptr [rcx]
-    cmp     r11, 16
-    jb      .asmWriteAllCommands25
-    mov     rax, qword ptr [rsp - 40]
-    lea     r12, [rax + r8]
-    mov     rdi, r12
-    sub     rdi, rcx
-    cmp     rdi, 128
-    jae     .asmWriteAllCommands29
-.asmWriteAllCommands25:
-    xor     edi, edi
-.asmWriteAllCommands38:
-    mov     rax, qword ptr [rsp - 40]
-    lea     rbx, [rax + r8]
-    .p2align    4, 0x90
-.asmWriteAllCommands39:
-    movzx   eax, byte ptr [rcx + rdi]
-    mov     byte ptr [rbx + rdi], al
-    inc     rdi
-    cmp     r11, rdi
-    jne     .asmWriteAllCommands39
-.asmWriteAllCommands40:
-    mov     rax, qword ptr [r14]
-    mov     rdi, qword ptr [rax + 8]
-    jmp     .asmWriteAllCommands41
-    .p2align    4, 0x90
-.asmWriteAllCommands26:
-    xor    edi, edi
-.asmWriteAllCommands41:
-    mov    rcx, rdx
-    mov    al, 32
-    sub    rcx, rdi
-    add    rdi, r15
-    add    r15, rdx
-    add    rdi, rsi
-    rep     stosb    byte ptr es:[rdi], al
-    mov     rcx, qword ptr [r14]
-    mov     rax, qword ptr [rcx + 24]
+    sub     rsp, 88
+    mov     rax, qword ptr [rdi + 80]
+    mov     rcx, qword ptr [rdi + 88]
+    mov     rbp, qword ptr [rdi + 72]
+    mov     r14, rdx
+    mov     r13, rsi
+    xor     r15d, r15d
+    mov     qword ptr [rsp + 24], rax
+    mov     qword ptr [rsp + 16], rcx
+1:
+    mov     rax, qword ptr [rsp + 16]
+    cmp     rax, qword ptr [rsp + 24]
+    je      2f
+    mov     rax, qword ptr [rbp + 8]
     test    rax, rax
-    je      .asmWriteAllCommands20
-    mov     rcx, qword ptr [rcx + 16]
-    cmp     rax, 16
-    jb      .asmWriteAllCommands43
-    lea     r12, [rbp + r8]
-    mov     rdi, r12
-    sub     rdi, rcx
-    cmp     rdi, 128
-    jae     .asmWriteAllCommands46
-.asmWriteAllCommands43:
-    xor     edi, edi
-.asmWriteAllCommands55:
-    add     r8, rbp
-    .p2align    4, 0x90
-.asmWriteAllCommands56:
-    movzx   ebx, byte ptr [rcx + rdi]
-    mov     byte ptr [r8 + rdi], bl
-    inc     rdi
-    cmp     rax, rdi
-    jne     .asmWriteAllCommands56
-.asmWriteAllCommands57:
-    mov     rax, qword ptr [r14]
+    je      3f
+    mov     rcx, qword ptr [rbp]
+    mov     qword ptr [rsp + 8], rax
+    lea     rax, [rip + c_lf]
+    lea     rdi, [r13 + r15]
+    lea     rsi, [rsp + 56]
+    vmovups xmm0, xmmword ptr [rcx]
+    vmovups xmmword ptr [rsp + 56], xmm0
+    mov     qword ptr [rsp + 72], rax
+    mov     qword ptr [rsp + 80], 2
+    push    2
+    pop     rdx
+    call    asmMemcpyMulti@PLT
+    mov     rdx, qword ptr [rbp]
+    add     r15, rax
+    mov     qword ptr [rsp + 32], r15
+    mov     r12, qword ptr [rdx + 24]
+    mov     rcx, qword ptr [rdx + 32]
+    mov     rax, qword ptr [rdx + 40]
+    mov     qword ptr [rsp + 48], rcx
+    mov     qword ptr [rsp + 40], rax
+6:
+    mov     rax, qword ptr [rsp + 40]
+    cmp     rax, qword ptr [rsp + 48]
+    je      7f
+    mov     rbp, qword ptr [r12 + 8]
+    test    rbp, rbp
+    je      9f
+    lea     rdi, [r13 + r15]
+    push    32
+    pop     rbx
+    mov     esi, ebx
+    push    4
+    pop     rdx
+    call    memset@PLT
+    mov     rax, qword ptr [r12]
+    lea     rdi, [r13 + r15 + 4]
+    mov     rsi, qword ptr [rax]
+    mov     rdx, qword ptr [rax + 8]
+    call    memcpy@PLT
+    mov     rax, qword ptr [r12]
+    mov     rdx, r14
+    mov     esi, ebx
+    mov     rax, qword ptr [rax + 8]
+    lea     rdi, [rax + r15 + 4]
+    sub     rdx, rax
+    add     rdi, r13
+    call    memset@PLT
+    mov     rax, qword ptr [r12]
+    lea     rbx, [r15 + r14 + 4]
+    lea     rdi, [r13 + rbx]
+    mov     rsi, qword ptr [rax + 16]
+    mov     rdx, qword ptr [rax + 24]
+    call    memcpy@PLT
+    mov     rax, qword ptr [r12]
+    mov     r12, rbp
     mov     rax, qword ptr [rax + 24]
-    jmp     .asmWriteAllCommands21
-.asmWriteAllCommands29:
-    cmp     r11, 128
-    jae     .asmWriteAllCommands31
-    xor     edi, edi
-    jmp     .asmWriteAllCommands35
-.asmWriteAllCommands46:
-    cmp     rax, 128
-    jae     .asmWriteAllCommands48
-    xor     edi, edi
-    jmp     .asmWriteAllCommands52
-.asmWriteAllCommands31:
-    mov     rax, qword ptr [rsp - 16]
-    mov     rdi, r11
-    xor     ebp, ebp
-    and     rdi, -128
-    lea     rbx, [rax + r8]
-    .p2align    4, 0x90
-.asmWriteAllCommands32:
-    vmovups ymm0, ymmword ptr [rcx + rbp]
-    vmovups ymm1, ymmword ptr [rcx + rbp + 32]
-    vmovups ymm2, ymmword ptr [rcx + rbp + 64]
-    vmovups ymm3, ymmword ptr [rcx + rbp + 96]
-    vmovups ymmword ptr [rbx + rbp - 96], ymm0
-    vmovups ymmword ptr [rbx + rbp - 64], ymm1
-    vmovups ymmword ptr [rbx + rbp - 32], ymm2
-    vmovups ymmword ptr [rbx + rbp], ymm3
-    sub     rbp, -128
-    cmp     rdi, rbp
-    jne     .asmWriteAllCommands32
-    mov     rbp, qword ptr [rsp - 48]
-    cmp     r11, rdi
-    je      .asmWriteAllCommands40
-    test    r11b, 112
-    je      .asmWriteAllCommands38
-.asmWriteAllCommands35:
-    mov     rbx, rdi
-    mov     rdi, r11
-    and     rdi, -16
-    .p2align    4, 0x90
-.asmWriteAllCommands36:
-    vmovups xmm0, xmmword ptr [rcx + rbx]
-    vmovups xmmword ptr [r12 + rbx], xmm0
-    add     rbx, 16
-    cmp     rdi, rbx
-    jne     .asmWriteAllCommands36
-    cmp     r11, rdi
-    je      .asmWriteAllCommands40
-    jmp     .asmWriteAllCommands38
-.asmWriteAllCommands48:
-    mov     rbp, qword ptr [rsp - 24]
-    mov     rdi, rax
-    xor     ebx, ebx
-    and     rdi, -128
-    add     rbp, r8
-    .p2align    4, 0x90
-.asmWriteAllCommands49:
-    vmovups ymm0, ymmword ptr [rcx + rbx]
-    vmovups ymm1, ymmword ptr [rcx + rbx + 32]
-    vmovups ymm2, ymmword ptr [rcx + rbx + 64]
-    vmovups ymm3, ymmword ptr [rcx + rbx + 96]
-    vmovups ymmword ptr [rbp + rbx - 96], ymm0
-    vmovups ymmword ptr [rbp + rbx - 64], ymm1
-    vmovups ymmword ptr [rbp + rbx - 32], ymm2
-    vmovups ymmword ptr [rbp + rbx], ymm3
-    sub     rbx, -128
-    cmp     rdi, rbx
-    jne     .asmWriteAllCommands49
-    mov     rbp, qword ptr [rsp - 48]
-    cmp     rax, rdi
-    je      .asmWriteAllCommands57
-    test    al, 112
-    je      .asmWriteAllCommands55
-.asmWriteAllCommands52:
-    mov     rbx, rdi
-    mov     rdi, rax
-    and     rdi, -16
-    .p2align    4, 0x90
-.asmWriteAllCommands53:
-    vmovups xmm0, xmmword ptr [rcx + rbx]
-    vmovups xmmword ptr [r12 + rbx], xmm0
-    add     rbx, 16
-    cmp     rdi, rbx
-    jne     .asmWriteAllCommands53
-    cmp     rax, rdi
-    je      .asmWriteAllCommands57
-    jmp     .asmWriteAllCommands55
-.asmWriteAllCommands58:
-    xor     r8d, r8d
-.asmWriteAllCommands59:
-    mov     rax, r8
+    lea     rcx, [rax + rbx]
+    lea     r15, [rax + rbx + 1]
+    mov     byte ptr [r13 + rcx], 10
+    jmp     6b
+7:
+    mov     rbp, qword ptr [rsp + 8]
+    mov     r15, qword ptr [rsp + 32]
+    jmp     1b
+9:
+    mov     rbp, qword ptr [rsp + 8]
+    jmp     1b
+2:
+    xor     r15d, r15d
+3:
+    mov     rax, r15
+    add     rsp, 88
     pop     rbx
     pop     r12
     pop     r13
     pop     r14
     pop     r15
     pop     rbp
-    vzeroupper
     ret
 
 asmRewind:
@@ -367,113 +185,68 @@ asmRewind:
   ret
 
 asmWriteEnv:
-    push	rbp
-    push	r15
-    push	r14
-    push	r13
-    push	r12
-    push	rbx
-    mov	rbx, rdi
-    mov	rdi, qword ptr [rdi + 32]
-    mov	eax, 83
-    mov	esi, 488
-    syscall
-    mov	eax, 2
-    mov	esi, 2818048
-    xor	edx, edx
-    mov	rdi, qword ptr [rbx + 32]
-    syscall
-    mov	r8, rax
-    lea	rsi, [rip + env]
-    mov	eax, 257
-    mov	edx, 524865
-    mov	rdi, r8
-    syscall
-    mov	rdi, rax
-    lea	rsi, [rip + zig_exe]
-    mov	edx, 35
-    mov	rax, qword ptr [rbx + 48]
-    mov	rbp, qword ptr [rbx]
-    mov	r9, qword ptr [rbx + 8]
-    mov	r12, qword ptr [rbx + 16]
-    mov	r13, qword ptr [rbx + 24]
-    mov	r14, qword ptr [rbx + 32]
-    mov	r15, qword ptr [rbx + 40]
-    mov	r10, qword ptr [rbx + 56]
-    lea	rbx, [rip + dq_sc_lf]
-    mov	qword ptr [rsp - 8], rax
-    mov	eax, 1
-    syscall
-    mov	eax, 1
-    mov	rsi, rbp
-    mov	rdx, r9
-    syscall
-    mov	eax, 1
-    mov	edx, 3
-    mov	rsi, rbx
-    syscall
-    lea	rsi, [rip + build_root]
-    mov	eax, 1
-    mov	edx, 38
-    syscall
-    mov	eax, 1
-    mov	rsi, r12
-    mov	rdx, r13
-    syscall
-    mov	eax, 1
-    mov	edx, 3
-    mov	rsi, rbx
-    syscall
-    lea	rsi, [rip + cache_dir]
-    mov	eax, 1
-    mov	edx, 37
-    syscall
-    mov	eax, 1
-    mov	rsi, r14
-    mov	rdx, r15
-    syscall
-    mov	eax, 1
-    mov	edx, 3
-    mov	rsi, rbx
-    syscall
-    lea	rsi, [rip + global_cache_dir]
-    mov	eax, 1
-    mov	edx, 44
-    syscall
-    mov	eax, 1
-    mov	rdx, r10
-    mov	rsi, qword ptr [rsp - 8]
-    syscall
-    mov	eax, 1
-    mov	edx, 3
-    mov	rsi, rbx
-    syscall
-    mov	eax, 3
-    mov	rdi, r8
-    syscall
-    pop	rbx
-    pop	r12
-    pop	r13
-    pop	r14
-    pop	r15
-    pop	rbp
+    sub     rsp, 64
+    vmovups xmm0, xmmword ptr [rsi]
+    vmovups xmm1, xmmword ptr [rsi + 16]
+    vmovups xmm2, xmmword ptr [rsi + 32]
+    vmovups xmm3, xmmword ptr [rsi + 48]
+    lea     rax, [rip + zig_exe]
+    movabs  rcx, 3
+    lea     rdx, [rip + build_root]
+    lea     r8, [rip + cache_dir]
+    lea     rsi, [rip + global_cache_dir]
+    movabs  r9, 8
+    mov     qword ptr [rsp - 128], rax
+    lea     rax, [rip + dq_sc_lf]
+    mov     qword ptr [rsp - 120], 35
+    vmovups xmmword ptr [rsp - 112], xmm0
+    mov     qword ptr [rsp - 96], rax
+    mov     qword ptr [rsp - 88], rcx
+    mov     qword ptr [rsp - 80], rdx
+    mov     qword ptr [rsp - 72], 38
+    vmovups xmmword ptr [rsp - 64], xmm1
+    mov     qword ptr [rsp - 48], rax
+    mov     qword ptr [rsp - 40], rcx
+    mov     qword ptr [rsp - 32], r8
+    movabs  r8, 1
+    mov     qword ptr [rsp - 24], 37
+    vmovups xmmword ptr [rsp - 16], xmm2
+    mov     qword ptr [rsp], rax
+    mov     qword ptr [rsp + 8], rcx
+    mov     qword ptr [rsp + 16], rsi
+    mov     qword ptr [rsp + 24], 44
+    vmovups xmmword ptr [rsp + 32], xmm3
+    mov     qword ptr [rsp + 48], rax
+    mov     qword ptr [rsp + 56], rcx
+1:
+    cmp     r9, 200
+    je      3f
+    mov     rsi, qword ptr [rsp + r9 - 136]
+    mov     rdx, qword ptr [rsp + r9 - 128]
+    mov     rax, r8
+    syscall # write
+    add     r9, 16
+    jmp     1b
+3:
+    add     rsp, 64
     ret
 
-env:
-    .asciz  "env.zig"
-    .size   env, 8
 zig_exe:
-    .asciz   "pub const zig_exe: [:0]const u8 = \""
-    .size    zig_exe, 36
-dq_sc_lf:
-    .asciz   "\";\n"
-    .size    dq_sc_lf, 4
+    .asciz  "pub const zig_exe: [:0]const u8 = \""
+    .size   zig_exe, 36
 build_root:
-    .asciz   "pub const build_root: [:0]const u8 = \""
-    .size    build_root, 39
+    .asciz  "pub const build_root: [:0]const u8 = \""
+    .size   build_root, 39
 cache_dir:
-    .asciz   "pub const cache_dir: [:0]const u8 = \""
-    .size    cache_dir, 38
+    .asciz  "pub const cache_dir: [:0]const u8 = \""
+    .size   cache_dir, 38
 global_cache_dir:
-    .asciz   "pub const global_cache_dir: [:0]const u8 = \""
-    .size    global_cache_dir, 45
+    .asciz  "pub const global_cache_dir: [:0]const u8 = \""
+    .size   global_cache_dir, 45
+dq_sc_lf:
+    .asciz  "\";\n"
+    .size   dq_sc_lf, 4
+c_lf:
+    .asciz  ":\n"
+    .size   c_lf, 3
+
