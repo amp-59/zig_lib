54ef9a52d84ef22302cd22f1f6cdad8b374f16fa
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-04-02 04:36:39 +0000

    Added mapping from `[]const types.Path` to `types.Files`.

--- top/build/command_line-template.zig
+++ top/build/command_line-template.zig
@@ -5,7 +5,14 @@ const types = @import("./types2.zig");
 const reinterpret_spec: mem.ReinterpretSpec = blk: {
     var tmp: mem.ReinterpretSpec = preset.reinterpret.print;
     tmp.composite.map = &.{
-        .{ .in = []const types.ModuleDependency, .out = types.ModuleDependencies },
+        .{
+            .in = []const types.ModuleDependency,
+            .out = types.ModuleDependencies,
+        },
+        .{
+            .in = []const types.Path,
+            .out = types.Files,
+        },
     };
     break :blk tmp;
 };
