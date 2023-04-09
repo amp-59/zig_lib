65e7148ef3fec8469317c89c4fd8d426f04e7683
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-04-09 16:07:45 +0000

    Removed unused serialiser objects.

--- build.zig
+++ build.zig
@@ -6,10 +6,11 @@ const meta = srg.meta;
 const build = srg.build2;
 const builtin = srg.builtin;
 
-pub const logging_override: builtin.Logging.Override = spec.logging.override.silent;
 pub const runtime_assertions: bool = false;
 
 pub const Builder: type = build.GenericBuilder(spec.builder.default);
+
+pub const message_style: [:0]const u8 = "\x1b[2m";
 
 const mods: []const build.Module = &.{
     .{ .name = "zig_lib", .path = "zig_lib.zig" },
@@ -87,10 +88,8 @@ pub fn buildMain(allocator: *Builder.Allocator, builder: *Builder) !void {
     const mg_params: *Builder.Target =          try mg.addTarget(allocator, obj_default,    "mg_params",            "top/mem/serial_params.zig");
     const mg_options: *Builder.Target =         try mg.addTarget(allocator, obj_default,    "mg_options",           "top/mem/serial_options.zig");
     const mg_abstract_specs: *Builder.Target =  try mg.addTarget(allocator, obj_default,    "mg_abstract_specs",    "top/mem/serial_abstract_specs.zig");
-    const mg_impl_detail: *Builder.Target =     try mg.addTarget(allocator, obj_default,    "mg_impl_detail",       "top/mem/serial_impl_detail.zig");
-    const mg_ctn_detail: *Builder.Target =      try mg.addTarget(allocator, obj_default,    "mg_ctn_detail",        "top/mem/serial_ctn_detail.zig");
     const mg_new_type_specs: *Builder.Target =  try mg.addTarget(allocator, exe_debug,      "mg_new_type_specs",    "top/mem/new_type_specs-aux.zig");
-    const mg_reference_impls: *Builder.Target = try mg.addTarget(allocator, exe_debug,      "mg_reference_impls",   "top/mem/reference_impls-aux.zig");
+    const mg_reference_impls: *Builder.Target = try mg.addTarget(allocator, exe_default,    "mg_reference_impls",   "top/mem/reference_impls-aux.zig");
     const mg_container_impls: *Builder.Target = try mg.addTarget(allocator, exe_debug,      "mg_container_impls",   "top/mem/container_impls-aux.zig");
     const mg_container_kinds: *Builder.Target = try mg.addTarget(allocator, exe_default,    "mg_container_kinds",   "top/mem/container_kinds-aux.zig");
     const mg_allocator_kinds: *Builder.Target = try mg.addTarget(allocator, exe_default,    "mg_allocator_kinds",   "top/mem/allocator_kinds-aux.zig");
@@ -134,8 +133,6 @@ pub fn buildMain(allocator: *Builder.Allocator, builder: *Builder) !void {
     mg_params.descr =           "Serialiser for `[]const [] Specifier`";
     mg_options.descr =          "Serialiser for `[]const []const Technique`";
     mg_abstract_specs.descr =   "Serialiser for `[]const AbstractSpecification`";
-    mg_impl_detail.descr =      "Serialiser for `[]const Implementation`";
-    mg_ctn_detail.descr =       "Serialiser for `[]const Container`";
     mg_new_type_specs.descr =   "Generate data and container->reference deductions";
     mg_container_kinds.descr =  "Generate function kind switch functions for container functions";
     mg_allocator_kinds.descr =  "Generate function kind switch functions for allocator functions";
@@ -149,15 +146,9 @@ pub fn buildMain(allocator: *Builder.Allocator, builder: *Builder) !void {
     mg_new_type_specs.dependOnObject(allocator,     mg_params);
     mg_new_type_specs.dependOnObject(allocator,     mg_techs);
     mg_new_type_specs.dependOnObject(allocator,     mg_specs);
-    mg_new_type_specs.dependOnObject(allocator,     mg_ctn_detail);
-    mg_new_type_specs.dependOnObject(allocator,     mg_impl_detail);
     mg_container_impls.dependOnRun(allocator,       mg_new_type_specs);
     mg_container_impls.dependOnRun(allocator,       mg_container_kinds);
-    mg_container_impls.dependOnObject(allocator,    mg_ctn_detail);
-    mg_container_impls.dependOnObject(allocator,    mg_impl_detail);
     mg_reference_impls.dependOnRun(allocator,       mg_new_type_specs);
-    mg_reference_impls.dependOnObject(allocator,    mg_ctn_detail);
-    mg_reference_impls.dependOnObject(allocator,    mg_impl_detail);
 
     build_test.addRunArgument(allocator, builder.zig_exe);
     build_test.addRunArgument(allocator, builder.build_root);
