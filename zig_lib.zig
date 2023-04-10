4321b3d985bc8c6bfbe33c7f6de23632d3ee5262
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-04-10 11:43:13 +0000

    Removed `build` from project directory.

--- zig_lib.zig
+++ zig_lib.zig
@@ -15,7 +15,6 @@ pub const time = @import("./top/time.zig");
 pub const spec = @import("./top/spec.zig");
 pub const thread = @import("./top/thread.zig");
 pub const serial = @import("./top/serial.zig");
-pub const build = @import("./top/build.zig");
 pub const build2 = @import("./top/build2.zig");
 pub const testing = @import("./top/testing.zig");
 pub const virtual = @import("./top/virtual.zig");
