f7764f06e6d1eb89c1af600f5acc77ace37ff331
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-04-28 21:22:17 +0000

    Added alternative method for mapping formatter types.

--- top/build/command_line-template.zig
+++ top/build/command_line-template.zig
@@ -1,17 +1,24 @@
 const mem = @import("../mem.zig");
 const spec = @import("../spec.zig");
 const types = @import("./types.zig");
+
 const reinterpret_spec: mem.ReinterpretSpec = blk: {
     var tmp: mem.ReinterpretSpec = spec.reinterpret.print;
     tmp.composite.map = &.{
-        .{
-            .in = []const types.ModuleDependency,
-            .out = types.ModuleDependencies,
-        },
-        .{
-            .in = []const types.Path,
-            .out = types.Files,
-        },
+        .{ .in = []const types.ModuleDependency, .out = types.ModuleDependencies },
+        .{ .in = []const types.Path, .out = types.Files },
     };
     break :blk tmp;
 };
+fn FormatMap(comptime T: type) type {
+    switch (T) {
+        []const types.ModuleDependency => return types.ModuleDependencies,
+        []const types.Path => return types.Files,
+        []const types.Macro => return types.Macros,
+        []const types.Module => return types.Modules,
+        else => @compileError(@typeName(T)),
+    }
+}
+fn formatMap(any: anytype) FormatMap(@TypeOf(any)) {
+    return .{ .value = any };
+}
