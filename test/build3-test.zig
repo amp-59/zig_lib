19c17a69ad954c1832e8705d8e7e34443bc2da2e
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-04-29 10:59:55 +0000

    Added more variety to options required by `BuildCommand` command line.

--- test/build3-test.zig
+++ test/build3-test.zig
@@ -1,51 +1,77 @@
 const zig_lib = @import("../zig_lib.zig");
 const mem = zig_lib.mem;
+const file = zig_lib.file;
+const spec = zig_lib.spec;
 const builtin = zig_lib.builtin;
 
 pub usingnamespace zig_lib.proc.start;
 
-comptime {
-    _ = zig_lib.mach;
-}
+pub const logging_override = spec.logging.override.verbose;
 
-const Allocator = mem.GenericArenaAllocator(.{});
-const AddressSpace = Allocator.AddressSpace;
+const AddressSpace = mem.GenericDiscreteAddressSpace(.{
+    .list = &.{
+        .{ .lb_addr = 0x40000000, .up_addr = 0x100000000 },
+    },
+    .errors = spec.address_space.errors.noexcept,
+    .logging = spec.address_space.logging.silent,
+});
+const BigAllocator = mem.GenericArenaAllocator(.{
+    .AddressSpace = AddressSpace,
+    .errors = spec.allocator.errors.noexcept,
+    .logging = spec.allocator.logging.silent,
+    .options = spec.allocator.options.small,
+    .arena_index = 0,
+});
+const Allocator = mem.SimpleAllocator;
 
-fn countArgs(buf: []u8) u64 {
-    var ret: u64 = undefined;
-    for (buf) |value| ret -= @boolToInt(value == 0);
-    return ret;
-}
-fn makeArgs(allocator: *Allocator, buf: []u8) [][*:0]u8 {
-    var args: [][*:0]u8 = allocator.allocateIrreversible([*:0]u8, countArgs(buf));
+fn makeArgPtrs(allocator: *Allocator, args: [:0]u8) [][*:0]u8 {
+    @setRuntimeSafety(false);
+    var count: u64 = 0;
+    for (args) |value| count +%= @boolToInt(value == 0);
+    const ptrs: [][*:0]u8 = allocator.allocate([*:0]u8, count +% 1);
+    var len: u64 = 0;
     var idx: u64 = 0;
     var pos: u64 = 0;
-    for (buf, 0..) |value, end| {
-        if (value == 0) {
-            args[idx] = buf[pos..end :0];
-            pos = idx;
+    while (idx != args.len) : (idx +%= 1) {
+        if (args[idx] == 0) {
+            ptrs[len] = args[pos..idx :0];
+            len = len +% 1;
+            pos = idx +% 1;
         }
     }
-    return args;
+    ptrs[len] = builtin.zero([*:0]u8);
+    return ptrs[0..len];
 }
-
-pub fn main() void {
-    var address_space: AddressSpace = .{};
-    var allocator: Allocator = Allocator.init(&address_space);
-
+fn addArg(buf: [*]u8, arg: []const u8) u64 {
+    @memcpy(buf, arg.ptr, arg.len);
+    buf[arg.len] = 0;
+    return arg.len +% 1;
+}
+pub fn main(_: anytype, vars: anytype) !void {
+    var allocator: Allocator = .{};
     const tasks = @import("../top/build/tasks3.zig");
     const command_line = @import("../top/build/command_line3.zig");
-
     const build_cmd: tasks.BuildCommand = tasks.BuildCommand{
         .kind = .exe,
         .emit_bin = .{ .yes = .{ .absolute = builtin.buildRoot(), .relative = "zig-out/bin/build3" } },
         .emit_asm = .{ .yes = .{ .absolute = builtin.buildRoot(), .relative = "zig-out/bin/build3.s" } },
-        .emit_llvm_bc = .{ .yes = .{ .absolute = builtin.buildRoot(), .relative = "zig-out/aux/build3.bc" } },
-        .emit_llvm_ir = .{ .yes = .{ .absolute = builtin.buildRoot(), .relative = "zig-out/aux/build3.ll" } },
+        .emit_llvm_ir = .{ .yes = .{ .absolute = builtin.buildRoot(), .relative = "zig-out/bin/build3.ll" } },
+        .emit_llvm_bc = .{ .yes = .{ .absolute = builtin.buildRoot(), .relative = "zig-out/bin/build3.bc" } },
         .macros = &.{.{ .name = "NAME", .value = .{ .symbol = "true" } }},
+        .dependencies = &.{ .{ .name = "zig_lib" }, .{ .name = "env" } },
+        .modules = &.{ .{ .name = "zig_lib", .path = "zig_lib.zig" }, .{ .name = "env", .path = "zig-cache/env.zig" } },
+        .mode = .ReleaseSmall,
+        .main_pkg_path = builtin.buildRoot(),
     };
     var buf: [1024 * 1024]u8 = undefined;
-    const len: u64 = command_line.buildWrite(&build_cmd, &buf);
+    var len: u64 = 0;
+    len = len +% addArg(&buf, builtin.zigExe());
+    len = len +% addArg(buf[len..].ptr, "build-exe");
+    len = len +% addArg(buf[len..].ptr, comptime builtin.buildRoot() ++ "/test/build3-test.zig");
+    len = len +% command_line.buildWrite(&build_cmd, buf[len..].ptr);
 
-    makeArgs(&allocator, buf[0..len]);
+    buf[len] = 0;
+    const args: [][*:0]u8 = makeArgPtrs(&allocator, buf[0..len :0]);
+
+    try file.execPath(.{}, builtin.zigExe(), args, vars);
 }
