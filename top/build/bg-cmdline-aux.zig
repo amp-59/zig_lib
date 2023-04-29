051917c0798158025095a0314d1a75d1e361f6ea
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-04-29 10:58:45 +0000

    Added function for more combinations of argument kinds.

--- top/build/bg-cmdline-aux.zig
+++ top/build/bg-cmdline-aux.zig
@@ -13,6 +13,8 @@ pub usingnamespace proc.start;
 pub const runtime_assertions: bool = false;
 pub const logging_override: builtin.Logging.Override = spec.logging.override.silent;
 
+pub const primitive: bool = true;
+
 const Array = mem.StaticString(1024 * 1024);
 const build_root: [:0]const u8 = builtin.buildRoot();
 const command_line_path: [:0]const u8 = build_root ++ "/top/build/command_line3.zig";
@@ -47,9 +49,9 @@ const stat_spec: file.StatusSpec = .{
     .errors = .{},
     .logging = .{},
 };
-fn writeIf(array: *Array, value: []const u8) void {
+fn writeIf(array: *Array, value_name: []const u8) void {
     array.writeMany("if(");
-    array.writeMany(value);
+    array.writeMany(value_name);
     array.writeMany("){\n");
 }
 fn writeIfField(array: *Array, field_name: []const u8) void {
@@ -62,6 +64,13 @@ fn writeIfOptionalField(array: *Array, field_name: []const u8) void {
     array.writeMany(field_name);
     array.writeMany(")|");
     array.writeMany(field_name);
+    array.writeMany("|{\n");
+}
+fn writeIfOptional(array: *Array, value_name: []const u8, capture_name: []const u8) void {
+    array.writeMany("if(");
+    array.writeMany(value_name);
+    array.writeMany(")|");
+    array.writeMany(capture_name);
     array.writeMany("|{\n");
 }
 fn writeYesOptionalIf(array: *Array) void {
@@ -150,7 +159,7 @@ fn writeTagString(array: *Array, arg_string: []const u8, variant: types.Variant)
             array.writeMany(").ptr,@tagName(");
             array.writeMany(arg_string);
             array.writeMany(").len);\n");
-            writeOptString(array, arg_string, .length);
+            writeTagString(array, arg_string, .length);
         },
         .length => {
             array.writeMany("len=len+%@tagName(");
@@ -176,6 +185,23 @@ fn writeOptString(array: *Array, opt_string: []const u8, variant: types.Variant)
         },
     }
 }
+fn writeOptAssignString(array: *Array, opt_string: []const u8, variant: types.Variant) void {
+    switch (variant) {
+        .write => {
+            array.writeMany("@memcpy(buf+len,\"");
+            array.writeMany(opt_string);
+            array.writeMany("=\",");
+            array.writeFormat(fmt.ud64(opt_string.len +% 1));
+            array.writeMany(");\n");
+            writeOptAssignString(array, opt_string, .length);
+        },
+        .length => {
+            array.writeMany("len=len+%");
+            array.writeFormat(fmt.ud64(opt_string.len +% 1));
+            array.writeMany(";\n");
+        },
+    }
+}
 fn writeArgString(array: *Array, arg_string: []const u8, variant: types.Variant) void {
     switch (variant) {
         .write => {
@@ -193,25 +219,7 @@ fn writeArgString(array: *Array, arg_string: []const u8, variant: types.Variant)
         },
     }
 }
-fn writeOptArgInteger(array: *Array, opt_string: []const u8, arg_string: []const u8, variant: types.Variant) void {
-    writeOptString(array, opt_string, variant);
-    writeIntegerString(array, arg_string, variant);
-    writeNull(array, variant);
-}
-fn writeOptArgString(array: *Array, opt_string: []const u8, arg_string: []const u8, variant: types.Variant) void {
-    writeOptString(array, opt_string, variant);
-    writeArgString(array, arg_string, variant);
-    writeNull(array, variant);
-}
-fn writeOptTagString(array: *Array, opt_string: []const u8, arg_string: []const u8, variant: types.Variant) void {
-    writeOptString(array, opt_string, variant);
-    writeTagString(array, arg_string, variant);
-    writeNull(array, variant);
-}
-fn writeFormatter(array: *Array, opt_switch_string: ?[]const u8, arg_string: []const u8, variant: types.Variant) void {
-    if (opt_switch_string) |switch_string| {
-        writeOptString(array, switch_string, variant);
-    }
+fn writeFormatterInternal(array: *Array, arg_string: []const u8, variant: types.Variant) void {
     switch (variant) {
         .write => {
             array.writeMany("len=len+%");
@@ -241,6 +249,33 @@ fn writeMapped(array: *Array, opt_switch_string: ?[]const u8, arg_string: []cons
             array.writeMany(").formatLength();\n");
         },
     }
+}
+fn writeOptArgInteger(array: *Array, opt_string: []const u8, arg_string: []const u8, variant: types.Variant) void {
+    writeOptString(array, opt_string, variant);
+    writeIntegerString(array, arg_string, variant);
+    writeNull(array, variant);
+}
+fn writeOptArgString(array: *Array, opt_string: []const u8, arg_string: []const u8, variant: types.Variant) void {
+    writeOptString(array, opt_string, variant);
+    writeArgString(array, arg_string, variant);
+    writeNull(array, variant);
+}
+fn writeOptTagString(array: *Array, opt_string: []const u8, arg_string: []const u8, variant: types.Variant) void {
+    writeOptString(array, opt_string, variant);
+    writeTagString(array, arg_string, variant);
+    writeNull(array, variant);
+}
+fn writeFormatter(array: *Array, opt_switch_string: ?[]const u8, arg_string: []const u8, variant: types.Variant) void {
+    if (opt_switch_string) |switch_string| {
+        writeOptString(array, switch_string, variant);
+    }
+    writeFormatterInternal(array, arg_string, variant);
+}
+fn writeOptionalFormatter(array: *Array, opt_switch_string: ?[]const u8, arg_string: []const u8, variant: types.Variant) void {
+    if (opt_switch_string) |switch_string| {
+        writeOptAssignString(array, switch_string, variant);
+    }
+    writeFormatterInternal(array, arg_string, variant);
 }
 pub fn writeFunctionBody(array: *Array, options: []const types.OptionSpec, variant: types.Variant) void {
     for (options) |opt_spec| {
@@ -278,6 +313,25 @@ pub fn writeFunctionBody(array: *Array, options: []const types.OptionSpec, varia
                     writeSwitch(array, opt_spec.name);
                     writeYesRequiredProng(array);
                     writeFormatter(array, opt_spec.string, "yes_arg", variant);
+                    writeProngClose(array);
+                    writeNoProng(array);
+                    writeOptString(array, no_opt_spec.string.?, variant);
+                    writeProngClose(array);
+                    writeIfClose(array);
+                    writeIfClose(array);
+                    continue;
+                }
+            }
+            if (opt_spec.arg_info.tag == .optional_formatter) {
+                if (no_opt_spec.arg_info.tag == .boolean) {
+                    writeIfOptionalField(array, opt_spec.name);
+                    writeSwitch(array, opt_spec.name);
+                    writeYesRequiredProng(array);
+                    writeIfOptional(array, "yes_arg", "yes_optional_arg");
+                    writeOptionalFormatter(array, opt_spec.string, "yes_optional_arg", variant);
+                    writeElse(array);
+                    writeOptString(array, opt_spec.string.?, variant);
+                    writeIfClose(array);
                     writeProngClose(array);
                     writeNoProng(array);
                     writeOptString(array, no_opt_spec.string.?, variant);
@@ -355,22 +409,22 @@ pub fn main() !void {
     var fd: u64 = file.open(open_spec, command_line_template_path);
     array.define(file.readSlice(read_spec, fd, array.referAllUndefined()[0..st.size]));
     file.close(close_spec, fd);
-    array.writeMany("pub fn buildWrite(cmd:*const types.BuildCommand,buf:[*]u8)u64{\n");
+    array.writeMany("pub fn buildWrite(cmd:*const tasks.BuildCommand,buf:[*]u8)u64{\n");
     array.writeMany("var len:u64=0;\n");
     writeFunctionBody(&array, attr.build_command_options, .write);
     array.writeMany("return len;\n");
     array.writeMany("}\n");
-    array.writeMany("pub fn buildLength(cmd: *const types.BuildCommand)u64{\n");
+    array.writeMany("pub fn buildLength(cmd: *const tasks.BuildCommand)u64{\n");
     array.writeMany("var len:u64=0;\n");
     writeFunctionBody(&array, attr.build_command_options, .length);
     array.writeMany("return len;\n");
     array.writeMany("}\n");
-    array.writeMany("pub fn formatLength(cmd:*const types.FormatCommand)u64{\n");
+    array.writeMany("pub fn formatLength(cmd:*const tasks.FormatCommand)u64{\n");
     array.writeMany("var len: u64 = 0;\n");
     writeFunctionBody(&array, attr.format_command_options, .length);
     array.writeMany("return len;\n");
     array.writeMany("}\n");
-    array.writeMany("pub fn formatWrite(cmd:*const types.FormatCommand,buf:[*]u8)u64{\n");
+    array.writeMany("pub fn formatWrite(cmd:*const tasks.FormatCommand,buf:[*]u8)u64{\n");
     array.writeMany("var len: u64 = 0;\n");
     writeFunctionBody(&array, attr.format_command_options, .write);
     array.writeMany("return len;\n");
