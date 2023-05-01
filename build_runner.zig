16f326dbce17b0ff1b9565693604bb293f4f7386
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-01 13:12:58 +0000

    Updated build runner.

--- build_runner.zig
+++ build_runner.zig
@@ -7,21 +7,37 @@ const srg = blk: {
         break :blk root.zig_lib;
     }
 };
-pub usingnamespace root;
-pub usingnamespace srg.proc.start;
+const proc = srg.proc;
+const meta = srg.meta;
+const spec = srg.spec;
+const build = srg.build;
 
-const Builder = if (@hasDecl(root, "Builder")) root.Builder else srg.build.GenericBuilder(srg.spec.builder.default);
+pub usingnamespace root;
+pub usingnamespace proc.start;
+
+const Builder = if (@hasDecl(root, "Builder"))
+    root.Builder
+else
+    build.GenericBuilder(spec.builder.default);
 
 pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
     var address_space: Builder.AddressSpace = .{};
     var thread_space: Builder.ThreadSpace = .{};
-    var allocator: Builder.Allocator = Builder.Allocator.init(&address_space, Builder.max_thread_count);
+
+    var allocator: Builder.Allocator = try meta.wrap(
+        Builder.Allocator.init(&address_space, Builder.max_thread_count),
+    );
     defer allocator.deinit(&address_space, Builder.max_thread_count);
+
     if (args.len < 5) {
         return error.MissingEnvironmentPaths;
     }
-    const build_fn = root.buildMain;
-    var builder: Builder = try srg.meta.wrap(Builder.init(args, vars));
-    try build_fn(&allocator, &builder);
-    builder.processCommands(&address_space, &thread_space, &allocator);
+    var builder: Builder = try meta.wrap(Builder.init(args, vars));
+
+    try meta.wrap(
+        root.buildMain(&allocator, &builder),
+    );
+    try meta.wrap(
+        builder.processCommands(&address_space, &thread_space, &allocator),
+    );
 }
