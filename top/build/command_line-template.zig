c1b546c4178e1fc06a37c0b6014377d576a08056
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-01 08:39:14 +0000

    Added mapping for `[]const []const u8` to `CFlags`. This solution will probably be insufficient in the long term.

--- top/build/command_line-template.zig
+++ top/build/command_line-template.zig
@@ -9,6 +9,7 @@ fn FormatMap(comptime T: type) type {
         []const types.Path => return types.Files,
         []const types.Macro => return types.Macros,
         []const types.Module => return types.Modules,
+        []const []const u8 => return types.CFlags,
         else => @compileError(@typeName(T)),
     }
 }
