b3c2672263b49533619b40c1e6b50178dcc51f2e
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-01 06:00:26 +0000

    Added test program for command line options.

--- test/build1-test.zig
+++ test/build1-test.zig
@@ -1,8 +1,11 @@
 const zig_lib = @import("../zig_lib.zig");
+
 const proc = zig_lib.proc;
+const spec = zig_lib.spec;
+const build = zig_lib.build;
 
 pub usingnamespace proc.start;
 
-pub fn main() void {
+const Builder = build.GenericBuilder(spec.builder.default);
 
-}
+pub fn main() void {}
