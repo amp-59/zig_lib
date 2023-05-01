edea3ba9b8d3a73985f543bbb896ca82ac725375
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-01 13:13:40 +0000

    Compacted.

--- top/mem/touch-aux.zig
+++ top/mem/touch-aux.zig
@@ -4,26 +4,13 @@ const proc = gen.proc;
 const file = gen.file;
 const spec = gen.spec;
 const builtin = gen.builtin;
-
 const config = @import("./config.zig");
-
 pub usingnamespace proc.start;
-
 pub const logging_override: builtin.Logging.Override = spec.logging.override.silent;
-
 const Array = mem.StaticString(0);
-
-const mkdir_spec: file.MakeDirSpec = .{
-    .errors = .{},
-};
-const create_spec: file.CreateSpec = .{
-    .errors = .{},
-    .options = .{ .write = .truncate, .exclusive = false },
-};
-const close_spec: file.CloseSpec = .{
-    .errors = .{},
-};
-
+const mkdir_spec: file.MakeDirSpec = .{ .errors = .{} };
+const create_spec: file.CreateSpec = .{ .errors = .{}, .options = .{ .write = .truncate, .exclusive = false } };
+const close_spec: file.CloseSpec = .{ .errors = .{} };
 pub fn main() void {
     file.makeDir(mkdir_spec, config.zig_out_dir, file.dir_mode);
     file.makeDir(mkdir_spec, config.zig_out_src_dir, file.dir_mode);
