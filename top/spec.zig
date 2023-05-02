016a6cd97a492432b72b07f298a6ef045abe09af
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-02 08:15:56 +0000

    * Added `create` preset namespace. * Added preset constants `truncate_zen` and `truncate_noexcept`. * Added preset constants `append_zen` and `append_noexcept` to `open`   preset namespace.

--- top/spec.zig
+++ top/spec.zig
@@ -575,7 +575,26 @@ const sys = struct {
             };
         };
     };
+    pub const create = struct {
+        /// Replace existing file
+        pub const truncate_zen: zig_lib.file.CreateSpec = .{
+            .errors = .{ .throw = sys.open.errors.all },
+            .options = .{ .write = .truncate, .exclusive = false },
+        };
+        pub const truncate_noexcept: zig_lib.file.CreateSpec = .{
+            .errors = .{},
+            .options = .{ .write = .truncate, .exclusive = false },
+        };
+    };
     pub const open = struct {
+        pub const append_zen: zig_lib.file.OpenSpec = .{
+            .errors = .{ .throw = sys.open.errors.all },
+            .options = .{ .write = .append },
+        };
+        pub const append_noexcept: zig_lib.file.OpenSpec = .{
+            .errors = .{},
+            .options = .{ .write = .append },
+        };
         pub const errors = struct {
             pub const all: []const zig_lib.sys.ErrorCode = &.{
                 .ACCES, .FBIG,        .NOTDIR,   .EXIST,  .OPNOTSUPP, .MFILE, .NOSPC,
