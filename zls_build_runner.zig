e723d10e76a00d36ba0d273e4ea47d0314399b4b
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-03 16:14:57 +0000

    Updated ZLS build runner.

--- zls_build_runner.zig
+++ zls_build_runner.zig
@@ -1,6 +1,5 @@
 const root = @import("@build");
-pub const dependencies = @import("@dependencies");
-const srg = blk: {
+const zig_lib = blk: {
     if (@hasDecl(root, "srg")) {
         break :blk root.srg;
     }
@@ -8,16 +7,19 @@ const srg = blk: {
         break :blk root.zig_lib;
     }
 };
-const proc = srg.proc;
-const mach = srg.mach;
-const file = srg.file;
-const meta = srg.meta;
-const spec = srg.spec;
-const build = srg.build;
-const builtin = srg.builtin;
-
+const proc = zig_lib.proc;
+const meta = zig_lib.meta;
+const file = zig_lib.file;
+const spec = zig_lib.spec;
+const build = zig_lib.build;
+const builtin = zig_lib.builtin;
+const dependencies = @import("@dependencies");
+pub usingnamespace root;
 pub usingnamespace proc.start;
-
+const Builder = if (@hasDecl(root, "Builder"))
+    root.Builder
+else
+    build.GenericBuilder(spec.builder.default);
 pub const BuildConfig = struct {
     packages: []Pkg,
     include_dirs: []const []const u8,
@@ -25,142 +27,132 @@ pub const BuildConfig = struct {
         name: []const u8,
         path: []const u8,
     };
-    pub fn formatWrite(cfg: BuildConfig, array: anytype) void {
-        array.writeMany("{ \"packages\": ");
-        if (cfg.packages.len == 0) {
-            array.writeMany("[],");
-        } else {
-            array.writeMany("[");
-            array.writeMany("{ \"name\": \"");
-            array.writeMany(cfg.packages[0].name);
-            array.writeMany("\", \"path\": \"");
-            array.writeMany(cfg.packages[0].path);
-            array.writeMany("\" }");
-            for (cfg.packages[1..]) |pkg| {
-                array.writeMany(",\n               { \"name\": \"");
-                array.writeMany(pkg.name);
-                array.writeMany("\", \"path\": \"");
-                array.writeMany(pkg.path);
-                array.writeMany("\" }");
-            }
-            array.writeMany("],\n");
+};
+inline fn cpy(buf: [*]u8, any: anytype) u64 {
+    @ptrCast(*@TypeOf(any), buf).* = any;
+    return any.len;
+}
+pub fn lengthJSON(cfg: *const BuildConfig) u64 {
+    @setRuntimeSafety(false);
+    var len: u64 = 34;
+    if (cfg.packages.len == 0) {
+        len +%= 3;
+    } else {
+        len +%= 27 +% cfg.packages[0].name.len +%
+            cfg.packages[0].path.len +%
+            cfg.packages[1..].len *% 43;
+        for (cfg.packages[1..]) |pkg| {
+            len +%= pkg.name.len +% pkg.path.len;
         }
-        array.writeMany("  \"include_dirs\": ");
-        if (cfg.include_dirs.len == 0) {
-            array.writeMany("[]");
-        } else {
-            array.writeMany("[\"");
-            array.writeMany(cfg.include_dirs[0]);
-            array.writeMany("\"");
-            for (cfg.include_dirs[1..]) |dir| {
-                array.writeMany(",\n                   \"");
-                array.writeMany(dir);
-                array.writeMany("\"");
-            }
-            array.writeMany("]");
-        }
-        array.writeMany("}\n");
+        len +%= 3;
     }
-    pub fn formatLength(cfg: BuildConfig) u64 {
-        var len: u64 = 0;
-        len +%= "{ \"packages\": ".len;
-        if (cfg.packages.len == 0) {
-            len +%= "[],".len;
-        } else {
-            len +%= "[".len;
-            len +%= "{ \"name\": \"".len;
-            len +%= cfg.packages[0].name.len;
-            len +%= "\", \"path\": \"".len;
-            len +%= cfg.packages[0].path.len;
-            len +%= "\" }".len;
-            for (cfg.packages[1..]) |pkg| {
-                len +%= ",\n               { \"name\": \"".len;
-                len +%= pkg.name.len;
-                len +%= "\", \"path\": \"".len;
-                len +%= pkg.path.len;
-                len +%= " }".len;
-            }
-            len +%= "],\n".len;
+    if (cfg.include_dirs.len == 0) {
+        len +%= 2;
+    } else {
+        len +%= 3 +% cfg.include_dirs[0].len +%
+            cfg.include_dirs[1..].len *% 23;
+        for (cfg.include_dirs[1..]) |dir| {
+            len +%= dir.len;
         }
-        len +%= "  \"include_dirs\": ".len;
-        if (cfg.include_dirs.len == 0) {
-            len +%= "[]".len;
-        } else {
-            len +%= "[\"".len;
-            len +%= cfg.include_dirs[0].len;
-            len +%= "\"".len;
-            for (cfg.include_dirs[1..]) |dir| {
-                len +%= ",\n                   \"".len;
-                len +%= dir.len;
-                len +%= "\"".len;
-            }
-            len +%= "]".len;
+        len +%= 1;
+    }
+    return len;
+}
+pub fn writeJSON(cfg: *const BuildConfig, buf: [*]u8) u64 {
+    @setRuntimeSafety(false);
+    var len: u64 = 0;
+    len +%= cpy(buf + len, "{ \"packages\": ".*);
+    if (cfg.packages.len == 0) {
+        len +%= cpy(buf + len, "[],".*);
+    } else {
+        len +%= cpy(buf + len, "[{ \"name\": \"".*);
+        @memcpy(buf + len, cfg.packages[0].name.ptr, cfg.packages[0].name.len);
+        len +%= cfg.packages[0].name.len;
+        len +%= cpy(buf + len, "\", \"path\": \"".*);
+        @memcpy(buf + len, cfg.packages[0].path.ptr, cfg.packages[0].path.len);
+        len +%= cfg.packages[0].path.len;
+        len +%= cpy(buf + len, "\" }".*);
+        for (cfg.packages[1..]) |pkg| {
+            len +%= cpy(buf + len, ",\n               { \"name\": \"".*);
+            @memcpy(buf + len, pkg.name.ptr, pkg.name.len);
+            len +%= pkg.name.len;
+            len +%= cpy(buf + len, "\", \"path\": \"".*);
+            @memcpy(buf + len, pkg.path.ptr, pkg.path.len);
+            len +%= pkg.path.len;
+            len +%= cpy(buf + len, "\" }".*);
         }
-        len +%= "}\n".len;
-        return len;
+        len +%= cpy(buf + len, "],\n".*);
     }
-};
-const Packages = Builder.Allocator.StructuredVector(BuildConfig.Pkg);
-const String = Builder.Allocator.StructuredVector(u8);
-
-pub const message_style: [:0]const u8 =
-    if (@hasDecl(root, "message_style")) root.message_style else "\x1b[2m";
-
-pub const logging_override: builtin.Logging.Override =
-    if (@hasDecl(root, "logging_override")) root.logging_override else .{
-    .Success = null,
-    .Acquire = null,
-    .Release = null,
-    .Error = null,
-    .Fault = null,
-};
-pub const logging_default: builtin.Logging.Default =
-    if (@hasDecl(root, "logging_default")) root.logging_default else .{
-    .Success = false,
-    .Acquire = false,
-    .Release = false,
-    .Error = true,
-    .Fault = true,
-};
-pub const signal_handlers: builtin.SignalHandlers =
-    if (@hasDecl(root, "signal_handlers")) root.signal_handlers else .{
-    .segmentation_fault = true,
-    .floating_point_error = false,
-    .illegal_instruction = false,
-    .bus_error = false,
-};
-pub const runtime_assertions: bool =
-    if (@hasDecl(root, "runtime_assertions")) root.runtime_assertions else false;
-
-pub const Builder =
-    if (@hasDecl(root, "Builder")) root.Builder else build.GenericBuilder(.{
-    .errors = spec.builder.errors.noexcept,
-    .logging = spec.builder.logging.silent,
-});
-pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
-    var address_space: Builder.AddressSpace = .{};
-    var allocator: Builder.Allocator = Builder.Allocator.init(&address_space, Builder.max_thread_count);
-    if (args.len < 5) {
-        return error.MissingEnvironmentPaths;
+    len +%= cpy(buf + len, "  \"include_dirs\": ".*);
+    if (cfg.include_dirs.len == 0) {
+        len +%= cpy(buf + len, "[]".*);
+    } else {
+        len +%= cpy(buf + len, "[\"".*);
+        @memcpy(buf + len, cfg.include_dirs[0].ptr, cfg.include_dirs[0].len);
+        len +%= cfg.include_dirs[0].len;
+        len +%= cpy(buf + len, "\"".*);
+        for (cfg.include_dirs[1..]) |dir| {
+            len +%= cpy(buf + len, ",\n                   \"".*);
+            @memcpy(buf + len, dir.ptr, dir.len);
+            len +%= dir.len;
+            len +%= cpy(buf + len, "\"".*);
+        }
+        len +%= cpy(buf + len, "]".*);
     }
-    const build_fn = root.buildMain;
-    var builder: Builder = try meta.wrap(Builder.init(args, vars));
-    try build_fn(&allocator, &builder);
-    var pkg_array: Packages = Packages.init(&allocator, 32);
+    len +%= cpy(buf + len, "}\n".*);
+    return len;
+}
+fn lengthModules(builder: *Builder) u64 {
+    var len: u64 = 0;
+    for (builder.groups()) |group| {
+        for (group.targets()) |target| {
+            if (target.build_cmd.modules) |mods| {
+                len +%= mods.len;
+            }
+        }
+    }
+    return len;
+}
+fn writeModules(pkgs: []BuildConfig.Pkg, builder: *Builder) []BuildConfig.Pkg {
+    var len: u64 = 0;
     for (builder.groups()) |group| {
         for (group.targets()) |target| {
             if (target.build_cmd.modules) |mods| {
                 for (mods) |mod| {
-                    pkg_array.appendOne(&allocator, .{ .name = mod.name, .path = mod.path });
+                    pkgs[len] = .{
+                        .name = mod.name,
+                        .path = mod.path,
+                    };
+                    len +%= 1;
                 }
             }
         }
     }
+    return pkgs[0..len];
+}
+pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
+    var address_space: Builder.AddressSpace = .{};
+    var allocator: Builder.Allocator = try meta.wrap(
+        Builder.Allocator.init(&address_space, Builder.max_thread_count),
+    );
+    defer allocator.deinit(&address_space, Builder.max_thread_count);
+    if (args.len < 5) {
+        return error.MissingEnvironmentPaths;
+    }
+    var builder: Builder = try meta.wrap(Builder.init(args, vars));
+    try meta.wrap(
+        root.buildMain(&allocator, &builder),
+    );
+    const pkgs_len: u64 = lengthModules(&builder);
+    const pkgs: []BuildConfig.Pkg = try meta.wrap(
+        allocator.allocateIrreversible(BuildConfig.Pkg, pkgs_len),
+    );
     const cfg: BuildConfig = .{
-        .packages = pkg_array.referAllDefined(),
+        .packages = writeModules(pkgs, &builder),
         .include_dirs = &.{},
     };
-    var str_array: String = String.init(&allocator, cfg.formatLength());
-    str_array.writeFormat(cfg);
-    try file.write(.{}, 1, str_array.readAll());
+    const buf: []u8 = try meta.wrap(
+        allocator.allocateIrreversible(u8, lengthJSON(&cfg)),
+    );
+    try file.writeSlice(.{}, 1, buf[0..writeJSON(&cfg, buf.ptr)]);
 }
