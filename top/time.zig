cfda207cd560ef284c17fede4f2724f7691a3962
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-05 12:56:41 +0000

    Added `PackedDateTime` data structure.

--- top/time.zig
+++ top/time.zig
@@ -72,7 +72,7 @@ pub const ClockSpec = struct {
 pub const ClockGetTime = *fn (Kind, *TimeSpec) u64;
 pub const GetTimeOfDay = *fn (*TimeVal, *TimeZone) u64;
 
-pub fn get(comptime spec: ClockSpec, kind: Kind) sys.Call(spec.errors, TimeSpec) {
+pub fn get(comptime spec: ClockSpec, kind: Kind) sys.ErrorUnion(spec.errors, TimeSpec) {
     var ts: TimeSpec = undefined;
     if (meta.wrap(sys.call(.clock_gettime, spec.errors, void, .{ @enumToInt(kind), @ptrToInt(&ts) }))) {
         return ts;
@@ -84,7 +84,7 @@ pub const SleepSpec = struct {
     return_type: type = void,
     errors: sys.ErrorPolicy = .{ .throw = sys.nanosleep_errors },
 };
-pub fn sleep(comptime spec: SleepSpec, ts: TimeSpec) sys.Call(spec.errors, void) {
+pub fn sleep(comptime spec: SleepSpec, ts: TimeSpec) sys.ErrorUnion(spec.errors, void) {
     meta.wrap(sys.call(.nanosleep, spec.errors, spec.return_type, .{ @ptrToInt(&ts), 0 })) catch |nanosleep_error| {
         return nanosleep_error;
     };
@@ -132,9 +132,9 @@ pub const DateTime = extern struct {
     pub fn getYearDay(dt: DateTime) u16 {
         return dt.yday + 1;
     }
-    pub inline fn init(epoch_seconds: u64) DateTime {
+    pub fn init(epoch_seconds: u64) DateTime {
         if (epoch_seconds < leap_epoch) {
-            builtin.debug.logFault("TODO: Dates before epoch");
+            builtin.proc.exitGroupFault("TODO: Dates before epoch", 2);
         } else {
             const secs: u64 = epoch_seconds - leap_epoch;
             const days: u64 = secs / 86_400;
@@ -145,39 +145,39 @@ pub const DateTime = extern struct {
                 const cycles: u64 = rem_days / days_per_100y;
                 break :blk cycles - builtin.int(u64, cycles == 4);
             };
-            rem_days -= c_cycles * days_per_100y;
+            rem_days -%= c_cycles *% days_per_100y;
             const q_cycles: u64 = blk: {
                 const cycles: u64 = rem_days / days_per_4y;
                 break :blk cycles - builtin.int(u64, cycles == 25);
             };
-            rem_days -= q_cycles * days_per_4y;
+            rem_days -%= q_cycles *% days_per_4y;
             const rem_years: u64 = blk: {
                 const rem: u64 = rem_days / days_per_year;
                 break :blk rem - builtin.int(u64, rem == 4);
             };
-            rem_days -= rem_years * days_per_year;
-            const years: u64 = rem_years +
-                (4 * q_cycles) + (100 * c_cycles) + (400 * qc_cycles);
+            rem_days -%= rem_years *% days_per_year;
+            const years: u64 = rem_years +%
+                (4 *% q_cycles) +% (100 *% c_cycles) +% (400 *% qc_cycles);
             const leap_day: u64 = blk: {
                 const b0: bool = rem_years == 0;
                 const b1: bool = builtin.int2v(bool, q_cycles != 0, c_cycles == 0);
                 break :blk builtin.int2v(u64, b0, b1);
             };
             const year_day: u64 = blk: {
-                const leap_days: u64 = rem_days + 31 + 28 + leap_day;
-                const leap_year: u64 = days_per_year + leap_day;
+                const leap_days: u64 = rem_days +% 31 +% 28 +% leap_day;
+                const leap_year: u64 = days_per_year +% leap_day;
                 break :blk leap_days - mach.cmov64z(leap_year < leap_days, leap_year);
             };
             var months: u8 = 0;
-            while (rem_days > days_in_month[months]) : (months += 1) {
-                rem_days -= days_in_month[months];
+            while (rem_days > days_in_month[months]) : (months +%= 1) {
+                rem_days -%= days_in_month[months];
             }
             months -%= mach.cmov8z(months > 9, 12);
-            const year: u64 = years + builtin.int(u64, months > 9);
+            const year: u64 = years +% builtin.int(u64, months > 9);
             return .{
                 .yday = @intCast(u16, year_day),
                 .mday = @intCast(u8, rem_days),
-                .wday = @intCast(u8, (days + 3) % 7),
+                .wday = @intCast(u8, (days +% 3) % 7),
                 .mon = @intCast(u8, months +% 2),
                 .hour = @intCast(u8, rem_secs / 3600),
                 .min = @intCast(u8, (rem_secs / 60) % 60),
@@ -185,5 +185,53 @@ pub const DateTime = extern struct {
                 .year = year,
             };
         }
+    }
+    pub fn pack(dt: DateTime) PackedDateTime {
+        return .{
+            .bits = mach.shl64(dt.year, 48) |
+                mach.shl64(dt.wday, 45) | mach.shl64(dt.mon, 41) |
+                mach.shl64(dt.yday, 32) | mach.shl64(dt.mday, 24) |
+                mach.shl64(dt.hour, 16) | mach.shl64(dt.min, 8) |
+                dt.sec,
+        };
+    }
+};
+pub const PackedDateTime = extern struct {
+    bits: u64,
+    pub fn getYear(pdt: PackedDateTime) u64 {
+        return mach.shr64(pdt.bits, 48) + 2000;
+    }
+    pub fn getSecond(pdt: PackedDateTime) u8 {
+        return mach.mask8L(pdt.bits);
+    }
+    pub fn getMinute(pdt: PackedDateTime) u8 {
+        return mach.shr64T(u8, pdt.bits, 8);
+    }
+    pub fn getHour(pdt: PackedDateTime) u8 {
+        return mach.shr64TM(u8, pdt.bits, 16, 5);
+    }
+    pub fn getMonth(pdt: PackedDateTime) u8 {
+        return mach.shr64TM(u8, pdt.bits, 41, 4) + 1;
+    }
+    pub fn getWeekDay(pdt: PackedDateTime) u8 {
+        return mach.shr64TM(u8, pdt.bits, 45, 3) + 1;
+    }
+    pub fn getMonthDay(pdt: PackedDateTime) u8 {
+        return mach.shr64TM(u8, pdt.bits, 24, 5) + 1;
+    }
+    pub fn getYearDay(pdt: PackedDateTime) u16 {
+        return mach.shr64TM(u16, pdt.bits, 32, 9) + 1;
+    }
+    pub fn unpack(pdt: PackedDateTime) DateTime {
+        return .{
+            .sec = pdt.getSecond(),
+            .min = pdt.getMinute(),
+            .hour = pdt.getHour(),
+            .mday = pdt.getMonthDay(),
+            .wday = pdt.getWeekDay(),
+            .mon = pdt.getMonth(),
+            .yday = pdt.getYearDay(),
+            .year = pdt.getYear(),
+        };
     }
 };
