c44326f8362e4420e827aec0e3cc045e39daa31b
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-12 10:55:31 +0000

    * Updated imports. * Added functions for left and right rotation. Maybe move to `mach`.

--- top/math.zig
+++ top/math.zig
@@ -1,3 +1,6 @@
+const mach = @import("./mach.zig");
+const builtin = @import("./builtin.zig");
+
 pub inline fn absoluteDifference(x: anytype, y: anytype) @TypeOf(x + y) {
     return @max(x, y) - @min(x, y);
 }
@@ -12,4 +15,24 @@ pub inline fn absoluteValue(i: anytype) Absolute(@TypeOf(i)) {
     } else {
         return @bitCast(Abs, i);
     }
+}
+pub fn rotr(comptime T: type, value: T, rot_amt: u16) T {
+    builtin.static.assert(@popCount(@typeInfo(T).Int.bits) == 1);
+    builtin.static.assert(@typeInfo(T).Int.signedness == .unsigned);
+    const S = builtin.ShiftAmount(T);
+    if (T == u0) {
+        return 0;
+    }
+    const shift_amt: S = @truncate(S, rot_amt % @typeInfo(T).Int.bits);
+    return (value >> shift_amt) | (value << (1 +% ~shift_amt));
+}
+pub fn rotl(comptime T: type, value: T, rot_amt: u16) T {
+    builtin.static.assert(@popCount(@typeInfo(T).Int.bits) == 1);
+    builtin.static.assert(@typeInfo(T).Int.signedness == .unsigned);
+    const S = builtin.ShiftAmount(T);
+    if (T == u0) {
+        return 0;
+    }
+    const shift_amt: S = @intCast(S, @mod(rot_amt, @typeInfo(T).Int.bits));
+    return (value << shift_amt) | (value >> (1 +% ~shift_amt));
 }
