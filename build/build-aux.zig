44424a7fdff05f42fff412ac8cb1a9a1c8c2fb66
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-15 18:31:38 +0000

    Removed unused files.

--- build/build-aux.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const build = std.build;
-const util = @import("./util.zig");
-
-pub fn main(builder: *build.Builder) !void {
-    util.Context.init(builder);
-    try util.writeEnv(builder);
-    _ = util.addProjectExecutable(builder, "generate_build", "top/build/generate_build.zig", .{ .build_mode = .ReleaseSmall });
-}
