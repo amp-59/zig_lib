24771edde4e18c203252b52e949b36de78b15bc2
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-22 06:14:18 +0000

    Updated imports.

--- top/build/cmdline.zig
+++ top/build/cmdline.zig
@@ -1,7 +1,7 @@
 const mach = @import("../mach.zig");
 const builtin = @import("../builtin.zig");
 const types = @import("./types.zig");
-const tasks = @import("./tasks3.zig");
+const tasks = @import("./tasks.zig");
 const safety: bool = false;
 fn FormatMap(comptime T: type) type {
     switch (T) {
