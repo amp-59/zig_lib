8100cbdd990c945f23b32bcfbc3bfc049c278458
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-24 19:37:56 +0000

    Made `ptr` public.

--- top/builtin.zig
+++ top/builtin.zig
@@ -543,7 +543,7 @@ pub fn max(comptime T: type, arg1: T, arg2: T) T {
         }
     }
 }
-fn ptr(comptime T: type) *T {
+pub fn ptr(comptime T: type) *T {
     var ret: T = 0;
     return &ret;
 }
