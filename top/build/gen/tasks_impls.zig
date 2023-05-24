e1e192a6175ef65e001383022f82c9dc49e20176
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-24 20:43:53 +0000

    * Updated for deletion of some helper types. * Updated for usage of `Attributes`.

--- top/build/gen/tasks_impls.zig
+++ top/build/gen/tasks_impls.zig
@@ -19,67 +19,71 @@ const creat_spec: file.CreateSpec = .{ .errors = .{}, .logging = .{}, .options =
 const write_spec: file.WriteSpec = .{ .errors = .{}, .logging = .{} };
 const read_spec: file.ReadSpec = .{ .errors = .{}, .logging = .{} };
 const close_spec: file.CloseSpec = .{ .errors = .{}, .logging = .{} };
-fn writeType(array: *Array, opt_spec: types.OptionSpec) void {
+
+fn writeType(array: *Array, opt_spec: types.ParamSpec) void {
     if (opt_spec.and_no) |no_opt_spec| {
-        const yes_bool: bool = opt_spec.arg_info.tag == .boolean;
-        const no_bool: bool = no_opt_spec.arg_info.tag == .boolean;
+        const yes_bool: bool = opt_spec.info.tag == .boolean_field;
+        const no_bool: bool = no_opt_spec.info.tag == .boolean_field;
         if (yes_bool != no_bool) {
-            const tmp: types.ProtoTypeDescr = .{ .type_decl = .{ .Composition = .{
+            array.writeFormat(types.ProtoTypeDescr{ .type_refer = .{ .spec = "?", .type = &.{ .type_decl = .{ .Composition = .{
                 .spec = "union(enum)",
                 .fields = &.{
-                    .{ .name = "yes", .type = if (yes_bool) null else opt_spec.arg_info.type },
-                    .{ .name = "no", .type = if (no_bool) null else no_opt_spec.arg_info.type },
+                    .{ .name = "yes", .type = if (yes_bool) null else opt_spec.info.type },
+                    .{ .name = "no", .type = if (no_bool) null else no_opt_spec.info.type },
                 },
-            } } };
-            array.writeFormat(types.ArgInfo.optional(&tmp));
+            } } } } });
         } else {
-            array.writeFormat(types.ArgInfo.optional(&types.ProtoTypeDescr.init(bool)));
+            array.writeFormat(types.ProtoTypeDescr.init(?bool));
         }
     } else {
-        array.writeFormat(opt_spec.arg_info.type);
+        array.writeFormat(opt_spec.info.type);
     }
 }
-fn writeFields(array: *Array, opt_specs: []const types.OptionSpec) void {
+fn writeFields(array: *Array, opt_specs: []const types.ParamSpec) void {
     for (opt_specs) |opt_spec| {
-        if (opt_spec.descr) |field_descr| {
-            for (field_descr) |line| {
-                array.writeMany("/// ");
-                array.writeMany(line);
-                array.writeMany("\n");
+        if (opt_spec.info.isField()) {
+            if (opt_spec.descr) |field_descr| {
+                for (field_descr) |line| {
+                    array.writeMany("/// ");
+                    array.writeMany(line);
+                    array.writeMany("\n");
+                }
+            }
+            array.writeMany(opt_spec.name);
+            array.writeMany(":");
+            writeType(array, opt_spec);
+            if (opt_spec.and_no == null and
+                opt_spec.info.tag == .boolean_field)
+            {
+                array.writeMany("=false,\n");
+            } else if (opt_spec.info.tag != .tag_field) {
+                array.writeMany("=null,\n");
+            } else {
+                array.writeMany(",\n");
             }
         }
-        array.writeMany(opt_spec.name);
-        array.writeMany(":");
-        writeType(array, opt_spec);
-        if (opt_spec.and_no == null and
-            opt_spec.arg_info.tag == .boolean)
-        {
-            array.writeMany("=false,\n");
-        } else if (opt_spec.arg_info.tag != .tag) {
-            array.writeMany("=null,\n");
-        } else {
-            array.writeMany(",\n");
-        }
     }
 }
-fn writeTaskStruct(array: *Array, name: []const u8, attributes: []const types.OptionSpec) void {
+fn writeTaskStructFromAttributes(array: *Array, attributes: types.Attributes) void {
     array.writeMany("pub const ");
-    array.writeMany(name);
+    array.writeMany(attributes.type_name);
     array.writeMany("=struct{\n");
-    writeFields(array, attributes);
+    writeFields(array, attributes.params);
     array.writeMany("};\n");
 }
-
 pub fn main() !void {
     var array: Array = undefined;
     array.undefineAll();
     const fd: u64 = file.open(open_spec, config.tasks_template_path);
     array.define(file.read(read_spec, fd, array.referAllUndefined()));
     file.close(close_spec, fd);
-    writeTaskStruct(&array, "BuildCommand", attr.build_command_options);
-    writeTaskStruct(&array, "FormatCommand", attr.format_command_options);
-    writeTaskStruct(&array, "ArchiveCommand", attr.archive_command_options);
-    writeTaskStruct(&array, "TableGenCommand", attr.tblgen_command_options);
+
+    writeTaskStructFromAttributes(&array, attr.zig_build_command_attributes);
+    writeTaskStructFromAttributes(&array, attr.zig_format_command_attributes);
+    writeTaskStructFromAttributes(&array, attr.zig_ar_command_attributes);
+    writeTaskStructFromAttributes(&array, attr.llvm_tblgen_command_attributes);
+    writeTaskStructFromAttributes(&array, attr.harec_attributes);
+
     gen.truncateFile(write_spec, config.tasks_path, array.readAll());
     array.undefineAll();
 }
