8a417304b52b163e9e8202a5dfee00a48affbc81
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-05-25 09:01:57 +0000

    Simplified `ParamInfo` structure.

--- top/build/gen/types.zig
+++ top/build/gen/types.zig
@@ -25,52 +25,63 @@ pub const ProtoTypeDescr = fmt.GenericTypeDescrFormat(.{
         .indent = "",
     },
 });
-pub const ParamInfo = struct {
+pub const ProtoTypeDescrMap = [2]ProtoTypeDescr;
+pub const boolean: ProtoTypeDescr = ProtoTypeDescr.init(bool);
+
+const Tag = enum {
+    boolean_field,
+    symbol_field,
+    string_field,
+    tag_field,
+    integer_field,
+    formatter_field,
+    mapped_field,
+    optional_boolean_field,
+    optional_string_field,
+    optional_tag_field,
+    optional_integer_field,
+    optional_formatter_field,
+    optional_mapped_field,
+    repeatable_string_field,
+    repeatable_tag_field,
+    string_param,
+    formatter_param,
+    mapped_param,
+    integer_literal,
+    string_literal,
+};
+pub const ParamSpec = struct {
+    /// Command struct field name
+    name: []const u8 = &.{},
+    /// For options with -f<name> and -fno-<name> variants
+    and_no: ?InverseParamSpec = null,
+    /// Command line flag/switch for non-mapped parameters.
+    string: []const u8 = &.{},
+    /// Description to be inserted above the field as documentation comment
+    descr: []const []const u8 = &.{},
     /// Describes how the argument should be written to the command line buffer
     tag: Tag = .boolean_field,
-    /// Describes how the field type should be written to the command struct
-    type: ProtoTypeDescr = ProtoTypeDescr.init(bool),
+    /// Describes how the field type should be written to the command struct,
+    /// can be `*const ProtoTypeDescr` or `*const [2]ProtoTypeDescr` depending
+    /// on the kind of parameter.
+    type: *align(8) const anyopaque = &boolean,
     /// Specifies whether option arguments are separated with '\x00' or '='
     /// If `null` the separator is determined by context
     /// If `immediate` (255) no separator is written
     char: ?u8 = null,
-    const Tag = enum(u8) {
-        boolean_field = 0,
-        symbol_field,
-        string_field,
-        tag_field,
-        integer_field,
-        formatter_field,
-        mapped_field,
-        optional_boolean_field,
-        optional_string_field,
-        optional_tag_field,
-        optional_integer_field,
-        optional_formatter_field,
-        optional_mapped_field,
-        repeatable_string_field,
-        repeatable_tag_field,
-
-        string_param,
-        formatter_param,
-        mapped_param,
-
-        integer_literal,
-        string_literal,
-    };
     pub const immediate: u8 = 255;
-    pub fn isField(param_info: ParamInfo) bool {
-        return !param_info.isFnParam() and !param_info.isLiteral();
+    pub fn isField(param_spec: ParamSpec) bool {
+        return !param_spec.isFnParam() and !param_spec.isLiteral();
     }
-    pub fn isLiteral(param_info: ParamInfo) bool {
-        switch (param_info.tag) {
+    pub fn isLiteral(param_spec: ParamSpec) bool {
+        switch (param_spec.tag) {
             .string_literal, .integer_literal => {
                 return true;
             },
             else => return false,
         }
     }
-    pub fn isFnParam(param_info: ParamInfo) bool {
+    pub fn isFnParam(param_info: ParamSpec) bool {
         switch (param_info.tag) {
             .string_param, .formatter_param, .mapped_param => {
                 return true;
@@ -78,25 +89,28 @@ pub const ParamInfo = struct {
             else => return false,
         }
     }
-};
-pub const ParamSpec = struct {
-    /// Command struct field name
-    name: []const u8 = &.{},
-    /// Command line flag/switch
-    string: []const u8 = &.{},
-    /// Simple argument type
-    info: ParamInfo = .{},
-    /// For options with -f<name> and -fno-<name> variants
-    and_no: ?InverseParamSpec = null,
-    /// Maybe define default value of this field. Should be false or null, but
-    /// allow the exception.
-    default_value: ?[]const u8 = null,
-    /// Description to be inserted above the field as documentation comment
-    descr: []const []const u8 = &.{},
+    pub fn parameter(param_spec: @This()) ProtoTypeDescr {
+        return @ptrCast(*const ProtoTypeDescr, param_spec.type).*;
+    }
+    pub fn formatter(param_spec: @This()) ProtoTypeDescr {
+        return @ptrCast(*const ProtoTypeDescrMap, param_spec.type)[1];
+    }
 };
 pub const InverseParamSpec = struct {
     /// Command line flag/switch
     string: []const u8 = &.{},
-    /// Simple argument type
-    info: ParamInfo = .{},
+    /// Describes how the argument should be written to the command line buffer
+    tag: Tag = .boolean_field,
+    /// Describes how the field type should be written to the command struct
+    type: *align(8) const anyopaque = &boolean,
+    /// Specifies whether option arguments are separated with '\x00' or '='
+    /// If `null` the separator is determined by context
+    /// If `immediate` (255) no separator is written
+    char: ?u8 = null,
+    pub fn parameter(param_spec: @This()) ProtoTypeDescr {
+        return @ptrCast(*const ProtoTypeDescr, param_spec.type).*;
+    }
+    pub fn formatter(param_spec: @This()) ProtoTypeDescr {
+        return @ptrCast(*const [2]ProtoTypeDescrMap, param_spec.type)[1];
+    }
 };
