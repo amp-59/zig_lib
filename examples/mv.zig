e5f095d483ace5c48e8d8748cd82ed6bf6158572
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-01 11:22:54 +0000

    Updated `mv` to use library implementation of `copy_file_range`.

--- examples/mv.zig
+++ examples/mv.zig
@@ -2,6 +2,7 @@ const srg = @import("../zig_lib.zig");
 const sys = srg.sys;
 const mem = srg.mem;
 const proc = srg.proc;
+const spec = srg.spec;
 const file = srg.file;
 const mach = srg.mach;
 const meta = srg.meta;
@@ -9,28 +10,24 @@ const builtin = srg.builtin;
 
 pub usingnamespace proc.start;
 
-pub const logging_override: builtin.Logging.Override = .{
-    .Attempt = false,
-    .Success = false,
-    .Acquire = false,
-    .Release = false,
-    .Error = false,
-    .Fault = false,
-};
+pub const logging_override: builtin.Logging.Override = spec.logging.override.verbose;
 
-pub fn main(args_in: [][*:0]u8) !void {
-    if (args_in.len == 1) {
+pub fn copy(src_pathname: [:0]const u8, dest_pathname: [:0]const u8) !void {
+    blk: {
+        const from_fd: u64 = file.open(.{ .errors = .{}, .options = .{ .read = true, .no_follow = true } }, src_pathname);
+        if (from_fd > 1024) break :blk;
+        const to_fd: u64 = file.create(.{ .errors = .{}, .options = .{ .exclusive = false } }, dest_pathname, file.mode.executable);
+        if (to_fd > 1024) break :blk;
+        const size: u64 = file.status(.{ .errors = .{} }, from_fd).size;
+        if (size == 0) break :blk;
+        builtin.assertEqual(u64, size, file.copy(.{ .errors = .{} }, from_fd, null, to_fd, null, size));
+    }
+}
+pub fn main(args: [][*:0]u8) !void {
+    if (args.len != 3) {
         return;
     }
-    const from: [:0]const u8 = meta.manyToSlice(args_in[1]);
-    const to: [:0]const u8 = meta.manyToSlice(args_in[2]);
-    const from_fd: u64 = file.open(.{ .errors = .{}, .options = .{ .read = true, .no_follow = true } }, from);
-    const to_fd: u64 = file.create(.{ .errors = .{}, .options = .{ .exclusive = false } }, to, file.mode.regular);
-    file.assert(.{ .errors = .{} }, from_fd, .regular);
-    file.assert(.{ .errors = .{} }, to_fd, .regular);
-    var buf: [4096]u8 = undefined;
-    var len: u64 = file.read(.{ .errors = .{} }, from_fd, &buf);
-    while (len != 0) : (len = file.read(.{ .errors = .{} }, from_fd, &buf)) {
-        file.write(.{ .errors = .{} }, to_fd, buf[0..len]);
-    }
+    const src_pathname: [:0]const u8 = mach.manyToSlice80(args[1]);
+    const dest_pathname: [:0]const u8 = mach.manyToSlice80(args[2]);
+    try copy(src_pathname, dest_pathname);
 }
