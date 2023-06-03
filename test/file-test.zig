ea6d432a2d88d1203792fd78bc4edcdfc76dd6c1
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-03 23:19:08 +0000

    * Substituted former test directory `/run/user/1000` with the project's   own test directory. * Added test functions for `linkAt`, `symbolicLink`, and `symbolicLinkAt`.

--- test/file-test.zig
+++ test/file-test.zig
@@ -26,6 +26,8 @@ const seek_spec: file.SeekSpec = .{};
 const create_spec: file.CreateSpec = .{
     .options = .{ .read = true, .write = true, .append = false },
 };
+const path_spec: file.PathSpec = .{};
+const file_path_spec: file.PathSpec = .{ .options = .{ .directory = false } };
 const link_spec: file.LinkSpec = .{};
 const copy_spec: file.CopySpec = .{};
 const open_spec: file.OpenSpec = .{
@@ -63,6 +65,15 @@ fn testPoll() !void {
     };
     try file.poll(poll_spec, &pollfds, 100);
 }
+
+const file_name: [:0]const u8 = "file_test";
+const test_dir: [:0]const u8 = @import("env").build_root ++ "/test/";
+
+const pathname1: [:0]const u8 = test_dir ++ file_name ++ "1";
+const pathname2: [:0]const u8 = test_dir ++ file_name ++ "2";
+const pathname_link1: [:0]const u8 = test_dir ++ file_name ++ "1";
+const pathname_link2: [:0]const u8 = test_dir ++ file_name ++ "2";
+
 fn testRecords() !void {
     const build_root: []const u8 = comptime builtin.buildRoot();
     const proj_stats_root: [:0]const u8 = comptime build_root ++ "/zig-stat/file_test";
@@ -81,8 +92,8 @@ fn testStatusExtended() !void {
 }
 fn testCopyFileRange() !void {
     builtin.debug.write(@src().fn_name ++ ":\n");
-    const src_fd: u64 = try meta.wrap(file.create(create_spec, "/run/user/1000/file_test1", file.mode.regular));
-    const dest_fd: u64 = try meta.wrap(file.create(create_spec, "/run/user/1000/file_test2", file.mode.regular));
+    const src_fd: u64 = try meta.wrap(file.create(create_spec, pathname1, file.mode.regular));
+    const dest_fd: u64 = try meta.wrap(file.create(create_spec, pathname2, file.mode.regular));
     var rng: file.DeviceRandomBytes(65536) = .{};
     try meta.wrap(file.write(write_spec, src_fd, &rng.readCount(u8, 65536)));
     builtin.assertEqual(u64, 0, try meta.wrap(file.seek(seek_spec, src_fd, 0, .set)));
@@ -92,21 +103,21 @@ fn testCopyFileRange() !void {
     _ = try meta.wrap(file.sync(.{}, dest_fd));
     try meta.wrap(file.close(close_spec, dest_fd));
     try meta.wrap(file.close(close_spec, src_fd));
-    try meta.wrap(file.unlink(unlink_spec, "/run/user/1000/file_test1"));
-    try meta.wrap(file.unlink(unlink_spec, "/run/user/1000/file_test2"));
+    try meta.wrap(file.unlink(unlink_spec, test_dir ++ "file_test1"));
+    try meta.wrap(file.unlink(unlink_spec, test_dir ++ "file_test2"));
 }
 fn testFileOperationsRound1() !void {
     builtin.debug.write(@src().fn_name ++ ":\n");
     var buf: [4096]u8 = undefined;
     _ = try file.getCwd(.{}, &buf);
-    try meta.wrap(file.makeDir(make_dir_spec, "/run/user/1000/file_test", file.mode.directory));
-    try meta.wrap(file.removeDir(remove_dir_spec, "/run/user/1000/file_test"));
-    const src_fd: u64 = try meta.wrap(file.create(create_spec, "/run/user/1000/file_test1", file.mode.regular));
-    const dest_fd: u64 = try meta.wrap(file.create(create_spec, "/run/user/1000/file_test2", file.mode.regular));
+    try meta.wrap(file.makeDir(make_dir_spec, test_dir ++ "file_test", file.mode.directory));
+    try meta.wrap(file.removeDir(remove_dir_spec, test_dir ++ "file_test"));
+    const src_fd: u64 = try meta.wrap(file.create(create_spec, test_dir ++ "file_test1", file.mode.regular));
+    const dest_fd: u64 = try meta.wrap(file.create(create_spec, test_dir ++ "file_test2", file.mode.regular));
     try meta.wrap(file.close(close_spec, dest_fd));
     try meta.wrap(file.close(close_spec, src_fd));
-    try meta.wrap(file.unlink(unlink_spec, "/run/user/1000/file_test1"));
-    try meta.wrap(file.unlink(unlink_spec, "/run/user/1000/file_test2"));
+    try meta.wrap(file.unlink(unlink_spec, test_dir ++ "file_test1"));
+    try meta.wrap(file.unlink(unlink_spec, test_dir ++ "file_test2"));
 }
 pub fn testSocketOpenAndClose() !void {
     builtin.debug.write(@src().fn_name ++ ":\n");
@@ -189,30 +200,30 @@ pub fn testClientAndServerIPv6(args: [][*:0]u8) !void {
 }
 pub fn testFileTests() !void {
     builtin.debug.write(@src().fn_name ++ ":\n");
-    try file.makeDir(make_dir_spec, "/run/user/1000/file_test", file.mode.directory);
-    try file.pathAssert(stat_spec, "/run/user/1000/file_test", .directory);
-    const fd: u64 = try file.open(open_dir_spec, "/run/user/1000/file_test");
-    try builtin.expect(try file.pathIs(stat_spec, "/run/user/1000/file_test", .directory));
-    try builtin.expect(try file.pathIsNot(stat_spec, "/run/user/1000/file_test", .regular));
-    try builtin.expect(try file.pathIsNot(stat_spec, "/run/user/1000/file_test", .block_special));
+    try file.makeDir(make_dir_spec, test_dir ++ "file_test", file.mode.directory);
+    try file.pathAssert(stat_spec, test_dir ++ "file_test", .directory);
+    const fd: u64 = try file.open(open_dir_spec, test_dir ++ "file_test");
+    try builtin.expect(try file.pathIs(stat_spec, test_dir ++ "file_test", .directory));
+    try builtin.expect(try file.pathIsNot(stat_spec, test_dir ++ "file_test", .regular));
+    try builtin.expect(try file.pathIsNot(stat_spec, test_dir ++ "file_test", .block_special));
     try builtin.expect(try file.is(stat_spec, fd, .directory));
     try file.close(close_spec, fd);
-    try file.removeDir(remove_dir_spec, "/run/user/1000/file_test");
+    try file.removeDir(remove_dir_spec, test_dir ++ "file_test");
 }
 fn testFileOperationsRound2() !void {
     builtin.debug.write(@src().fn_name ++ ":\n");
-    try meta.wrap(file.makeDir(make_dir_spec, "/run/user/1000/file_test", file.mode.directory));
-    const dir_fd: u64 = try meta.wrap(file.open(open_dir_spec, "/run/user/1000/file_test"));
+    try meta.wrap(file.makeDir(make_dir_spec, test_dir ++ "file_test", file.mode.directory));
+    const dir_fd: u64 = try meta.wrap(file.open(open_dir_spec, test_dir ++ "file_test"));
     try meta.wrap(file.makeDirAt(make_dir_spec, dir_fd, "file_test", file.mode.directory));
-    var path_dir_fd: u64 = try meta.wrap(file.path(.{}, "/run/user/1000/file_test/file_test"));
-    try meta.wrap(file.close(close_spec, try meta.wrap(file.create(create_spec, "/run/user/1000/file_test/file_test/file_test", file.mode.regular))));
-    const path_reg_fd: u64 = try meta.wrap(file.path(.{ .options = .{ .directory = false } }, "/run/user/1000/file_test/file_test/file_test"));
+    var path_dir_fd: u64 = try meta.wrap(file.path(path_spec, test_dir ++ "file_test/file_test"));
+    try meta.wrap(file.close(close_spec, try meta.wrap(file.create(create_spec, test_dir ++ "file_test/file_test/file_test", file.mode.regular))));
+    const path_reg_fd: u64 = try meta.wrap(file.path(file_path_spec, test_dir ++ "file_test/file_test/file_test"));
     try file.assertNot(stat_spec, path_reg_fd, .unknown);
     try file.assert(stat_spec, path_reg_fd, .regular);
-    try file.makeNode(make_node_spec, "/run/user/1000/file_test/regular", .{}, .{});
-    try file.unlink(unlink_spec, "/run/user/1000/file_test/regular");
-    try file.makeNode(make_node_spec, "/run/user/1000/file_test/fifo", .{ .kind = .named_pipe }, .{});
-    try file.unlink(unlink_spec, "/run/user/1000/file_test/fifo");
+    try file.makeNode(make_node_spec, test_dir ++ "file_test/regular", .{}, .{});
+    try file.unlink(unlink_spec, test_dir ++ "file_test/regular");
+    try file.makeNode(make_node_spec, test_dir ++ "file_test/fifo", .{ .kind = .named_pipe }, .{});
+    try file.unlink(unlink_spec, test_dir ++ "file_test/fifo");
     const new_in_fd: u64 = try file.duplicate(.{}, 0);
     try file.write(.{}, new_in_fd, builtin.fmt.ud64(new_in_fd).readAll());
     try file.duplicateTo(.{}, new_in_fd, new_in_fd +% 1);
@@ -220,15 +231,15 @@ fn testFileOperationsRound2() !void {
     try meta.wrap(file.close(close_spec, path_reg_fd));
     try meta.wrap(file.unlinkAt(unlink_spec, path_dir_fd, "file_test"));
     try meta.wrap(file.close(close_spec, path_dir_fd));
-    try meta.wrap(file.removeDir(remove_dir_spec, "/run/user/1000/file_test/file_test"));
-    try meta.wrap(file.removeDir(remove_dir_spec, "/run/user/1000/file_test"));
+    try meta.wrap(file.removeDir(remove_dir_spec, test_dir ++ "file_test/file_test"));
+    try meta.wrap(file.removeDir(remove_dir_spec, test_dir ++ "file_test"));
     try meta.wrap(file.close(close_spec, dir_fd));
     const mem_fd: u64 = try meta.wrap(mem.fd(.{}, "buffer"));
     try meta.wrap(file.truncate(ftruncate_spec, mem_fd, 4096));
 }
 fn testPathOperations() !void {
     builtin.debug.write(@src().fn_name ++ ":\n");
-    try meta.wrap(testing.expectEqualMany(u8, "file_test", file.basename("/run/user/1000/file_test")));
+    try meta.wrap(testing.expectEqualMany(u8, "file_test", file.basename(test_dir ++ "file_test")));
     try meta.wrap(testing.expectEqualMany(u8, "file_test", file.basename("1000/file_test")));
     try meta.wrap(testing.expectEqualMany(u8, "file", file.basename("file")));
     try meta.wrap(testing.expectEqualMany(u8, "/run/user/1000", file.dirname("/run/user/1000/file_test")));
@@ -293,27 +304,47 @@ fn testStandardChannel() !void {
     }
 }
 fn testLink() !void {
-    const fd: u64 = try file.create(create_spec, "/run/user/1000/file_test", file.mode.regular);
+    builtin.debug.write(@src().fn_name ++ ":\n");
+    const fd: u64 = try file.create(create_spec, test_dir ++ "file_test", file.mode.regular);
     try file.close(close_spec, fd);
-    try file.link(link_spec, "/run/user/1000/file_test", "/run/user/1000/file_test_link");
-    try file.unlink(unlink_spec, "/run/user/1000/file_test");
-    try file.unlink(unlink_spec, "/run/user/1000/file_test_link");
+    try file.link(link_spec, test_dir ++ "file_test", test_dir ++ "file_test_link");
+    try file.unlink(unlink_spec, test_dir ++ "file_test");
+    try file.unlink(unlink_spec, test_dir ++ "file_test_link");
 }
 fn testSymbolicLink() !void {
-    const fd: u64 = try file.create(create_spec, "/run/user/1000/file_test", file.mode.regular);
+    builtin.debug.write(@src().fn_name ++ ":\n");
+    const fd: u64 = try file.create(create_spec, test_dir ++ "file_test", file.mode.regular);
     try file.close(close_spec, fd);
-    try file.symbolicLink(link_spec, "/run/user/1000/file_test", "/run/user/1000/file_test_link");
-    try file.unlink(unlink_spec, "/run/user/1000/file_test");
-    try file.unlink(unlink_spec, "/run/user/1000/file_test_link");
+    try file.symbolicLink(link_spec, test_dir ++ "file_test", test_dir ++ "file_test_link");
+    try file.unlink(unlink_spec, test_dir ++ "file_test");
+    try file.unlink(unlink_spec, test_dir ++ "file_test_link");
 }
-fn testLinkAt() void {}
-fn testSymbolicLinkAt() void {}
+fn testLinkAt() !void {
+    builtin.debug.write(@src().fn_name ++ ":\n");
+    const dir_fd: u64 = try file.path(path_spec, test_dir);
+    const fd: u64 = try file.createAt(create_spec, dir_fd, "file_test", file.mode.regular);
+    try file.close(close_spec, fd);
+    try file.linkAt(link_spec, dir_fd, "file_test", dir_fd, "file_test_link");
+    try file.unlinkAt(unlink_spec, dir_fd, "file_test");
+    try file.unlinkAt(unlink_spec, dir_fd, "file_test_link");
+    try file.close(close_spec, dir_fd);
+}
+fn testSymbolicLinkAt() !void {
+    builtin.debug.write(@src().fn_name ++ ":\n");
+    const dir_fd: u64 = try file.path(path_spec, test_dir);
+    const fd: u64 = try file.createAt(create_spec, dir_fd, "file_test", file.mode.regular);
+    try file.close(close_spec, fd);
+    try file.symbolicLinkAt(link_spec, test_dir ++ "file_test", dir_fd, "file_test_link");
+    try file.unlinkAt(unlink_spec, dir_fd, "file_test");
+    try file.unlinkAt(unlink_spec, dir_fd, "file_test_link");
+    try file.close(close_spec, dir_fd);
+}
 fn testPreClean() !void {
-    file.unlink(unlink_spec, "/run/user/1000/file_test1") catch {};
-    file.unlink(unlink_spec, "/run/user/1000/file_test2") catch {};
-    file.unlink(unlink_spec, "/run/user/1000/file_test/file_test/file_test") catch {};
-    file.removeDir(remove_dir_spec, "/run/user/1000/file_test/file_test") catch {};
-    file.removeDir(remove_dir_spec, "/run/user/1000/file_test") catch {};
+    file.unlink(unlink_spec, test_dir ++ "file_test1") catch {};
+    file.unlink(unlink_spec, test_dir ++ "file_test2") catch {};
+    file.unlink(unlink_spec, test_dir ++ "file_test/file_test/file_test") catch {};
+    file.removeDir(remove_dir_spec, test_dir ++ "file_test/file_test") catch {};
+    file.removeDir(remove_dir_spec, test_dir ++ "file_test") catch {};
 }
 
 pub fn main(args: [][*:0]u8) !void {
@@ -331,5 +362,7 @@ pub fn main(args: [][*:0]u8) !void {
     try meta.wrap(testClientAndServerIPv6(args));
     try meta.wrap(testCopyFileRange());
     try meta.wrap(testLink());
+    try meta.wrap(testLinkAt());
     try meta.wrap(testSymbolicLink());
+    try meta.wrap(testSymbolicLinkAt());
 }
