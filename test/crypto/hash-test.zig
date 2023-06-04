5ca842f61af68ebe07ef695a87c81278dbd0cf20
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-04 19:10:18 +0000

    Added two easily sourced additional tests for MD5.

--- test/crypto/hash-test.zig
+++ test/crypto/hash-test.zig
@@ -293,6 +293,8 @@ fn testSha512AlignedFinal() !void {
     h.final(out[0..]);
 }
 fn testMd5Single(allocator: *mem.SimpleAllocator) !void {
+    try htest.assertEqualHash(allocator, crypto.hash.Md5, "9e107d9d372bb6826bd81d3542a419d6", "The quick brown fox jumps over the lazy dog");
+    try htest.assertEqualHash(allocator, crypto.hash.Md5, "e4d909c290d0fb1ca068ffaddf22cbd0", "The quick brown fox jumps over the lazy dog.");
     try htest.assertEqualHash(allocator, crypto.hash.Md5, "d41d8cd98f00b204e9800998ecf8427e", "");
     try htest.assertEqualHash(allocator, crypto.hash.Md5, "0cc175b9c0f1b6a831c399e269772661", "a");
     try htest.assertEqualHash(allocator, crypto.hash.Md5, "900150983cd24fb0d6963f7d28e17f72", "abc");
