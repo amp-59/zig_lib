6958a9e8fbe4ffafbafff05b5f876537bfadd608
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-04 19:12:35 +0000

    Added parameters for `Sha2`.

--- top/crypto/tab.zig
+++ top/crypto/tab.zig
@@ -72,6 +72,41 @@ pub const init_vec = .{
         0x1F83D9ABFB41BD6B, 0x5BE0CD19137E2179,
     },
 };
+/// Compiler bug preventing inclusion above.
+pub const init_vec_sha2_w = [64]u32{
+    0x428A2F98, 0x71374491,
+    0xB5C0FBCF, 0xE9B5DBA5,
+    0x3956C25B, 0x59F111F1,
+    0x923F82A4, 0xAB1C5ED5,
+    0xD807AA98, 0x12835B01,
+    0x243185BE, 0x550C7DC3,
+    0x72BE5D74, 0x80DEB1FE,
+    0x9BDC06A7, 0xC19BF174,
+    0xE49B69C1, 0xEFBE4786,
+    0x0FC19DC6, 0x240CA1CC,
+    0x2DE92C6F, 0x4A7484AA,
+    0x5CB0A9DC, 0x76F988DA,
+    0x983E5152, 0xA831C66D,
+    0xB00327C8, 0xBF597FC7,
+    0xC6E00BF3, 0xD5A79147,
+    0x06CA6351, 0x14292967,
+    0x27B70A85, 0x2E1B2138,
+    0x4D2C6DFC, 0x53380D13,
+    0x650A7354, 0x766A0ABB,
+    0x81C2C92E, 0x92722C85,
+    0xA2BFE8A1, 0xA81A664B,
+    0xC24B8B70, 0xC76C51A3,
+    0xD192E819, 0xD6990624,
+    0xF40E3585, 0x106AA070,
+    0x19A4C116, 0x1E376C08,
+    0x2748774C, 0x34B0BCB5,
+    0x391C0CB3, 0x4ED8AA4A,
+    0x5B9CCA4F, 0x682E6FF3,
+    0x748F82EE, 0x78A5636F,
+    0x84C87814, 0x8CC70208,
+    0x90BEFFFA, 0xA4506CEB,
+    0xBEF9A3F7, 0xC67178F2,
+};
 pub const sigma = .{
     .blake_2s = [10][16]u8{
         .{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
@@ -212,7 +247,6 @@ pub const rounds = .{
         .{ .a = 2, .b = 7, .c = 8, .d = 13, .x = 12, .y = 13 },
         .{ .a = 3, .b = 4, .c = 9, .d = 14, .x = 14, .y = 15 },
     },
-
     .md5_0 = [16]RoundParamST{
         .{ .a = 0, .b = 1, .c = 2, .d = 3, .k = 0, .s = 7, .t = 0xD76AA478 },
         .{ .a = 3, .b = 0, .c = 1, .d = 2, .k = 1, .s = 12, .t = 0xE8C7B756 },
