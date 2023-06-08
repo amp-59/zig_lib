bcb2ecbbf616cd6541c94330be48edc55533a026
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-08 21:18:52 +0000

    Updated public declarations compilation test to reference the entire library and several large data structures.

--- test/decl-test.zig
+++ test/decl-test.zig
@@ -2,24 +2,28 @@ const zig_lib = @import("../zig_lib.zig");
 
 pub usingnamespace zig_lib.proc.start;
 
-pub const AddressSpace = zig_lib.spec.address_space.regular_128;
-
+pub const logging_override: zig_lib.builtin.Logging.Override = zig_lib.spec.logging.override.verbose;
+pub const AddressSpace = zig_lib.spec.address_space.exact_8;
+const Allocator0 = zig_lib.mem.GenericArenaAllocator(.{
+    .arena_index = 0,
+    .AddressSpace = zig_lib.spec.address_space.regular_128,
+});
+const Allocator1 = zig_lib.mem.GenericArenaAllocator(.{
+    .arena_index = 1,
+    .AddressSpace = zig_lib.spec.address_space.exact_8,
+});
+const Builder = zig_lib.build.GenericNode(.{});
+const render = struct {
+    pub const UnionFormat = zig_lib.fmt.UnionFormat(.{}, union { one: u64, two: struct { u32, u32 } });
+    pub const StructForm = zig_lib.fmt.UnionFormat(.{}, struct { one: u64, two: *u64, three: []u8 });
+    pub const EnumFormat = zig_lib.fmt.UnionFormat(.{}, enum { one, two, three });
+};
 pub fn main() void {
-    _ = zig_lib.testing.refAllDecls(zig_lib.algo);
-    _ = zig_lib.testing.refAllDecls(zig_lib.exe);
-    _ = zig_lib.testing.refAllDecls(zig_lib.file);
-    _ = zig_lib.testing.refAllDecls(zig_lib.meta);
-    _ = zig_lib.testing.refAllDecls(zig_lib.proc);
-    _ = zig_lib.testing.refAllDecls(zig_lib.lit);
-    _ = zig_lib.testing.refAllDecls(zig_lib.mach);
-    _ = zig_lib.testing.refAllDecls(zig_lib.sys);
-    _ = zig_lib.testing.refAllDecls(zig_lib.time);
-    _ = zig_lib.testing.refAllDecls(zig_lib.math);
-    _ = zig_lib.testing.refAllDecls(zig_lib.serial);
-    _ = zig_lib.testing.refAllDecls(zig_lib.fmt);
-    _ = zig_lib.testing.refAllDecls(zig_lib.crypto);
-    _ = zig_lib.testing.refAllDecls(@import("../top/render.zig"));
-    _ = zig_lib.testing.refAllDecls(@import("../top/mem/allocator-template.zig"));
-    _ = zig_lib.testing.refAllDecls(@import("../top/mem/reference-template.zig"));
-    _ = zig_lib.testing.refAllDecls(@import("../top/mem/gen/specs.zig"));
+    _ = zig_lib.testing.refAllDecls(zig_lib);
+    _ = zig_lib.testing.refAllDecls(render.UnionFormat);
+    _ = zig_lib.testing.refAllDecls(render.StructForm);
+    _ = zig_lib.testing.refAllDecls(render.EnumFormat);
+    _ = zig_lib.testing.refAllDecls(Builder);
+    _ = zig_lib.testing.refAllDecls(Allocator0);
+    _ = zig_lib.testing.refAllDecls(Allocator1);
 }
