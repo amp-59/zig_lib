8d384570b1e9ebcafa048f6df700d1c448ebfc32
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-09 00:14:55 +0000

    Merged functionality of `mg_touch` with `mg_specs`.

--- top/mem/gen/touch.zig
+++ top/mem/gen/touch.zig
@@ -8,17 +8,6 @@ pub usingnamespace proc.start;
 pub const logging_override: builtin.Logging.Override = spec.logging.override.silent;
 const Array = mem.StaticString(0);
 const mkdir_spec: file.MakeDirSpec = .{ .errors = .{} };
-const create_spec: file.CreateSpec = .{ .errors = .{}, .options = .{ .exclusive = false } };
+const create_spec: file.CreateSpec = .{ .errors = .{}, .options = .{ .exclusive = false, .truncate = true } };
 const close_spec: file.CloseSpec = .{ .errors = .{} };
-pub fn main() void {
-    file.makeDir(mkdir_spec, config.zig_out_dir, file.mode.directory);
-    file.makeDir(mkdir_spec, config.zig_out_src_dir, file.mode.directory);
-    file.makeDir(mkdir_spec, config.container_dir_path, file.mode.directory);
-    file.makeDir(mkdir_spec, config.reference_dir_path, file.mode.directory);
-    file.makeDir(mkdir_spec, config.container_kinds_path, file.mode.regular);
-    file.close(close_spec, file.create(create_spec, config.container_file_path, file.mode.regular));
-    file.close(close_spec, file.create(create_spec, config.reference_file_path, file.mode.regular));
-    file.close(close_spec, file.create(create_spec, config.allocator_file_path, file.mode.regular));
-    file.close(close_spec, file.create(create_spec, config.container_kinds_path, file.mode.regular));
-    file.close(close_spec, file.create(create_spec, config.reference_kinds_path, file.mode.regular));
-}
+pub fn main() void {}
