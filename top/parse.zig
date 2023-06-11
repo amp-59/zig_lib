e7aaebf09b1d4a2c9a8f12d48e345c1af312e8c1
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-11 23:01:38 +0000

    Added `noexcept` namespace for `readLEB128`.

--- top/parse.zig
+++ top/parse.zig
@@ -1,15 +1,18 @@
+const meta = @import("./meta.zig");
 const builtin = @import("./builtin.zig");
-const _float = @import("./parse/float.zig");
 
-pub usingnamespace _float;
+pub usingnamespace @import("./parse/float.zig");
 
-pub fn readLEB128(comptime Int: type, bytes: []const u8) !struct { Int, u8 } {
+pub fn readLEB128(comptime T: type, bytes: []const u8) !extern struct { T, u8 } {
+    const Int = meta.Child(T);
     const bit_size_of: comptime_int = @bitSizeOf(Int);
     const max_idx: comptime_int = (bit_size_of +% 6) / 7;
-
+    const Shift = @Type(.{ .Int = .{
+        .bits = bit_size_of -% @clz(@as(Int, bit_size_of) -% 1),
+        .signedness = .unsigned,
+    } });
     if (@typeInfo(Int).Int.signedness == .unsigned) {
-        const ShiftAmount = builtin.ShiftAmount(Int);
-        var idx: ShiftAmount = 0;
+        var idx: Shift = 0;
         var value: Int = 0;
         while (idx != max_idx) {
             const byte: Int = bytes[idx];
@@ -20,7 +23,7 @@ pub fn readLEB128(comptime Int: type, bytes: []const u8) !struct { Int, u8 } {
             }
             value |= ov[0];
             if (byte & 0x80 == 0) {
-                return .{ value, idx };
+                return .{ if (T == Int) value else @intToEnum(T, value), idx };
             }
         }
         return error.Overflow;
@@ -29,13 +32,12 @@ pub fn readLEB128(comptime Int: type, bytes: []const u8) !struct { Int, u8 } {
             .signedness = .unsigned,
             .bits = bit_size_of,
         } });
-        const ShiftAmount = builtin.ShiftAmount(Abs);
-        var idx: ShiftAmount = 0;
+        var idx: Shift = 0;
         var value: Abs = 0;
         while (idx != max_idx) {
             const byte: u8 = bytes[idx];
             const ored: i8 = @bitCast(i8, byte | 0x80);
-            const shift_amt: ShiftAmount = idx *% 7;
+            const shift_amt: Shift = idx *% 7;
             idx +%= 1;
             const ov: struct { Abs, u1 } = @shlWithOverflow(@as(Abs, byte & 0x7f), shift_amt);
             if (ov[1] != 0) {
@@ -58,9 +60,55 @@ pub fn readLEB128(comptime Int: type, bytes: []const u8) !struct { Int, u8 } {
                 if (byte & 0x40 != 0 and idx != max_idx) {
                     value |= @bitCast(Abs, @as(Int, -1)) << (shift_amt +% 7);
                 }
-                return .{ @bitCast(Int, value), idx };
+                return .{ if (T == Int) @bitCast(Int, value) else @intToEnum(T, @bitCast(Int, value)), idx };
             }
         }
         return error.Overflow;
     }
 }
+pub const noexcept = struct {
+    pub fn readLEB128(comptime T: type, bytes: []const u8) extern struct { T, u8 } {
+        const Int = meta.Child(T);
+        const bit_size_of: comptime_int = @bitSizeOf(Int);
+        const max_idx: comptime_int = (bit_size_of +% 6) / 7;
+        const Shift = @Type(.{ .Int = .{
+            .bits = bit_size_of -% @clz(@as(Int, bit_size_of) -% 1),
+            .signedness = .unsigned,
+        } });
+        if (@typeInfo(Int).Int.signedness == .unsigned) {
+            var idx: Shift = 0;
+            var value: Int = 0;
+            while (idx != max_idx) {
+                const byte: Int = bytes[idx];
+                const ov: struct { Int, u1 } = @shlWithOverflow(byte & 0x7f, idx *% 7);
+                idx +%= 1;
+                value |= ov[0];
+                if (byte & 0x80 == 0) {
+                    return .{ if (T == Int) value else @intToEnum(T, value), idx };
+                }
+            }
+            unreachable;
+        } else {
+            const Abs = @Type(.{ .Int = .{
+                .signedness = .unsigned,
+                .bits = bit_size_of,
+            } });
+            var idx: Shift = 0;
+            var value: Abs = 0;
+            while (idx != max_idx) {
+                const byte: u8 = bytes[idx];
+                const shift_amt: Shift = idx *% 7;
+                idx +%= 1;
+                const ov: struct { Abs, u1 } = @shlWithOverflow(@as(Abs, byte & 0x7f), shift_amt);
+                value |= ov[0];
+                if (byte & 0x80 == 0) {
+                    if (byte & 0x40 != 0 and idx != max_idx) {
+                        value |= @bitCast(Abs, @as(Int, -1)) << (shift_amt +% 7);
+                    }
+                    return .{ if (T == Int) @bitCast(Int, value) else @intToEnum(T, @bitCast(Int, value)), idx };
+                }
+            }
+            unreachable;
+        }
+    }
+};
