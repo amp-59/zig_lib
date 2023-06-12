b45d54b84a7c1c0591dea71bda40d0a88ec3d669
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-12 02:10:43 +0000

    Added new path type `NewPath`.

--- top/build/types.zig
+++ top/build/types.zig
@@ -39,6 +39,71 @@ pub const State = enum(u8) {
 };
 // zig fmt: on
 pub const Lock = mem.ThreadSafeSet(State.list.len, State, Task);
+pub const NewPath = union(enum) {
+    pathname: []const u8,
+    names: [][]const u8,
+    const Format = @This();
+    pub fn formatWrite(format: Format, array: anytype) void {
+        switch (format) {
+            .pathname => |pathname| {
+                array.writeMany(pathname);
+            },
+            .names => |names| {
+                if (names.len != 0) {
+                    array.writeMany(names[0]);
+                    for (format.names[1..]) |name| {
+                        array.writeOne('/');
+                        array.writeMany(name);
+                    }
+                }
+            },
+        }
+        array.writeOne(0);
+    }
+    pub fn formatWriteBuf(format: Format, buf: [*]u8) u64 {
+        @setRuntimeSafety(false);
+        var len: u64 = 0;
+        switch (format) {
+            .pathname => |pathname| {
+                mach.memcpy(buf, pathname.ptr, pathname.len);
+                len +%= pathname.len;
+            },
+            .names => |names| {
+                if (names.len != 0) {
+                    mach.memcpy(buf + len, names[0].ptr, names[0].len);
+                    len +%= names[0].len;
+                    for (names[1..]) |name| {
+                        buf[len] = '/';
+                        len +%= 1;
+                        mach.memcpy(buf + len, name.ptr, name.len);
+                        len +%= name.len;
+                    }
+                }
+            },
+        }
+        buf[len] = 0;
+        len +%= 1;
+        return len;
+    }
+    pub fn formatLength(format: Format) u64 {
+        @setRuntimeSafety(false);
+        var len: u64 = 0;
+        switch (format) {
+            .pathname => |pathname| {
+                len +%= pathname.len;
+            },
+            .names => |names| {
+                if (names.len != 0) {
+                    len +%= names[0].len;
+                    for (names[1..]) |name| {
+                        len +%= 1 +% name.len;
+                    }
+                }
+            },
+        }
+        return len +% 1;
+    }
+};
 pub const Path = struct {
     absolute: [:0]const u8,
     relative: [:0]const u8 = &.{},
