ba192074cf97a3902c7ac91d66a4e015ee380345
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-16 10:30:36 +0000

    Updated usage of operators in `algo` namespace test program.

--- test/algo-test.zig
+++ test/algo-test.zig
@@ -21,8 +21,8 @@ const show_best_cases: bool = false;
 fn write(buf: []u8, off: u64, ss: []const []const u8) u64 {
     var len: u64 = 0;
     for (ss) |s| {
-        for (s, 0..) |c, i| buf[off + len + i] = c;
-        len += s.len;
+        for (s, 0..) |c, i| buf[off +% len +% i] = c;
+        len +%= s.len;
     }
     return len;
 }
@@ -88,27 +88,27 @@ fn approximationTest() void {
     var n_aligned_bytes: u32 = 1;
     var total_requested: u64 = 0;
     var total_returned: u64 = 0;
-    while (n_aligned_bytes < lit.max_bit_u16) : (n_aligned_bytes += 1) {
+    while (n_aligned_bytes < lit.max_bit_u16) : (n_aligned_bytes +%= 1) {
         const s_lb_counts: u16 = algo.partialPackSingleApprox(n_aligned_bytes);
         const o_aligned_bytes: u64 = algo.partialUnpackSingleApprox(s_lb_counts);
         const s_ub_counts: u16 = algo.partialPackDoubleApprox(n_aligned_bytes, o_aligned_bytes);
         const s_aligned_bytes: u64 = algo.partialUnpackDoubleApprox(o_aligned_bytes, s_ub_counts);
-        total_requested += n_aligned_bytes;
-        total_returned += s_aligned_bytes;
-        if (n_aligned_bytes - s_aligned_bytes == 0 and show_best_cases) {
+        total_requested +%= n_aligned_bytes;
+        total_returned +%= s_aligned_bytes;
+        if (n_aligned_bytes -% s_aligned_bytes == 0 and show_best_cases) {
             const ss: []const []const u8 = &[_][]const u8{
                 builtin.fmt.ud32(@intCast(u32, s_aligned_bytes)).readAll(), " ",
                 builtin.fmt.ub32(@intCast(u32, s_aligned_bytes)).readAll(), "\n",
             };
-            if (len + 128 > buf.len) {
+            if (len +% 128 > buf.len) {
                 print(&buf, len, ss);
                 len = 0;
             } else {
-                len += write(&buf, len, ss);
+                len +%= write(&buf, len, ss);
             }
         }
     }
-    builtin.assertBelow(u64, total_returned - total_requested, (2 * total_requested) / 100);
+    builtin.assertBelow(u64, total_returned - total_requested, (2 *% total_requested) / 100);
 }
 pub fn main() !void {
     if (builtin.is_fast) {
