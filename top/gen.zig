42e07e87940151ea2b289cbfc0237528dd8ee946
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-21 23:58:26 +0000

    Added a function useful for generating bit fields out of declarations.

--- top/gen.zig
+++ top/gen.zig
@@ -1,5 +1,8 @@
 const mem = @import("./mem.zig");
+const fmt = @import("./fmt.zig");
 const file = @import("./file.zig");
+const mach = @import("./mach.zig");
+const meta = @import("./meta.zig");
 const spec = @import("./spec.zig");
 const builtin = @import("./builtin.zig");
 pub const ListKind = enum {
@@ -52,4 +55,99 @@ pub fn readFile(comptime read_spec: file.ReadSpec, pathname: [:0]const u8, buf: 
     const fd: u64 = file.open(spec.open.append_noexcept, pathname);
     defer file.close(spec.generic.noexcept, fd);
     return file.read(read_spec, fd, buf);
+}
+pub fn containerDeclsToBitField(comptime Container: type, type_name: []const u8) void {
+    const ShiftAmount = builtin.ShiftAmount(usize);
+    const bit_field_sets: []const meta.BitFieldSet = comptime meta.containerDeclsToBitFieldSets(Container);
+    var array: mem.StaticString(4096) = undefined;
+    array.undefineAll();
+    var bits: u16 = 0;
+    var diff: u16 = 0;
+    var enum_count: u16 = 0;
+    array.writeMany("const ");
+    array.writeMany(type_name);
+    array.writeMany("=packed struct(usize){\n");
+    for (bit_field_sets) |set| {
+        if (set.tag == .E) {
+            var value: usize = 0;
+            for (set.pairs) |pair| {
+                value |= pair.value;
+            }
+            const bit_size_of_enum: u16 = @bitSizeOf(usize) -% (@clz(value) +% @ctz(value));
+            const shift_amt: ShiftAmount = meta.bitCast(ShiftAmount, bits);
+            array.writeOne('e');
+            array.writeFormat(fmt.ud16(enum_count));
+            array.writeMany(":enum(u");
+            array.writeFormat(fmt.id64(bit_size_of_enum));
+            array.writeMany("){\n");
+            for (set.pairs) |pair| {
+                array.writeFormat(fmt.IdentifierFormat{ .value = pair.name });
+                array.writeMany("=");
+                array.writeFormat(fmt.ud64(pair.value >> shift_amt));
+                array.writeMany(",\n");
+            }
+            array.writeMany("},\n");
+            bits +%= bit_size_of_enum;
+            enum_count +%= 1;
+        } else {
+            for (set.pairs) |pair| {
+                if (pair.value != 0) {
+                    diff = ((@bitSizeOf(usize) -% @clz(pair.value)) -% 1) -% bits;
+                    if (diff >= 1) {
+                        array.writeMany("zb");
+                        array.writeFormat(fmt.ud16(bits));
+                        array.writeMany(":u");
+                        array.writeFormat(fmt.ud16(diff));
+                        array.writeMany("=0,\n");
+                    }
+                    array.writeFormat(fmt.IdentifierFormat{ .value = pair.name });
+                    array.writeMany(":bool=false,\n");
+                    bits +%= diff +% 1;
+                }
+            }
+        }
+    }
+    diff = @bitSizeOf(usize) -% bits;
+    if (diff >= 1) {
+        array.writeMany("zb");
+        array.writeFormat(fmt.ud16(bits));
+        array.writeMany(":u");
+        array.writeFormat(fmt.ud16(diff));
+        array.writeMany("=0,\n");
+    }
+    array.writeMany("fn assert(flags:@This(),val:usize)void{\nbuiltin.assert(@bitCast(usize,flags)==val);\n}\n");
+    array.writeMany("comptime{\n");
+    enum_count = 0;
+    for (bit_field_sets) |set| {
+        if (set.tag == .E) {
+            var value: usize = 0;
+            for (set.pairs) |pair| {
+                value |= pair.value;
+            }
+            for (set.pairs) |pair| {
+                array.writeMany("assert(.{.");
+                array.writeOne('e');
+                array.writeFormat(fmt.ud16(enum_count));
+                array.writeMany("=.");
+                array.writeFormat(fmt.IdentifierFormat{ .value = pair.name });
+                array.writeMany("},");
+                array.writeFormat(fmt.uxsize(pair.value));
+                array.writeMany(");\n");
+            }
+            enum_count +%= 1;
+        } else {
+            for (set.pairs) |pair| {
+                if (pair.value != 0) {
+                    array.writeMany("assert(.{.");
+                    array.writeFormat(fmt.IdentifierFormat{ .value = pair.name });
+                    array.writeMany("=true},");
+                    array.writeFormat(fmt.uxsize(pair.value));
+                    array.writeMany(");\n");
+                }
+            }
+        }
+    }
+    array.writeMany("}\n");
+    array.writeMany("};\n");
+    file.write(.{ .errors = .{} }, 1, array.readAll());
 }
