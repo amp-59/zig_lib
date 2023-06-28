31fc2be43cbbc4e14cb0f48e8e29301eb2b722ff
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-06-28 17:29:20 +0000

    Added test functions for `Edwards25519`.

--- test/crypto/dh-test.zig
+++ test/crypto/dh-test.zig
@@ -89,12 +89,12 @@ fn testEd25519WithBlindKeys() !void {
     const kp = try crypto.dh.Ed25519.KeyPair.create(null);
     var blind: [32]u8 = undefined;
     crypto.utils.bytes(&blind);
-    const blind_kp: crypto.dh.Ed25519.key_blinding.BlindKeyPair =
+    const blind_key_pair: crypto.dh.Ed25519.key_blinding.BlindKeyPair =
         try crypto.dh.Ed25519.key_blinding.BlindKeyPair.init(kp, blind, "ctx");
     const msg = "test";
-    const sig = try blind_kp.sign(msg, null);
-    try sig.verify(msg, blind_kp.blind_public_key.key);
-    const pk = try blind_kp.blind_public_key.unblind(blind, "ctx");
+    const sig = try blind_key_pair.sign(msg, null);
+    try sig.verify(msg, blind_key_pair.blind_public_key.key);
+    const pk = try blind_key_pair.blind_public_key.unblind(blind, "ctx");
     try testing.expectEqualMany(u8, &pk.toBytes(), &kp.public_key.toBytes());
 }
 fn testEd25519TestVectors() !void {
@@ -103,13 +103,13 @@ fn testEd25519TestVectors() !void {
         _ = try fmt.hexToBytes(&msg, entry.msg);
         var public_key_bytes: [32]u8 = undefined;
         _ = try fmt.hexToBytes(&public_key_bytes, entry.key);
-        const public_key = crypto.dh.Ed25519.PublicKey.fromBytes(public_key_bytes) catch |err| {
+        const public_key: crypto.dh.Ed25519.PublicKey = crypto.dh.Ed25519.PublicKey.fromBytes(public_key_bytes) catch |err| {
             try builtin.expectEqual(anyerror, entry.expected.?, err);
             continue;
         };
         var sig_bytes: [64]u8 = undefined;
         _ = try fmt.hexToBytes(&sig_bytes, entry.sig);
-        const sig = crypto.dh.Ed25519.Signature.fromBytes(sig_bytes);
+        const sig: crypto.dh.Ed25519.Signature = crypto.dh.Ed25519.Signature.fromBytes(sig_bytes);
         if (entry.expected) |error_type| {
             try builtin.expect(error_type == sig.verify(&msg, public_key));
         } else {
@@ -117,10 +117,64 @@ fn testEd25519TestVectors() !void {
         }
     }
 }
+fn testEdwards25519ToCurve25519Map(allocator: *mem.SimpleAllocator) !void {
+    const ed_key_pair: crypto.dh.Ed25519.KeyPair =
+        try crypto.dh.Ed25519.KeyPair.create([_]u8{0x42} ** 32);
+    const mont_key_pair: crypto.dh.X25519.KeyPair =
+        try crypto.dh.X25519.KeyPair.fromEd25519(ed_key_pair);
+    try htest.assertEqual(
+        allocator,
+        "90e7595fc89e52fdfddce9c6a43d74dbf6047025ee0462d2d172e8b6a2841d6e",
+        &mont_key_pair.secret_key,
+    );
+    try htest.assertEqual(
+        allocator,
+        "cc4f2cdb695dd766f34118eb67b98652fed1d8bc49c330b119bbfa8a64989378",
+        &mont_key_pair.public_key,
+    );
+}
+fn testEdwards25519PointAdditionSubtraction() !void {
+    var s1: [32]u8 = undefined;
+    var s2: [32]u8 = undefined;
+    crypto.utils.bytes(&s1);
+    crypto.utils.bytes(&s2);
+    const p: crypto.dh.Edwards25519 = try crypto.dh.Edwards25519.base_point.clampedMul(s1);
+    const q: crypto.dh.Edwards25519 = try crypto.dh.Edwards25519.base_point.clampedMul(s2);
+    const r: crypto.dh.Edwards25519 = p.add(q).add(q).sub(q).sub(q);
+    try r.rejectIdentity();
+    try builtin.expect(error.IdentityElement == r.sub(p).rejectIdentity());
+    try builtin.expect(error.IdentityElement == p.sub(p).rejectIdentity());
+    try builtin.expect(error.IdentityElement == p.sub(q).add(q).sub(p).rejectIdentity());
+}
+fn testEdwards25519UniformToPoint(allocator: *mem.SimpleAllocator) !void {
+    var r: [32]u8 = .{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+    var p: crypto.dh.Edwards25519 = crypto.dh.Edwards25519.fromUniform(r);
+    try htest.assertEqual(allocator, "0691eee3cf70a0056df6bfa03120635636581b5c4ea571dfc680f78c7e0b4137", p.toBytes()[0..]);
+    r[31] = 0xff;
+    p = crypto.dh.Edwards25519.fromUniform(r);
+    try htest.assertEqual(allocator, "f70718e68ef42d90ca1d936bb2d7e159be6c01d8095d39bd70487c82fe5c973a", p.toBytes()[0..]);
+}
+fn testEdwards25519HashToCurveOperation(allocator: *mem.SimpleAllocator) !void {
+    var p: crypto.dh.Edwards25519 = crypto.dh.Edwards25519.fromString(true, "QUUX-V01-CS02-with-edwards25519_XMD:SHA-512_ELL2_RO_", "abc");
+    try htest.assertEqual(allocator, "31558a26887f23fb8218f143e69d5f0af2e7831130bd5b432ef23883b895839a", p.toBytes()[0..]);
+    p = crypto.dh.Edwards25519.fromString(false, "QUUX-V01-CS02-with-edwards25519_XMD:SHA-512_ELL2_NU_", "abc");
+    try htest.assertEqual(allocator, "42fa27c8f5a1ae0aa38bb59d5938e5145622ba5dedd11d11736fa2f9502d7367", p.toBytes()[0..]);
+}
+fn testEdwards25519ImplicitReductionOfInvalidScalars(allocator: *mem.SimpleAllocator) !void {
+    const s: [32]u8 = [1]u8{0} ** 31 ++ [1]u8{255};
+    const p1: crypto.dh.Edwards25519 = try crypto.dh.Edwards25519.base_point.mulPublic(s);
+    const p2: crypto.dh.Edwards25519 = try crypto.dh.Edwards25519.base_point.mul(s);
+    const p3: crypto.dh.Edwards25519 = try p1.mulPublic(s);
+    const p4: crypto.dh.Edwards25519 = try p1.mul(s);
+    try testing.expectEqualMany(u8, p1.toBytes()[0..], p2.toBytes()[0..]);
+    try testing.expectEqualMany(u8, p3.toBytes()[0..], p4.toBytes()[0..]);
+    try htest.assertEqual(allocator, "339f189ecc5fbebe9895345c72dc07bda6e615f8a40e768441b6f529cd6c671a", p1.toBytes()[0..]);
+    try htest.assertEqual(allocator, "a501e4c595a3686d8bee7058c7e6af7fd237f945c47546910e37e0e79b1bafb0", p3.toBytes()[0..]);
+}
 pub fn dhTestMain() !void {
     var allocator: mem.SimpleAllocator = .{};
     defer allocator.unmap();
-
+    // Ed25519
     try testEd25519KeyPairCreation();
     try testEd25519Signature();
     try testEd25519BatchVerification();
@@ -128,5 +182,11 @@ pub fn dhTestMain() !void {
     try testEd25519KeyPairFromSecretKey();
     try testEd25519WithBlindKeys();
     try testEd25519TestVectors();
+    // Edwards25519
+    try testEdwards25519ToCurve25519Map(&allocator);
+    try testEdwards25519PointAdditionSubtraction();
+    try testEdwards25519UniformToPoint(&allocator);
+    try testEdwards25519HashToCurveOperation(&allocator);
+    try testEdwards25519ImplicitReductionOfInvalidScalars(&allocator);
 }
 pub const main = dhTestMain;
