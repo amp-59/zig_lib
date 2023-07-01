9485f00c61a1be22fddb2d8660fac09e168a03fb
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-01 00:16:22 +0000

    Updated builder stress test program to use `*At` variants of convenience file system functions.

--- test/build-test.zig
+++ test/build-test.zig
@@ -3,6 +3,7 @@ const mem = srg.mem;
 const gen = srg.gen;
 const sys = srg.sys;
 const proc = srg.proc;
+const spec = srg.spec;
 const file = srg.file;
 const meta = srg.meta;
 const build = srg.build;
@@ -13,6 +14,8 @@ pub usingnamespace proc.start;
 const Node = build.GenericNode(.{
     .options = .{ .max_thread_count = 8 },
 });
+
+pub const logging_override: builtin.Logging.Override = spec.logging.override.verbose;
 
 pub const logging_default: builtin.Logging.Default = .{
     .Attempt = false,
@@ -60,30 +63,37 @@ const text =
 ;
 fn buildMain(allocator: *build.Allocator, toplevel: *Node) !void {
     @setEvalBranchQuota(~@as(u32, 0));
-    try file.makePath(.{}, "test/stress", file.mode.directory);
+    const test_dir_fd: u64 = try file.path(.{}, "test");
+    file.makeDirAt(.{}, test_dir_fd, "stress", file.mode.directory) catch |err| {
+        if (err != error.FileExists) {
+            return err;
+        }
+    };
+    const stress_dir_fd: u64 = try file.pathAt(.{}, test_dir_fd, "stress");
     const node: *Node = try toplevel.addBuild(allocator, .{ .kind = .exe }, "top", "test/stress/top.zig");
     try gen.truncateFile(.{ .return_type = void }, "test/stress/top.zig", text ++
         \\pub fn main() void {}
     );
     inline for (0..10) |x| {
         const x_s = comptime builtin.fmt.cx(x);
-        const x_root = "test/stress/f_" ++ x_s ++ ".zig";
-        const x_node: *Node = try toplevel.addBuild(allocator, .{ .kind = .obj }, x_s, x_root);
+        const x_root = "f_" ++ x_s ++ ".zig";
+        const x_node: *Node = try toplevel.addBuild(allocator, .{ .kind = .obj }, x_s, "test/stress/" ++ x_root);
         x_node.flags.is_hidden = true;
         node.dependOnObject(allocator, x_node);
-        try gen.truncateFile(.{ .return_type = void }, x_root, text ++ "export fn func_" ++ x_s ++ "() void {}");
+
+        try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, x_root, text ++ "export fn func_" ++ x_s ++ "() void {}");
         inline for (0..10) |y| {
             const y_s = x_s ++ comptime builtin.fmt.cx(y);
-            const y_root = "test/stress/f_" ++ y_s ++ ".zig";
-            const y_node: *Node = try toplevel.addBuild(allocator, .{ .kind = .obj }, y_s, y_root);
-            try gen.truncateFile(.{ .return_type = void }, y_root, text ++ "export fn func_" ++ y_s ++ "() void {}");
+            const y_root = "f_" ++ y_s ++ ".zig";
+            const y_node: *Node = try toplevel.addBuild(allocator, .{ .kind = .obj }, y_s, "test/stress/" ++ y_root);
+            try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, y_root, text ++ "export fn func_" ++ y_s ++ "() void {}");
             y_node.flags.is_hidden = true;
             x_node.dependOn(allocator, y_node, .build);
             inline for (0..10) |z| {
                 const z_s = y_s ++ comptime builtin.fmt.cx(z);
-                const z_root = "test/stress/f_" ++ z_s ++ ".zig";
-                const z_node: *Node = try toplevel.addBuild(allocator, .{ .kind = .obj }, z_s, z_root);
-                try gen.truncateFile(.{ .return_type = void }, z_root, text ++ "export fn func_" ++ z_s ++ "() void {}");
+                const z_root = "f_" ++ z_s ++ ".zig";
+                const z_node: *Node = try toplevel.addBuild(allocator, .{ .kind = .obj }, z_s, "test/stress/" ++ z_root);
+                try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, z_root, text ++ "export fn func_" ++ z_s ++ "() void {}");
                 z_node.flags.is_hidden = true;
                 y_node.dependOn(allocator, z_node, .build);
             }
