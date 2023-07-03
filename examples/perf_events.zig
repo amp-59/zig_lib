27bccb61212f312c2e77bc2680c805397b8431a8
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-03 04:51:19 +0000

    Refactored `main`.

--- examples/perf_events.zig
+++ examples/perf_events.zig
@@ -17,8 +17,9 @@ pub usingnamespace proc.start;
 pub const logging_override: builtin.Logging.Override = spec.logging.override.silent;
 
 const event_spec: perf.PerfEventSpec = .{ .errors = .{} };
+const event_ctl_spec: perf.PerfEventControlSpec = .{ .errors = .{} };
 const read_spec: file.ReadSpec = .{ .errors = .{}, .return_type = void, .child = u64 };
-const wait_spec: proc.WaitSpec = .{ .errors = .{} };
+const wait_spec: proc.WaitSpec = .{ .errors = .{}, .return_type = void };
 const close_spec: file.CloseSpec = .{ .errors = .{} };
 const fork_spec: proc.ForkSpec = .{ .errors = .{} };
 const path_spec: file.PathSpec = .{ .errors = .{} };
@@ -39,45 +40,75 @@ const sw_counters: []const perf.Measurement = &.{
     .{ .name = about ++ "task-clock\t\t", .config = .{ .software = .task_clock } },
     .{ .name = about ++ "page-faults\t\t", .config = .{ .software = .page_faults } },
 };
-pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
-    const event_flags: perf.Event.Flags = .{
-        .disabled = true,
-        .exclude_kernel = true,
-        .exclude_hv = true,
-        .inherit = true,
-        .enable_on_exec = true,
-    };
-    if (args.len <= 1) {
-        return error.MissingArguments;
+const Fds = struct {
+    hw: [hw_counters.len]u64,
+    sw: [sw_counters.len]u64,
+
+    fn init() Fds {
+        var hw_fds: [hw_counters.len]u64 = undefined;
+        var sw_fds: [sw_counters.len]u64 = undefined;
+        hw_fds[0] = ~@as(u32, 0);
+        const event_flags: perf.Event.Flags = .{
+            .disabled = true,
+            .exclude_kernel = true,
+            .exclude_hv = true,
+            .inherit = true,
+            .enable_on_exec = true,
+        };
+        var event: perf.Event = .{ .flags = event_flags };
+        var idx: usize = 0;
+        event.type = .hardware;
+        while (idx != hw_counters.len) : (idx +%= 1) {
+            event.config = hw_counters[idx].config;
+            hw_fds[idx] = perf.eventOpen(event_spec, &event, .self, .any, hw_fds[0], .{ .fd_close_on_exec = true });
+        }
+        event.type = .software;
+        idx = 0;
+        while (idx != sw_counters.len) : (idx +%= 1) {
+            event.config = sw_counters[idx].config;
+            sw_fds[idx] = perf.eventOpen(event_spec, &event, .self, .any, hw_fds[0], .{ .fd_close_on_exec = true });
+        }
+        return .{ .hw = hw_fds, .sw = sw_fds };
     }
-    var hw_fds: [hw_counters.len]u64 = undefined;
-    hw_fds[0] = ~@as(u32, 0);
-    var sw_fds: [sw_counters.len]u64 = undefined;
-    sw_fds[0] = ~@as(u32, 0);
-    var result: u64 = 0;
-    var event: perf.Event = .{ .flags = event_flags };
-    var idx: u64 = 0;
-    event.type = .hardware;
-    while (idx != hw_counters.len) : (idx +%= 1) {
-        event.config = hw_counters[idx].config;
-        hw_fds[idx] = perf.eventOpen(event_spec, &event, .self, .any, hw_fds[0], .{ .fd_close_on_exec = true });
+    fn deinit(fds: *Fds) void {
+        for (fds.hw) |fd| {
+            file.close(close_spec, fd);
+        }
+        for (fds.sw) |fd| {
+            file.close(close_spec, fd);
+        }
     }
-    event.type = .software;
-    idx = 0;
-    while (idx != sw_counters.len) : (idx +%= 1) {
-        event.config = sw_counters[idx].config;
-        sw_fds[idx] = perf.eventOpen(event_spec, &event, .self, .any, hw_fds[0], .{ .fd_close_on_exec = true });
+    fn print(fds: *Fds) void {
+        var array: Array = undefined;
+        array.undefineAll();
+        var result: usize = 0;
+        var idx: usize = 0;
+        while (idx != hw_counters.len) : (idx +%= 1) {
+            file.readOne(read_spec, fds.hw[idx], &result);
+            array.writeAny(spec.reinterpret.fmt, .{ hw_counters[idx].name, fmt.udh(result), '\n' });
+        }
+        idx = 0;
+        while (idx != sw_counters.len) : (idx +%= 1) {
+            file.readOne(read_spec, fds.sw[idx], &result);
+            array.writeAny(spec.reinterpret.fmt, .{ sw_counters[idx].name, fmt.udh(result), '\n' });
+        }
+        builtin.debug.write(array.readAll());
+        fds.reset();
     }
-    const leader_fd: u64 = hw_fds[0];
+    fn reset(fds: *Fds) void {
+        fds.deinit();
+        fds.* = init();
+    }
+};
+fn findDirFd(vars: [][*:0]u8, name: [:0]const u8) !u64 {
     var itr: proc.PathIterator = .{
         .paths = proc.environmentValue(vars, "PATH").?,
     };
-    const name: [:0]const u8 = meta.manyToSlice(args[1]);
-    const dir_fd: u64 = while (itr.next()) |dirname| {
+    while (itr.next()) |dirname| {
         const dir_fd: u64 = file.path(path_spec, dirname);
         if (file.accessAt(.{}, dir_fd, name, .{ .exec = true })) {
             itr.done();
-            break dir_fd;
+            return dir_fd;
         } else |err| {
             if (err == error.NoSuchFileOrDirectory or
                 err == error.Access)
@@ -85,30 +116,20 @@ pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
         }
     } else {
         return error.NoExecutableInPath;
-    };
-    try perf.eventControl(.{}, leader_fd, .reset, true);
+    }
+}
+pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
+    if (args.len <= 1) {
+        return error.MissingArguments;
+    }
+    const name: [:0]const u8 = meta.manyToSlice(args[1]);
+    const dir_fd: u64 = try findDirFd(vars, name);
+    var fds: Fds = Fds.init();
     const pid: u64 = proc.fork(fork_spec);
     if (pid == 0) {
         return file.execAt(.{}, dir_fd, name, args[1..], vars);
     }
-    const ret: proc.Return = proc.waitPid(wait_spec, .{ .pid = pid });
-    _ = ret;
-    try perf.eventControl(.{}, leader_fd, .disable, true);
-    var array: Array = undefined;
-    array.undefineAll();
-    idx = 0;
-    while (idx != hw_counters.len) : (idx +%= 1) {
-        file.readOne(read_spec, hw_fds[idx], &result);
-        array.writeAny(spec.reinterpret.fmt, .{ hw_counters[idx].name, fmt.udh(result), '\n' });
-    }
-    idx = 0;
-    while (idx != sw_counters.len) : (idx +%= 1) {
-        file.readOne(read_spec, sw_fds[idx], &result);
-        array.writeAny(spec.reinterpret.fmt, .{ sw_counters[idx].name, fmt.udh(result), '\n' });
-    }
-    builtin.debug.write(array.readAll());
-    for (hw_fds ++ sw_fds) |fd| {
-        file.close(close_spec, fd);
-    }
+    proc.waitPid(wait_spec, .{ .pid = pid });
+    fds.print();
     file.close(close_spec, dir_fd);
 }
