3792c5d367b2dd83fe22ea9b33e4f633d6fcf659
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-07 06:24:14 +0000

    Added test function for `HkdfSha256`.

--- test/crypto/auth-test.zig
+++ test/crypto/auth-test.zig
@@ -26,6 +26,21 @@ fn testHmacSha256(allocator: *mem.SimpleAllocator) !void {
     crypto.auth.HmacSha256.create(out[0..], "The quick brown fox jumps over the lazy dog", "key");
     try htest.assertEqual(allocator, "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8", out[0..]);
 }
+fn testHkdfSha256(allocator: *mem.SimpleAllocator) !void {
+    const ikm: [22]u8 = .{0x0b} ** 22;
+    const salt: [13]u8 = .{ 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c };
+    const context: [10]u8 = .{ 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9 };
+    const prk: [crypto.auth.HkdfSha256.prk_len]u8 = crypto.auth.HkdfSha256.extract(&salt, &ikm);
+    try htest.assertEqual(allocator, "077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5", &prk);
+    var out: [42]u8 = undefined;
+    crypto.auth.HkdfSha256.expand(&out, &context, prk);
+    try htest.assertEqual(allocator, "3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf34007208d5b887185865", &out);
+    var hkdf: crypto.auth.HmacSha256 = crypto.auth.HkdfSha256.extractInit(&salt);
+    hkdf.update(&ikm);
+    var prk2: [crypto.auth.HkdfSha256.prk_len]u8 = undefined;
+    hkdf.final(&prk2);
+    try htest.assertEqual(allocator, "077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5", &prk2);
+}
 fn testSiphash6424Sanity() !void {
     const SipHash64 = crypto.auth.GenericSipHash64(2, 4);
     var buffer: [64]u8 = undefined;
@@ -111,6 +126,7 @@ fn authTestMain() !void {
     defer allocator.unmap();
     try testHmacMd5(&allocator);
     try testHmacSha256(&allocator);
+    try testHkdfSha256(&allocator);
     try testSiphash6424Sanity();
     try testSiphash12824Sanity();
     try testCmacAes128Example1Len0();
