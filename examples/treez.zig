ba870d5fe585d4c9bdc1c60f16f6f7fb19329f9c
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-15 16:54:40 +0000

    Updated usage of `clone`.

--- examples/treez.zig
+++ examples/treez.zig
@@ -338,7 +338,7 @@ pub fn main(args: [][*:0]u8) !void {
             var tid: u64 = undefined;
             var stack_buf: [16384]u8 align(16) = undefined;
             const stack_addr: u64 = @intFromPtr(&stack_buf);
-            tid = proc.callClone(thread_spec, stack_addr, stack_buf.len, {}, printAlong, .{ &status, &allocator_1, &array });
+            tid = proc.clone(thread_spec, stack_addr, stack_buf.len, {}, printAlong, .{ &status, &allocator_1, &array });
             @call(.always_inline, writeAndWalk, .{
                 &allocator_0, &allocator_1, &array, &alts_buf, &link_buf,
                 &status,      null,         arg,    0,
