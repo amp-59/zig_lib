cfdbf1fc5b9f1ea6f8b4466f559f057ed27897a3
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-17 04:54:31 +0000

    Added common types for use by `targetgen` programs.

---
+++ top/target/gen/types.zig
@@ -0,0 +1,18 @@
+const mem = @import("../../mem.zig");
+const fmt = @import("../../fmt.zig");
+
+pub const Array = mem.StaticString(1024 * 1024);
+pub const TypeDescr = fmt.GenericTypeDescrFormat(.{
+    .options = .{
+        .default_field_values = true,
+        .identifier_name = true,
+    },
+    .tokens = .{
+        .lbrace = "{\n",
+        .equal = "=",
+        .rbrace = "}",
+        .next = ",\n",
+        .colon = ":",
+        .indent = "",
+    },
+});
