2adf7402b620877e3bfdea07b1bff9dd0d1c95f8
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-18 07:34:03 +0000

    Updated startup/imports.

--- test/decl-test.zig
+++ test/decl-test.zig
@@ -1,6 +1,6 @@
-const zig_lib = @import("../zig_lib.zig");
+const zl = @import("../zig_lib.zig");
 
-pub usingnamespace zig_lib.proc.start;
+pub usingnamespace zl.start;
 
 const @"test" = struct {
     const algo = @import("algo-test.zig");
@@ -27,40 +27,51 @@ const @"test" = struct {
     const time = @import("time-test.zig");
     const virtual = @import("virtual-test.zig");
 };
-pub const logging_override: zig_lib.builtin.Logging.Override = zig_lib.spec.logging.override.verbose;
-pub const AddressSpace = zig_lib.spec.address_space.exact_8;
-const Allocator0 = zig_lib.mem.GenericArenaAllocator(.{
+pub const logging_override: zl.builtin.Logging.Override = zl.spec.logging.override.verbose;
+pub const AddressSpace = zl.spec.address_space.exact_8;
+const Allocator0 = zl.mem.GenericArenaAllocator(.{
     .arena_index = 0,
-    .AddressSpace = zig_lib.spec.address_space.regular_128,
+    .AddressSpace = zl.spec.address_space.regular_128,
 });
-const Allocator1 = zig_lib.mem.GenericArenaAllocator(.{
+const Allocator1 = zl.mem.GenericArenaAllocator(.{
     .arena_index = 1,
-    .AddressSpace = zig_lib.spec.address_space.exact_8,
+    .AddressSpace = zl.spec.address_space.exact_8,
 });
-const Builder = zig_lib.build.GenericNode(.{});
-const List = zig_lib.mem.GenericLinkedList(.{
+const Builder = zl.build.GenericNode(.{});
+const List = zl.mem.GenericLinkedList(.{
     .child = u8,
     .low_alignment = 1,
     .Allocator = Allocator0,
 });
-const ListView = zig_lib.mem.GenericLinkedListView(.{
+const ListView = zl.mem.GenericLinkedListView(.{
     .child = u8,
     .low_alignment = 1,
 });
-const render = struct {
-    pub const UnionFormat = zig_lib.fmt.UnionFormat(.{}, union { one: u64, two: struct { u32, u32 } });
-    pub const StructForm = zig_lib.fmt.UnionFormat(.{}, struct { one: u64, two: *u64, three: []u8 });
-    pub const EnumFormat = zig_lib.fmt.UnionFormat(.{}, enum { one, two, three });
+const other = struct {
+    pub const UnionFormat = zl.fmt.UnionFormat(.{}, union { one: u64, two: struct { u32, u32 } });
+    pub const StructForm = zl.fmt.UnionFormat(.{}, struct { one: u64, two: *u64, three: []u8 });
+    pub const EnumFormat = zl.fmt.UnionFormat(.{}, enum { one, two, three });
+    pub const object_format = std.Target.ObjectFormat.elf;
+    pub const mode = std.builtin.Mode.Debug;
+    pub const link_libc = false;
+    pub const link_libcpp = false;
+    pub const have_error_return_tracing = true;
+    pub const valgrind_support = true;
+    pub const sanitize_thread = false;
+    pub const position_independent_code = false;
+    pub const position_independent_executable = false;
+    pub const strip_debug_info = false;
+    pub const code_model = std.builtin.CodeModel.default;
 };
-pub fn main() void {
-    _ = zig_lib.meta.refAllDecls(zig_lib, &.{ "FnArg", "tls" });
-    _ = zig_lib.meta.refAllDecls(@"test", &.{"FnArg"});
-    _ = zig_lib.meta.refAllDecls(List, null);
-    _ = zig_lib.meta.refAllDecls(ListView, null);
-    _ = zig_lib.meta.refAllDecls(render.UnionFormat, null);
-    _ = zig_lib.meta.refAllDecls(render.StructForm, null);
-    _ = zig_lib.meta.refAllDecls(render.EnumFormat, null);
-    _ = zig_lib.meta.refAllDecls(Builder, null);
-    _ = zig_lib.meta.refAllDecls(Allocator0, null);
-    _ = zig_lib.meta.refAllDecls(Allocator1, null);
+pub fn main() !void {
+    _ = zl.meta.refAllDecls(zl, &.{ "FnArg", "tls" });
+    _ = zl.meta.refAllDecls(@"test", &.{"FnArg"});
+    _ = zl.meta.refAllDecls(List, null);
+    _ = zl.meta.refAllDecls(ListView, null);
+    _ = zl.meta.refAllDecls(other, null);
+    _ = zl.meta.refAllDecls(Builder, null);
+    _ = zl.meta.refAllDecls(Allocator0, null);
+    _ = zl.meta.refAllDecls(Allocator1, null);
 }
+
+const std = if (@hasDecl(zl.builtin.root, "is_zig_lib") and zl.builtin.root.is_zig_lib) zl else @import("std");
