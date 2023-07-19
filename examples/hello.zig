6338eb12eb63181e1ebaabce3321f98b91374309
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-19 23:42:40 +0000

    * Updated usage of `write`. * Updated usage of `exit`.

--- examples/hello.zig
+++ examples/hello.zig
@@ -13,23 +13,23 @@ fn NTuple(comptime n: usize) type {
 pub export fn _start() void {
     @setAlignStack(16);
     if (true) {
-        return builtin.debug.write(hello_world);
+        return debug.write(hello_world);
     }
     if (false) {
         const x: []const u8 = (comptime mem.view("Hello, world!\n")).readAll();
-        builtin.debug.write(x);
+        debug.write(x);
     }
     if (false) {
         var array: mem.StaticString(4096) = undefined;
         array.impl.ub_word = 0;
         array.writeAny(mem.follow_wr_spec, .{ "Hello", ",", " World", "!", "\n" });
-        builtin.debug.write(array.readAll());
+        debug.write(array.readAll());
     }
     if (true) {
         var array: mem.StaticString(4096) = undefined;
         array.undefineAll();
         array.writeMany("Hello, world!\n");
-        builtin.debug.write(array.readAll());
+        debug.write(array.readAll());
     }
     if (false) {
         const S = struct {
@@ -39,7 +39,7 @@ pub export fn _start() void {
         var s: S = .{};
         var z: S = .{};
         inline for (hello_world, 0..) |c, i| s.auto[i] = c;
-        builtin.debug.write(z.auto[0..hello_world.len]);
+        debug.write(z.auto[0..hello_world.len]);
     }
-    builtin.proc.exit(0);
+    proc.exit(0);
 }
