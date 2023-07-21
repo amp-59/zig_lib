d53ebd235effdb19e5fc4d214f3ba8b17ba9e507
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-21 05:09:41 +0000

    Added `std` namespace to more closely match the standard--the compiler needs this in zig_lib_dir else will crash.

---
+++ top/std.zig
@@ -0,0 +1,32 @@
+pub const zig_lib = {};
+pub const builtin = @import("./builtin.zig");
+pub const tab = @import("./tab.zig");
+pub const sys = @import("./sys.zig");
+pub const mem = @import("./mem.zig");
+pub const exe = @import("./exe.zig");
+pub const gen = @import("./gen.zig");
+pub const rng = @import("./rng.zig");
+pub const fmt = @import("./fmt.zig");
+pub const proc = @import("./proc.zig");
+pub const meta = @import("./meta.zig");
+pub const mach = @import("./mach.zig");
+pub const math = @import("./math.zig");
+pub const file = @import("./file.zig");
+pub const algo = @import("./algo.zig");
+pub const time = @import("./time.zig");
+pub const spec = @import("./spec.zig");
+pub const parse = @import("./parse.zig");
+pub const start = @import("./start.zig");
+pub const debug = @import("./debug.zig");
+pub const build = @import("./build.zig");
+pub const trace = @import("./trace.zig");
+pub const dwarf = @import("./dwarf.zig");
+pub const thread = @import("./thread.zig");
+pub const crypto = @import("./crypto.zig");
+pub const serial = @import("./serial.zig");
+pub const target = @import("./target.zig");
+pub const testing = @import("./testing.zig");
+pub const virtual = @import("./virtual.zig");
+comptime {
+    _ = start;
+}
