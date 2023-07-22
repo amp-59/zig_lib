5bd71f32792f2db59eacbd1d029cff6d3810818a
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-22 23:34:45 +0000

    Added toplevel comment to `start` namespace.

--- top/start.zig
+++ top/start.zig
@@ -1,16 +1,28 @@
+//! This file is always evaluated by `std.zig` (when zig_lib is acting as the
+//! standard library), but not by `zig_lib.zig` (when the library is used as a
+//! package). It serves the same purpose in both cases, achieved by different
+//! means.
 const proc = @import("./proc.zig");
 const debug = @import("./debug.zig");
 const builtin = @import("./builtin.zig");
+
 comptime {
-    _ = @import("./mach.zig");
-}
-pub usingnamespace blk: {
     if (builtin.is_zig_lib) {
-        // zl is captain now.
-        if (!@hasDecl(builtin.root, "_start")) {
-            // User has not defined `_start` in `root`.
+        if (@hasDecl(builtin.root, "_start")) {
+            @export(builtin.root._start, .{ .name = "_start" });
+        } else {
             @export(_0._start, .{ .name = "_start" });
         }
+    } else {
+        //
+    }
+    if (builtin.output_mode != .Exe) {
+        _ = @import("./mach.zig");
+    }
+}
+
+pub usingnamespace blk: {
+    if (builtin.is_zig_lib) {
         break :blk _0;
     } else if (builtin.output_mode == .Exe) {
         if (!@hasDecl(builtin.root, "_start")) {
@@ -19,6 +31,7 @@ pub usingnamespace blk: {
     }
     break :blk debug;
 };
+
 const _0 = struct {
     fn _start() callconv(.Naked) if (builtin.output_mode == .Exe) noreturn else void {
         proc.static.stack_addr = asm volatile (
