137913179226625ff3fd73dc5bdbf0c974f3584d
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-24 18:14:03 +0000

    Removed `ud64` and added `builtin` to imports.

--- top/build/gen/tasks-template.zig
+++ top/build/gen/tasks-template.zig
@@ -1,5 +1,5 @@
-const ud64 = @import("../builtin.zig").fmt.ud64;
 const fmt = @import("../fmt.zig");
 const mach = @import("../mach.zig");
+const builtin = @import("../builtin.zig");
 const types = @import("./types.zig");
 const safety: bool = false;
