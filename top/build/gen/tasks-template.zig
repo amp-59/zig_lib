4caec970f0f093f0c30ea81b9066907ffe594311
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-26 23:51:22 +0000

    Updated imports.

--- top/build/gen/tasks-template.zig
+++ top/build/gen/tasks-template.zig
@@ -1,4 +1,6 @@
 const fmt = @import("../fmt.zig");
+const mem = @import("../mem.zig");
+const meta = @import("../meta.zig");
 const mach = @import("../mach.zig");
 const builtin = @import("../builtin.zig");
 const types = @import("./types.zig");
