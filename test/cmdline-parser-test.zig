b6c98dcc88e2e90b9ba5933d636b0d487f7aff9c
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-26 00:11:07 +0000

    Updated to use `BuildCommand` parser.

--- test/cmdline-parser-test.zig
+++ test/cmdline-parser-test.zig
@@ -4,22 +4,19 @@ const mach = zl.mach;
 const spec = zl.spec;
 const build = zl.build;
 const debug = zl.debug;
-const parser = @import("../top/build/parsers.zig");
 const builtin = zl.builtin;
 
 pub usingnamespace zl.start;
 
-pub const logging_override: debug.Logging.Override = spec.logging.override.silent;
+pub const logging_override: debug.Logging.Override = spec.logging.override.verbose;
 
 pub fn main(args: anytype) void {
     @setRuntimeSafety(false);
     var allocator: mem.SimpleAllocator = .{};
     var cmd: build.BuildCommand = .{ .kind = .exe };
-
-    parser.buildParseArgs(&cmd, &allocator, args);
-
-    var buf: [4096]u8 = undefined;
-    var len: usize = cmd.formatWriteBuf(builtin.root.zig_exe, &.{}, &buf);
+    var buf: []u8 = allocator.allocate(u8, 65536);
+    cmd.formatParseArgs(&allocator, args);
+    var len: usize = cmd.formatWriteBuf(builtin.root.zig_exe, &.{}, buf.ptr);
 
     var pos: usize = 0;
     for (buf[0..len], 0..) |byte, idx| {
