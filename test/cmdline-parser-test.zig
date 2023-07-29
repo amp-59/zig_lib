78cc73a6773ce7d6eefea3990fb5d883336d2211
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-29 19:36:29 +0000

    Added test program to display a translation of user command line to a `BuildCommand` struct.

--- test/cmdline-parser-test.zig
+++ test/cmdline-parser-test.zig
@@ -1,5 +1,10 @@
 const zl = @import("../zig_lib.zig");
+const sys = zl.sys;
+const fmt = zl.fmt;
 const mem = zl.mem;
+const elf = zl.elf;
+const file = zl.file;
+const proc = zl.proc;
 const mach = zl.mach;
 const spec = zl.spec;
 const build = zl.build;
@@ -10,20 +15,68 @@ pub usingnamespace zl.start;
 
 pub const logging_override: debug.Logging.Override = spec.logging.override.verbose;
 
-pub fn main(args: anytype) void {
-    @setRuntimeSafety(false);
-    var allocator: mem.SimpleAllocator = .{};
-    var cmd: build.BuildCommand = .{ .kind = .exe };
-    var buf: []u8 = allocator.allocate(u8, 65536);
-    cmd.formatParseArgs(&allocator, args);
-    var len: usize = cmd.formatWriteBuf(builtin.root.zig_exe, &.{}, buf.ptr);
+var s: struct { x: ?usize = null, y: usize = 50 } = .{};
 
-    var pos: usize = 0;
-    for (buf[0..len], 0..) |byte, idx| {
-        if (byte == 0) {
-            debug.write(buf[pos..idx]);
-            debug.write("\n");
-            pos = idx +% 1;
+fn loadAll(comptime Pointers: type, pathname: [:0]const u8) Pointers {
+    @setRuntimeSafety(builtin.is_safe);
+    const prot: file.Map.Protection = .{ .exec = true };
+    const flags: file.Map.Flags = .{};
+    var addr: usize = 0x80000000;
+    var st: file.Status = undefined;
+    const fd: u64 = sys.call_noexcept(.open, u64, .{ @intFromPtr(pathname.ptr), 0, 0 });
+    if (fd > 1024) {
+        proc.exitErrorFault(error.NoSuchFileOrDirectory, pathname, 2);
+    }
+    sys.call_noexcept(.fstat, void, .{ fd, @intFromPtr(&st) });
+    const len: usize = mach.alignA64(st.size, 4096);
+    const rc_addr1: usize = sys.call_noexcept(.mmap, usize, [6]usize{ addr, len, @bitCast(prot), @bitCast(flags), fd, 0 });
+    if (rc_addr1 != addr) {
+        proc.exitErrorFault(error.OutOfMemory, pathname, 2);
+    }
+    const elf_info: elf.ElfInfo = elf.ElfInfo.init(addr);
+    addr +%= elf_info.executableOffset();
+    const rc_addr2: usize = sys.call_noexcept(.mmap, usize, [6]usize{ addr, len, @bitCast(prot), @bitCast(flags), fd, 0 });
+    if (rc_addr2 != addr) {
+        proc.exitErrorFault(error.OutOfMemory, pathname, 2);
+    }
+    return elf_info.loadAll(Pointers);
+}
+fn writeField(array: *mem.StaticArray(u8, 4096), field_name: []const u8, field_value: anytype) void {
+    array.writeOne('.');
+    array.writeMany(field_name);
+    array.writeMany(" = ");
+    array.writeFormat(fmt.render(.{}, field_value));
+    array.writeMany(", ");
+}
+pub fn main(args: anytype) !void {
+    var allocator: mem.SimpleAllocator = .{};
+    const ptrs: build.ParseCommand = loadAll(build.ParseCommand, "zig-out/lib/libparse.so");
+    var cmd: build.BuildCommand = .{ .kind = .exe };
+    ptrs.build(&cmd, &allocator, args.ptr, args.len);
+    var array: mem.StaticArray(u8, 4096) = undefined;
+    array.undefineAll();
+    array.writeMany("node.addBuild(allocator, .{ ");
+    inline for (@typeInfo(build.BuildCommand).Struct.fields) |field| {
+        if (@typeInfo(field.type) == .Optional) {
+            if (@field(cmd, field.name)) |field_value| {
+                writeField(&array, field.name, field_value);
+            }
+        } else {
+            if (field.default_value) |ptr| {
+                if (@field(cmd, field.name) != mem.pointerOpaque(field.type, ptr).*) {
+                    writeField(&array, field.name, @field(cmd, field.name));
+                }
+            } else {
+                writeField(&array, field.name, @field(cmd, field.name));
+            }
         }
     }
+    array.writeMany("}, \"");
+    if (cmd.name) |name| {
+        array.writeMany(name);
+    } else {
+        array.writeMany("anonymous");
+    }
+    array.writeMany("\", \"<path>\");\n");
+    debug.write(array.readAll());
 }
