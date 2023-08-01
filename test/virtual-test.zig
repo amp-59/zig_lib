ac8e18d7e933f2dd49c986e6f641907a0912afa5
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-01 10:38:31 +0000

    Fixed-up `virtual` test.

--- test/virtual-test.zig
+++ test/virtual-test.zig
@@ -12,88 +12,16 @@ const builtin = zl.builtin;
 const testing = zl.testing;
 
 pub usingnamespace zl.start;
-pub usingnamespace root;
 
-const root = opaque {
-    pub const AddressSpace = spec.address_space.regular_128;
-};
+const tab = @import("./tab.zig");
 
-pub const logging_override: debug.Logging.Override = spec.logging.override.silent;
+pub const logging_override: debug.Logging.Override = spec.logging.override.verbose;
 
 const PrintArray = mem.StaticString(16384);
 var print_array: PrintArray = undefined;
 
 pub const runtime_assertions: bool = true;
 pub const render_radix: u16 = 10;
-
-pub const trivial_list: []const virtual.Arena = &.{
-    .{ .lb_addr = 0x000004000000, .up_addr = 0x010000000000 },
-    .{ .lb_addr = 0x010000000000, .up_addr = 0x110000000000 },
-    .{ .lb_addr = 0x110000000000, .up_addr = 0x120000000000, .options = .{ .thread_safe = true } },
-};
-pub const simple_list: []const virtual.Arena = &.{
-    .{ .lb_addr = 0x000040000000, .up_addr = 0x010000000000 },
-    .{ .lb_addr = 0x100000000000, .up_addr = 0x110000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x110000000000, .up_addr = 0x120000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x120000000000, .up_addr = 0x130000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x7f0000000000, .up_addr = 0x800000000000 },
-};
-pub const rare_sub_list: []const virtual.Arena = &.{
-    .{ .lb_addr = 0x000040000000, .up_addr = 0x010000000000 },
-    .{ .lb_addr = 0x110000000000, .up_addr = 0x120000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x7f0000000000, .up_addr = 0x800000000000 },
-};
-// zig fmt: off
-pub const complex_list: []const virtual.Arena = &.{
-    .{ .lb_addr = 0x0f0000000000, .up_addr = 0x100000000000 },
-    .{ .lb_addr = 0x100000000000, .up_addr = 0x110000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x110000000000, .up_addr = 0x120000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x120000000000, .up_addr = 0x130000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x130000000000, .up_addr = 0x140000000000 }, // [X]
-    .{ .lb_addr = 0x140000000000, .up_addr = 0x150000000000 }, // [X]
-    .{ .lb_addr = 0x150000000000, .up_addr = 0x160000000000 },
-    .{ .lb_addr = 0x2a0000000000, .up_addr = 0x2b0000000000 },
-    .{ .lb_addr = 0x2d0000000000, .up_addr = 0x2e0000000000 }, // [X]
-    .{ .lb_addr = 0x2e0000000000, .up_addr = 0x2f0000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x2f0000000000, .up_addr = 0x300000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x300000000000, .up_addr = 0x310000000000 },
-    .{ .lb_addr = 0x310000000000, .up_addr = 0x320000000000 }, // [X]
-    .{ .lb_addr = 0x320000000000, .up_addr = 0x330000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x330000000000, .up_addr = 0x340000000000 },
-    .{ .lb_addr = 0x340000000000, .up_addr = 0x350000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x350000000000, .up_addr = 0x360000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x360000000000, .up_addr = 0x370000000000 }, // [X]
-    .{ .lb_addr = 0x370000000000, .up_addr = 0x380000000000 }, // [X]
-    .{ .lb_addr = 0x380000000000, .up_addr = 0x390000000000 }, // [X]
-    .{ .lb_addr = 0x390000000000, .up_addr = 0x3a0000000000 }, // [X]
-    .{ .lb_addr = 0x430000000000, .up_addr = 0x440000000000 },
-    .{ .lb_addr = 0x440000000000, .up_addr = 0x450000000000 },
-    .{ .lb_addr = 0x450000000000, .up_addr = 0x460000000000 },
-    .{ .lb_addr = 0x460000000000, .up_addr = 0x470000000000 },
-    .{ .lb_addr = 0x470000000000, .up_addr = 0x480000000000 },
-    .{ .lb_addr = 0x480000000000, .up_addr = 0x490000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x490000000000, .up_addr = 0x4a0000000000 }, // [X]
-    .{ .lb_addr = 0x4a0000000000, .up_addr = 0x4b0000000000 },
-    .{ .lb_addr = 0x4b0000000000, .up_addr = 0x4c0000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x4c0000000000, .up_addr = 0x4d0000000000 }, // [X]
-    .{ .lb_addr = 0x4d0000000000, .up_addr = 0x4e0000000000 },
-    .{ .lb_addr = 0x4e0000000000, .up_addr = 0x4f0000000000 },
-    .{ .lb_addr = 0x4f0000000000, .up_addr = 0x500000000000 }, // [X]
-    .{ .lb_addr = 0x500000000000, .up_addr = 0x510000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x510000000000, .up_addr = 0x520000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x520000000000, .up_addr = 0x530000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x530000000000, .up_addr = 0x540000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x540000000000, .up_addr = 0x550000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x550000000000, .up_addr = 0x560000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x560000000000, .up_addr = 0x570000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x570000000000, .up_addr = 0x580000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x580000000000, .up_addr = 0x590000000000, .options = .{ .thread_safe = true } },
-    .{ .lb_addr = 0x590000000000, .up_addr = 0x5a0000000000, .options = .{ .thread_safe = true } }, // [X]
-    .{ .lb_addr = 0x5a0000000000, .up_addr = 0x5b0000000000 }, // [X]
-    .{ .lb_addr = 0x5b0000000000, .up_addr = 0x5c0000000000 },
-    .{ .lb_addr = 0x5c0000000000, .up_addr = 0x5d0000000000 },
-};
-// zig fmt: on
 
 /// Initial concern by increased binary size switching to new (formulaic)
 /// address space. However code scores much better on MCA.
@@ -119,28 +47,27 @@ fn testRegularAddressSpace() !void {
     var allocator: Allocator = try Allocator.init(&address_space);
     defer allocator.deinit(&address_space);
     var i: u8 = 1;
-    while (i != AddressSpace.addr_spec.divisions) : (i += 1) {
+    while (i != AddressSpace.addr_spec.divisions) : (i +%= 1) {
         try mem.acquire(AddressSpace, &address_space, i);
     }
     try debug.expectEqual(u64, AddressSpace.addr_spec.divisions, address_space.count());
 }
 fn testDiscreteAddressSpace(comptime list: anytype) !void {
     const AddressSpace = virtual.GenericDiscreteAddressSpace(.{ .list = list });
-
     var address_space: AddressSpace = .{};
     const Allocator = mem.GenericArenaAllocator(.{ .arena_index = 0, .AddressSpace = AddressSpace });
     var allocator: Allocator = try Allocator.init(&address_space);
-    defer allocator.deinit(&address_space);
-    comptime var i: u8 = 1;
-    inline while (i != AddressSpace.addr_spec.list.len) : (i += 1) {
-        try mem.acquireStatic(AddressSpace, &address_space, i);
+    comptime var idx: u8 = 1;
+    inline while (idx != AddressSpace.addr_spec.list.len) : (idx +%= 1) {
+        try mem.acquireStatic(AddressSpace, &address_space, idx);
     }
     try debug.expectEqual(u64, AddressSpace.addr_spec.list.len, address_space.count());
-    i = 1;
-    inline while (i != AddressSpace.addr_spec.list.len) : (i += 1) {
-        mem.releaseStatic(AddressSpace, &address_space, i);
+    idx = 1;
+    inline while (idx != AddressSpace.addr_spec.list.len) : (idx +%= 1) {
+        mem.releaseStatic(AddressSpace, &address_space, idx);
     }
-    try debug.expectEqual(u64, AddressSpace.addr_spec.list.len, 0);
+    allocator.deinit(&address_space);
+    try debug.expectEqual(u64, address_space.count(), 0);
 }
 fn testDiscreteSubSpaceFromDiscrete(comptime sup_spec: virtual.DiscreteAddressSpaceSpec, comptime sub_spec: virtual.DiscreteAddressSpaceSpec) !void {
     const AddressSpace = comptime blk: {
@@ -174,12 +101,11 @@ fn testRegularAddressSubSpaceFromDiscrete(comptime sup_spec: virtual.DiscreteAdd
     var allocator_2: Allocator2 = try Allocator2.init(&sub_space);
     defer allocator_2.deinit(&sub_space);
 }
-
 fn testTaggedSets() !void {
     const E = enum { a, b, c, d };
     const K = virtual.GenericDiscreteAddressSpace(.{
+        .index_type = E,
         .label = "tagged",
-        .idx_type = E,
         .list = &.{
             .{ .lb_addr = 0x40000000, .up_addr = 0x80000000 },
             .{ .lb_addr = 0x80000000, .up_addr = 0x100000000 },
@@ -207,17 +133,17 @@ pub fn main() !void {
     try meta.wrap(testArenaIntersection());
     try meta.wrap(testRegularAddressSpace());
     try meta.wrap(testTaggedSets());
-    try meta.wrap(testDiscreteAddressSpace(trivial_list));
-    try meta.wrap(testDiscreteAddressSpace(complex_list));
-    try meta.wrap(testDiscreteAddressSpace(simple_list));
+    try meta.wrap(testDiscreteAddressSpace(tab.trivial_list));
+    try meta.wrap(testDiscreteAddressSpace(tab.complex_list));
+    try meta.wrap(testDiscreteAddressSpace(tab.simple_list));
     try meta.wrap(testRegularAddressSubSpaceFromDiscrete(.{
-        .list = complex_list,
+        .list = tab.complex_list,
         .subspace = &[_]meta.Generic{virtual.generic(.{
-            .lb_addr = complex_list[34].lb_addr,
-            .up_addr = complex_list[42].up_addr,
+            .lb_addr = tab.complex_list[34].lb_addr,
+            .up_addr = tab.complex_list[42].up_addr,
             .divisions = 16,
             .options = .{ .thread_safe = true },
         })},
     }));
-    try meta.wrap(testDiscreteSubSpaceFromDiscrete(.{ .list = simple_list }, .{ .list = rare_sub_list }));
+    try meta.wrap(testDiscreteSubSpaceFromDiscrete(.{ .list = tab.simple_list }, .{ .list = tab.rare_sub_list }));
 }
