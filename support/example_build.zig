c2d52a3e632aa1971733ade7c749dc1ff178ba55
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-04 14:19:50 +0000

    Added build command declaration for clarity.

--- support/example_build.zig
+++ support/example_build.zig
@@ -4,7 +4,13 @@ pub const zl = @import("./zig_lib/zig_lib.zig");
 const build = zl.build;
 const Node = build.GenericNode(.{});
 
+const build_cmd: build.BuildCommand = .{
+    .kind = .exe,
+    .strip = true,
+    .compiler_rt = false,
+};
+
 pub fn buildMain(allocator: *build.Allocator, toplevel: *Node) void {
-    const main: *Node = toplevel.addBuild(allocator, .{ .kind = .exe, .strip = true }, "main", "src/main.zig");
+    const main: *Node = toplevel.addBuild(allocator, build_cmd, "main", "src/main.zig");
     main.descr = "Build the program.";
 }
