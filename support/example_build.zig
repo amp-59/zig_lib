dfd61434f7dd2aecabcd5f8787c1d9aa2099e312
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-04 08:22:11 +0000

    Updated usage of `addBuild`.

--- support/example_build.zig
+++ support/example_build.zig
@@ -5,6 +5,6 @@ const build = zl.build;
 const Node = build.GenericNode(.{});
 
 pub fn buildMain(allocator: *build.Allocator, toplevel: *Node) void {
-    const main: *Node = toplevel.addBuild(allocator, "main", "src/main.zig");
+    const main: *Node = toplevel.addBuild(allocator, .{ .kind = .exe, .strip = true }, "main", "src/main.zig");
     main.descr = "Build the program.";
 }
