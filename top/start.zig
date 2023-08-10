6dcb760d5160f9e74b0ce569dd1ec22a137b6b7b
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-10 23:35:49 +0000

    start: * Removed unnecessary usage of `@as`. * Simplified return type of `_start` to always be `void` instead of   `noreturn`.

--- top/start.zig
+++ top/start.zig
@@ -42,7 +42,7 @@ pub usingnamespace blk: {
 };
 
 const _0 = struct {
-    fn _start() callconv(.Naked) if (builtin.output_mode == .Exe) noreturn else void {
+    fn _start() callconv(.Naked) void {
         asm volatile (switch (builtin.cpu.arch) {
                 .x86_64 =>
                 \\ xorl %%ebp, %%ebp
@@ -68,7 +68,7 @@ const _0 = struct {
 pub var stack: usize = undefined;
 
 const _1 = struct {
-    pub export fn _start() callconv(.Naked) if (builtin.output_mode == .Exe) noreturn else void {
+    pub export fn _start() callconv(.Naked) void {
         asm volatile (switch (builtin.cpu.arch) {
                 .x86_64 =>
                 \\ xorl %%ebp, %%ebp
@@ -108,15 +108,15 @@ pub fn start() callconv(.C) noreturn {
         if (main_type_info.Fn.params.len == 1) {
             const args_len: u64 = @as(*u64, @ptrFromInt(stack)).*;
             const args_addr: u64 = stack +% 8;
-            const args: [*][*:0]u8 = @as([*][*:0]u8, @ptrFromInt(args_addr));
+            const args: [*][*:0]u8 = @ptrFromInt(args_addr);
             break :blk_0 .{args[0..args_len]};
         }
         if (main_type_info.Fn.params.len == 2) {
             const args_len: u64 = @as(*u64, @ptrFromInt(stack)).*;
             const args_addr: u64 = stack +% 8;
             const vars_addr: u64 = stack +% 16 +% (args_len * 8);
-            const args: [*][*:0]u8 = @as([*][*:0]u8, @ptrFromInt(args_addr));
-            const vars: [*][*:0]u8 = @as([*][*:0]u8, @ptrFromInt(vars_addr));
+            const args: [*][*:0]u8 = @ptrFromInt(args_addr);
+            const vars: [*][*:0]u8 = @ptrFromInt(vars_addr);
             const vars_len: u64 = blk_1: {
                 var len: u64 = 0;
                 while (@intFromPtr(vars[len]) != 0) len += 1;
@@ -129,19 +129,18 @@ pub fn start() callconv(.C) noreturn {
             const args_len: u64 = @as(*u64, @ptrFromInt(stack)).*;
             const args_addr: u64 = stack +% 8;
             const vars_addr: u64 = args_addr +% 8 +% (args_len * 8);
-            const args: [*][*:0]u8 = @as([*][*:0]u8, @ptrFromInt(args_addr));
-            const vars: [*][*:0]u8 = @as([*][*:0]u8, @ptrFromInt(vars_addr));
+            const args: [*][*:0]u8 = @ptrFromInt(args_addr);
+            const vars: [*][*:0]u8 = @ptrFromInt(vars_addr);
             const vars_len: u64 = blk_1: {
                 var len: u64 = 0;
                 while (@intFromPtr(vars[len]) != 0) len += 1;
                 break :blk_1 len;
             };
             const auxv_addr: u64 = vars_addr +% 8 +% (vars_len * 8);
-            const auxv: auxv_type = @as(auxv_type, @ptrFromInt(auxv_addr));
+            const auxv: auxv_type = @ptrFromInt(auxv_addr);
             break :blk_0 .{ args[0..args_len], vars[0..vars_len], auxv };
         }
     };
-
     if (@as(u5, @bitCast(builtin.signal_handlers)) != 0) {
         proc.enableExceptionHandlers();
     }
@@ -158,19 +157,17 @@ pub fn start() callconv(.C) noreturn {
         if (@call(.auto, builtin.root.main, params)) {
             proc.exitNotice(0);
         } else |err| {
-            proc.exitError(err, @as(u8, @intCast(@intFromError(err))));
+            proc.exitError(err, @intCast(@intFromError(err)));
         }
     }
-
     if (main_return_type_info == .ErrorUnion and
         main_return_type_info.ErrorUnion.payload == u8)
     {
         if (@call(.auto, builtin.root.main, params)) |rc| {
             proc.exitNotice(rc);
         } else |err| {
-            proc.exitError(err, @as(u8, @intCast(@intFromError(err))));
+            proc.exitError(err, @intCast(@intFromError(err)));
         }
     }
-
     debug.assert(main_return_type_info != .ErrorSet);
 }
