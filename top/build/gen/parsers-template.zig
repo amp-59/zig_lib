f0cc1e11a1041c7b3d952a45768ec1c9cf743922
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-10 23:22:45 +0000

    buildgen: Updated imports of `parsers` template source.

--- top/build/gen/parsers-template.zig
+++ top/build/gen/parsers-template.zig
@@ -1,3 +1,5 @@
 const mem = @import("../mem.zig");
 const types = @import("./types.zig");
+const debug = @import("../debug.zig");
+const parse = @import("../parse.zig");
 pub usingnamespace @import("../start.zig");
