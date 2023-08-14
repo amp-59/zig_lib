b504f2980b6c0a8f5ce848af7d9cbc4e7ce4c3d4
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-14 05:43:20 +0000

    Disabled test for `clock_gettime`, as this is currently not possible using the new loader. However it is possible using the loader defined in the function body of `testVClockGettime`.

--- test/proc-test.zig
+++ test/proc-test.zig
@@ -2,7 +2,7 @@ const zl = @import("../zig_lib.zig");
 const mem = zl.mem;
 const sys = zl.sys;
 const fmt = zl.fmt;
-const exe = zl.elf;
+const elf = zl.elf;
 const proc = zl.proc;
 const mach = zl.mach;
 const time = zl.time;
@@ -14,15 +14,18 @@ const build = zl.build;
 const builtin = zl.builtin;
 const testing = zl.testing;
 pub usingnamespace zl.start;
+
 pub const runtime_assertions: bool = true;
+pub const is_safe: bool = true;
 pub const logging_override: debug.Logging.Override = spec.logging.override.verbose;
 
+pub const want_stack_traces: bool = true;
 pub const signal_handlers: debug.SignalHandlers = .{
-    .SegmentationFault = false,
-    .BusError = false,
-    .IllegalInstruction = false,
-    .FloatingPointError = false,
-    .Trap = false,
+    .SegmentationFault = true,
+    .BusError = true,
+    .IllegalInstruction = true,
+    .FloatingPointError = true,
+    .Trap = true,
 };
 const Array = mem.UnstructuredStreamView(8, 8, struct {}, .{});
 const Mapping = extern struct {
@@ -71,6 +74,7 @@ fn testCloneAndFutex() !void {
     try debug.expectEqual(u32, 32, futex1);
     try debug.expectEqual(u32, 32, futex2);
 }
+
 fn testFindNameInPath(vars: [][*:0]u8) !void {
     var itr: proc.PathIterator = .{
         .paths = proc.environmentValue(vars, "PATH").?,
@@ -99,13 +103,86 @@ fn testFindNameInPath(vars: [][*:0]u8) !void {
         }
     }
 }
+
 fn testVClockGettime(aux: *const anyopaque) !void {
-    const ElfInfo = zl.elf.GenericElfInfo(.{});
+    const DynamicLoader = zl.elf.GenericDynamicLoader(.{ .options = .{ .show_defined = true, .show_sections = true } });
+    _ = DynamicLoader;
     const vdso_addr: u64 = proc.auxiliaryValue(aux, .vdso_addr).?;
-    var elf_info: ElfInfo = ElfInfo.init(vdso_addr, 0);
+
+    const ElfInfo = extern struct {
+        addr: usize,
+        pairs: [tags.len]Pair,
+        const Pair = extern struct {
+            tag: Tag,
+            shdr: *elf.Elf64_Shdr,
+        };
+        const Tag = enum(u8) {
+            @".dynsym" = 0,
+            @".dynstr" = 1,
+            @".rodata" = 2,
+            @".text" = 3,
+            @".strtab" = 4,
+            @".dynamic" = 5,
+        };
+        const dynsym_idx: comptime_int = @intFromEnum(Tag.@".dynsym");
+        const dynstr_idx: comptime_int = @intFromEnum(Tag.@".dynstr");
+        const rodata_idx: comptime_int = @intFromEnum(Tag.@".rodata");
+        const text_idx: comptime_int = @intFromEnum(Tag.@".text");
+        const strtab_idx: comptime_int = @intFromEnum(Tag.@".strtab");
+        const dynamic_idx: comptime_int = @intFromEnum(Tag.@".dynamic");
+
+        const tags = meta.tagList(Tag);
+        pub fn init(elf_addr: usize) @This() {
+            @setRuntimeSafety(builtin.is_safe);
+            const ehdr: *elf.Elf64_Ehdr = @ptrFromInt(elf_addr);
+            var shdr: *elf.Elf64_Shdr = @ptrFromInt(elf_addr +% ehdr.e_shoff +% (ehdr.e_shstrndx *% ehdr.e_shentsize));
+            var pairs: [tags.len]Pair = comptime builtin.zero([tags.len]Pair);
+            var strtab_addr: u64 = elf_addr +% shdr.sh_offset;
+            var addr: u64 = elf_addr +% ehdr.e_shoff;
+            var shdr_idx: u64 = 0;
+            while (shdr_idx != ehdr.e_shnum) : ({
+                shdr_idx +%= 1;
+                addr +%= ehdr.e_shentsize;
+                shdr = @ptrFromInt(addr);
+            }) {
+                var tags_idx: usize = 0;
+                while (tags_idx != tags.len) : (tags_idx +%= 1) {
+                    const str: [*:0]u8 = @ptrFromInt(strtab_addr +% shdr.sh_name);
+                    var idx: usize = 0;
+                    while (str[idx] != 0) : (idx +%= 1) {
+                        if (@tagName(tags[tags_idx])[idx] != str[idx]) {
+                            break;
+                        }
+                    } else {
+                        pairs[tags_idx].shdr = shdr;
+                        pairs[tags_idx].tag = tags[tags_idx];
+                    }
+                }
+            }
+            return .{ .addr = elf_addr, .pairs = pairs };
+        }
+        fn lookup(elf_info: *@This(), symbol: []const u8) ?elf.Elf64_Sym {
+            var dyn_idx: usize = 1;
+            const pairs = elf_info.pairs;
+            const dynsym: [*]elf.Elf64_Sym = @ptrFromInt(elf_info.addr +% pairs[dynsym_idx].shdr.sh_offset);
+            while (dyn_idx != @divExact(pairs[dynsym_idx].shdr.sh_size, @sizeOf(elf.Elf64_Sym))) : (dyn_idx +%= 1) {
+                const sym: elf.Elf64_Sym = dynsym[dyn_idx];
+                const str: [*]u8 = @ptrFromInt(elf_info.addr +% pairs[dynstr_idx].shdr.sh_offset +% sym.st_name);
+                var idx: usize = 0;
+                while (str[idx] != 0) : (idx +%= 1) {
+                    if (symbol[idx] != str[idx]) break;
+                } else {
+                    return sym;
+                }
+            }
+            return null;
+        }
+    };
+
+    //var elf_info: DynamicLoader.Info = DynamicLoader.Info.init(vdso_addr);
+    var elf_info: ElfInfo = ElfInfo.init(vdso_addr);
     if (elf_info.lookup("clock_gettime")) |symbol| {
         const clock_gettime: time.ClockGetTime = @ptrFromInt(vdso_addr +% symbol.st_value);
-
         const ts1: time.TimeSpec = try time.get(.{}, .realtime);
         const ts2: time.TimeSpec = try time.get(.{}, .realtime);
         var vts1: time.TimeSpec = undefined;
@@ -140,9 +217,10 @@ fn recursion(buf: *[4096]u8) void {
     recursion(&next);
 }
 pub fn main(_: [][*:0]u8, vars: [][*:0]u8, aux: anytype) !void {
+    _ = aux;
     try testCloneAndFutex();
     try testFindNameInPath(vars);
-    try testVClockGettime(aux);
+    //try testVClockGettime(aux);
     try testUpdateSignalAction();
     proc.about.sampleAllReports();
 }
