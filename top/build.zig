813be2c34ead36900dd88a430a99758d9ec6dce6
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-16 09:09:16 +0000

    build: Restored usage of `addGeneric`, with `usize` implied.

--- top/build.zig
+++ top/build.zig
@@ -485,7 +485,7 @@ pub fn GenericNode(comptime builder_spec: BuilderSpec) type {
             @setRuntimeSafety(builder_spec.options.enable_safety);
             const size_of: comptime_int = @sizeOf(types.Path);
             const addr_buf: *u64 = @ptrCast(&node.impl.paths);
-            const ret: *types.Path = @ptrFromInt(allocator.addGenericSize(Size, size_of, //
+            const ret: *types.Path = @ptrFromInt(allocator.addGeneric(size_of, //
                 builder_spec.options.init_len.paths, addr_buf, &node.impl.paths_max_len, node.impl.paths_len));
             node.impl.paths_len +%= 1;
             return ret;
@@ -494,7 +494,7 @@ pub fn GenericNode(comptime builder_spec: BuilderSpec) type {
             @setRuntimeSafety(builder_spec.options.enable_safety);
             const size_of: comptime_int = @sizeOf(*Node);
             const addr_buf: *u64 = @ptrCast(&node.impl.nodes);
-            const ret: **Node = @ptrFromInt(allocator.addGenericSize(Size, size_of, //
+            const ret: **Node = @ptrFromInt(allocator.addGeneric(size_of, //
                 builder_spec.options.init_len.nodes, addr_buf, &node.impl.nodes_max_len, node.impl.nodes_len));
             node.impl.nodes_len +%= 1;
             return ret;
@@ -503,7 +503,7 @@ pub fn GenericNode(comptime builder_spec: BuilderSpec) type {
             @setRuntimeSafety(builder_spec.options.enable_safety);
             const size_of: comptime_int = @sizeOf(Dependency);
             const addr_buf: *u64 = @ptrCast(&node.impl.deps);
-            const ret: *Dependency = @ptrFromInt(allocator.addGenericSize(Size, size_of, //
+            const ret: *Dependency = @ptrFromInt(allocator.addGeneric(size_of, //
                 builder_spec.options.init_len.deps, addr_buf, &node.impl.deps_max_len, node.impl.deps_len));
             node.impl.deps_len +%= 1;
             mem.zero(Dependency, ret);
@@ -513,7 +513,7 @@ pub fn GenericNode(comptime builder_spec: BuilderSpec) type {
             @setRuntimeSafety(builder_spec.options.enable_safety);
             const size_of: comptime_int = @sizeOf([*:0]u8);
             const addr_buf: *u64 = @ptrCast(&node.impl.args);
-            const ret: *[*:0]u8 = @ptrFromInt(allocator.addGenericSize(Size, size_of, //
+            const ret: *[*:0]u8 = @ptrFromInt(allocator.addGeneric(size_of, //
                 builder_spec.options.init_len.args, addr_buf, &node.impl.args_max_len, node.impl.args_len));
             node.impl.args_len +%= 1;
             return ret;
@@ -534,7 +534,7 @@ pub fn GenericNode(comptime builder_spec: BuilderSpec) type {
             @setRuntimeSafety(builder_spec.options.enable_safety);
             const size_of: comptime_int = @sizeOf(Config);
             const addr_buf: *u64 = @ptrCast(&node.impl.cfgs);
-            const ptr: *Config = @ptrFromInt(allocator.addGenericSize(Size, size_of, //
+            const ptr: *Config = @ptrFromInt(allocator.addGeneric(size_of, //
                 builder_spec.options.init_len.cfgs, addr_buf, &node.impl.cfgs_max_len, node.impl.cfgs_len));
             ptr.* = .{ .name = name, .value = value };
             node.impl.cfgs_len +%= 1;
