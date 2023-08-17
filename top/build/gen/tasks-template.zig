825dc06cb911cc6723a0b8ad9013d3a0b04290a3
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-17 18:45:41 +0000

    buildgen: Updated imports of `tasks` template source.

--- top/build/gen/tasks-template.zig
+++ top/build/gen/tasks-template.zig
@@ -1,7 +1,7 @@
 const fmt = @import("../fmt.zig");
 const mem = @import("../mem.zig");
-const meta = @import("../meta.zig");
-const mach = @import("../mach.zig");
+const debug = @import("../debug.zig");
+const parse = @import("../parse.zig");
 const builtin = @import("../builtin.zig");
 const types = @import("./types.zig");
 const safety: bool = false;
