5b84f9bb6523d8678edb224d41796e293cf09b96
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-22 18:32:34 +0000

    cp: Rewrote example program to use errors.

--- examples/cp.zig
+++ examples/cp.zig
@@ -14,21 +14,18 @@ pub usingnamespace zl.start;
 pub const logging_override: debug.Logging.Override = spec.logging.override.verbose;
 
 pub fn copy(src_pathname: [:0]const u8, dest_pathname: [:0]const u8) !void {
-    blk: {
-        const from_fd: u64 = file.open(.{ .errors = .{}, .options = .{ .read = true, .no_follow = true } }, src_pathname);
-        if (from_fd > 1024) break :blk;
-        const to_fd: u64 = file.create(.{ .errors = .{}, .options = .{ .exclusive = false } }, dest_pathname, file.mode.executable);
-        if (to_fd > 1024) break :blk;
-        const size: u64 = file.status(.{ .errors = .{} }, from_fd).size;
-        if (size == 0) break :blk;
-        debug.assertEqual(u64, size, file.copy(.{ .errors = .{} }, from_fd, null, to_fd, null, size));
+    const from_fd: usize = try file.open(.{ .options = .{ .no_follow = true, .read_write = true } }, src_pathname);
+    const to_fd: usize = try file.create(.{ .options = .{ .exclusive = false, .write = true } }, dest_pathname, file.mode.regular);
+    const from_st: file.Status = try file.status(.{}, from_fd);
+    if (from_st.size != 0) {
+        debug.assertEqual(u64, from_st.size, try file.copy(.{}, to_fd, null, from_fd, null, from_st.size));
     }
 }
 pub fn main(args: [][*:0]u8) !void {
     if (args.len != 3) {
         return;
     }
-    const src_pathname: [:0]const u8 = mach.manyToSlice80(args[1]);
-    const dest_pathname: [:0]const u8 = mach.manyToSlice80(args[2]);
+    const src_pathname: [:0]const u8 = mem.terminate(args[1], 0);
+    const dest_pathname: [:0]const u8 = mem.terminate(args[2], 0);
     try copy(src_pathname, dest_pathname);
 }
