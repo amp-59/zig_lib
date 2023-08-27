b1c3d0f6fa0e8dadafc09c928338f7ca2ffb25e7
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-08-27 23:17:42 +0000

    build: Added autoloader extension source files.

---
+++ top/build/archive.auto.zig
@@ -0,0 +1,51 @@
+const build = @import("./types.zig");
+const source = build.GenericCommand(build.ArchiveCommand);
+renderWriteBuf: *const fn (
+    p_0: *const build.ArchiveCommand,
+    p_1: [*]u8,
+) usize = @ptrFromInt(8),
+fieldEditDistance: *const fn (
+    p_0: *build.ArchiveCommand,
+    p_1: *build.ArchiveCommand,
+) usize = @ptrFromInt(8),
+writeFieldEditDistance: *const fn (
+    p_0: [*]u8,
+    p_1: []const u8,
+    p_2: *build.ArchiveCommand,
+    p_3: *build.ArchiveCommand,
+    p_4: bool,
+) usize = @ptrFromInt(8),
+indexOfCommonLeastDifference: *const fn (
+    p_0: *build.Allocator,
+    p_1: []*build.ArchiveCommand,
+) usize = @ptrFromInt(8),
+formatWriteBuf: *const fn (
+    p_0: *build.ArchiveCommand,
+    p_1: []const u8,
+    p_2: []const build.Path,
+    p_3: [*]u8,
+) usize = @ptrFromInt(8),
+formatLength: *const fn (
+    p_0: *build.ArchiveCommand,
+    p_1: []const u8,
+    p_2: []const build.Path,
+) usize = @ptrFromInt(8),
+formatParseArgs: *const fn (
+    p_0: *build.ArchiveCommand,
+    p_1: *build.Allocator,
+    p_2: [][*:0]u8,
+) void = @ptrFromInt(8),
+fn load(ptrs: *@This()) callconv(.C) void {
+    ptrs.renderWriteBuf = @ptrCast(&source.renderWriteBuf);
+    ptrs.fieldEditDistance = @ptrCast(&source.fieldEditDistance);
+    ptrs.writeFieldEditDistance = @ptrCast(&source.writeFieldEditDistance);
+    ptrs.indexOfCommonLeastDifference = @ptrCast(&source.indexOfCommonLeastDifference);
+    ptrs.formatWriteBuf = @ptrCast(&source.formatWriteBuf);
+    ptrs.formatLength = @ptrCast(&source.formatLength);
+    ptrs.formatParseArgs = @ptrCast(&source.formatParseArgs);
+}
+comptime {
+    if (@import("builtin").output_mode != .Exe) {
+        @export(load, .{ .name = "load", .linkage = .Strong });
+    }
+}
