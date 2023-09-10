8b75dea6268a2070423d6054cf0c83ea94a158da
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-09-10 03:43:44 +0000

    build: Updated core library function for file system restructure.

--- top/build/config_core.auto.zig
+++ top/build/config_core.auto.zig
@@ -1,18 +1,18 @@
 const zl = @import("zl");
 const fmt = zl.fmt;
-const types = zl.build;
+const build = zl.build;
 const start = zl.start;
 pub usingnamespace start;
-pub const comptime_errors: bool = false;
 
-pub fn main() void {}
-
-fn formatWriteBuf(spec: *const types.BuilderSpec, buf: [*]u8) usize {
+fn formatWriteBuf(spec: *const build.BuilderSpec, buf: [*]u8) usize {
     var ptr: [*]u8 = fmt.strcpyEqu(buf, "pub const Builder=@import(\"zl\").build.GenericBuilder(");
     ptr += fmt.render(.{ .infer_type_names = true }, spec.*).formatWriteBuf(ptr);
     return fmt.strlen(fmt.strcpyEqu(ptr, ");\n"), buf);
 }
-
-export fn load(vtable: *zl.meta.Field(types.VTable, "core")) callconv(.C) void {
-    vtable.formatWriteBuf = formatWriteBuf;
+export fn load(vtable: *extern struct {
+    core: extern struct {
+        formatWriteBuf: *const @TypeOf(formatWriteBuf),
+    },
+}) void {
+    vtable.core.formatWriteBuf = formatWriteBuf;
 }
