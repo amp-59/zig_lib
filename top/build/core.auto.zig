16e11495f7dc30d305a9219f2e7a9932343ed28d
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-09-16 22:30:34 +0000

    build: Updated definition of `formatWriteBuf` for `BuilderSpec` to omit        extra declaration.

--- top/build/core.auto.zig
+++ top/build/core.auto.zig
@@ -4,15 +4,20 @@ const build = zl.build;
 const start = zl.start;
 pub usingnamespace start;
 
-fn formatWriteBuf(spec: *const build.BuilderSpec, buf: [*]u8) usize {
-    var ptr: [*]u8 = fmt.strcpyEqu(buf, "pub const Builder=@import(\"zl\").build.GenericBuilder(");
+fn formatWriteBuf(any: *const anyopaque, buf: [*]u8) usize {
+    const spec: *const zl.build.BuilderSpec = @ptrCast(@alignCast(any));
+    var ptr: [*]u8 = fmt.strcpyEqu(buf, "@import(\"zl\").build.GenericBuilder(");
     ptr += fmt.render(.{ .infer_type_names = true }, spec.*).formatWriteBuf(ptr);
-    return fmt.strlen(fmt.strcpyEqu(ptr, ");\n"), buf);
+    return fmt.strlen(fmt.strcpyEqu(ptr, ")"), buf);
 }
-export fn load(vtable: *extern struct {
+fn load(vtable: *extern struct {
     core: extern struct {
         formatWriteBuf: *const @TypeOf(formatWriteBuf),
     },
-}) void {
+}) callconv(.C) void {
     vtable.core.formatWriteBuf = formatWriteBuf;
+}
+
+comptime {
+    @export(load, .{ .name = "load", .linkage = .Weak });
 }
