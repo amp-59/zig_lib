e811820a9e686189e555d02d936c370cfe8e39c6
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-09-19 23:54:48 +0000

    build: Regenerated `archive` library source.

--- top/build/archive.auto.zig
+++ top/build/archive.auto.zig
@@ -1,29 +1,9 @@
-const types = @import("./types.zig");
-pub usingnamespace @import("../start.zig");
-const source = types.GenericCommand(types.ArchiveCommand);
-formatWriteBuf: *const fn (
-    p_0: *types.ArchiveCommand,
-    p_1: []const u8,
-    p_2: []const types.Path,
-    p_3: [*]u8,
-) usize = @ptrFromInt(8),
-formatLength: *const fn (
-    p_0: *types.ArchiveCommand,
-    p_1: []const u8,
-    p_2: []const types.Path,
-) usize = @ptrFromInt(8),
-formatParseArgs: *const fn (
-    p_0: *types.ArchiveCommand,
-    p_1: *types.Allocator,
-    p_2: [][*:0]u8,
-) void = @ptrFromInt(8),
-fn load(ptrs: *@This()) callconv(.C) void {
-    ptrs.formatWriteBuf = @ptrCast(&source.formatWriteBuf);
-    ptrs.formatLength = @ptrCast(&source.formatLength);
-    ptrs.formatParseArgs = @ptrCast(&source.formatParseArgs);
-}
-comptime {
-    if (@import("builtin").output_mode != .Exe) {
-        @export(load, .{ .name = "load", .linkage = .Strong });
-    }
+const zl = @import("zl");
+pub usingnamespace zl.start;
+export fn load(fp: *zl.builtin.root.Builder.FunctionPointers) void {
+    fp.archive = .{
+        .formatWriteBuf = zl.build.GenericCommand(zl.build.ArchiveCommand).formatWriteBuf,
+        .formatLength = zl.build.GenericCommand(zl.build.ArchiveCommand).formatLength,
+        .formatParseArgs = zl.build.GenericCommand(zl.build.ArchiveCommand).formatParseArgs,
+    };
 }
