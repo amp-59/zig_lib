cd30d9adf424ae077678f7b63aa93c837fe9f785
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-09-23 22:25:13 +0000

    build: Rewrote stress test to be less time-consuming to compile.

--- test/build.zig
+++ test/build.zig
@@ -10,13 +10,29 @@ const meta = zl.meta;
 const debug = zl.debug;
 const build = zl.build;
 const builtin = zl.builtin;
+const testing = zl.testing;
 pub usingnamespace zl.start;
-
-pub const Builder = build.GenericBuilder(.{ .options = .{ .max_thread_count = 8 } });
-pub const message_style = "";
-pub const enable_debugging: bool = true;
+pub const Builder = build.GenericBuilder(.{
+    .errors = build.spec.errors.kill(),
+    .options = .{
+        .max_thread_count = 8,
+        .extensions_policy = .emergency,
+        .max_error_count = 1,
+    },
+});
+pub const message_style = "\x1b[93m";
+pub const enable_debugging: bool = false;
+pub const never_exit_group: bool = true;
 pub const exec_mode = .Run;
 pub const want_stack_traces: bool = enable_debugging;
+pub const logging_override: debug.Logging.Override = .{
+    .Attempt = enable_debugging,
+    .Success = enable_debugging,
+    .Acquire = enable_debugging,
+    .Release = enable_debugging,
+    .Error = enable_debugging,
+    .Fault = enable_debugging,
+};
 pub const logging_default: debug.Logging.Default = .{
     .Attempt = enable_debugging,
     .Success = enable_debugging,
@@ -38,94 +54,158 @@ pub const trace: debug.Trace = .{
     .Signal = enable_debugging,
     .options = .{},
 };
-fn buildRunner(args: [][*:0]u8, vars: [][*:0]u8, comptime Container: anytype) !void {
+const Node = Builder.Node;
+pub fn buildRunner(args: [][*:0]u8, vars: [][*:0]u8, comptime any: anytype) !void {
     var address_space: Builder.AddressSpace = .{};
     var thread_space: Builder.ThreadSpace = .{};
-    var allocator: build.Allocator = build.Allocator.init_arena(
-        Builder.AddressSpace.arena(Builder.specification.options.max_thread_count),
+    var allocator: build.Allocator = build.Allocator.fromArena(
+        Builder.AddressSpace.arena(Builder.max_thread_count),
     );
-    defer allocator.unmapAll();
     if (args.len < 5) {
-        proc.exitError(error.MissingEnvironmentPaths, 2);
+        zl.proc.exitError(error.MissingEnvironmentPaths, 2);
     }
-    const toplevel: *build.Node = try meta.wrap(build.Node.create(&allocator, "toplevel", args, vars));
-    try meta.wrap(
-        Container.buildMain(&allocator, toplevel),
-    );
-    Builder.updateCommands(&allocator, toplevel);
-    try meta.wrap(
-        Builder.processCommands(&address_space, &thread_space, &allocator, toplevel),
-    );
+    const top: *Node = Node.init(&allocator, "top", args, vars);
+    try meta.wrap(any(&allocator, top));
+    try zl.meta.wrap(Builder.processCommands(&address_space, &thread_space, &allocator, top));
+    allocator.unmapAll();
 }
 const text =
     \\const zl = @import("../../../zig_lib.zig");
     \\pub usingnamespace zl.start;
     \\
 ;
-fn ManyCAndZigBuildTasks(comptime options: anytype) type {
-    return struct {
-        const Node = build.GenericNode(options);
-        fn buildMain(allocator: *build.Allocator, toplevel: *Node) !void {
-            @setEvalBranchQuota(~@as(u32, 0));
-            const test_dir_fd: usize = try file.path(.{}, "test");
-            file.makeDirAt(.{}, test_dir_fd, "build", file.mode.directory) catch |err| {
-                if (err != error.FileExists) {
-                    return err;
-                }
-            };
-            const test_build_dir_fd: usize = try file.pathAt(.{}, test_dir_fd, "build");
-            file.makeDirAt(.{}, test_build_dir_fd, "stress", file.mode.directory) catch |err| {
-                if (err != error.FileExists) {
-                    return err;
-                }
-            };
-            const build_exe_cmd: build.BuildCommand = .{ .kind = .exe, .mode = .ReleaseSmall, .compiler_rt = false };
-            const build_obj_cmd: build.BuildCommand = .{ .kind = .obj, .mode = .ReleaseSmall, .compiler_rt = false };
-            const stress_dir_fd: usize = try file.pathAt(.{}, test_build_dir_fd, "stress");
-            const node: *Node = toplevel.addBuild(allocator, build_exe_cmd, "top", "test/build/stress/top.zig");
-            try gen.truncateFile(.{ .return_type = void }, "test/build/stress/top.zig", text ++ "pub fn main() void {}");
-            inline for (0..4) |x| {
-                const x_s = comptime fmt.cx(x);
-                const x_root = "f_" ++ x_s ++ ".zig";
-                const x_node: *Node = toplevel.addBuild(allocator, build_obj_cmd, x_s, "test/build/stress/" ++ x_root);
-                x_node.flags.is_hidden = true;
-                node.dependOn(allocator, x_node);
-                try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, x_root, text ++ "export fn func_" ++ x_s ++ "() void {}");
-                inline for (0..4) |y| {
-                    const y_s = x_s ++ comptime fmt.cx(y);
-                    const y_root = "f_" ++ y_s ++ ".zig";
-                    const y_root_c = "f_" ++ y_s ++ ".c";
-                    const y_node: *Node = toplevel.addBuild(allocator, build_obj_cmd, y_s, "test/build/stress/" ++ y_root);
-                    const y_node_c: *Node = toplevel.addBuild(allocator, build_obj_cmd, y_s ++ "_c", "test/build/stress/" ++ y_root_c);
-                    try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, y_root, text ++ "export fn func_" ++ y_s ++ "() void {}");
-                    try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, y_root_c, "void c_func_" ++ y_s ++ "() {}");
-                    y_node.flags.is_hidden = true;
-                    y_node_c.flags.is_hidden = true;
-                    x_node.dependOn(allocator, y_node);
-                    x_node.dependOn(allocator, y_node_c);
-                    inline for (0..4) |z| {
-                        const z_s = y_s ++ comptime fmt.cx(z);
-                        const z_root = "f_" ++ z_s ++ ".zig";
-                        const z_root_c = "f_" ++ z_s ++ ".c";
-                        const z_node: *Node = toplevel.addBuild(allocator, build_obj_cmd, z_s, "test/build/stress/" ++ z_root);
-                        const z_node_c: *Node = toplevel.addBuild(allocator, build_obj_cmd, z_s ++ "_c", "test/build/stress/" ++ z_root_c);
-                        try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, z_root, text ++ "export fn func_" ++ z_s ++ "() void {}");
-                        try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, z_root_c, "void c_func_" ++ z_s ++ "() {}");
-                        z_node.flags.is_hidden = true;
-                        z_node_c.flags.is_hidden = true;
-                        y_node.dependOn(allocator, z_node);
-                        y_node.dependOn(allocator, z_node_c);
-                    }
-                }
-            }
+fn testConfig(allocator: *build.Allocator, top: *Node) !void {
+    top.addConfigString(allocator, "zig_exe", top.zigExe());
+    var buf1: [4096]u8 = undefined;
+    var len: usize = top.getConfigs()[0].formatWriteBuf(&buf1);
+    try debug.expect(mem.testEqualManyIn(u8, top.zigExe(), buf1[0..len]));
+}
+fn buildMain(allocator: *build.Allocator, top: *Node) !void {
+    @setRuntimeSafety(false);
+    const test_dir_fd: usize = try file.path(.{}, .{}, "test");
+    file.makeDirAt(.{}, test_dir_fd, "build", file.mode.directory) catch |err| {
+        if (err != error.FileExists) {
+            return err;
         }
     };
+    const test_build_dir_fd: usize = try file.pathAt(.{}, .{}, test_dir_fd, "build");
+    file.makeDirAt(.{}, test_build_dir_fd, "stress", file.mode.directory) catch |err| {
+        if (err != error.FileExists) {
+            return err;
+        }
+    };
+    const build_exe_cmd: build.BuildCommand = .{ .kind = .exe, .mode = .ReleaseSmall, .compiler_rt = false };
+    const build_obj_cmd: build.BuildCommand = .{ .kind = .lib, .mode = .ReleaseSmall, .compiler_rt = false, .dynamic = true };
+    const stress_dir_fd: usize = try file.pathAt(.{}, .{}, test_build_dir_fd, "stress");
+    const node: *Node = top.addBuild(allocator, build_exe_cmd, "exe", "test/build/stress/exe.zig");
+    try gen.truncateFile(.{ .return_type = void }, "test/build/stress/exe.zig", text ++ "pub fn main() void {}");
+    const f = "test/build/stress/f_";
+    var buf: [8192]u8 = undefined;
+    for (0..10) |x| {
+        var ud64: fmt.Type.Ud64 = .{ .value = x };
+        var ptr: [*]u8 = &buf;
+        ptr = fmt.strcpyEqu(ptr, f);
+        ptr += ud64.formatWriteBuf(ptr);
+        ptr[0] = 0;
+        const x_name: [:0]const u8 = mem.terminate(buf[f.len -% 2 ..], 0);
+        ptr = fmt.strcpyEqu(ptr, ".zig");
+        ptr[0] = 0;
+        const x_root: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+        const x_node: *Node = top.addBuild(allocator, build_obj_cmd, x_name, x_root);
+        ptr = &buf;
+        ptr = fmt.strcpyEqu(ptr, text);
+        ptr = fmt.strcpyEqu(ptr, "export fn func_");
+        ptr += ud64.formatWriteBuf(ptr);
+        ptr = fmt.strcpyEqu(ptr, "() void {}");
+        ptr[0] = 0;
+        const x_file: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+        try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, x_node.getPaths()[1].concatenate(allocator), x_file);
+        node.dependOn(allocator, x_node);
+        for (0..10) |y| {
+            ud64.value = y + 10 *% x;
+            ptr = &buf;
+            ptr = fmt.strcpyEqu(ptr, f);
+            ptr += ud64.formatWriteBuf(ptr);
+            ptr[0] = 0;
+            const y_name: [:0]const u8 = mem.terminate(buf[f.len -% 2 ..], 0);
+            ptr = fmt.strcpyEqu(ptr, ".zig");
+            ptr[0] = 0;
+            const y_root: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+            const y_node: *Node = top.addBuild(allocator, build_obj_cmd, y_name, y_root);
+            ptr = &buf;
+            ptr = fmt.strcpyEqu(ptr, text);
+            ptr = fmt.strcpyEqu(ptr, "export fn func_");
+            ptr += ud64.formatWriteBuf(ptr);
+            ptr = fmt.strcpyEqu(ptr, "() void {}");
+            ptr[0] = 0;
+            const y_file: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+            try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, y_node.getPaths()[1].concatenate(allocator), y_file);
+            ptr = &buf;
+            ptr = fmt.strcpyEqu(ptr, f);
+            ptr += ud64.formatWriteBuf(ptr);
+            ptr = fmt.strcpyEqu(ptr, "_c");
+            ptr[0] = 0;
+            const y_name_c: [:0]const u8 = mem.terminate(buf[f.len -% 2 ..], 0);
+            ptr = fmt.strcpyEqu(ptr, ".c");
+            ptr[0] = 0;
+            const y_root_c: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+            const y_node_c: *Node = top.addBuild(allocator, build_obj_cmd, y_name_c, y_root_c);
+            ptr = &buf;
+            ptr = fmt.strcpyEqu(ptr, "int c_func_");
+            ptr += ud64.formatWriteBuf(ptr);
+            ptr = fmt.strcpyEqu(ptr, "() {}");
+            ptr[0] = 0;
+            const y_file_c: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+            try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, y_node_c.getPaths()[1].concatenate(allocator), y_file_c);
+            x_node.dependOn(allocator, y_node);
+            x_node.dependOn(allocator, y_node_c);
+            for (0..10) |z| {
+                ud64.value = z +% (y *% 10) + (100 *% x);
+                ptr = &buf;
+                ptr = fmt.strcpyEqu(ptr, f);
+                ptr += ud64.formatWriteBuf(ptr);
+                ptr[0] = 0;
+                const z_name: [:0]const u8 = mem.terminate(buf[f.len -% 2 ..], 0);
+                ptr = fmt.strcpyEqu(ptr, ".zig");
+                ptr[0] = 0;
+                const z_root: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+                const z_node: *Node = top.addBuild(allocator, build_obj_cmd, z_name, z_root);
+                ptr = &buf;
+                ptr = fmt.strcpyEqu(ptr, text);
+                ptr = fmt.strcpyEqu(ptr, "export fn func_");
+                ptr += ud64.formatWriteBuf(ptr);
+                ptr = fmt.strcpyEqu(ptr, "() void {}");
+                ptr[0] = 0;
+                const z_file: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+                try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, z_node.getPaths()[1].concatenate(allocator), z_file);
+                ptr = &buf;
+                ptr = fmt.strcpyEqu(ptr, f);
+                ptr += ud64.formatWriteBuf(ptr);
+                ptr = fmt.strcpyEqu(ptr, "_c");
+                ptr[0] = 0;
+                const z_name_c: [:0]const u8 = mem.terminate(buf[f.len -% 2 ..], 0);
+                ptr = fmt.strcpyEqu(ptr, ".c");
+                ptr[0] = 0;
+                const z_root_c: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+                const z_node_c: *Node = top.addBuild(allocator, build_obj_cmd, z_name_c, z_root_c);
+                ptr = &buf;
+                ptr = fmt.strcpyEqu(ptr, "int c_func_");
+                ptr += ud64.formatWriteBuf(ptr);
+                ptr = fmt.strcpyEqu(ptr, "() {}");
+                ptr[0] = 0;
+                const z_file_c: [:0]const u8 = buf[0..fmt.strlen(ptr, &buf) :0];
+                try gen.truncateFileAt(.{ .return_type = void }, stress_dir_fd, z_node_c.getPaths()[1].concatenate(allocator), z_file_c);
+                y_node.dependOn(allocator, z_node);
+                y_node.dependOn(allocator, z_node_c);
+            }
+        }
+    }
 }
 const SingleThreaded = struct {
-    fn buildMain(allocator: *build.Allocator, toplevel: *build.Node) !void {
-        _ = toplevel.addBuild(allocator, .{ .kind = .exe }, "main", "./build");
+    fn buildMain(allocator: *build.Allocator, top: *build.Node) !void {
+        _ = top.addBuild(allocator, .{ .kind = .exe }, "main", "./build");
     }
 };
 pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
-    try buildRunner(args, vars, SingleThreaded);
+    try buildRunner(args, vars, buildMain);
 }
