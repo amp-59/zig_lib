629bb8c5058758bf321e191d725ead4514b288f7
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-09-23 23:47:58 +0000

    addrspace: * Updated imports. * Updated usage of `mem` preset constants.

--- examples/addrspace.zig
+++ examples/addrspace.zig
@@ -4,19 +4,16 @@ const fmt = zl.fmt;
 const proc = zl.proc;
 const meta = zl.meta;
 const file = zl.file;
-const virtual = zl.virtual;
 const testing = zl.testing;
-
 pub usingnamespace zl.start;
-
 pub fn main() !void {
-    const regular_address_space_with_two_subspaces = virtual.RegularAddressSpaceSpec{
+    const regular_address_space_with_two_subspaces = mem.RegularAddressSpaceSpec{
         .label = "regular-4x5B",
         .lb_addr = 0,
         .up_addr = 20,
         .divisions = 4,
         .alignment = 1,
-        .subspace = virtual.genericSlice(.{
+        .subspace = mem.genericSlice(.{
             .{
                 .label = "discrete-1x10B+1x3B",
                 .list = &[2]mem.Arena{
@@ -34,20 +31,16 @@ pub fn main() !void {
             },
         }),
     };
-
     {
         const SuperSpace = regular_address_space_with_two_subspaces.instantiate();
         const SubSpace0 = SuperSpace.SubSpace("discrete-1x10B+1x3B");
         const SubSpace1 = SuperSpace.SubSpace("regular-5x1B");
-
         meta.refAllDecls(SuperSpace, null);
         meta.refAllDecls(SubSpace0, null);
         meta.refAllDecls(SubSpace1, null);
-
         var super_space: SuperSpace = .{};
         var sub_space_0: SubSpace0 = .{};
         var sub_space_1: SubSpace1 = .{};
-
         const rspec = .{ .omit_default_fields = false, .radix = 2 };
         testing.printN(16384, .{
             fmt.render(rspec, super_space), '\n',
@@ -56,13 +49,13 @@ pub fn main() !void {
         });
     }
     zl.debug.write(&[1]u8{0xa});
-    const builder_like_address_space = virtual.RegularAddressSpaceSpec{
+    const builder_like_address_space = mem.RegularAddressSpaceSpec{
         .label = "builder_space",
         .lb_addr = 0x40000000,
         .up_addr = 0x80000000,
         .divisions = 1,
         .alignment = 4096,
-        .subspace = virtual.genericSlice(.{
+        .subspace = mem.genericSlice(.{
             .{
                 .label = "Loader",
                 .list = &[1]mem.Arena{.{
@@ -93,19 +86,15 @@ pub fn main() !void {
         const SubSpace0 = SuperSpace.SubSpace("Loader");
         const SubSpace1 = SuperSpace.SubSpace("Heap");
         const SubSpace2 = SuperSpace.SubSpace("Stack");
-
         meta.refAllDecls(SuperSpace, null);
         meta.refAllDecls(SubSpace0, null);
         meta.refAllDecls(SubSpace1, null);
         meta.refAllDecls(SubSpace2, null);
-
         var super_space: SuperSpace = .{};
         var sub_space_0: SubSpace0 = .{};
         var sub_space_1: SubSpace1 = .{};
         var sub_space_2: SubSpace2 = .{};
-
         try zl.debug.expect(!super_space.set(0));
-
         const rspec = .{ .omit_default_fields = false, .radix = 2 };
         testing.printN(16384, .{
             fmt.render(rspec, super_space), '\n',
