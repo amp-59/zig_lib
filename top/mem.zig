1d49cd752100c9beb3e50da0eb3b95262466b5e4
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-09-25 00:08:38 +0000

    mem: Relocated `addrcpy` declaration.

--- top/mem.zig
+++ top/mem.zig
@@ -1746,6 +1746,7 @@ pub inline fn zero(comptime T: type, ptr: *T) void {
 pub inline fn unstable(comptime T: type, val: T) T {
     return @as(*const volatile T, @ptrCast(&val)).*;
 }
+pub const addrcpy = @as(*const fn (dest: usize, src: usize, len: usize) void, @ptrCast(&builtin.memcpy));
 pub fn terminate(ptr: [*]const u8, comptime value: u8) [:value]u8 {
     @setRuntimeSafety(false);
     var idx: usize = 0;
