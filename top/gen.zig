bc935024cced037511c7c778b699dfdd6d015da2
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-09-25 23:42:29 +0000

    gen: Updated imports--removed `mach`.

--- top/gen.zig
+++ top/gen.zig
@@ -2,7 +2,6 @@ const sys = @import("./sys.zig");
 const mem = @import("./mem.zig");
 const fmt = @import("./fmt.zig");
 const file = @import("./file.zig");
-const mach = @import("./mach.zig");
 const meta = @import("./meta.zig");
 const debug = @import("./debug.zig");
 const builtin = @import("./builtin.zig");
