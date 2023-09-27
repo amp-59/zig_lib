aff455c69e1b81cb67ea28b53e06bbbf5f957c63
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-09-27 20:27:45 +0000

    build_runner: Added `AbsoluteState` declaration.

--- build_runner.zig
+++ build_runner.zig
@@ -3,22 +3,26 @@ const zl = blk: {
     if (@hasDecl(root, "zl")) {
         break :blk root.zl;
     }
-    if (@hasDecl(root, "zig_lib")) {
-        break :blk root.zig_lib;
-    }
+    @compileError("Add the following declaration to build.zig:\n\n\rpub const zl = @import(\"./zig_lib/zig_lib.zig\");\n");
 };
-const build = zl.build;
 pub usingnamespace zl.start;
-pub const Node = if (@hasDecl(root, "Node")) root.Node else build.GenericNode(.{});
-pub const exec_mode = .Run;
+pub const is_safe: bool = enable_debugging;
+pub const runtime_assertions: bool = enable_debugging;
 pub const want_stack_traces: bool = enable_debugging;
 pub const have_stack_traces: bool = false;
+pub const AbsoluteState = struct {
+    home: [:0]u8,
+    cwd: [:0]u8,
+    proj: [:0]u8,
+    pid: u16,
+};
+pub const Builder =
+    if (@hasDecl(root, "Builder")) root.Builder else zl.build.GenericBuilder(.{});
 pub const message_style: [:0]const u8 =
     if (@hasDecl(root, "message_style")) root.message_style else "\x1b[2m";
 pub const enable_debugging: bool =
     if (@hasDecl(root, "enable_debugging")) root.enable_debugging else false;
-pub const logging_override: zl.debug.Logging.Override =
-    if (@hasDecl(root, "logging_override")) root.logging_override else .{
+pub const logging_override: zl.debug.Logging.Override = .{
     .Attempt = enable_debugging,
     .Success = enable_debugging,
     .Acquire = enable_debugging,
@@ -46,21 +50,17 @@ pub const trace: zl.debug.Trace = .{
     .Fault = enable_debugging,
     .Signal = enable_debugging,
 };
-pub fn main(args: [][*:0]u8, vars: [][*:0]u8) void {
-    var address_space: Node.AddressSpace = .{};
-    var thread_space: Node.ThreadSpace = .{};
-    var allocator: build.Allocator = build.Allocator.init_arena(
-        Node.AddressSpace.arena(Node.specification.options.max_thread_count),
+pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
+    var address_space: Builder.AddressSpace = .{};
+    var thread_space: Builder.ThreadSpace = .{};
+    var allocator: zl.build.Allocator = zl.build.Allocator.fromArena(
+        Builder.AddressSpace.arena(Builder.max_thread_count),
     );
     if (args.len < 5) {
         zl.proc.exitError(error.MissingEnvironmentPaths, 2);
     }
-    const toplevel: *Node = Node.init(&allocator, args, vars);
-    try zl.meta.wrap(
-        root.buildMain(&allocator, toplevel),
-    );
-    Node.updateCommands(&allocator, toplevel);
-    try zl.meta.wrap(
-        Node.processCommands(&address_space, &thread_space, &allocator, toplevel),
-    );
+    const top: *Builder.Node = Builder.Node.init(&allocator, "toplevel", args, vars);
+    root.buildMain(&allocator, top);
+    try zl.meta.wrap(Builder.processCommands(&address_space, &thread_space, &allocator, top));
+    allocator.unmapAll();
 }
