e458b0d2c2fd5d76ab7148cc0a74610100af65c7
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-10-03 05:03:39 +0000

    serial: * Converted `serial` functions to configurable namespace. * Added `spec` sub-namespace.

--- top/serial.zig
+++ top/serial.zig
@@ -43,7 +43,7 @@ fn readStruct(comptime struct_info: builtin.Type.Struct, addr: u64, offset: u64,
     @setRuntimeSafety(false);
     var len: u64 = offset;
     inline for (struct_info.fields) |field| {
-        len = read(addr, len, &@field(any, field.name));
+        len = readAny(addr, len, &@field(any, field.name));
     }
     return len;
 }
@@ -52,31 +52,31 @@ fn readUnion(comptime union_info: builtin.Type.Union, addr: u64, offset: u64, an
     if (union_info.tag_type) |tag_type| {
         inline for (union_info.fields) |field| {
             if (any.* == @field(tag_type, field.name)) {
-                return read(addr, offset, &@field(any, field.name));
+                return readAny(addr, offset, &@field(any, field.name));
             }
         }
     }
     return offset;
 }
-fn writeStruct(comptime struct_info: builtin.Type.Struct, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
+fn writeStruct(comptime struct_info: builtin.Type.Struct, allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*).allocate_payload(@TypeOf(any)) {
     @setRuntimeSafety(false);
     const T: type = @TypeOf(any);
     var ret: T = any;
     inline for (struct_info.fields) |field| {
         @field(ret, field.name) = try meta.wrap(
-            write(allocator, addr, @field(any, field.name)),
+            writeAny(allocator, addr, @field(any, field.name)),
         );
     }
     return ret;
 }
-fn writeUnion(comptime union_info: builtin.Type.Union, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
+fn writeUnion(comptime union_info: builtin.Type.Union, allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*).allocate_payload(@TypeOf(any)) {
     @setRuntimeSafety(false);
     const T: type = @TypeOf(any);
     if (union_info.tag_type) |tag_type| {
         inline for (union_info.fields) |field| {
             if (any == @field(tag_type, field.name)) {
                 return @unionInit(T, field.name, try meta.wrap(
-                    write(allocator, addr, @field(any, field.name)),
+                    writeAny(allocator, addr, @field(any, field.name)),
                 ));
             }
         }
@@ -89,7 +89,7 @@ fn readPointerOne(comptime pointer_info: builtin.Type.Pointer, addr: u64, offset
     var len: u64 = offset;
     len = math.sub64(bits.alignA64(addr +% len, @alignOf(pointer_info.child)), addr);
     len +%= @sizeOf(pointer_info.child);
-    len = read(addr, len, next);
+    len = readAny(addr, len, next);
     any.* = next;
     return len;
 }
@@ -100,7 +100,7 @@ fn readPointerSlice(comptime pointer_info: builtin.Type.Pointer, addr: u64, offs
     len = math.sub64(bits.alignA64(addr +% len, @alignOf(pointer_info.child)), addr);
     len +%= @sizeOf(pointer_info.child) *% (next.len +% @intFromBool(pointer_info.sentinel != null));
     for (next) |*value| {
-        len = read(addr, len, value);
+        len = readAny(addr, len, value);
     }
     any.* = next;
     return len;
@@ -114,22 +114,27 @@ fn readPointerMany(comptime pointer_info: builtin.Type.Pointer, addr: u64, offse
     len = math.sub64(bits.alignA64(addr +% len, @alignOf(pointer_info.child)), addr);
     len +%= @sizeOf(pointer_info.child) *% (idx +% 1);
     for (next[0..idx]) |*value| {
-        len = read(addr, len, value);
+        len = readAny(addr, len, value);
     }
     any.* = next;
     return len;
 }
-fn writePointerOne(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
+fn writePointerOne(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*)
+    .allocate_payload(@TypeOf(any)) {
     @setRuntimeSafety(false);
+    if (@typeInfo(pointer_info.child) == .Fn) {
+        return any;
+    }
     const ret: @TypeOf(any) = try meta.wrap(
         allocator.create(pointer_info.child),
     );
     ret.* = try meta.wrap(
-        write(allocator, addr, any.*),
+        writeAny(allocator, addr, any.*),
     );
     return toOffset(ret, addr);
 }
-fn writePointerSlice(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
+fn writePointerSlice(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*)
+    .allocate_payload(@TypeOf(any)) {
     @setRuntimeSafety(false);
     if (pointer_info.sentinel) |sentinel_ptr| {
         const sentinel: pointer_info.child = comptime mem.pointerOpaque(pointer_info.child, sentinel_ptr).*;
@@ -139,7 +144,7 @@ fn writePointerSlice(comptime pointer_info: builtin.Type.Pointer, allocator: any
         ret.ptr[ret.len] = sentinel;
         for (ret, 0..) |*ptr, i| {
             ptr.* = try meta.wrap(
-                write(allocator, addr, any[i]),
+                writeAny(allocator, addr, any[i]),
             );
         }
         return toOffset(ret, addr)[0.. :sentinel];
@@ -149,16 +154,19 @@ fn writePointerSlice(comptime pointer_info: builtin.Type.Pointer, allocator: any
         );
         for (ret, 0..) |*ptr, i| {
             ptr.* = try meta.wrap(
-                write(allocator, addr, any[i]),
+                writeAny(allocator, addr, any[i]),
             );
         }
         return toOffset(ret, addr);
     }
 }
-fn writePointerMany(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(any) {
-    return writePointerSlice(pointer_info, allocator, addr, meta.manyToSlice(any)).ptr;
+fn writePointerMany(comptime pointer_info: builtin.Type.Pointer, allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*)
+    .allocate_payload(@TypeOf(any)) {
+    const ret = try meta.wrap(writePointerSlice(pointer_info, allocator, addr, meta.manyToSlice(any)));
+    return ret.ptr;
 }
-pub fn write(allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*).allocate_payload(@TypeOf(any)) {
+pub fn writeAny(allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*)
+    .allocate_payload(@TypeOf(any)) {
     switch (@typeInfo(@TypeOf(any))) {
         .Struct => |struct_info| {
             return writeStruct(struct_info, allocator, addr, any);
@@ -180,7 +188,7 @@ pub fn write(allocator: anytype, addr: u64, any: anytype) @TypeOf(allocator.*).a
         else => return any,
     }
 }
-pub fn read(addr: u64, offset: u64, any: anytype) u64 {
+pub fn readAny(addr: u64, offset: u64, any: anytype) u64 {
     switch (@typeInfo(@TypeOf(any.*))) {
         .Struct => |struct_info| {
             return readStruct(struct_info, addr, offset, any);
@@ -228,7 +236,7 @@ fn genericSerializeValuesLoop(comptime T: type, allocator: anytype, s_ab_addr: u
     var t_ab_addr: u64 = s_ab_addr;
     if (@TypeOf(any) == T) {
         mem.pointerOne(T, t_ab_addr).* = try meta.wrap(
-            write(allocator, s_up_addr, any),
+            writeAny(allocator, s_up_addr, any),
         );
         return t_ab_addr +% @sizeOf(T);
     } else for (any) |value| {
@@ -241,7 +249,7 @@ fn genericSerializeValuesLoop(comptime T: type, allocator: anytype, s_ab_addr: u
 fn genericDeserializeValuesLoop(comptime T: type, s_up_addr: u64, s_aligned_bytes: u64, any: anytype) u64 {
     var t_aligned_bytes: u64 = s_aligned_bytes;
     if (@TypeOf(@constCast(any)) == *T) {
-        return read(s_up_addr, t_aligned_bytes, any);
+        return readAny(s_up_addr, t_aligned_bytes, any);
     } else for (any.*) |*value| {
         t_aligned_bytes = genericDeserializeValuesLoop(T, s_up_addr, t_aligned_bytes, value);
     }
@@ -283,7 +291,7 @@ pub const SerialSpec = struct {
     Allocator: type,
     errors: Errors = .{},
     logging: Logging = .{},
-    pub const Logging = struct {
+    pub const Logging = packed struct {
         create: debug.Logging.AcquireError = .{},
         open: debug.Logging.AcquireError = .{},
         read: debug.Logging.SuccessError = .{},
@@ -299,83 +307,122 @@ pub const SerialSpec = struct {
         write: sys.ErrorPolicy = .{ .throw = file.spec.write.errors.all },
         close: sys.ErrorPolicy = .{ .abort = file.spec.close.errors.all },
     };
-    fn stat(comptime serial_spec: SerialSpec) file.StatusSpec {
-        return .{ .logging = serial_spec.logging.stat, .errors = serial_spec.errors.stat };
-    }
-    fn read(comptime serial_spec: SerialSpec) file.ReadSpec {
-        return .{ .return_type = void, .logging = serial_spec.logging.read, .errors = serial_spec.errors.read };
-    }
-    fn write(comptime serial_spec: SerialSpec) file.WriteSpec {
-        return .{ .logging = serial_spec.logging.read, .errors = serial_spec.errors.read };
-    }
-    fn close(comptime serial_spec: SerialSpec) file.CloseSpec {
-        return .{ .logging = serial_spec.logging.close, .errors = serial_spec.errors.close };
-    }
-    fn create(comptime serial_spec: SerialSpec) file.CreateSpec {
-        return .{
-            .logging = serial_spec.logging.create,
-            .errors = serial_spec.errors.create,
-        };
-    }
-    fn open(comptime serial_spec: SerialSpec) file.OpenSpec {
-        return .{
-            .logging = serial_spec.logging.open,
-            .errors = serial_spec.errors.open,
-        };
-    }
 };
-pub fn serialWrite(comptime serial_spec: SerialSpec, comptime S: type, allocator: *serial_spec.Allocator, pathname: [:0]const u8, value: S) sys.ErrorUnion(.{
-    .throw = serial_spec.Allocator.map_error_policy.throw ++ serial_spec.errors.create.throw ++
-        serial_spec.errors.open.throw ++ serial_spec.errors.write.throw ++ serial_spec.errors.close.throw,
-    .abort = serial_spec.Allocator.map_error_policy.abort ++ serial_spec.errors.create.abort ++
-        serial_spec.errors.open.abort ++ serial_spec.errors.write.abort ++ serial_spec.errors.close.abort,
-}, void) {
-    const create_spec: file.CreateSpec = comptime serial_spec.create();
-    const write_spec: file.WriteSpec = comptime serial_spec.write();
-    const close_spec: file.CloseSpec = comptime serial_spec.close();
-    const save = allocator.save();
-    defer allocator.restore(save);
-    const s_ab_addr: u64 = allocator.alignAbove(16);
-    const bytes: []const u8 = try meta.wrap(
-        genericSerializeInternal(allocator, s_ab_addr, value),
-    );
-    const fd: usize = try meta.wrap(
-        file.create(create_spec, .{ .exclusive = false }, pathname, file.mode.regular),
-    );
-    try meta.wrap(
-        file.write(write_spec, fd, bytes),
-    );
-    try meta.wrap(
-        file.close(close_spec, fd),
-    );
+
+pub fn GenericSerializer(comptime serial_spec: SerialSpec) type {
+    const T = struct {
+        pub fn serialWrite(comptime S: type, allocator: *serial_spec.Allocator, pathname: [:0]const u8, value: S) sys.ErrorUnion(.{
+            .throw = serial_spec.Allocator.map_error_policy.throw ++ serial_spec.errors.create.throw ++
+                serial_spec.errors.open.throw ++ serial_spec.errors.write.throw ++ serial_spec.errors.close.throw,
+            .abort = serial_spec.Allocator.map_error_policy.abort ++ serial_spec.errors.create.abort ++
+                serial_spec.errors.open.abort ++ serial_spec.errors.write.abort ++ serial_spec.errors.close.abort,
+        }, void) {
+            const save = allocator.save();
+            defer allocator.restore(save);
+            const s_ab_addr: u64 = allocator.alignAbove(16);
+            const bytes: []const u8 = try meta.wrap(
+                genericSerializeInternal(allocator, s_ab_addr, value),
+            );
+            const fd: usize = try meta.wrap(
+                file.create(create(), .{ .exclusive = false, .truncate = true }, pathname, file.mode.regular),
+            );
+            try meta.wrap(
+                file.write(write(), fd, bytes),
+            );
+            try meta.wrap(
+                file.close(close(), fd),
+            );
+        }
+        pub fn serialRead(comptime S: type, allocator: *serial_spec.Allocator, pathname: [:0]const u8) sys.ErrorUnion(.{
+            .throw = serial_spec.Allocator.map_error_policy.throw ++
+                serial_spec.errors.open.throw ++ serial_spec.errors.read.throw ++ serial_spec.errors.close.throw,
+            .abort = serial_spec.Allocator.map_error_policy.abort ++
+                serial_spec.errors.open.abort ++ serial_spec.errors.read.abort ++ serial_spec.errors.close.abort,
+        }, meta.Mutable(S)) {
+            const t_ab_addr: u64 = allocator.alignAbove(16);
+            const fd: usize = try meta.wrap(
+                file.open(open(), .{}, pathname),
+            );
+            const st: file.Status = try meta.wrap(
+                file.getStatus(stat(), fd),
+            );
+            const buf: []u8 = try meta.wrap(
+                allocator.allocate(u8, st.size),
+            );
+            try meta.wrap(
+                file.read(read(), fd, buf[0..st.size]),
+            );
+            try meta.wrap(
+                file.close(close(), fd),
+            );
+            return try meta.wrap(
+                genericDeserializeInternal(meta.Mutable(S), t_ab_addr),
+            );
+        }
+
+        fn stat() file.StatusSpec {
+            return .{
+                .logging = serial_spec.logging.stat,
+                .errors = serial_spec.errors.stat,
+            };
+        }
+        fn read() file.ReadSpec {
+            return .{
+                .return_type = void,
+                .logging = serial_spec.logging.read,
+                .errors = serial_spec.errors.read,
+            };
+        }
+        fn write() file.WriteSpec {
+            return .{
+                .logging = serial_spec.logging.read,
+                .errors = serial_spec.errors.read,
+            };
+        }
+        fn close() file.CloseSpec {
+            return .{
+                .logging = serial_spec.logging.close,
+                .errors = serial_spec.errors.close,
+            };
+        }
+        fn create() file.CreateSpec {
+            return .{
+                .logging = serial_spec.logging.create,
+                .errors = serial_spec.errors.create,
+            };
+        }
+        fn open() file.OpenSpec {
+            return .{
+                .logging = serial_spec.logging.open,
+                .errors = serial_spec.errors.open,
+            };
+        }
+    };
+    return T;
 }
-pub fn serialRead(comptime serial_spec: SerialSpec, comptime S: type, allocator: *serial_spec.Allocator, pathname: [:0]const u8) sys.ErrorUnion(.{
-    .throw = serial_spec.Allocator.map_error_policy.throw ++
-        serial_spec.errors.open.throw ++ serial_spec.errors.read.throw ++ serial_spec.errors.close.throw,
-    .abort = serial_spec.Allocator.map_error_policy.abort ++
-        serial_spec.errors.open.abort ++ serial_spec.errors.read.abort ++ serial_spec.errors.close.abort,
-}, meta.Mutable(S)) {
-    const open_spec: file.OpenSpec = comptime serial_spec.open();
-    const stat_spec: file.StatusSpec = comptime serial_spec.stat();
-    const read_spec: file.ReadSpec = comptime serial_spec.read();
-    const close_spec: file.CloseSpec = comptime serial_spec.close();
-    const t_ab_addr: u64 = allocator.alignAbove(16);
-    const fd: usize = try meta.wrap(
-        file.open(open_spec, .{}, pathname),
-    );
-    const st: file.Status = try meta.wrap(
-        file.getStatus(stat_spec, fd),
-    );
-    const buf: []u8 = try meta.wrap(
-        allocator.allocate(u8, st.size),
-    );
-    try meta.wrap(
-        file.read(read_spec, fd, buf[0..st.size]),
-    );
-    try meta.wrap(
-        file.close(close_spec, fd),
-    );
-    return try meta.wrap(
-        genericDeserializeInternal(meta.Mutable(S), t_ab_addr),
-    );
-}
+pub const spec = struct {
+    pub const serializer = struct {
+        pub const errors = struct {
+            pub const noexcept: SerialSpec.Errors = .{
+                .create = .{},
+                .open = .{},
+                .close = .{},
+                .stat = .{},
+                .read = .{},
+                .write = .{},
+            };
+            pub const all: SerialSpec.Errors = .{
+                .create = .{ .throw = sys.open.errors.all },
+                .open = .{ .throw = sys.open.errors.all },
+                .close = .{ .throw = sys.close.errors.all },
+                .stat = .{ .throw = sys.stat.errors.all },
+                .read = .{ .throw = sys.read.errors.all },
+                .write = .{ .throw = sys.write.errors.all },
+            };
+        };
+        pub const logging = struct {
+            pub const verbose: SerialSpec.Logging = builtin.all(SerialSpec.Logging);
+            pub const silent: SerialSpec.Logging = builtin.zero(SerialSpec.Logging);
+        };
+    };
+};
