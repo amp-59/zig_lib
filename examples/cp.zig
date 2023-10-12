b94ef7be8a81f15ad71b404fdc1358abf99d541c
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-10-12 23:06:18 +0000

    cp: * Updated imports. * Updated usage of preset constants.

--- examples/cp.zig
+++ examples/cp.zig
@@ -2,7 +2,6 @@ const zl = @import("../zig_lib.zig");
 const sys = zl.sys;
 const mem = zl.mem;
 const proc = zl.proc;
-const spec = zl.spec;
 const file = zl.file;
 const mach = zl.mach;
 const meta = zl.meta;
@@ -11,7 +10,7 @@ const builtin = zl.builtin;
 
 pub usingnamespace zl.start;
 
-pub const logging_override: debug.Logging.Override = spec.logging.override.verbose;
+pub const logging_override: debug.Logging.Override = debug.spec.logging.override.verbose;
 
 pub fn copy(src_pathname: [:0]const u8, dest_pathname: [:0]const u8) !void {
     const from_fd: usize = try file.open(.{}, .{ .no_follow = true, .read_write = true }, src_pathname);
