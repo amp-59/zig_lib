7e22d2919000bdc501d3a69d1e4bd6554588026c
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-10-14 18:39:05 +0000

    elfcmp: Added `PATH` executables resolution.

--- examples/elfcmp.zig
+++ examples/elfcmp.zig
@@ -14,9 +14,7 @@ pub const LoaderSpace = mem.GenericDiscreteAddressSpace(.{
     },
 });
 pub const DynamicLoader = zl.elf.GenericDynamicLoader(.{
-    .options = .{},
     .logging = .{
-        .hide_unchanged_sections = false,
         .show_elf_header = true,
         .show_relocations = true,
     },
@@ -26,24 +24,60 @@ pub const DynamicLoader = zl.elf.GenericDynamicLoader(.{
 const about_s: fmt.AboutSrc = fmt.about("xelf");
 const width: usize = fmt.aboutCentre(about_s);
 
-pub fn main(args: [][*:0]u8) !void {
+fn findPath(allocator: *mem.SimpleAllocator, vars: [][*:0]u8, name: [:0]u8) ![:0]u8 {
+    if (name[0] == '/') {
+        return name;
+    }
+    if (zl.file.accessAt(.{}, .{ .symlink_no_follow = false }, zl.file.cwd, name, .{ .exec = true })) |_| {
+        return name;
+    } else |err| {
+        if (err != error.NoSuchFileOrDirectory and
+            err != error.Access)
+        {
+            return err;
+        }
+    }
+    var itr: zl.proc.PathIterator = .{
+        .paths = zl.proc.environmentValue(vars, "PATH").?,
+    };
+    defer itr.done();
+    while (itr.next()) |dirname| {
+        const dir_fd: usize = try zl.file.path(.{}, .{}, dirname);
+        defer zl.file.close(.{ .errors = .{} }, dir_fd);
+        const ret: bool = zl.file.accessAt(.{}, .{ .symlink_no_follow = false }, dir_fd, name, .{ .exec = true }) catch {
+            continue;
+        };
+        if (ret) {
+            const buf: [*]u8 = @ptrFromInt(allocator.allocateRaw(dirname.len, 1));
+            var ptr: [*]u8 = fmt.strcpyEqu(buf, dirname);
+            ptr[0] = '/';
+            ptr += 1;
+            ptr = fmt.strcpyEqu(ptr, name);
+            ptr[1] = 0;
+            return mem.terminate(buf, 0);
+        }
+    }
+    return name;
+}
+pub fn main(args: [][*:0]u8, vars: [][*:0]u8) !void {
     var allocator: mem.SimpleAllocator = .{};
     var info1: *DynamicLoader.Info = @ptrFromInt(8);
     var info2: *DynamicLoader.Info = @ptrFromInt(8);
-    var buf: [*]u8 = @ptrFromInt(allocator.allocateRaw(1024 *% 1024, 1));
-    var end: [*]u8 = @ptrFromInt(1);
+    var buf: [*]u8 = @ptrFromInt(allocator.allocateRaw(1024 *% 1024 *% 1024, 1));
     var ld: DynamicLoader = .{};
     if (args.len == 1) {
         return;
     }
     if (args.len >= 2) {
-        info1 = try ld.load(mem.terminate(args[1], 0));
+        const name: [:0]u8 = mem.terminate(args[1], 0);
+        info1 = try ld.load(try findPath(&allocator, vars, name));
     }
     if (args.len == 2) {
-        end = DynamicLoader.about.writeBinary(buf, info1, width);
-    } else {
-        info2 = try ld.load(mem.terminate(args[2], 0));
-        end = DynamicLoader.about.writeBinaryDifference(buf, info1, info2, width);
+        debug.write(buf[0..fmt.strlen(DynamicLoader.about.writeBinary(buf, info1, width), buf)]);
+    } else for (args[2..]) |arg| {
+        const name: [:0]u8 = mem.terminate(arg, 0);
+        info2 = try ld.load(try findPath(&allocator, vars, name));
+        debug.write(buf[0..fmt.strlen(DynamicLoader.about.writeBinaryDifference(buf, info1, info2, width), buf)]);
+        info1 = info2;
     }
-    debug.write(buf[0..fmt.strlen(end, buf)]);
 }
