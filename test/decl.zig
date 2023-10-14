16b5386c31f0bf5d56bc35e6051be324c2df5f3e
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-10-14 10:23:46 +0000

    decls: Removed whole library reference. This will be moved to each        namespace.

--- test/decl.zig
+++ test/decl.zig
@@ -1,28 +1,5 @@
 const zl = @import("../zig_lib.zig");
 pub usingnamespace zl.start;
-const @"test" = struct {
-    const algo = @import("algo.zig");
-    const builtin = @import("builtin.zig");
-    const container = @import("container.zig");
-    const crypto = @import("crypto.zig");
-    const file = @import("file.zig");
-    const fmt = @import("fmt.zig");
-    const impl = @import("impl.zig");
-    const junk = @import("junk.zig");
-    const list = @import("list.zig");
-    const math = @import("math.zig");
-    const mem2 = @import("mem2.zig");
-    const mem = @import("mem.zig");
-    const meta = @import("meta.zig");
-    const parse = @import("parse.zig");
-    const proc = @import("proc.zig");
-    const render = @import("render.zig");
-    const rng = @import("rng.zig");
-    const serial = @import("serial.zig");
-    const thread = @import("thread.zig");
-    const time = @import("time.zig");
-    const virtual = @import("virtual.zig");
-};
 pub const logging_override: zl.debug.Logging.Override = zl.debug.spec.logging.override.verbose;
 pub const AddressSpace = zl.mem.spec.address_space.exact_8;
 const Allocator0 = zl.mem.GenericArenaAllocator(.{
@@ -47,6 +24,8 @@ const other = struct {
     pub const UnionFormat = zl.fmt.UnionFormat(.{}, union { one: u64, two: struct { u32, u32 } });
     pub const StructForm = zl.fmt.UnionFormat(.{}, struct { one: u64, two: *u64, three: []u8 });
     pub const EnumFormat = zl.fmt.UnionFormat(.{}, enum { one, two, three });
+    pub const AnyFormatAbi = zl.fmt.AnyFormat(.{}, @TypeOf(@import("builtin").abi));
+    pub const AnyFormatTarget = zl.fmt.AnyFormat(.{}, @TypeOf(@import("builtin").target));
     pub const object_format = std.Target.ObjectFormat.elf;
     pub const mode = std.builtin.Mode.Debug;
     pub const link_libc = false;
@@ -60,11 +39,9 @@ const other = struct {
     pub const code_model = std.builtin.CodeModel.default;
 };
 pub fn main() !void {
-    _ = zl.meta.refAllDecls(zl, &.{ "FnArg", "tls" });
-    _ = zl.meta.refAllDecls(@"test", &.{"FnArg"});
+    _ = zl.meta.refAllDecls(other, null);
     _ = zl.meta.refAllDecls(List, null);
     _ = zl.meta.refAllDecls(ListView, null);
-    _ = zl.meta.refAllDecls(other, null);
     _ = zl.meta.refAllDecls(Builder, null);
     _ = zl.meta.refAllDecls(Allocator0, null);
     _ = zl.meta.refAllDecls(Allocator1, null);
