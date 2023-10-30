3b2d977af79d988d93079a483517dc26b67a11b3
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-10-30 17:21:50 +0000

    build.build: Removed field editor function.

--- top/build/build.auto.zig
+++ top/build/build.auto.zig
@@ -2,11 +2,8 @@ const zl = @import("zl");
 pub usingnamespace zl.start;
 export fn load(fp: *zl.builtin.root.Builder.FunctionPointers) void {
     fp.build = .{
-        .formatWriteBuf = zl.build.GenericCommand(zl.build.BuildCommand).formatWriteBuf,
-        .formatLength = zl.build.GenericCommand(zl.build.BuildCommand).formatLength,
-        .formatParseArgs = zl.build.GenericCommand(zl.build.BuildCommand).formatParseArgs,
-        .fieldEditDistance = zl.build.GenericCommand(zl.build.BuildCommand).fieldEditDistance,
-        .writeFieldEditDistance = zl.build.GenericCommand(zl.build.BuildCommand).writeFieldEditDistance,
-        .indexOfCommonLeastDifference = zl.build.GenericCommand(zl.build.BuildCommand).indexOfCommonLeastDifference,
+        .formatWriteBuf = zl.build.BuildCommand.formatWriteBuf,
+        .formatLength = zl.build.BuildCommand.formatLength,
+        .formatParseArgs = zl.build.BuildCommand.formatParseArgs,
     };
 }
