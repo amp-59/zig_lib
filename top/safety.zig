cefeba8a7aac0f15b79024fb96db6c940c542d6a
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-11-11 12:30:55 +0000

    safety: Added function to express compile command groups.

--- top/safety.zig
+++ top/safety.zig
@@ -11,7 +11,7 @@ const builtin = @import("builtin.zig");
 ///     Names of functions, function parameters, fields, types, and
 ///     compile commands may be changed.
 ///
-const RuntimeSafetyCheck = struct {
+pub const RuntimeSafetyCheck = struct {
     mismatched_arguments: ?bool = null,
     reached_unreachable: ?bool = null,
     accessed_invalid_memory: ?bool = null,
@@ -19,41 +19,81 @@ const RuntimeSafetyCheck = struct {
     arith_lost_precision: ?bool = null,
     arith_overflowed: ?bool = null,
     cast_from_invalid: ?bool = null,
+    pub fn causes(comptime tag: Tag) []const PanicCause.Tag {
+        switch (tag) {
+            .reached_unreachable => return &.{
+                .message,
+                .discarded_error,
+                .corrupt_switch,
+                .returned_noreturn,
+                .reached_unreachable,
+                .accessed_null_value,
+            },
+            .mismatched_arguments => return &.{
+                .memcpy_argument_aliasing,
+                .memcpy_argument_lengths_mismatched,
+                .for_loop_capture_lengths_mismatched,
+            },
+            .accessed_invalid_memory => return &.{
+                .accessed_out_of_bounds,
+                .accessed_out_of_order,
+                .accessed_inactive_field,
+            },
+            .mismatched_sentinel => return &.{
+                .mismatched_sentinel,
+                .mismatched_non_scalar_sentinel,
+            },
+            .arith_lost_precision => return &.{
+                .div_with_remainder,
+                .shl_overflowed,
+                .shr_overflowed,
+                .shift_amt_overflowed,
+            },
+            .arith_overflowed => return &.{
+                .mul_overflowed,
+                .add_overflowed,
+                .sub_overflowed,
+            },
+            .cast_from_invalid => return &.{
+                .cast_to_int_from_invalid,
+                .cast_truncated_data,
+                .cast_to_unsigned_from_negative,
+                .cast_to_pointer_from_invalid,
+                .cast_to_enum_from_invalid,
+                .cast_to_error_from_invalid,
+            },
+        }
+    }
+    pub const Tag = meta.TagFromList(meta.fieldNames(RuntimeSafetyCheck));
 };
-const PanicCause = union(enum) {
+pub const PanicCause = union(enum) {
     /// -f[no-]panic-reached-unreachable
     message,
     corrupt_switch,
     discarded_error,
     returned_noreturn,
     reached_unreachable,
-
     /// -f[no-]panic-accessed-invalid-memory
     accessed_out_of_bounds,
     accessed_out_of_order,
     accessed_inactive_field,
     accessed_null_value,
-
     /// -f[no-]panic-mismatched-arguments
     memcpy_argument_aliasing,
     memcpy_argument_lengths_mismatched,
     for_loop_capture_lengths_mismatched,
-
     /// -f[no-]panic-mismatched-sentinel
     mismatched_sentinel: type,
     mismatched_non_scalar_sentinel: type,
-
     /// -f[no-]panic-arith-lost-precision
     shl_overflowed: type,
     shr_overflowed: type,
     shift_amt_overflowed: type,
     div_with_remainder: type,
-
     /// -f[no-]panic-arith-overflowed
     mul_overflowed: type,
     add_overflowed: type,
     sub_overflowed: type,
-
     /// -f[no-]panic-cast-from-invalid
     cast_truncated_data: struct {
         to: type,
@@ -70,8 +110,9 @@ const PanicCause = union(enum) {
         to: type,
         from: type,
     },
+    pub const Tag = @typeInfo(@This()).Union.tag_type.?;
 };
-fn PanicData(comptime panic_extra_cause: PanicCause) type {
+pub fn PanicData(comptime panic_extra_cause: PanicCause) type {
     switch (panic_extra_cause) {
         // reached_unreachable
         .message,
@@ -86,7 +127,6 @@ fn PanicData(comptime panic_extra_cause: PanicCause) type {
         => {
             return void;
         },
-
         // accessed_invalid_memory
         .accessed_out_of_bounds => {
             return struct { index: usize, length: usize };
@@ -97,7 +137,6 @@ fn PanicData(comptime panic_extra_cause: PanicCause) type {
         .accessed_inactive_field => {
             return struct { expected: []const u8, found: []const u8 };
         },
-
         // mismatched_arguments
         .memcpy_argument_aliasing => {
             return struct { dest_start: usize, dest_len: usize, src_start: usize, src_len: usize };
@@ -108,14 +147,12 @@ fn PanicData(comptime panic_extra_cause: PanicCause) type {
         .for_loop_capture_lengths_mismatched => {
             return struct { prev_index: usize, prev_len: usize, next_len: usize };
         },
-
         // mismatched_sentinel
         .mismatched_sentinel,
         .mismatched_non_scalar_sentinel,
         => |child| {
             return struct { expected: child, found: child };
         },
-
         // arith_overflowed
         .mul_overflowed,
         .add_overflowed,
@@ -123,7 +160,6 @@ fn PanicData(comptime panic_extra_cause: PanicCause) type {
         => |val_type| {
             return struct { lhs: val_type, rhs: val_type };
         },
-
         // arith_lost_precision
         .div_with_remainder => |num_type| {
             return struct { numerator: num_type, denominator: num_type };
@@ -142,7 +178,6 @@ fn PanicData(comptime panic_extra_cause: PanicCause) type {
                 shift_amt: builtin.ShiftAmount(int_type),
             };
         },
-
         // cast_from_invalid
         .cast_to_int_from_invalid => |num_type| {
             return num_type.from;
@@ -181,73 +216,73 @@ pub inline fn panic(comptime cause: PanicCause, data: PanicData(cause), st: ?*bu
         .accessed_null_value => {
             builtin.alarm("attempt to use null value", st, ret_addr);
         },
-        .accessed_out_of_order => @call(.never_inline, safety.panicAccessOutOfOrder, .{
+        .accessed_out_of_order => @call(.never_inline, panicAccessOutOfOrder, .{
             data.start, data.finish, st, ret_addr,
         }),
-        .accessed_out_of_bounds => @call(.never_inline, safety.panicAccessOutOfBounds, .{
+        .accessed_out_of_bounds => @call(.never_inline, panicAccessOutOfBounds, .{
             data.index, data.length, st, ret_addr,
         }),
-        .accessed_inactive_field => @call(.never_inline, safety.panicAccessInactiveField, .{
+        .accessed_inactive_field => @call(.never_inline, panicAccessInactiveField, .{
             data.expected, data.found, st, ret_addr,
         }),
-        .discarded_error => @call(.never_inline, safety.panicUnwrappedError, .{
+        .discarded_error => @call(.never_inline, panicUnwrappedError, .{
             data, st, ret_addr,
         }),
-        .memcpy_argument_aliasing => @call(.never_inline, safety.panicMemcpyArgumentsAlias, .{
+        .memcpy_argument_aliasing => @call(.never_inline, panicMemcpyArgumentsAlias, .{
             data.dest_start, data.dest_len, data.src_start, data.src_len, st, ret_addr,
         }),
-        .memcpy_argument_lengths_mismatched => @call(.never_inline, safety.panicMismatchedMemcpyArgumentLengths, .{
+        .memcpy_argument_lengths_mismatched => @call(.never_inline, panicMismatchedMemcpyArgumentLengths, .{
             data.dest_len, data.src_len, st, ret_addr,
         }),
-        .for_loop_capture_lengths_mismatched => @call(.never_inline, safety.panicMismatchedForLoopCaptureLengths, .{
+        .for_loop_capture_lengths_mismatched => @call(.never_inline, panicMismatchedForLoopCaptureLengths, .{
             data.prev_len, data.next_len, st, ret_addr,
         }),
-        .cast_to_pointer_from_invalid => |child_type| @call(.never_inline, safety.panicCastToPointerFromInvalid, .{
+        .cast_to_pointer_from_invalid => |child_type| @call(.never_inline, panicCastToPointerFromInvalid, .{
             @typeName(child_type), data, @alignOf(child_type), st, ret_addr,
         }),
-        .mismatched_sentinel => |child_type| @call(.never_inline, safety.panicSentinelMismatch, .{
+        .mismatched_sentinel => |child_type| @call(.never_inline, panicSentinelMismatch, .{
             meta.BestNum(child_type), @typeName(child_type), data.expected, data.found, st, ret_addr,
         }),
-        .cast_to_int_from_invalid => |num_types| @call(.never_inline, safety.panicCastToIntFromInvalid, .{
+        .cast_to_int_from_invalid => |num_types| @call(.never_inline, panicCastToIntFromInvalid, .{
             meta.BestFloat(num_types.from), @typeName(num_types.from), data, st, ret_addr,
         }),
-        .cast_to_enum_from_invalid => |enum_type| @call(.never_inline, safety.panicCastToTagFromInvalid, .{
+        .cast_to_enum_from_invalid => |enum_type| @call(.never_inline, panicCastToTagFromInvalid, .{
             meta.BestInt(enum_type), @typeName(enum_type), data, st, ret_addr,
         }),
-        .cast_to_error_from_invalid => |error_type| @call(.never_inline, safety.panicCastToTagFromInvalid, .{
+        .cast_to_error_from_invalid => |error_type| @call(.never_inline, panicCastToTagFromInvalid, .{
             meta.BestInt(error_type), @typeName(error_type), data, st, ret_addr,
         }),
-        .mismatched_non_scalar_sentinel => |child_type| @call(.never_inline, safety.panicNonScalarSentinelMismatch, .{
+        .mismatched_non_scalar_sentinel => |child_type| @call(.never_inline, panicNonScalarSentinelMismatch, .{
             child_type, data.expected, data.found, st, ret_addr,
         }),
-        .mul_overflowed => |int_type| @call(.never_inline, safety.panicArithOverflow(meta.BestInt(int_type)).mul, .{
+        .mul_overflowed => |int_type| @call(.never_inline, panicArithOverflow(meta.BestInt(int_type)).mul, .{
             @typeName(int_type), math.bestExtrema(int_type), data.lhs, data.rhs, st, ret_addr,
         }),
-        .add_overflowed => |int_type| @call(.never_inline, safety.panicArithOverflow(meta.BestInt(int_type)).add, .{
+        .add_overflowed => |int_type| @call(.never_inline, panicArithOverflow(meta.BestInt(int_type)).add, .{
             @typeName(int_type), math.bestExtrema(int_type), data.lhs, data.rhs, st, ret_addr,
         }),
-        .sub_overflowed => |int_type| @call(.never_inline, safety.panicArithOverflow(meta.BestInt(int_type)).sub, .{
+        .sub_overflowed => |int_type| @call(.never_inline, panicArithOverflow(meta.BestInt(int_type)).sub, .{
             @typeName(int_type), math.bestExtrema(int_type), data.lhs, data.rhs, st, ret_addr,
         }),
-        .shl_overflowed => |int_type| @call(.never_inline, safety.panicArithOverflow(meta.BestInt(int_type)).shl, .{
+        .shl_overflowed => |int_type| @call(.never_inline, panicArithOverflow(meta.BestInt(int_type)).shl, .{
             @typeName(int_type), data.value, data.shift_amt, ~@abs(@as(int_type, 0)), st, ret_addr,
         }),
-        .shr_overflowed => |int_type| @call(.never_inline, safety.panicArithOverflow(meta.BestInt(int_type)).shr, .{
+        .shr_overflowed => |int_type| @call(.never_inline, panicArithOverflow(meta.BestInt(int_type)).shr, .{
             @typeName(int_type), data.value, data.shift_amt, ~@abs(@as(int_type, 0)), st, ret_addr,
         }),
-        .shift_amt_overflowed => |int_type| @call(.never_inline, safety.panicArithOverflow(meta.BestInt(int_type)).rhs, .{
+        .shift_amt_overflowed => |int_type| @call(.never_inline, panicArithOverflow(meta.BestInt(int_type)).rhs, .{
             @typeName(int_type), data.bit_count, data.shift_amt, st, ret_addr,
         }),
-        .div_with_remainder => |num_type| @call(.never_inline, safety.panicExactDivisionWithRemainder, .{
+        .div_with_remainder => |num_type| @call(.never_inline, panicExactDivisionWithRemainder, .{
             meta.BestNum(num_type), @typeName(num_type), data.numerator, data.denominator, st, ret_addr,
         }),
-        .cast_to_unsigned_from_negative => |int_types| @call(.never_inline, safety.panicCastToUnsignedFromNegative, .{
+        .cast_to_unsigned_from_negative => |int_types| @call(.never_inline, panicCastToUnsignedFromNegative, .{
             meta.BestNum(int_types.to),   @typeName(int_types.to),
             meta.BestNum(int_types.from), @typeName(int_types.from),
             data,                         st,
             ret_addr,
         }),
-        .cast_truncated_data => |num_types| @call(.never_inline, safety.panicCastTruncatedData, .{
+        .cast_truncated_data => |num_types| @call(.never_inline, panicCastTruncatedData, .{
             meta.BestNum(num_types.to),     @typeName(num_types.to),
             meta.BestNum(num_types.from),   @typeName(num_types.from),
             math.bestExtrema(num_types.to), data,
@@ -255,7 +290,6 @@ pub inline fn panic(comptime cause: PanicCause, data: PanicData(cause), st: ?*bu
         }),
     }
 }
-
 pub fn writeAboveOrBelowLimit(
     buf: [*]u8,
     comptime To: type,
