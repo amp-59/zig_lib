e518f525a29e91ac40962f7d9d41a2a55c52a350
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-11-15 18:11:22 +0000

    cache: Rewrote `GenericMirrorCache` to be more similar to `imports`.

--- top/cache.zig
+++ top/cache.zig
@@ -8,6 +8,7 @@ const start = @import("./start.zig");
 const debug = @import("./debug.zig");
 const builtin = @import("./builtin.zig");
 const testing = @import("./testing.zig");
+const INodes = mem.array.StaticArray(usize, 512);
 pub const MirrorCacheSpec = struct {
     options: Options = .{},
     logging: Logging = .{},
@@ -68,8 +69,29 @@ pub const MirrorCacheSpec = struct {
         futex: sys.ErrorPolicy = .{},
     };
 };
-
 pub fn GenericMirrorCache(comptime cache_spec: MirrorCacheSpec) type {
+    const open_flags = .{
+        .path = true,
+        .directory = true,
+    };
+    const creat_flags = .{
+        .truncate = true,
+        .read_write = true,
+    };
+    const mmap_flags = .{
+        .fixed = true,
+        .fixed_noreplace = false,
+    };
+    _ = mmap_flags;
+    const statx_fields = .{
+        .ino = true,
+        .mtime = true,
+        .atime = true,
+        .ctime = true,
+    };
+    const stat_at_flags = .{
+        .empty_path = true,
+    };
     const map = .{ .errors = cache_spec.errors.map, .logging = cache_spec.logging.map };
     const stat = .{ .errors = cache_spec.errors.stat, .logging = cache_spec.logging.stat };
     const open = .{ .errors = cache_spec.errors.open, .logging = cache_spec.logging.open };
@@ -80,124 +102,87 @@ pub fn GenericMirrorCache(comptime cache_spec: MirrorCacheSpec) type {
     const close = .{ .errors = cache_spec.errors.close, .logging = cache_spec.logging.close };
     const unmap = .{ .errors = cache_spec.errors.unmap, .logging = cache_spec.logging.unmap };
     const create = .{ .errors = cache_spec.errors.create, .logging = cache_spec.logging.create };
+    _ = map;
     _ = stat;
+    _ = open;
     _ = read;
     _ = mkdir;
     _ = write;
+    _ = close;
+    _ = unmap;
     _ = create;
-    const T = extern struct {
-        path: mem.Bounds = .{ .lb_addr = lb_path_addr, .up_addr = lb_path_addr },
-        file: mem.Bounds = .{ .lb_addr = lb_file_addr, .up_addr = lb_file_addr },
+    const T = struct {
+        path: mem.SimpleAllocator = .{
+            .start = lb_path_addr,
+            .next = lb_path_addr,
+            .finish = lb_path_addr,
+        },
+        file: mem.SimpleAllocator = .{
+            .start = lb_file_addr,
+            .next = lb_file_addr,
+            .finish = lb_file_addr,
+        },
+        futex: u32 = 0,
         const MirrorCache = @This();
-        const open_flags = .{
-            .path = true,
-            .directory = true,
-        };
-        const creat_flags = .{
-            .truncate = true,
-            .read_write = true,
-        };
-        const mmap_flags = .{
-            .fixed = true,
-            .fixed_noreplace = false,
-        };
-        const statx_fields = .{
-            .ino = true,
-            .mtime = true,
-            .atime = true,
-            .ctime = true,
-        };
-        const stat_at_flags = .{
-            .empty_path = true,
-        };
-        const INodes = mem.array.StaticArray(usize, cache_spec.options.max_file_number);
         pub const lb_path_addr: comptime_int = cache_spec.AddressSpace.arena(0).lb_addr;
         pub const up_path_addr: comptime_int = cache_spec.AddressSpace.arena(0).up_addr;
         pub const lb_file_addr: comptime_int = cache_spec.AddressSpace.arena(1).lb_addr;
         pub const up_file_addr: comptime_int = cache_spec.AddressSpace.arena(1).up_addr;
-        pub fn scan(
-            mirror: *MirrorCache,
-            build_root: [:0]const u8,
-            build_root_fd: usize,
-            cache_root: [:0]const u8,
-            root_pathname: [:0]const u8,
-        ) !bool {
-            @setRuntimeSafety(builtin.is_safe);
-            while (@cmpxchgWeak(usize, &mirror.path.up_addr, lb_path_addr, up_path_addr, .SeqCst, .SeqCst)) |val| {
-                proc.futexWait(futex, @ptrCast(&mirror.path.up_addr), @intCast(val), &.{ .sec = 10 });
-            }
-            mem.map(map, .{}, .{}, lb_path_addr, 1024 * 1024);
-            const cache_m_root: [:0]const u8 = mirror.allocatePath(cache_root, "m");
-            var build_inodes: INodes = undefined;
-            var cache_inodes: INodes = undefined;
-            mem.zero(INodes, &build_inodes);
-            mem.zero(INodes, &cache_inodes);
-            build_inodes.undefineAll();
-            cache_inodes.undefineAll();
-            file.makeDirAt(.{ .errors = .{} }, build_root_fd, cache_root, file.mode.directory);
-            file.makeDirAt(.{ .errors = .{} }, build_root_fd, cache_m_root, file.mode.directory);
-            const cache_m_root_fd: usize = file.openAt(open, open_flags, file.cwd, cache_m_root);
-            const misses: usize = mirror.createMirrorFileSystemCache(
-                &build_inodes,
-                build_root,
-                build_root_fd,
-                &cache_inodes,
-                cache_m_root,
-                cache_m_root_fd,
-                root_pathname[build_root.len +% 1 ..],
-            ) catch 1;
-            file.close(close, cache_m_root_fd);
-            mem.unmap(unmap, lb_path_addr, 1024 * 1024);
-            proc.futexWake(futex, @ptrCast(&mirror.path.up_addr), ~@as(u32, 0));
-            mirror.path.up_addr = lb_path_addr;
-            return misses == 0;
-        }
         fn allocatePath(
-            mirror: *MirrorCache,
+            cache: *MirrorCache,
             dirname: []const u8,
             basename: []const u8,
         ) [:0]const u8 {
             @setRuntimeSafety(builtin.is_safe);
-            const buf: [*]u8 = @ptrFromInt(@atomicRmw(usize, &mirror.path.up_addr, .Add, dirname.len +% basename.len +% 2, .SeqCst));
+            const len: usize = dirname.len +% basename.len +% 1;
+            const buf: [*]u8 = @ptrFromInt(cache.path.allocateAtomic(len +% 1, 1));
             var ptr: [*]u8 = fmt.strcpyEqu(buf, dirname);
             if (basename.len == 1 and basename[0] == '.') {
                 ptr[0] = 0;
                 return buf[0..dirname.len :0];
             }
             ptr[0] = '/';
-            ptr += @intFromBool(dirname.len != 0 and basename.len != 0);
+            ptr += @intFromBool(dirname.len != 0);
             ptr = fmt.strcpyEqu(ptr, basename);
             ptr[0] = 0;
-            return buf[0 .. dirname.len +% 1 +% basename.len :0];
+            return buf[0..len :0];
         }
-        fn createMirrorFileSystemCache(
+        fn allocateFileBuf(
+            cache: *MirrorCache,
+            root_fd: usize,
+            file_st: *const file.StatusExtended,
+            name: [:0]const u8,
+        ) ![*:0]u8 {
+            @setRuntimeSafety(builtin.is_safe);
+            var buf: [*]u8 = @ptrFromInt(cache.file.allocateAtomic(file_st.size +% 1, 8));
+            var cache_file_fd: usize = file.openAt(.{ .errors = .{} }, .{}, root_fd, name);
+            try file.read(.{ .return_type = void }, cache_file_fd, buf[0..file_st.size]);
+            try file.close(.{}, cache_file_fd);
+            buf[file_st.size] = 0;
+            return buf[0..file_st.size :0];
+        }
+        fn scanInternal(
             mirror: *MirrorCache,
             build_inodes: *INodes,
             build_root: [:0]const u8,
             build_root_fd: usize,
             cache_inodes: *INodes,
-            cache_m_root: [:0]const u8,
-            cache_m_root_fd: usize,
+            cache_root: [:0]const u8,
+            cache_root_fd: usize,
             name: [:0]const u8,
         ) !usize {
             @setRuntimeSafety(builtin.is_safe);
             var misses: usize = 0;
             var cache_file_st: file.StatusExtended = .{};
             var build_file_st: file.StatusExtended = .{};
-            file.statusExtended(.{ .errors = .{} }, .{}, statx_fields, cache_m_root_fd, name, &cache_file_st);
+            file.statusExtended(.{ .errors = .{} }, .{}, statx_fields, cache_root_fd, name, &cache_file_st);
             for (cache_inodes.readAll()) |ino| {
                 if (ino == cache_file_st.ino) return misses;
             }
-            file.statusExtended(.{ .errors = .{} }, .{}, statx_fields, build_root_fd, name, &build_file_st);
-            if (build_file_st.mode.kind != .regular) {
-                return 0;
-            }
+            try file.statusExtended(.{}, .{}, statx_fields, build_root_fd, name, &build_file_st);
             if (cache_file_st.mode.kind == .regular) blk: {
-                if (cache_file_st.size == 0) {
-                    return 0;
-                }
-                const path_save: usize = mirror.path.up_addr;
-                const file_save: usize = mirror.file.up_addr;
+                const path_save: usize = mirror.path.next;
+                const file_save: usize = mirror.file.next;
                 if (cache_file_st.mtime.sec < build_file_st.mtime.sec or
                     cache_file_st.mtime.sec == build_file_st.mtime.sec and
                     cache_file_st.mtime.nsec < build_file_st.mtime.nsec)
@@ -206,38 +191,27 @@ pub fn GenericMirrorCache(comptime cache_spec: MirrorCacheSpec) type {
                     break :blk;
                 }
                 cache_inodes.writeOne(cache_file_st.ino);
-                const len: usize = bits.alignA4096(cache_file_st.size);
-                const addr: usize = @atomicRmw(usize, &mirror.file.up_addr, .Add, len, .SeqCst);
-                const fd: usize = try file.openAt(.{}, .{}, cache_m_root_fd, name);
-                try file.map(.{}, .{}, mmap_flags, fd, addr, len, 0);
-                file.close(close, fd);
-                const buf: [*]u8 = @ptrFromInt(addr);
+                const buf: [*:0]u8 = try mirror.allocateFileBuf(cache_root_fd, &cache_file_st, name);
                 var ptr: [*]u8 = buf;
-                while (fmt.strlen(ptr, buf) != cache_file_st.size) : (mirror.path.up_addr = path_save) {
+                while (buf + cache_file_st.size != ptr) : (mirror.path.next = path_save) {
                     const import: []const u8 = mem.terminate(ptr, 0);
-                    const root_pathname: [:0]const u8 = mirror.allocatePath(file.dirname(name), import);
-                    misses +%= try mirror.createMirrorFileSystemCache(build_inodes, build_root, build_root_fd, cache_inodes, cache_m_root, cache_m_root_fd, root_pathname);
+                    misses +%= try mirror.scanInternal(build_inodes, build_root, build_root_fd, cache_inodes, cache_root, cache_root_fd, mirror.allocatePath(file.dirname(name), import));
                     ptr += import.len +% 1;
                 }
-                mirror.file.up_addr = file_save;
+                mirror.file.next = file_save;
                 return misses;
             }
             for (build_inodes.readAll()) |ino| {
                 if (ino == build_file_st.ino) return misses;
             }
-            const save: usize = mirror.file.up_addr;
+            const save: usize = mirror.file.next;
             const dirname: []const u8 = file.dirname(name);
             if (dirname.len != 0) {
-                try file.makePathAt(.{}, cache_m_root_fd, dirname, file.mode.directory);
+                try file.makePathAt(.{}, cache_root_fd, dirname, file.mode.directory);
             }
-            const len: usize = bits.alignA4096(build_file_st.size);
-            const addr: usize = @atomicRmw(usize, &mirror.file.up_addr, .Add, len *% 2, .SeqCst);
-            const build_file_fd: usize = try file.openAt(.{}, .{}, build_root_fd, name);
-            try file.map(.{}, .{}, mmap_flags, build_file_fd, addr, len, 0);
-            file.close(close, build_file_fd);
-            try mem.map(.{}, .{}, mmap_flags, addr +% len, len);
-            const buf: [*]u8 = @ptrFromInt(addr);
-            const tmp: [*]u8 = @ptrFromInt(addr +% len);
+            try file.statusExtended(.{}, stat_at_flags, statx_fields, cache_root_fd, &.{}, &cache_file_st);
+            const buf: [*:0]u8 = try mirror.allocateFileBuf(build_root_fd, &build_file_st, name);
+            const tmp: [*]u8 = @ptrFromInt(mirror.file.allocateRaw(build_file_st.size, 1));
             tmp[build_file_st.size] = 0;
             var itr: builtin.parse.TokenIterator = .{ .buf = buf[0..build_file_st.size :0] };
             var tok: builtin.parse.Token = .{ .tag = .invalid, .loc = .{} };
@@ -252,11 +226,41 @@ pub fn GenericMirrorCache(comptime cache_spec: MirrorCacheSpec) type {
                     }
                 }
             }
-            const cache_file_fd: usize = try file.createAt(.{}, creat_flags, cache_m_root_fd, name, file.mode.regular);
+            const cache_file_fd: usize = try file.createAt(.{}, creat_flags, cache_root_fd, name, file.mode.regular);
             try file.write(.{}, cache_file_fd, fmt.slice(ptr, tmp));
             try file.close(.{}, cache_file_fd);
-            mirror.file.up_addr = save;
-            return misses +% try mirror.createMirrorFileSystemCache(build_inodes, build_root, build_root_fd, cache_inodes, cache_m_root, cache_m_root_fd, name);
+            mirror.file.next = save;
+            return misses +% try mirror.scanInternal(build_inodes, build_root, build_root_fd, cache_inodes, cache_root, cache_root_fd, name);
+        }
+        pub fn scan(
+            mirror: *MirrorCache,
+            build_root: [:0]const u8,
+            build_root_fd: usize,
+            cache_root: [:0]const u8,
+            root_pathname: [:0]const u8,
+        ) !bool {
+            testing.printBufN(4096, .{
+                .build_root = build_root,
+                .build_root_fd = build_root_fd,
+                .cache_root = cache_root,
+                .root_pathname = root_pathname,
+            });
+            while (@cmpxchgWeak(u32, &mirror.futex, 0, 1, .SeqCst, .SeqCst)) |val| {
+                proc.futexWait(futex, &mirror.futex, val, &.{ .sec = 10 });
+            }
+            const cache_m_root: [:0]const u8 = mirror.allocatePath(cache_root, "m");
+            var build_inodes: INodes = undefined;
+            var cache_inodes: INodes = undefined;
+            build_inodes.undefineAll();
+            cache_inodes.undefineAll();
+            file.makeDirAt(.{ .errors = .{} }, build_root_fd, "zig-cache", file.mode.directory);
+            file.makeDirAt(.{ .errors = .{} }, build_root_fd, "zig-cache/m", file.mode.directory);
+            const cache_m_root_fd: usize = try file.openAt(.{}, open_flags, file.cwd, cache_m_root);
+            const misses: usize = try mirror.scanInternal(&build_inodes, build_root, build_root_fd, &cache_inodes, cache_m_root, cache_m_root_fd, root_pathname);
+            try file.close(.{}, cache_m_root_fd);
+            mirror.futex = 0;
+            proc.futexWake(futex, &mirror.futex, ~@as(u32, 0));
+            return misses == 0;
         }
     };
     return T;
