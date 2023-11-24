bc5260c9b981d532bead666e02ed4734a3f738c0
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-11-24 23:44:26 +0000

    exit_with_code: Simplified file-scope declarations to match recent                 programs.

--- test/exit_with_code.zig
+++ test/exit_with_code.zig
@@ -1,12 +1,8 @@
 const zl = @import("zig_lib");
-const proc = zl.proc;
-const builtin = zl.builtin;
-
 pub usingnamespace zl.start;
-
 pub fn main(args: anytype) u8 {
     if (args.len == 0) {
         return 0;
     }
-    return builtin.parse.any(u8, args[1]) catch 255;
+    return zl.builtin.parse.any(u8, args[1]) catch 255;
 }
