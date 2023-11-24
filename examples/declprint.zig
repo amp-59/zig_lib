0eeae8ccc9f66978eb271b1fb7663a186184e983
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-11-24 23:54:36 +0000

    declprint: Updated convention for addition to new examples group.

--- examples/declprint.zig
+++ examples/declprint.zig
@@ -1,26 +1,14 @@
 const zl = @import("../zig_lib.zig");
-const mem = zl.mem;
-const fmt = zl.fmt;
-const proc = zl.proc;
-const meta = zl.meta;
-const file = zl.file;
-const debug = zl.debug;
-const builtin = zl.builtin;
-
 pub usingnamespace zl.start;
-
-pub const AddressSpace = mem.spec.address_space.exact_8;
-
+pub const AddressSpace = zl.mem.spec.address_space.exact_8;
+const descr_spec: zl.fmt.TypeDescrFormatSpec = .{ .decls = true };
 pub fn main() !void {
-    const Allocator = mem.dynamic.GenericArenaAllocator(.{
+    const Allocator = zl.mem.dynamic.GenericArenaAllocator(.{
         .AddressSpace = AddressSpace,
         .arena_index = 0,
     });
     var address_space: AddressSpace = .{};
     var allocator: Allocator = try Allocator.init(&address_space);
     defer allocator.deinit(&address_space);
-
-    try file.write(.{}, 1, fmt.typeDescr(.{
-        .decls = true,
-    }, zl.file));
+    try zl.file.write(.{}, 1, zl.fmt.typeDescr(descr_spec, zl.file));
 }
