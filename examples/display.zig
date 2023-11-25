ab975973c6cfed940c71a4ed5d1ac67ade84cef4
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-11-25 00:03:18 +0000

    display: Updated for inclusion in new examples group.

--- examples/display.zig
+++ examples/display.zig
@@ -1,16 +1,5 @@
 const zl = @import("../zig_lib.zig");
-const sys = zl.sys;
-const fmt = zl.fmt;
-const file = zl.file;
-const mem = zl.mem;
-const proc = zl.proc;
-const meta = zl.meta;
-const spec = zl.spec;
-const builtin = zl.builtin;
-const testing = zl.testing;
-
 pub usingnamespace zl.start;
-
 const ModeRes = extern struct {
     fbs: [*]u32,
     crtcs: [*]u32,
@@ -33,11 +22,12 @@ const Ioc = packed struct(u32) {
     read: bool,
 };
 pub fn main() !void {
-    var array: mem.StaticString(4096) = undefined;
+    var array: zl.mem.array.StaticString(4096) = undefined;
     array.undefineAll();
-    var fd = try file.open(.{ .options = .{ .read_write = true } }, "/dev/dri/card0");
-    defer file.close(.{ .errors = .{} }, fd);
-    var res = comptime builtin.zero(ModeRes);
+    var fd = try zl.file.open(.{}, .{ .read_write = true }, "/dev/dri/card0");
+    defer zl.file.close(.{ .errors = .{} }, fd);
+    var res: ModeRes = undefined;
+    zl.mem.zero(ModeRes, &res);
     const ioc: Ioc = .{
         .read = true,
         .write = true,
@@ -45,11 +35,12 @@ pub fn main() !void {
         .number = 0xA0,
         .size = @sizeOf(ModeRes),
     };
-    try sys.call(.ioctl, .{ .throw = sys.ioctl_errors }, void, .{
+    try zl.sys.call(.ioctl, .{ .throw = zl.file.spec.ioctl.errors.all }, void, .{
         fd,
         @as(u32, @bitCast(ioc)),
         @intFromPtr(&res),
     });
-    array.writeFormat(fmt.any(res));
+    array.writeFormat(zl.fmt.any(res));
     array.writeOne('\n');
+    zl.debug.write(array.readAll());
 }
