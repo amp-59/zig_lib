7aa54c2b808b07ed9d957a64428f66e4437cb3aa
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-12-10 17:33:54 +0000

    imports: Updated example program.

--- examples/imports.zig
+++ examples/imports.zig
@@ -1,22 +1,15 @@
 const zl = @import("../zig_lib.zig");
 pub usingnamespace zl.start;
-pub const signal_handlers: zl.debug.SignalHandlers = .{
-    .SegmentationFault = false,
-    .IllegalInstruction = false,
-    .BusError = false,
-    .FloatingPointError = false,
-    .Trap = false,
-};
 pub const logging_override = zl.debug.spec.logging.override.silent;
 const MirrorCache = zl.cache.GenericMirrorCache(.{ .AddressSpace = zl.mem.spec.address_space.exact_8 });
 pub fn main(args: [][*:0]u8) !void {
     var mirror: MirrorCache = undefined;
     zl.mem.zero(MirrorCache, &mirror);
-    const build_root: [:0]const u8 = zl.mem.terminate(args[2], 0);
-    const cache_root: [:0]const u8 = zl.mem.terminate(args[3], 0);
-    const pathname: [:0]const u8 = zl.mem.terminate(args[5], 0);
-    const build_root_fd: usize = try zl.file.openAt(.{}, .{}, zl.file.cwd, build_root);
-    if (try mirror.scan(build_root, build_root_fd, cache_root, pathname)) {
+    if (try mirror.scan(
+        try zl.file.openAt(.{}, .{}, zl.file.cwd, zl.mem.terminate(args[2], 0)),
+        zl.mem.terminate(args[3], 0),
+        zl.mem.terminate(args[5], 0),
+    )) {
         zl.debug.write("OK\n");
     }
 }
