da38312e4b55feed5d98303b9caa09283a5bf36a
amp-59 <114923809+amp-59@users.noreply.github.com>
2024-01-05 22:52:01 +0000

    build: Replaced `ArchiveCommand.formatWriteBuf` with `ArchiveCommand.write`.

--- top/build/archive.auto.zig
+++ top/build/archive.auto.zig
@@ -2,7 +2,7 @@ const zl = @import("zl");
 pub usingnamespace zl.start;
 export fn load(fp: *zl.builtin.root.Builder.FunctionPointers) void {
     fp.archive = .{
-        .formatWriteBuf = zl.build.ArchiveCommand.formatWriteBuf,
+        .write = zl.build.ArchiveCommand.write,
         .formatLength = zl.build.ArchiveCommand.formatLength,
         .formatParseArgs = zl.build.ArchiveCommand.formatParseArgs,
     };
