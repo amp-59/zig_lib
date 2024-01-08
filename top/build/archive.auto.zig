4ee6c5391111d0cc0bd0c3a45f6d7e9cadbbb6e0
amp-59 <114923809+amp-59@users.noreply.github.com>
2024-01-08 09:51:17 +0000

    build: Updated definition of `FunctionPointers` by archive loader.

--- top/build/archive.auto.zig
+++ top/build/archive.auto.zig
@@ -3,7 +3,7 @@ pub usingnamespace zl.start;
 export fn load(fp: *zl.builtin.root.Builder.FunctionPointers) void {
     fp.archive = .{
         .write = zl.build.ArchiveCommand.write,
-        .formatLength = zl.build.ArchiveCommand.formatLength,
+        .length = zl.build.ArchiveCommand.length,
         .formatParseArgs = zl.build.ArchiveCommand.formatParseArgs,
     };
 }
