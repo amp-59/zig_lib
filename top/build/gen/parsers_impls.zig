0a5357a9080eabf97b10ca1926aefa2b1582ae95
amp-59 <114923809+amp-59@users.noreply.github.com>
2024-01-10 23:10:38 +0000

    buildgen: Updated usage of `Array` by `parsers_impls`.

--- top/build/gen/parsers_impls.zig
+++ top/build/gen/parsers_impls.zig
@@ -2,6 +2,7 @@ const mem = @import("../../mem.zig");
 const gen = @import("../../gen.zig");
 const debug = @import("../../debug.zig");
 const attr = @import("attr.zig");
+const types = @import("types.zig");
 const config = @import("config.zig");
 const common = @import("common_impls.zig");
 pub usingnamespace @import("../../start.zig");
@@ -10,11 +11,11 @@ pub const context = .Lib;
 pub fn main() !void {
     var allocator: mem.SimpleAllocator = .{};
     defer allocator.unmapAll();
-    const array: *common.Array = allocator.create(common.Array);
+    const array: *types.Array = allocator.create(types.Array);
     const len: usize = try gen.readFile(.{ .return_type = usize }, config.parsers_template_path, array.referAllUndefined());
     array.define(len);
     for (attr.all) |attributes| {
-        common.writeParserFunction(array, .C, attributes);
+        common.writeParserFunction(array, .{ .language = .C }, attributes);
     }
     for (attr.all) |attributes| {
         common.writeParserFunctionHelp(array, attributes);
