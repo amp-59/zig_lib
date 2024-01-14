47d4759bc6dd281b2093063a22631457522f15ef
amp-59 <114923809+amp-59@users.noreply.github.com>
2024-01-14 12:08:15 +0000

    buildgen: Updated imports for task struct template source.

--- top/build/gen/tasks-template.zig
+++ top/build/gen/tasks-template.zig
@@ -1,5 +1,7 @@
 const fmt = @import("../fmt.zig");
 const mem = @import("../mem.zig");
+const proc = @import("../proc.zig");
+const file = @import("../file.zig");
 const debug = @import("../debug.zig");
 const parse = @import("../parse.zig");
 const builtin = @import("../builtin.zig");
